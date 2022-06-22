8237afaaa6d1572223208eeb24b3d210107ba5b8
Manoel José <manoel.jose@unilinktransportes.com.br>
2022-06-22 13:43:23 -0300

    add T18 custo MDO; eliminação de arquivos grandes

--- manutenção/FA - OS.sql
+++ manutenção/FA - OS.sql
@@ -41,7 +41,7 @@ select distinct
 
 	case when trim(STL.TL_CODIGO) in ('11380003', '11380004', '11380005') and STL.TL_LOCAL = '80' then ADESIVO_CUSTO.B9_CM
 	else
-		case when trim(STL.TL_CODIGO) in ('T05', 'T12', 'T15', 'T16', 'T17') then ST1.T1_SALARIO
+		case when trim(STL.TL_CODIGO) in ('T05', 'T12', 'T15', 'T16', 'T17', 'T18') then ST1.T1_SALARIO
 		else
 			case when STL.TL_QUANTID != 0.0 then STL.TL_CUSTO / STL.TL_QUANTID
 			else
