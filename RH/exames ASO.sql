91a6d3cc7278f4531c7ff5236840ee527cf210c7
Manoel José <manoel.jose@unilinktransportes.com.br>
2022-07-20 11:22:50 -0300

    relatório de exames médicos

--- RH/exames ASO.sql
+++ RH/exames ASO.sql
@@ -1,23 +1,35 @@
 select
-    TMY.TMY_FILIAL,
-    TMY.TMY_NUMFIC,
+    TM0.TM0_FILIAL,
+    TM0.TM0_NUMFIC,
+    TM5.TM5_FILIAL,
     TM5.TM5_EXAME,
+    TM5.TM5_EXAME as contador,
+    convert(date, TM5.TM5_DTPROG, 103) as TM5_DTPROG,
+    trim(TM4.TM4_NOMEXA) as TM4_NOMEXA,
+    trim(TM4.TM4_DESEXA) as TM4_DESEXA,
     TM5.TM5_FORNEC,
     TM5.TM5_LOJA,
     TM5.TM5_FILFUN,
     TM5.TM5_MAT,
-    SRA.RA_NOME,
     TM5.TM5_CC,
     TM5.TM5_CODFUN,
-    TM5.TM5_CBO
-from TMY010 TMY (nolock)
-    inner join TM5010 TM5 (nolock)
-        on TM5.D_E_L_E_T_ = ''
-        and TM5.TM5_NUMASO = TMY.TMY_NUMASO
-        and TM5.TM5_NUMFIC = TMY.TMY_NUMFIC
+    TM5.TM5_CBO,
 
-        inner join SRA010 SRA (nolock)
-            on SRA.D_E_L_E_T_ = ''
-            and SRA.RA_FILIAL = TM5.TM5_FILFUN
-            and SRA.RA_MAT = TM5.TM5_MAT
-where TMY.D_E_L_E_T_ = ''
+    year(TM5.TM5_DTPROG) as ano_prog,
+    month(TM5.TM5_DTPROG) as mes_prog
+from TM0010 TM0 (nolock)
+    inner join TMY010 TMY (nolock)
+        on TMY.D_E_L_E_T_ = ''
+        and TMY.TMY_FILIAL = TM0.TM0_FILIAL
+        and TMY.TMY_NUMFIC = TM0.TM0_NUMFIC
+
+        inner join TM5010 TM5 (nolock)
+            on TM5.D_E_L_E_T_ = ''
+            and TM5.TM5_FILIAL = TMY.TMY_FILIAL
+            and TM5.TM5_NUMASO = TMY.TMY_NUMASO
+            and TM5.TM5_NUMFIC = TMY.TMY_NUMFIC
+
+            inner join TM4010 TM4 (nolock)
+                on TM4.D_E_L_E_T_ = ''
+                and TM4.TM4_EXAME = TM5.TM5_EXAME
+where TM0.D_E_L_E_T_ = ''
