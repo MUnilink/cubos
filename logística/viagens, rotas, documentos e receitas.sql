e508c1efb0a6c264b503eb260f0978c57cbce1d2
Manoel José <manoel.jose@unilinktransportes.com.br>
2022-07-25 11:55:15 -0300

    vinculação de receitas extras fora da viagem (custom)

--- logística/viagens, rotas, documentos e receitas.sql
+++ logística/viagens, rotas, documentos e receitas.sql
@@ -189,7 +189,24 @@ select
         when '5' then 'FECHADA'
         when '9' then 'CANCELADA'
         else 'OUTROS'
-    end as DTQ_STATUS
+    end as DTQ_STATUS,
+
+    SE1.E1_NUM as ND_NUM,
+    SE1.E1_PREFIXO as ND_PREFIXO,
+    SE1.E1_TIPO as ND_TIPO,
+    SE1.E1_VALOR as ND_VALOR,
+
+    SC5.C5_NUM as RPS_PEDIDO,
+    RPS.D2_DOC as RPS_DOC,
+    RPS.D2_SERIE as RPS_SERIE,
+    RPS.D2_TOTAL as RPS_TOTAL,
+    RPS.D2_VALIPI as RPS_VALIPI,
+    RPS.D2_VALICM as RPS_VALICM,
+    convert(date, RPS.D2_EMISSAO, 103) as RPS_EMISSAO,
+
+    DOC_ANU.DTC_DOC as DOCAV_DOC,
+    DOC_ANU.DTC_SERIE as DOCAV_SERIE,
+    convert(date, DOC_ANU.DTC_DATENT, 103) as DOCAV_DATEMI
 
 from DTQ010 DTQ (nolock)
     inner join DA8010 DA8 (nolock)
@@ -250,6 +267,7 @@ from DTQ010 DTQ (nolock)
 
             left join SD2010 SD2 (nolock)
                 on SD2.D_E_L_E_T_ = ''
+                and SD2.D2_FILIAL = DT6.DT6_FILDOC
                 and SD2.D2_NFORI = DT6.DT6_DOC
                 and SD2.D2_SERIORI = DT6.DT6_SERIE
                 and SD2.D2_CLIENTE = DT6.DT6_CLIDEV
@@ -268,5 +286,24 @@ from DTQ010 DTQ (nolock)
                     on DF1.D_E_L_E_T_ = ''
                     and DF1.DF1_FILDOC = DTC.DTC_FILORI
                     and DF1.DF1_DOC = DTC.DTC_NUMSOL
+    
+    left join SE1010 SE1 (nolock)
+        on SE1.D_E_L_E_T_ = ''
+        and SE1.E1_YVIAGEM = DTQ.DTQ_VIAGEM
+    left join SC5010 SC5 (nolock)
+        on SC5.D_E_L_E_T_ = ''
+        and SC5.C5_YVIAGEM = DTQ.DTQ_VIAGEM
+
+        left join SD2010 RPS (nolock)
+            on RPS.D_E_L_E_T_ = ''
+            and RPS.D2_FILIAL = SC5.C5_FILIAL
+            and RPS.D2_DOC = SC5.C5_NOTA
+            and RPS.D2_SERIE = SC5.C5_SERIE
+            and RPS.D2_CLIENTE = SC5.C5_CLIENTE
+            and RPS.D2_LOJA = SC5.C5_LOJACLI
+
+    left join DTC010 DOC_ANU (nolock)
+        on DOC_ANU.D_E_L_E_T_ = ''
+        and DOC_ANU.DTC_YVIAGE = DTQ.DTQ_VIAGEM
 where 
         DTQ.D_E_L_E_T_ = ''
