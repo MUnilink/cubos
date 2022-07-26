330328b468862525f1749a797eecaef411608f08
Manoel José <manoel.jose@unilinktransportes.com.br>
2022-07-26 10:58:16 -0300

    novos diagnóstico e correções pneus

--- manutenção/estrutura de bens.sql
+++ manutenção/estrutura de bens.sql
@@ -1,9 +1,10 @@
 select
-    ST9.T9_CCUSTO,
-    isnull(STC.TC_CODBEM, 'PNEU OU SR DESATRELADO'),
-    case when ST9.T9_CODFAMI = 'VP' and STC.TC_CODBEM is null then 'SR DESATRELADO' else '-' end as STATUS,
+    trim(CTT.CTT_DESC01) as CTT_DESC01,
+    isnull(STC.TC_CODBEM, 'PNEU OU SR DESATRELADO') as TC_CODBEM,
     STC.TC_COMPONE,
-    TQT.TQT_DESMED
+    case when STC.TC_COMPONE like 'SR%' then 1000 else 1 end as qtd_compone,
+    isnull(TQT.TQT_DESMED, 'SR') as TQT_DESMED,
+    ST9.T9_CODFAMI
 from ST9010 ST9 (nolock)
     left join STC010 STC (nolock)
         on STC.D_E_L_E_T_ = ''
@@ -16,4 +17,8 @@ from ST9010 ST9 (nolock)
             left join TQT010 TQT (nolock)
                 on TQT.D_E_L_E_T_ = ''
                 and TQT.TQT_MEDIDA = TQS.TQS_MEDIDA
+
+    inner join CTT010 CTT (nolock)
+        on CTT.D_E_L_E_T_ = ''
+        and CTT.CTT_CUSTO = ST9.T9_CCUSTO
 where ST9.D_E_L_E_T_ = ''
