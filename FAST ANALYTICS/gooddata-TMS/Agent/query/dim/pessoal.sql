6ec3988e7ec69a8d9a8507b0987930661eb0fd68
Manoel José <manoel.jose@unilinktransportes.com.br>
2022-08-02 16:20:59 -0300

    início consultas custo TMS

--- FAST ANALYTICS/gooddata-TMS/Agent/query/dim/pessoal.sql
+++ FAST ANALYTICS/gooddata-TMS/Agent/query/dim/pessoal.sql
@@ -1,28 +1,70 @@
 select
     SRD.RD_PERIODO + SRD.RD_MAT + SRV.CONTA as ID_LANCAMENTO,
-    SRV.CONTA
-from SRD010 SRD (nolock)
+    SRD.RD_PERIODO,
+    SRV.CONTA,
+    trim(CTD.CTD_DESC01) as ATIVIDADE,
+    trim(CTT.CTT_DESC01) as CENTRO_CUSTO,
+    trim(SRA.RA_MAT) as MATRICULA,
+    trim(SRA.RA_NOME) as NOME,
+	trim(SRJ.RJ_DESC) as FUNCAO
+from SRA010 SRA (nolock)
+    inner join SRJ010 SRJ (nolock)
+        on SRJ.D_E_L_E_T_ = ''
+        and SRJ.RJ_FILIAL = substring(SRA.RA_FILIAL, 1, 4)
+        and SRJ.RJ_FUNCAO = SRA.RA_CODFUNC
+    inner join CTT010 CTT (nolock)
+        on CTT.D_E_L_E_T_ = ''
+        and CTT.CTT_CUSTO = SRA.RA_CC
+    inner join CTD010 CTD (nolock)
+        on CTD.D_E_L_E_T_ = ''
+        and CTD.CTD_ITEM = SRA.RA_ITEM
+    left join SRD010 SRD (nolock)
+        on SRD.D_E_L_E_T_ = ''
+        and SRD.RD_FILIAL = SRA.RA_FILIAL
+        and SRD.RD_MAT = SRA.RA_MAT
+        and SRD.RD_PERIODO > '20211231'
+
+        inner join
+        (
+            select
+                SRV010.RV_FILIAL,
+                SRV010.RV_COD,
+                case when SRV010.RV_COD in ('008', '020', '025', '031', '039', '041', '051', '072', '094', '106', '201', '215', '220', '223', '343', '365', '783') then 'Salários e Ordenados'
+                else
+                    case when SRV010.RV_COD in ('029', '111', '113') then 'Hora Extra'
+                    else
+                        case when SRV010.RV_COD in ('038', '711', '719', '738', '749', '796') then 'Benefícios'
+                        else
+                            case when SRV010.RV_COD in ('739', '759', '760', '800', '817', '950', '955', '960', '961', '962') then 'Encargos Sociais'
+                            else 'N/A Custo'
+                            end
+                        end
+                    end
+                end as CONTA
+            from SRV010 (nolock)
+            where SRV010.D_E_L_E_T_ = ''
+        ) SRV
+            on SRV.RV_FILIAL = substring(SRD.RD_FILIAL, 1, 4)
+            and SRV.RV_COD = SRD.RD_PD
+
     inner join
     (
         select
-            SRV.RV_FILIAL,
-            SRV.RV_COD,
-            case when SRV010.RV_COD in ('008', '020', '025', '031', '039', '041', '051', '072', '094', '106', '201', '215', '220', '223', '343', '365', '783') then 'Salários e Ordenados'
+            SRT010.RT_FILIAL,
+            SRT010.RT_VERBA,
+            case when SRT010.RT_VERBA in ('845', '846') then '13º Salário'
             else
-                case when SRV010.RV_COD in ('029', '111', '113') then 'Hora Extra'
+                case when SRT010.RT_VERBA in ('833', '834', '847', '848') then 'Encargos Sociais (13º e Férias)'
                 else
-                    case when SRV010.RV_COD in ('038', '711', '719', '738', '749', '796') then 'Benefícios'
-                    else
-                        case when SRV010.RV_COD in ('739', '759', '760', '800', '817', '950', '955', '960', '961', '962') then 'Encargos Sociais'
-                        else 'N/A Custo'
-                        end
+                    case when SRT010.RT_VERBA in ('830', '831', '832') then 'Férias'
+                    else 'N/A Custo'
                     end
                 end
             end as CONTA
-        from SRV010 (nolock)
-        when SRV010.D_E_L_E_T_ = ''
-    ) SRV
-    on SRV.RV_FILIAL = substring(SRD.RD_FILIAL, 1, 4)
-    and SRV.RV_COD = SRD.RD_PD
+        from SRT010 (nolock)
+        where SRT010.D_E_L_E_T_ = ''
+    ) SRT
+        on SRT.RV_FILIAL = substring(SRD.RD_FILIAL, 1, 4)
+        and SRT.RT_COD = SRD.RD_PD
 where
-        SRD.D_E_L_E_T_ = ''
+        SRA.D_E_L_E_T_ = ''
