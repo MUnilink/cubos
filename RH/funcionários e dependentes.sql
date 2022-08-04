218c3f245b6caeaa688773700a02bbaa72e69659
Manoel José <manoel.jose@unilinktransportes.com.br>
2022-08-04 20:19:46 -0300

    ajuste data e consulta

--- RH/funcionários e dependentes.sql
+++ RH/funcionários e dependentes.sql
@@ -7,12 +7,12 @@ select
 	
 	trim(SRA.RA_MAT) as MATRICULA,
 	trim(SRA.RA_NOME) as FUNCIONARIO,
-	cast(convert(date, SRA.RA_NASC, 103) as varchar) NASC_FUNC,
+	convert(date, SRA.RA_NASC, 103) as NASC_FUNC,
 	trim(SRA.RA_SEXO) as SEXO_FUNC,
 	trim(CTT.CTT_DESC01) as CENTRO_CUSTO,
 
 	trim(SRB.RB_NOME) DEPENDENTE,
-	cast(convert(date, SRB.RB_DTNASC, 103) as varchar) NASC_DEP,
+	convert(date, SRB.RB_DTNASC, 103) as NASC_DEP,
 	trim(SRB.RB_SEXO) as SEXO_DEP,
 
 	month(SRA.RA_NASC) as MES_FUNC,
@@ -25,12 +25,12 @@ select
 		else '-'
 	end as PARENTESCO
 
-from SRA010 as SRA (nolock)
-	inner join SRB010 as SRB (nolock)
+from SRA010 SRA (nolock)
+	inner join SRB010 SRB (nolock)
 		on SRB.D_E_L_E_T_ = ''
 		and SRB.RB_FILIAL = SRA.RA_FILIAL
 		and SRB.RB_MAT = SRA.RA_MAT
-	inner join CTT010 as CTT (nolock)
+	inner join CTT010 CTT (nolock)
     	on CTT.D_E_L_E_T_ = ''
     	and substring(SRA.RA_FILIAL, 1, 4) = CTT.CTT_FILIAL
     	and SRA.RA_CC = CTT.CTT_CUSTO
