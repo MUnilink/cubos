a82a83f4a8e8acfac492b8e829d50c78972b8c4a
Manoel José <manoel.jose@unilinktransportes.com.br>
2022-08-22 16:41:11 -0300

    ver rateio manutenção viagem; início combustível

--- FAST ANALYTICS/gooddata-TMS/Agent/query/fact/custo TMS.sql
+++ FAST ANALYTICS/gooddata-TMS/Agent/query/fact/custo TMS.sql
@@ -138,7 +138,14 @@ select
 
     DIARIAS.DYV_IDCDIA,
     DIARIAS.DYX_DATDIA,
-    DIARIAS.DYX_VLRUNI
+    DIARIAS.DYX_VLRUNI,
+
+    MANUTENCAO.TJ_CODBEM,
+    MANUTENCAO.TJ_ORDEM,
+    MANUTENCAO.TL_DTINICI,
+    MANUTENCAO.INSUMO,
+    MANUTENCAO.DESC_INSUMO,
+    MANUTENCAO.TL_CUSTO
 
 from DUD010 DUD (nolock)
     left join DTQ010 DTQ (nolock)
@@ -216,29 +223,239 @@ from DUD010 DUD (nolock)
             DYX010.DYX_ITEM,
             convert(date, DYX010.DYX_DATDIA, 103) as DYX_DATDIA,
             DYX010.DYX_QTDE,
-            sum(DYX010.DYX_VLRUNI) as DYX_VLRUNI
+            DYX010.DYX_VLRUNI
         from DYV010 (nolock)
             inner join DYX010 (nolock)
                 on DYX010.D_E_L_E_T_ = ''
                 and DYX010.DYX_IDCDIA = DYV010.DYV_IDCDIA
                 and year(DYX010.DYX_DATDIA) = 2022
         where DYV010.D_E_L_E_T_ = ''
-        group by
-            DYV010.DYV_IDCDIA,
-            DYX010.DYX_IDCDIA,
-            DYX010.DYX_DATDIA,
-            DYX010.DYX_ITEM,
-            DYV010.DYV_VIAGEM,
-            DYV010.DYV_CODMOT,
-            DYX010.DYX_QTDE
     ) DIARIAS
         on DIARIAS.DYV_VIAGEM = DTQ.DTQ_VIAGEM
     left join
     (
-        select *
-        from STL010 (nolock)
+        select distinct
+            STJ.TJ_CODBEM,
+            STJ.TJ_ORDEM,
+            convert(date, STL.TL_DTINICI, 103) as TL_DTINICI,
+            convert(date, STJ.TJ_DTORIGI, 103) as TJ_DTORIGI,
+            convert(date, STJ.TJ_DTMRFIM, 103) as TJ_DTMRFIM,
+            STL.TL_QUANTID,
+            STL.TL_SEQRELA,
+
+            case when STL.TL_CODIGO = ST0.T0_ESPECIA or STL.TL_CODIGO = ST1.T1_CODFUNC then 'MÃO-DE-OBRA'
+            else
+                case when STL.TL_CODIGO = SB1.B1_COD and SB1.B1_COD like '1%' then 'PEÇAS'
+                else
+                    case when SA2.A2_COD + SA2.A2_LOJA = STL.TL_FORNEC + STL.TL_LOJA then 'TERCEIROS'
+                    else
+                        case when STL.TL_CODIGO = SH4.H4_CODIGO then 'FERRAMENTA'
+                        else 'OUTROS'
+                        end
+                    end
+                end
+            end as NATUREZA_CUSTO,
+
+            case when STL.TL_CODIGO = ST1.T1_CODFUNC then trim(isnull(ST1.T1_CODFUNC, isnull(ST0.T0_ESPECIA, '-')))
+            else
+                case when STL.TL_CODIGO = SB1.B1_COD and SB1.B1_COD like '1%' then trim(SB1.B1_COD)
+                else
+                    case when SA2.A2_COD + SA2.A2_LOJA = STL.TL_FORNEC + STL.TL_LOJA then isnull(trim(SA2.A2_COD) + '-' + trim(SA2.A2_LOJA), '-')
+                    else
+                        case when STL.TL_CODIGO = SH4.H4_CODIGO then trim(SH4.H4_CODIGO)
+                        else 'OUTROS'
+                        end
+                    end
+                end
+            end as INSUMO,
+
+            case when STL.TL_CODIGO = ST1.T1_CODFUNC then trim(isnull(ST1.T1_NOME, isnull(ST0.T0_NOME, '-')))
+            else
+                case when STL.TL_CODIGO = SB1.B1_COD and SB1.B1_COD like '1%' then trim(SB1.B1_DESC)
+                else
+                    case when SA2.A2_COD + SA2.A2_LOJA = STL.TL_FORNEC + STL.TL_LOJA then trim(SA2.A2_NOME)
+                    else
+                        case when STL.TL_CODIGO = SH4.H4_CODIGO then trim(SH4.H4_DESCRI)
+                        else 'OUTROS'
+                        end
+                    end
+                end
+            end as DESC_INSUMO,
+
+            year(STL.TL_DTINICI) as ano_APP,
+            month(STL.TL_DTINICI) as mes_APP,
+
+            case when STJ.TJ_SERVICO = 'PNEMOV' then 'PNEU' else 'MANUTENÇÃO' end as TIPO_CUSTO,
+
+            case when trim(STL.TL_CODIGO) in ('11380003', '11380004', '11380005') and STL.TL_LOCAL = '80' then ADESIVO_CUSTO.B9_CM * STL.TL_QUANTID
+            else
+                case when trim(STL.TL_CODIGO) like ('1130%') then PNEU_CUSTO.B9_CM * STL.TL_QUANTID
+                else
+                    STL.TL_CUSTO
+                end
+            end as TL_CUSTO
+        from STJ010 STJ (nolock)
+            inner join ST9010 ST9 (nolock)
+                on ST9.D_E_L_E_T_ = ''
+                and ST9.T9_CODBEM = STJ.TJ_CODBEM
+
+                left join TQR010 TQR (nolock)
+                    on TQR.D_E_L_E_T_ = ''
+                    and TQR.TQR_TIPMOD = ST9.T9_TIPMOD
+
+            inner join STL010 STL (nolock)
+                on STL.D_E_L_E_T_ = ''
+                and STL.TL_ORDEM = STJ.TJ_ORDEM
+                and STL.TL_PLANO = STJ.TJ_PLANO
+                and STL.TL_FILIAL = STJ.TJ_FILIAL
+
+                left join SA2010 SA2 (nolock)
+                    on SA2.D_E_L_E_T_ = ''
+                    and SA2.A2_COD + SA2.A2_LOJA = STL.TL_FORNEC + STL.TL_LOJA
+                left join SB1010 SB1 (nolock)
+                    on SB1.D_E_L_E_T_ = ''
+                    and SB1.B1_COD = STL.TL_CODIGO
+                left join
+                (
+                    select
+                        SB9010.B9_COD,
+                        min(SB9010.B9_VINI1/SB9010.B9_QINI) as B9_CM,
+                        min(SB9010.B9_DATA) as B9_DATA
+                    from SB9010 (nolock)
+                    where
+                            SB9010.D_E_L_E_T_ = ''
+                        and SB9010.B9_LOCAL = '01'
+                        and SB9010.B9_COD in ('11380003', '11380004', '11380005')
+                        and SB9010.B9_QINI != 0
+                    group by
+                        SB9010.B9_COD
+                ) ADESIVO_CUSTO
+                    on ADESIVO_CUSTO.B9_COD = STL.TL_CODIGO
+                left join
+                (
+                    select
+                        SB9010.B9_COD,
+                        min(SB9010.B9_VINI1/SB9010.B9_QINI) as B9_CM,
+                        min(SB9010.B9_DATA) as B9_DATA
+                    from SB9010 (nolock)
+                    where
+                            SB9010.D_E_L_E_T_ = ''
+                        and SB9010.B9_LOCAL = '20'
+                        and SB9010.B9_COD like '1130%'
+                        and SB9010.B9_QINI != 0
+                    group by
+                        SB9010.B9_COD
+                ) PNEU_CUSTO
+                    on PNEU_CUSTO.B9_COD = STL.TL_CODIGO
+                left join SH4010 SH4 (nolock)
+                    on SH4.D_E_L_E_T_ = ''
+                    and SH4.H4_CODIGO = STL.TL_CODIGO
+                left join ST0010 ST0 (nolock)
+                    on ST0.D_E_L_E_T_ = ''
+                    and ST0.T0_ESPECIA = STL.TL_CODIGO
+                left join ST1010 ST1 (nolock)
+                    on ST1.D_E_L_E_T_ = ''
+                    and ST1.T1_FILIAL = STL.TL_FILIAL
+                    and ST1.T1_CODFUNC = STL.TL_CODIGO
+        where
+                STJ.D_E_L_E_T_ = ''
+            and STL.TL_SEQRELA > 0
+            and STL.TL_DTINICI > 20211231
+            and STJ.TJ_CCUSTO = 304
+            and ST9.T9_CODFAMI != 'PN'
+
     ) MANUTENCAO
-        on MANUTENCAO.TL_DTINICI
-        and MANUTENCAO.T9_CODBEM = DTR.DTR_CODVEI
-where
-        DUD.D_E_L_E_T_ = ''
+        on MANUTENCAO.NATUREZA_CUSTO != 'MÃO-DE-OBRA'
+        and MANUTENCAO.TL_DTINICI = 
+        and MANUTENCAO.TJ_CODBEM = DTR.DTR_CODVEI
+    
+    left join
+    (
+        select
+            ZD3.ZD3_LITROS,
+            ZD3.ZD3_VLUNI,
+            ZD3.ZD3_HODOM,
+            ZD3.ZD3_KMRD,
+            ZD3.ZD3_KML,
+            ZD3.ZD3_TOTAL,
+            trim(isnull(ZD3.ZD3_DATA, '-')) as ZD3_DATA,
+
+            trim(isnull(TQI.TQI_TANQUE, '-')) as TQI_TANQUE,
+            trim(isnull(ST9.T9_CODBEM, '-')) as T9_CODBEM,
+            trim(isnull(TQM.TQM_CODCOM, '-')) as TQM_CODCOM,
+            trim(isnull(ZD3.TQN_CCUSTO, '-')) as TQN_CCUSTO
+        from
+            (
+                select
+                    case cast(ZD3010.ZD3_TANQUE as int)
+                        when 12 then '010102'
+                        else trim(isnull(ZD3010.ZD3_FILIAL, '-'))
+                    end as ZD3_FILIAL,
+                    ZD3010.ZD3_KM as ZD3_HODOM,
+
+                    ZD3010.ZD3_VEICUL,
+                    ZD3010.ZD3_LITROS,
+                    ZD3010.ZD3_VLUNI,
+                    ZD3010.ZD3_TOTAL,
+                    
+                    ZD3010.ZD3_TANQUE,
+                    ZD3010.ZD3_COMB,
+                    substring(ZD3010.ZD3_DATA, 1, 8) as ZD3_DATA,
+                    ZD3010.ZD3_KML,
+                    ZD3010.ZD3_KMRD,
+                    TQN010.TQN_CCUSTO
+                from ZD3010 (nolock)
+                    inner join TQN010 (nolock)
+                        on TQN010.D_E_L_E_T_ = ''
+                        and TQN010.TQN_FROTA = ZD3010.ZD3_VEICUL
+                        and TQN010.TQN_DTABAS + TQN010.TQN_HRABAS = substring(ZD3010.ZD3_DATA, 1, 8) + substring(ZD3010.ZD3_DATA, 10, 14)
+                where ZD3010.D_E_L_E_T_ = ''
+            ) as ZD3
+
+            left join
+            (
+                select
+                    case cast(TQI010.TQI_CODPOS as int)
+                        when 59 then '010102'
+                        else trim(isnull(TQI010.TQI_FILIAL, '-'))
+                    end as TQI_FILIAL,
+
+                    TQI010.TQI_CODPOS,
+                    TQI010.TQI_LOJA,
+                    TQI010.TQI_TANQUE,
+                    TQI010.TQI_YDETAN,
+                    TQI010.TQI_CODCOM,
+                    TQI010.TQI_PRODUT,
+                    TQI010.TQI_FABRIC
+                from TQI010
+                where TQI010.D_E_L_E_T_ = ''
+            ) as TQI
+                on TQI.TQI_FILIAL = ZD3.ZD3_FILIAL
+                and TQI.TQI_TANQUE = ZD3.ZD3_TANQUE
+
+                left join
+                (
+                    select
+                        case cast(TQF010.TQF_CODIGO as int)
+                            when 59 then '010102'
+                            else trim(isnull(TQF010.TQF_CODFIL, '-'))
+                        end as TQF_FILIAL,
+                        TQF010.TQF_CODIGO,
+                        TQF010.TQF_LOJA
+                    from TQF010
+                    where TQF010.D_E_L_E_T_ = ''
+                ) as TQF
+                    on TQF.TQF_FILIAL = TQI.TQI_FILIAL
+                    and TQF.TQF_CODIGO + TQF.TQF_LOJA = TQI.TQI_CODPOS + TQI.TQI_LOJA
+
+            left join ST9010 as ST9
+                on ST9.D_E_L_E_T_ = ''
+                and ST9.T9_CODBEM = ZD3.ZD3_VEICUL
+            left join TQM010 as TQM
+                on TQM.D_E_L_E_T_ = ''
+                and TQM.TQM_CODCOM = ZD3.ZD3_COMB
+        where ZD3.TQN_CCUSTO = 304
+    ) COMBUSTIVEL
+        on COMBUSTIVEL.T9_CODBEM = DTR.DTR_CODVEI
+where DUD.DUD_VIAGEM in (7263, 7268, 7269, 7277, 7283, 7284, 7286, 7287, 7291, 7292, 7296, 7298, 7299, 7300, 7301, 7302, 7307, 7308, 7309, 7310, 7311, 7312, 7313, 7314, 7316, 7318, 7320, 7325, 7329, 7330, 7333, 7334, 7336, 7337, 7338, 7339, 7340, 7341, 7349, 7351, 7353, 7354, 7355, 7356, 7357, 7358, 7359, 7362, 7363, 7371, 7372, 7373, 7375, 7376, 7376, 7376, 7376, 7377, 7379, 7380, 7385, 7389, 7390, 7392, 7395, 7398, 7399, 7403, 7404, 7407, 7408, 7409, 7410, 7411, 7413, 7414, 7415, 7416, 7418, 7419, 7420, 7422, 7423, 7424, 7427, 7428, 7429, 7431, 7433, 7438, 7439, 7440, 7441, 7442, 7443, 7444, 7445, 7448, 7449, 7450, 7454, 7455, 7456, 7458, 7459, 7460, 7462, 7463, 7464, 7465, 7468, 7469, 7470, 7471, 7473, 7474, 7476, 7477, 7478, 7479, 7481, 7482, 7483, 7484, 7487, 7488, 7490, 7491, 7492, 7493, 7494, 7495, 7496, 7503, 7504, 7506, 7507, 7509, 7510, 7512, 7516, 7517, 7518, 7519, 7520, 7521, 7523, 7524, 7528, 7533, 7534, 7535, 7536, 7537, 7538, 7539, 7540, 7541, 7542, 7543, 7544, 7545, 7546, 7548, 7552, 7553, 7554, 7555, 7556, 7557, 7558, 7559, 7560, 7561, 7562, 7566, 7567, 7568, 7569, 7570, 7571, 7575, 7576, 7577, 7579, 7580, 7581, 7583, 7584, 7585, 7588, 7589, 7590, 7591, 7592, 7594, 7595, 7596, 7597, 7598, 7599, 7600, 7601, 7602, 7603, 7604, 7605, 7606, 7607, 7609, 7610, 7611, 7612, 7613, 7614, 7616, 7617, 7618, 7619, 7620, 7621, 7622, 7623, 7623, 7623, 7623, 7625, 7626, 7627, 7630, 7633, 7634, 7635, 7636, 7637, 7638, 7639, 7640, 7641, 7644, 7648, 7649, 7650, 7651, 7652, 7653, 7654, 7657, 7659, 7661, 7662, 7663, 7664, 7665, 7666, 7667, 7668, 7669, 7671, 7673, 7674, 7676, 7677, 7678, 7679, 7680, 7681, 7682, 7683, 7684, 7685, 7686, 7687, 7688, 7689, 7690, 7691, 7694, 7695, 7696, 7697, 7698, 7699, 7700, 7707, 7710, 7711, 7712, 7713, 7714, 7715, 7716, 7717, 7718, 7728, 7729, 7732, 7733, 7736, 7740, 7741, 7742, 7743, 7751, 7752, 7753, 7755, 7756, 7757, 7758, 7759, 7760, 7761, 7762, 7763, 7764, 7765, 7766, 7767, 7777, 7778, 7779, 7780, 7781, 7782, 7787, 7790, 7791, 7792, 7793, 7794, 7795, 7796, 7797, 7798, 7799, 7801, 7802, 7805, 7806, 7807, 7808, 7809, 7810, 7811, 7812, 7813, 7814, 7815, 7816, 7823, 7824, 7825, 7826, 7827, 7828, 7829, 7835, 7839, 7841, 7842, 7844, 7846, 7847, 7857, 7861, 7862, 7866, 7867, 7876, 7877, 7878, 7879, 7880, 7882, 7883, 7884, 7885, 7886, 7887, 7888, 7889, 7890, 7892, 7893, 7899, 7900, 7901, 7902, 7903, 7904, 7905, 7906, 7908, 7911, 7912, 7913, 7914, 7916, 7917, 7918, 7921, 7922, 7923, 7925, 7927, 7928, 7929, 7930, 7934, 7935, 7937, 7938, 7939, 7943, 7947, 7948, 7326, 7672, 7321)
+    and DUD.D_E_L_E_T_ = ''
