e494c349c18c6f7b735f9b5065074e2477211407
Manoel <manoel.jose@unilinktransportes.com.br>
2022-09-14 16:27:54 -0300

    ver integração e taxa de depreciação por grupo ou bem

--- patrimônio/ativos fixos.sql
+++ patrimônio/ativos fixos.sql
@@ -3,9 +3,9 @@ select
 	trim(isnull(SN1.N1_CBASE, '-')) as N1_CBASE,
 	trim(isnull(SN1.N1_DESCRIC, '-')) as N1_DESCRIC,
     trim(isnull(ST9.T9_CODBEM, '-')) as T9_CODBEM,
-	TPN.TPN_CCUSTO,
 	convert(date, SN3.N3_DINDEPR, 103) as N3_DINDEPR,
-	SNG.NG_TXDEPR1 /12 as TXDEPRECMENSAL,
+	SN3.N3_TXDEPR1 /12 as TXDEPRECMENSAL,
+	SNG.NG_TXDEPR1 /12 as TXDEPRECMENSALGRUPO,
 
 	SN1.N1_QUANTD,
 	SN3.N3_VORIG1,
@@ -23,20 +23,15 @@ select
 	SN3.N3_TXDEPR5,
 
 	datediff(month, SN3.N3_DINDEPR, cast('20220731' as date)) TEMPO_ATIVO,
-	100 / (SNG.NG_TXDEPR1 /12) as TEMPO_DEPREC,
-	SN3.N3_VORIG1 * (SNG.NG_TXDEPR1 / 1200) as DEPRECMENSAL,
-    case when (12 * (100 / SNG.NG_TXDEPR1)) > datediff(month, SN3.N3_DINDEPR, cast('20220731' as date)) then SN3.N3_VORIG1 * (SNG.NG_TXDEPR1 / 1200) else 0.0 end as DEPRECATUAL,
-    case when (12 * (100 / SNG.NG_TXDEPR1)) > datediff(month, SN3.N3_DINDEPR, cast('20220731' as date)) then ((12 * (100 / SNG.NG_TXDEPR1)) - datediff(month, SN3.N3_DINDEPR, cast('20220731' as date))) * SN3.N3_VORIG1 * (SNG.NG_TXDEPR1 / 1200) else 0.0 end as RESIDUAL
+	100 / (SN3.N3_TXDEPR1 /12) as TEMPO_DEPREC,
+	SN3.N3_VORIG1 * (SN3.N3_TXDEPR1 / 1200) as DEPRECMENSAL,
+    case when (12 * (100 / SN3.N3_TXDEPR1)) > datediff(month, SN3.N3_DINDEPR, cast('20220731' as date)) then SN3.N3_VORIG1 * (SN3.N3_TXDEPR1 / 1200) else 0.0 end as DEPRECATUAL,
+    case when (12 * (100 / SN3.N3_TXDEPR1)) > datediff(month, SN3.N3_DINDEPR, cast('20220731' as date)) then ((12 * (100 / SN3.N3_TXDEPR1)) - datediff(month, SN3.N3_DINDEPR, cast('20220731' as date))) * SN3.N3_VORIG1 * (SN3.N3_TXDEPR1 / 1200) else 0.0 end as RESIDUAL
 
 from SN1010 SN1 (nolock)
     left join ST9010 ST9 (nolock)
         on ST9.D_E_L_E_T_ = ''
         and ST9.T9_CODBEM = SN1.N1_CODBEM
-
-		inner join TPN010 TPN (nolock)
-			on TPN.D_E_L_E_T_ = ''
-			and TPN.TP_CODBEM = ST9.T9_CODBEM
-
 	inner join SNG010 SNG (nolock)
 		on SNG.D_E_L_E_T_ = ''
 		and SNG.NG_GRUPO = SN1.N1_GRUPO
@@ -47,4 +42,4 @@ from SN1010 SN1 (nolock)
 		and SN3.N3_CBASE = SN1.N1_CBASE
 where
         SN1.D_E_L_E_T_ = ''
-    and cast(SNG.NG_TXDEPR1 as decimal) > 0
+    and cast(SN3.N3_TXDEPR1 as decimal) > 0
