d2b53e1816e2be6cd35cca3399e7957cd2c2c9bc
Manoel <manoel.jose@unilinktransportes.com.br>
2022-09-20 11:54:00 -0300

    tabela e relatório de depreciação; validar custo do TMS

--- FAST ANALYTICS/gooddata-TMS/Agent/query/fact/custo TMS.sql
+++ FAST ANALYTICS/gooddata-TMS/Agent/query/fact/custo TMS.sql
@@ -558,6 +558,33 @@ from DUD010 DUD (nolock)
             DOCUMENTACAO.T9_CODBEM = VIAGEM.DTR_CODRB2 or
             DOCUMENTACAO.T9_CODBEM = VIAGEM.DTR_CODRB3
         )
+    left join /* ver amortização das taxas dos veículos */
+    (
+        select
+            ST9010.T9_CODBEM,
+            SN1010.N1_CBASE,
+            SN4010.N4_DATA,
+            SN4010.N4_VLROC1
+        from SN4010
+            left join SN1010 (nolock)
+                on SN1010.D_E_L_E_T_ = ''
+                and SN1010.N1_CBASE = SN4010.N4_CBASE
+            
+                inner join ST9010 (nolock)
+                    on ST9010.D_E_L_E_T_ = ''
+                    and ST9010.T9_CODBEM = SN1010.N1_CODBEM
+        where
+                SN4010.D_E_L_E_T_ = ''
+            and SN4010.N4_LA = 'S'
+    ) DEPRECIACAO
+        on substring(DEPRECIACAO.N4_DATA, 1, 6) = substring(VIAGEM.DATAFIM, 1, 6)
+        and
+        (
+            DEPRECIACAO.T9_CODBEM = VIAGEM.DTR_CODVEI or
+            DEPRECIACAO.T9_CODBEM = VIAGEM.DTR_CODRB1 or
+            DEPRECIACAO.T9_CODBEM = VIAGEM.DTR_CODRB2 or
+            DEPRECIACAO.T9_CODBEM = VIAGEM.DTR_CODRB3
+        )
     inner join
         (
             select
