bd3f980f796aaeafc6fb4d65d9410894a1525e64
Manoel <manoel.jose@unilinktransportes.com.br>
2022-09-29 14:26:13 -0300

    aparentemente não traz o plano em aberto

--- manutenção/relação de preventivas.sql
+++ manutenção/relação de preventivas.sql
@@ -1,7 +1,14 @@
 select
-    ST9.T9_CODBEM,
-    STF.TF_SERVICO,
-    STF.TF_SEQRELA,
+    STI.TI_FILIAL,
+    STI.TI_DATAPLA,
+    STI.TI_DESCRIC,
+
+    STJ.TJ_CODBEM,
+    STJ.TJ_ORDEM,
+    STJ.TJ_PLANO,
+    STJ.TJ_SERVICO,
+    STJ.TJ_SEQRELA,
+
     STF.TF_NOMEMAN,
     STF.TF_PADRAO,
     STF.TF_DTULTMA,
@@ -9,6 +16,28 @@ select
     STF.TF_PARADA,
     STF.TF_CONMANU,
     STF.TF_INENMAN,
-    STF.TF_ATIVO
-from STF010 STF (nolock)
-where STF.D_E_L_E_T_ = ''
+    STF.TF_ATIVO,
+    
+    STG.TG_SEQRELA,
+    STG.TG_TAREFA,
+    STG.TG_TIPOREG,
+    STG.TG_CODIGO,
+    STG.TG_QUANTID,
+    STG.TG_UNIDADE,
+    STG.TG_LOCAL
+from STJ010 STJ (nolock)
+    inner join STI010 STI (nolock)
+        on STI.D_E_L_E_T_ = ''
+        and STI.TI_FILIAL = STJ.TJ_FILIAL
+        and STI.TI_PLANO = STJ.TJ_PLANO
+    inner join STF010 STF (nolock)
+        on STF.D_E_L_E_T_ = ''
+        and STF.TF_CODBEM = STJ.TJ_CODBEM
+        and STF.TF_SERVICO = STJ.TJ_SERVICO
+        and STF.TF_SEQRELA = STJ.TJ_SEQRELA
+    inner join STG010 STG (nolock)
+        on STG.D_E_L_E_T_ = ''
+        and STG.TG_CODBEM = STF.TF_CODBEM
+        and STG.TG_SERVICO = STF.TF_SERVICO
+        and STG.TG_SEQRELA = STF.TF_SEQRELA
+where STJ.D_E_L_E_T_ = ''
