3fc5f64fd45c9b78eabc81f0a0e53f467b37af5d
Manoel <manoel.jose@unilinktransportes.com.br>
2022-09-30 13:50:53 -0300

    verificar saldo dos insumos da preventiva do plano

--- manutenção/relação de preventivas.sql
+++ manutenção/relação de preventivas.sql
@@ -8,6 +8,7 @@ select
     STJ.TJ_PLANO,
     STJ.TJ_SERVICO,
     STJ.TJ_SEQRELA,
+    STJ.TJ_SITUACA,
 
     STF.TF_NOMEMAN,
     STF.TF_PADRAO,
@@ -22,9 +23,12 @@ select
     STG.TG_TAREFA,
     STG.TG_TIPOREG,
     STG.TG_CODIGO,
+    SB1.B1_DESC,
+    SB1.B1_MSBLQL,
     STG.TG_QUANTID,
     STG.TG_UNIDADE,
-    STG.TG_LOCAL
+    STG.TG_LOCAL,
+    SB2.B2_CM1
 from STJ010 STJ (nolock)
     inner join STI010 STI (nolock)
         on STI.D_E_L_E_T_ = ''
@@ -35,9 +39,19 @@ from STJ010 STJ (nolock)
         and STF.TF_CODBEM = STJ.TJ_CODBEM
         and STF.TF_SERVICO = STJ.TJ_SERVICO
         and STF.TF_SEQRELA = STJ.TJ_SEQRELA
-    inner join STG010 STG (nolock)
-        on STG.D_E_L_E_T_ = ''
-        and STG.TG_CODBEM = STF.TF_CODBEM
-        and STG.TG_SERVICO = STF.TF_SERVICO
-        and STG.TG_SEQRELA = STF.TF_SEQRELA
-where STJ.D_E_L_E_T_ = ''
+
+        inner join STG010 STG (nolock)
+            on STG.D_E_L_E_T_ = ''
+            and STG.TG_CODBEM = STF.TF_CODBEM
+            and STG.TG_SERVICO = STF.TF_SERVICO
+            and STG.TG_SEQRELA = STF.TF_SEQRELA
+
+            left join SB1010 SB1 (nolock)
+                on SB1.D_E_L_E_T_ = ''
+                and SB1.B1_COD = STG.TG_CODIGO
+
+            left join SB2010 SB2 (nolock)
+                on SB2.D_E_L_E_T_ = ''
+                and SB2.B2_COD = STG.TG_CODIGO
+                and SB2.B2_LOCAL = case STG.TG_LOCAL when '80' then '01' else STG.TG_LOCAL end
+where STJ.D_E_L_E_T_ = '' and substring(STI.TI_DATAPLA, 1, 6) > '202112'
