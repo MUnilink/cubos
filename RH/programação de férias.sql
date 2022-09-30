05e21601c6fe0b3bb5667984047e71f9b72ad8a2
Manoel <manoel.jose@unilinktransportes.com.br>
2022-09-30 14:49:28 -0300

    add depto férias; testar cubo programação de férias

--- RH/programação de férias.sql
+++ RH/programação de férias.sql
@@ -9,40 +9,32 @@ select
 	trim(CTT.CTT_DESC01) as CCUSTO,
 	trim(CTD.CTD_ITEM) as AT,
 	trim(CTD.CTD_DESC01) as ATIVIDADE,
+    trim(SQB.QB_DEPTO) as DEPTO,
+    trim(SQB.QB_DESCRIC) as DEPARTAMENTO,
 
 	trim(SRJ.RJ_CODCBO) as CBO,
 	trim(SRA.RA_SEXO) as SEXO,
 	trim(SRA.RA_CIC) as CPF,
 
-    SRR.RR_PERIODO,
-    convert(date, RH_DATABAS, 103) as INI_PERIODO,
-    convert(date, RH_DBASEAT, 103) as FIM_PERIODO,
-    convert(date, RH_DATAINI, 103) as INI_FERIAS,
-    convert(date, RH_DATAFIM, 103) as FIM_FERIAS,
-    convert(date, RH_DTAVISO, 103) as AVISO,
-    convert(date, RH_DTRECIB, 103) as PAGAMENTO,
-    
-    SRH.RH_DFERVEN,
-    SRH.RH_DFERIAS,
-    SRH.RH_DABONPE as DIASABONO,
-    SRH.RH_DFALTAS,
-    SRH.RH_ABOPEC,
-    SRH.RH_PERC13S
+    SRF.RF_STATUS,
 
-from SRH010 SRH (nolock)
+from SRF010 SRF (nolock)
     inner join SRA010 SRA (nolock)
         on SRA.D_E_L_E_T_ = ''
-        and SRA.RA_FILIAL = SRH.RH_FILIAL
-        and SRA.RA_MAT = SRH.RH_MAT
+        and SRA.RA_FILIAL = SRF.RF_FILIAL
+        and SRA.RA_MAT = SRF.RF_MAT
 
+        inner join SQB010 SQB (nolock)
+            on SQB.D_E_L_E_T_ = ''
+            and SQB.QB_FILIAL = substring(SRA.RA_FILIAL, 1, 4)
         inner join SRJ010 SRJ (nolock)
             on SRJ.D_E_L_E_T_ = ''
-            and substring(SRA.RA_FILIAL, 1, 4) = SRJ.RJ_FILIAL
-            and SRA.RA_CODFUNC = SRJ.RJ_FUNCAO
+            and SRJ.RJ_FILIAL = substring(SRA.RA_FILIAL, 1, 4)
+            and SRJ.RJ_FUNCAO = SRA.RA_CODFUNC
         inner join CTT010 CTT (nolock)
             on CTT.D_E_L_E_T_ = ''
-            and SRA.RA_CC = CTT.CTT_CUSTO
+            and CTT.CTT_CUSTO = SRA.RA_CC
         inner join CTD010 CTD (nolock)
             on CTD.D_E_L_E_T_ = ''
-    	    and SRA.RA_ITEM = CTD.CTD_ITEM
+    	    and CTD.CTD_ITEM = SRA.RA_ITEM
 where SRH.D_E_L_E_T_ = ''
