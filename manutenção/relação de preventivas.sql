d69d5ad40df4437364be0751f90d6768931d8052
Manoel <manoel.jose@unilinktransportes.com.br>
2022-10-01 10:28:17 -0300

    visão dos insumos da preventiva ok

--- manutenção/relação de preventivas.sql
+++ manutenção/relação de preventivas.sql
@@ -1,34 +1,50 @@
 select
-    STI.TI_FILIAL,
-    STI.TI_DATAPLA,
-    STI.TI_DESCRIC,
+    trim(STI.TI_FILIAL) as FILIAL,
+    trim(STI.TI_DATAPLA) as PERIODO,
+    trim(STI.TI_DESCRIC) as NOME_PLANO,
 
-    STJ.TJ_CODBEM,
-    STJ.TJ_ORDEM,
-    STJ.TJ_PLANO,
-    STJ.TJ_SERVICO,
-    STJ.TJ_SEQRELA,
-    STJ.TJ_SITUACA,
+    trim(STJ.TJ_CCUSTO) as CC,
+    trim(STJ.TJ_CODBEM) as EQUIPAMENTO,
+    trim(STJ.TJ_ORDEM) as OS,
+    trim(STJ.TJ_PLANO) as PLANO,
+    trim(STJ.TJ_SERVICO) as SERVICO,
+    STJ.TJ_SEQRELA as TJ_SEQRELA,
+    case STJ.TJ_SITUACA when 'L' then 'LIBERADA' when 'P' then 'PENDENTE' else 'CANCELADA' end as STATUS_OS,
 
-    STF.TF_NOMEMAN,
-    STF.TF_PADRAO,
-    STF.TF_DTULTMA,
-    STF.TF_TIPACOM,
-    STF.TF_PARADA,
-    STF.TF_CONMANU,
-    STF.TF_INENMAN,
-    STF.TF_ATIVO,
-    
-    STG.TG_SEQRELA,
-    STG.TG_TAREFA,
-    STG.TG_TIPOREG,
-    STG.TG_CODIGO,
-    SB1.B1_DESC,
-    SB1.B1_MSBLQL,
-    STG.TG_QUANTID,
-    STG.TG_UNIDADE,
-    STG.TG_LOCAL,
-    SB2.B2_CM1
+    trim(STF.TF_NOMEMAN) as PREVENTIVA,
+    trim(STF.TF_PADRAO) as MAN_PADRAO,
+    STF.TF_DTULTMA as ULTIMA_MAN,
+    trim(STF.TF_TIPACOM) as TIPO_ACOMP,
+    trim(STF.TF_PARADA) as PARADA,
+    STF.TF_CONMANU as NUM_MAN,
+    STF.TF_INENMAN as VENCIMENTO,
+    STF.TF_ATIVO as ATIVO,
+    STG.TG_SEQRELA as TG_SEQRELA,
+    trim(STG.TG_TAREFA) as TAREFA,
+    trim(STG.TG_TIPOREG) as TIPO_INSUMO,
+    STG.TG_CODIGO as INSUMO,
+
+    (
+        select count(*)
+        from STF010 (nolock)
+            inner join STG010 (nolock)
+                on STG010.D_E_L_E_T_ = ''
+                and STG010.TG_CODBEM = STF010.TF_CODBEM
+                and STG010.TG_SERVICO = STF010.TF_SERVICO
+                and STG010.TG_SEQRELA = STF010.TF_SEQRELA
+        where
+                STF010.D_E_L_E_T_ = ''
+            and STG010.TG_CODIGO = STG.TG_CODIGO
+            and STF010.TF_CONMANU = STF.TF_CONMANU
+            and STF010.TF_CODBEM = STJ.TJ_CODBEM
+    ) as contador,
+
+    trim(SB1.B1_DESC) as PRODUTO,
+    case SB1.B1_MSBLQL when 1 then 'SIM' else 'NAO' end as BLOQUEADO,
+    STG.TG_QUANTID as QTD,
+    STG.TG_UNIDADE as UN,
+    STG.TG_LOCAL as ARMAZEM
+
 from STJ010 STJ (nolock)
     inner join STI010 STI (nolock)
         on STI.D_E_L_E_T_ = ''
@@ -50,8 +66,4 @@ from STJ010 STJ (nolock)
                 on SB1.D_E_L_E_T_ = ''
                 and SB1.B1_COD = STG.TG_CODIGO
 
-            left join SB2010 SB2 (nolock)
-                on SB2.D_E_L_E_T_ = ''
-                and SB2.B2_COD = STG.TG_CODIGO
-                and SB2.B2_LOCAL = case STG.TG_LOCAL when '80' then '01' else STG.TG_LOCAL end
 where STJ.D_E_L_E_T_ = '' and substring(STI.TI_DATAPLA, 1, 6) > '202112'
