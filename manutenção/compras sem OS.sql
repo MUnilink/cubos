6dde2e592265c1562197f54389f525f4773bb8e4
Manoel <manoel.jose@unilinktransportes.com.br>
2022-10-01 11:53:01 -0300

    VISÃO POR SC EM VEZ DEPOR PC

--- manutenção/compras sem OS.sql
+++ manutenção/compras sem OS.sql
@@ -18,8 +18,7 @@ select
 	trim(isnull(CTT.CTT_DESC01, '-')) as CC,
 
 	convert(date, SC1.C1_EMISSAO, 103) as DATA_SOLICITA,
-	year(SC1.C1_EMISSAO) as ANO_SOLICITA,
-	month(SC1.C1_EMISSAO) as MES_SOLICITA,
+	substring(SC1.C1_EMISSAO, 1, 6) as PERIODO_SC,
 	trim(isnull(upper(SC1.C1_SOLICIT), '-')) as SOLIC_SC,
 	
 	case SC7.C7_CONAPRO
@@ -30,8 +29,7 @@ select
 	end as APROVACAO,
 
 	convert(date, SC7.C7_EMISSAO, 103) as DATA_PEDIDO,
-	year(SC7.C7_EMISSAO) as ANO_PEDIDO,
-	month(SC7.C7_EMISSAO) as MES_PEDIDO,
+	substring(SC7.C7_EMISSAO, 1, 6) as PERIODO_PC,
 	trim(isnull(upper(SY1.Y1_NOME), '-')) as SOLIC_PC,
 
 	SC7.C7_QUANT as QTD_PEDIDA,
@@ -62,39 +60,41 @@ select
 
 	trim(isnull(SD1.D1_DOC, '-')) as D1_DOC,
 	trim(isnull(SD1.D1_SERIE, '-')) as D1_SERIE,
-	convert(date, SD1.D1_DTDIGIT, 103) DATA_EMI_NF,
+	convert(date, SD1.D1_DTDIGIT, 103) DATA_EMINF,
 	convert(date, SD1.D1_DTDIGIT, 103) DATA_NF
 
-from SC7010 SC7 (nolock)
+from SC1010 SC1 (nolock)
 	left join SB1010 SB1 (nolock)
 		on SB1.D_E_L_E_T_ = ''
-		and SB1.B1_COD = SC7.C7_PRODUTO
-	left join SA2010 SA2 (nolock)
-		on SA2.D_E_L_E_T_ = ''
-		and SA2.A2_COD = SC7.C7_FORNECE
-		and SA2.A2_LOJA = SC7.C7_LOJA
-	right join SC1010 SC1 (nolock)
-		on SC1.D_E_L_E_T_ = ''
-		and SC1.C1_FILIAL = SC7.C7_FILIAL
-		and SC1.C1_PRODUTO = SC7.C7_PRODUTO
-		and SC1.C1_NUM = SC7.C7_NUMSC
-		and SC1.C1_ITEM = SC7.C7_ITEMSC
-	left join SD1010 SD1 (nolock)
-		on SD1.D_E_L_E_T_ = ''
-		and SD1.D1_FILIAL = SC7.C7_FILIAL
-		and SD1.D1_COD = SC7.C7_PRODUTO
-		and SD1.D1_FORNECE = SC7.C7_FORNECE
-		and SD1.D1_LOJA = SC7.C7_LOJA
-		and SD1.D1_PEDIDO = SC7.C7_NUM
-	left join SY1010 SY1 (nolock)
-		on SY1.D_E_L_E_T_ = ''
-		and SY1.Y1_USER = SC7.C7_USER
+		and SB1.B1_COD = SC1.C1_PRODUTO
+		and SB1.B1_GRUPO like '1%'
+	left join SC7010 SC7 (nolock)
+		on SC7.D_E_L_E_T_ = ''
+		and SC7.C7_FILIAL = SC1.C1_FILIAL
+		and SC7.C7_PRODUTO = SC1.C1_PRODUTO
+		and SC7.C7_NUMSC = SC1.C1_NUM
+		and SC7.C7_ITEMSC = SC1.C1_ITEM
+
+		left join SA2010 SA2 (nolock)
+			on SA2.D_E_L_E_T_ = ''
+			and SA2.A2_COD = SC7.C7_FORNECE
+			and SA2.A2_LOJA = SC7.C7_LOJA
+		left join SD1010 SD1 (nolock)
+			on SD1.D_E_L_E_T_ = ''
+			and SD1.D1_FILIAL = SC7.C7_FILIAL
+			and SD1.D1_COD = SC7.C7_PRODUTO
+			and SD1.D1_FORNECE = SC7.C7_FORNECE
+			and SD1.D1_LOJA = SC7.C7_LOJA
+			and SD1.D1_PEDIDO = SC7.C7_NUM
+		left join SY1010 SY1 (nolock)
+			on SY1.D_E_L_E_T_ = ''
+			and SY1.Y1_USER = SC7.C7_USER
+
 	left join CTT010 CTT (nolock)
 		on CTT.D_E_L_E_T_ = ''
-		and CTT.CTT_CUSTO = SC7.C7_CC
+		and CTT.CTT_CUSTO = SC1.C1_CC
 	left join CTD010 CTD (nolock)
 		on CTD.D_E_L_E_T_ = ''
-		and CTD.CTD_ITEM = SC7.C7_ITEMCTA
+		and CTD.CTD_ITEM = SC1.C1_ITEMCTA
 where
-		SC7.D_E_L_E_T_ = ''
-	and SB1.B1_GRUPO like '1%'
+		SC1.D_E_L_E_T_ = ''
