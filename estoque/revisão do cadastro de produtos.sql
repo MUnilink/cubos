b2543040a02a6e8fbfac9dd4eda5091c0ef98827
Manoel <manoel.jose@unilinktransportes.com.br>
2022-10-04 15:45:10 -0300

    produtos com nome e part number repetido

--- estoque/revisão do cadastro de produtos.sql
+++ estoque/revisão do cadastro de produtos.sql
@@ -2,19 +2,20 @@ select
     SB1010.B1_GRUPO as GRUPO,
     SB1010.B1_COD as PRODUTO,
     SB1010.B1_DESC as DESCRICAO,
-    SB1010.B1_MSBLQL as BLOQUEADO,
-    SB1010.B1_DATREF as ,
-    SB1010.B1_YPARTNU as ,
+    case SB1010.B1_MSBLQL when 1 then 'SIM' else 'NAO' end as BLOQUEADO,
+    convert(date, SB1010.B1_DATREF, 103) as DATA_CRIADO,
+    SB1010.B1_YPARTNU as PARTNUMBER,
 
     SB1.B1_GRUPO as dup_GRUPO,
-    SB1.B1_COD as dup_COD,
-    SB1.B1_DESC as dup_DESC,
-    SB1.B1_MSBLQL as dup_MSBLQL,
-    SB1.B1_DATREF as dup_DATREF,
-    SB1.B1_YPARTNU as dup_YPARTNU
+    SB1.B1_COD as dup_PRODUTO,
+    SB1.B1_DESC as dup_DESCRICAO,
+    case SB1.B1_MSBLQL when 1 then 'SIM' else 'NAO' end as dup_BLOQUEADO,
+    convert(date, SB1.B1_DATREF, 103) as dup_DATA_CRIADO,
+    SB1.B1_YPARTNU as dup_PARTNUMBER
 from SB1010 (nolock)
     inner join SB1010 SB1 (nolock)
-        on SB1010.B1_COD != SB1.B1_COD
+        on SB1.D_E_L_E_T_ = ''
+        and SB1010.B1_COD != SB1.B1_COD
         and SB1010.B1_DESC = SB1.B1_DESC
         and SB1010.B1_YPARTNU = SB1.B1_YPARTNU
         and SB1.B1_COD like '1%'
