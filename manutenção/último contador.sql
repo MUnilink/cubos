b34597d94cd50ac2b562f30e1f64f1c09cf0aea7
Manoel <manoel.jose@unilinktransportes.com.br>
2022-10-07 16:16:35 -0300

    ver formato de data, mas parece ok

--- manutenção/último contador.sql
+++ manutenção/último contador.sql
@@ -1,9 +1,10 @@
 select
-    STP.TP_CODBEM,
-    STP.TP_POSCONT,
-    max(STP.TP_DTLEITU) as ULT_CONT
+    trim(STP.TP_CODBEM) as TP_CODBEM,
+    max(STP.TP_POSCONT) as MAX_CONT,
+    convert(date, max(STP.TP_DTLEITU), 103) as ULT_CONT
 from STP010 STP (nolock)
-where STP.D_E_L_E_T_ = ''
+where
+        STP.D_E_L_E_T_ = ''
+    and STP.TP_CODBEM like 'CM50%'
 group by
-    STP.TP_CODBEM,
-    STP.TP_POSCONT
+    STP.TP_CODBEM
