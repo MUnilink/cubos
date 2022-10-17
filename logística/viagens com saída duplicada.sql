27f78789c8dd0dc4e9ed8d4befb25715ab94d056
Manoel <manoel.jose@unilinktransportes.com.br>
2022-10-17 09:48:00 -0300

    viagens com saída de viagem duplicada

---
+++ logística/viagens com saída duplicada.sql
@@ -0,0 +1,230 @@
+select
+    DTQ.DTQ_FILORI,
+    DTQ.DTQ_VIAGEM,
+    DT6.DT6_DOC,
+    DT6.DT6_SERIE,
+    convert(date, DT6.DT6_DATEMI, 103) as DT6_DATEMI,
+    DA8.DA8_DESC,
+    DTQ.DTQ_KMVGE,
+    
+    DTQ.DTQ_DATGER,
+    DTQ.DTQ_DATFEC,
+    DTQ.DTQ_DATENC,
+
+    trim(DUYORI.DUY_DESCRI) as ORIGEM,
+    trim(DUYDES.DUY_DESCRI) as DESTINO,
+    trim(DUYDEV.DUY_DESCRI) as DEVEDOR,
+    trim(DEV.A1_COD) as A1_COD,
+    trim(DEV.A1_LOJA) as A1_LOJA,
+    trim(DEV.A1_NOME) as CLIENTE,
+
+    (
+        select substring(ZB1010.ZB1_MSGTXT, 2, len(ZB1010.ZB1_MSGTXT))
+        from DTW010 (nolock)
+            inner join ZB1010 (nolock)
+                on ZB1010.D_E_L_E_T_ = ''
+                and ZB1010.ZB1_MSGTXT like '&_%' escape '&'
+                and
+                    dateadd(hour, -3, datetimefromparts(substring(ZB1010.ZB1_MSGTIM, 1, 4), substring(ZB1010.ZB1_MSGTIM, 6, 2), substring(ZB1010.ZB1_MSGTIM, 9, 2), substring(ZB1010.ZB1_MSGTIM, 12, 2), substring(ZB1010.ZB1_MSGTIM, 15, 2), 0, 0))
+                    =
+                    datetimefromparts(year(DTW010.DTW_DATREA), month(DTW010.DTW_DATREA), day(DTW010.DTW_DATREA), substring(DTW010.DTW_HORREA, 1, 2), substring(DTW010.DTW_HORREA, 3, 4), 0, 0)
+        where 
+                DTW010.D_E_L_E_T_ = ''
+            and DTW010.DTW_FILORI = DTQ.DTQ_FILORI
+            and DTW010.DTW_VIAGEM = DTQ.DTQ_VIAGEM
+            and ZB1010.ZB1_CODDA3 = DTR.DTR_CODVEI
+            and DTW010.DTW_ATIVID in ('050')
+    ) as km_fim,
+    (
+        select substring(ZB1010.ZB1_MSGTXT, 2, len(ZB1010.ZB1_MSGTXT))
+        from DTW010 (nolock)
+            inner join ZB1010 (nolock)
+                on ZB1010.D_E_L_E_T_ = ''
+                and ZB1010.ZB1_MSGTXT like '&_%' escape '&'
+                and
+                    dateadd(hour, -3, datetimefromparts(substring(ZB1010.ZB1_MSGTIM, 1, 4), substring(ZB1010.ZB1_MSGTIM, 6, 2), substring(ZB1010.ZB1_MSGTIM, 9, 2), substring(ZB1010.ZB1_MSGTIM, 12, 2), substring(ZB1010.ZB1_MSGTIM, 15, 2), 0, 0))
+                    =
+                    datetimefromparts(year(DTW010.DTW_DATREA), month(DTW010.DTW_DATREA), day(DTW010.DTW_DATREA), substring(DTW010.DTW_HORREA, 1, 2), substring(DTW010.DTW_HORREA, 3, 4), 0, 0)
+        where
+                DTW010.D_E_L_E_T_ = ''
+            and DTW010.DTW_FILORI = DTQ.DTQ_FILORI
+            and DTW010.DTW_VIAGEM = DTQ.DTQ_VIAGEM
+            and ZB1010.ZB1_CODDA3 = DTR.DTR_CODVEI
+            and DTW010.DTW_ATIVID in ('049')
+    ) as km_ini,
+
+    DTR.DTR_ITEM,
+    DUP.DUP_CODMOT,
+    DA4.DA4_MAT,
+    DA4.DA4_NOME,
+    DA4.DA4_FORNEC,
+    DA4.DA4_LOJA,
+
+    DTR.DTR_CODVEI,
+    (select DA3010.DA3_PLACA from DA3010 where DA3010.DA3_COD = DTR.DTR_CODVEI) as PLACA_VEI,
+    DTR.DTR_CODRB1,
+    (select DA3010.DA3_PLACA from DA3010 where DA3010.DA3_COD = DTR.DTR_CODRB1) as PLACA_RB1,
+    DTR.DTR_CODRB2,
+    DTR.DTR_CODRB3,
+
+    DT6.DT6_VALFRE / (select count(DTR010.DTR_CODVEI) from DTR010 where DTR010.DTR_VIAGEM = DTQ.DTQ_VIAGEM) as CTE_CM,
+    DT6.DT6_VALFRE as CTE_TOTAL,
+    DT6.DT6_VALIMP / (select count(DTR010.DTR_CODVEI) from DTR010 where DTR010.DTR_VIAGEM = DTQ.DTQ_VIAGEM) IMPOSTO_CM,
+    DT6.DT6_VALIMP as IMPOSTO_TOTAL,
+    DT6.DT6_VALTOT,
+
+    DT6.DT6_CLIDEV,
+    DT6.DT6_LOJDEV,
+
+    DTC.DTC_FILORI,
+    DTC.DTC_DOC,
+    DTC.DTC_SERIE,
+    DTC.DTC_NUMNFC,
+    DTC.DTC_SERNFC,
+    DTC.DTC_CODPRO,
+    DTC.DTC_VALOR,
+
+    case when DT5.DT5_STATUS = '4' then 'INTERNA' else case when DT5.DT5_STATUS like '[0-9]' then 'COLETA' else 'ENTREGA' end end as STATUS,
+
+    DT5.DT5_NUMSOL,
+    DT5.DT5_DOC,
+    DT5.DT5_SERIE,
+    DT5.DT5_STATUS,
+    DT5.DT5_TIPCOL,
+    DT5.DT5_CODSOL,
+    DT5.DT5_CODOBC,
+
+    DF1.DF1_NUMAGE,
+    DF1.DF1_ITEAGE,
+
+    (
+        select cast(DTW010.DTW_DATREA as date)
+        from DTW010 (nolock)
+        where 
+                DTW010.D_E_L_E_T_ = ''
+            and DTW010.DTW_FILORI = DTQ.DTQ_FILORI
+            and DTW010.DTW_VIAGEM = DTQ.DTQ_VIAGEM
+            and DTW010.DTW_ATIVID = '049'
+    ) as DATAINI,
+    (
+        select DTW010.DTW_HORREA
+        from DTW010 (nolock)
+        where 
+                DTW010.D_E_L_E_T_ = ''
+            and DTW010.DTW_FILORI = DTQ.DTQ_FILORI
+            and DTW010.DTW_VIAGEM = DTQ.DTQ_VIAGEM
+            and DTW010.DTW_ATIVID = '049'
+    ) as HORAINI,
+    (
+        select cast(DTW010.DTW_DATREA as date)
+        from DTW010 (nolock)
+        where 
+                DTW010.D_E_L_E_T_ = ''
+            and DTW010.DTW_FILORI = DTQ.DTQ_FILORI
+            and DTW010.DTW_VIAGEM = DTQ.DTQ_VIAGEM
+            and DTW010.DTW_ATIVID = '050'
+    ) as DATAFIM,
+    (
+        select DTW010.DTW_HORREA
+        from DTW010 (nolock)
+        where 
+                DTW010.D_E_L_E_T_ = ''
+            and DTW010.DTW_FILORI = DTQ.DTQ_FILORI
+            and DTW010.DTW_VIAGEM = DTQ.DTQ_VIAGEM
+            and DTW010.DTW_ATIVID = '050'
+    ) as HORAFIM,
+    (
+        select substring(DTW010.DTW_DATREA, 1, 6)
+        from DTW010 (nolock)
+        where 
+                DTW010.D_E_L_E_T_ = ''
+            and DTW010.DTW_FILORI = DTQ.DTQ_FILORI
+            and DTW010.DTW_VIAGEM = DTQ.DTQ_VIAGEM
+            and DTW010.DTW_ATIVID = '050'
+    ) as COMPETENCIA,
+
+    case DTQ.DTQ_STATUS
+        when '1' then 'EXCLUÍDA'
+        when '2' then 'EM TRANSITO'
+        when '3' then 'ENCERRADA'
+        when '4' then 'CHEGADA EM FILIAL'
+        when '5' then 'FECHADA'
+        when '9' then 'CANCELADA'
+        else 'OUTROS'
+    end as DTQ_STATUS
+
+from DTQ010 DTQ (nolock)
+    inner join DA8010 DA8 (nolock)
+        on DA8.D_E_L_E_T_ = ''
+        and DA8.DA8_COD = DTQ.DTQ_ROTA
+    inner join DTR010 DTR (nolock)
+        on DTR.D_E_L_E_T_ = ''
+        and DTR.DTR_FILORI = DTQ.DTQ_FILORI
+        and DTR.DTR_VIAGEM = DTQ.DTQ_VIAGEM
+
+        inner join DUP010 DUP (nolock)
+            on DUP.D_E_L_E_T_ = ''
+            and DUP.DUP_FILORI = DTR.DTR_FILORI
+            and DUP.DUP_VIAGEM = DTR.DTR_VIAGEM
+            and DUP.DUP_ITEDTR = DTR.DTR_ITEM
+            and DUP.DUP_CODVEI = DTR.DTR_CODVEI
+
+            inner join DA4010 DA4 (nolock)
+                on DA4.DA4_COD = DUP.DUP_CODMOT
+
+    left join DUD010 DUD (nolock)
+        on DUD.D_E_L_E_T_ = ''
+        and DUD.DUD_VIAGEM = DTQ.DTQ_VIAGEM
+
+        left join DT5010 DT5 (nolock)
+            on DT5.D_E_L_E_T_ = ''
+            and DT5.DT5_FILDOC = DUD.DUD_FILDOC
+            and DT5.DT5_NUMSOL = DUD.DUD_DOC
+            and DUD.DUD_SERIE = 'COL'
+
+		left join DT6010 DT6 (nolock)
+			on DT6.D_E_L_E_T_ = ''
+			and DT6.DT6_FILDOC = DUD.DUD_FILDOC
+			and DT6.DT6_DOC = DUD.DUD_DOC
+			and DT6.DT6_SERIE = DUD.DUD_SERIE
+            and DT6.DT6_DATEMI > '20211231'
+
+            left join SD2010 COMP (nolock)
+                on COMP.D_E_L_E_T_ = ''
+                and COMP.D2_NFORI = DT6.DT6_DOC
+                and COMP.D2_SERIORI = DT6.DT6_SERIE
+                and COMP.D2_CLIENTE = DT6.DT6_CLIDEV
+                and COMP.D2_LOJA = DT6.DT6_LOJDEV
+
+            INNER JOIN SA1010 DEV
+                ON DEV.A1_FILIAL = '      '
+                AND DEV.A1_COD = DT6.DT6_CLIDEV
+                AND DEV.A1_LOJA = DT6.DT6_LOJDEV
+                AND DEV.D_E_L_E_T_ = ' '
+
+        LEFT JOIN DUY010 DUYORI
+            ON DUYORI.DUY_FILIAL = DT6_FILIAL
+            AND DUYORI.DUY_GRPVEN = DT6.DT6_CDRORI
+            AND DUYORI.D_E_L_E_T_ = ' '
+        LEFT JOIN DUY010 DUYDES
+            ON DUYDES.DUY_FILIAL = DT6_FILIAL
+            AND DUYDES.DUY_GRPVEN = DT6.DT6_CDRDES
+            AND DUYDES.D_E_L_E_T_ = ' '
+        LEFT JOIN DUY010 DUYDEV
+            ON DUYDEV.DUY_FILIAL = DT6_FILIAL
+            AND DUYDEV.DUY_GRPVEN = DT6.DT6_CDRCAL
+            AND DUYDEV.D_E_L_E_T_ = ' '
+
+            left join DTC010 DTC (nolock)
+                on DTC.D_E_L_E_T_ = ''
+                and DTC.DTC_FILORI = DT6.DT6_FILDOC
+                and DTC.DTC_DOC = DT6.DT6_DOC
+                and DTC.DTC_SERIE = DT6.DT6_SERIE
+
+                left join DF1010 DF1 (nolock)
+                    on DF1.D_E_L_E_T_ = ''
+                    and DF1.DF1_FILDOC = DTC.DTC_FILORI
+                    and DF1.DF1_DOC = DTC.DTC_NUMSOL
+
+where DTQ.D_E_L_E_T_ = '' and DTQ_VIAGEM in ('008816', '008818', '008819', '008820', '008822', '008824', '008829', '008831')
+
