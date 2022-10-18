67f109ad84675e5ff89c7546ffd4a23ee1985595
Manoel <manoel.jose@unilinktransportes.com.br>
2022-10-18 11:46:57 -0300

    verificar visão de produtos excluídos

--- estoque/lista de produtos.sql
+++ estoque/lista de produtos.sql
@@ -1,8 +1,12 @@
 select
+    SB1.B1_GRUPO as GRUPO,
     SB1.B1_COD as PRODUTO,
-    trim(SB1.B1_DESC) as NOMEPRODUTO,
-    case SB1.B1_MSBLQL when 1 then 'SIM' else 'NAO' end as BLOQUEADO
+    SB1.B1_COD as contador,
+    trim(SB1.B1_DESC) as DESCRICAO,
+    case SB1.B1_MSBLQL when 1 then 'SIM' else 'NAO' end as BLOQUEADO,
+    case SB1.D_E_L_E_T_ when '*' then 'SIM' else 'NAO' end as EXCLUIDO,
+    convert(date, SB1.B1_DATREF, 103) as DATA_CRIADO,
+    trim(SB1.B1_YPARTNU) as PARTNUMBER
 from SB1010 SB1 (nolock)
 where
-        SB1.D_E_L_E_T_ = ''
-    and SB1.B1_COD like '1%'
+        SB1.B1_COD like '1%'
