d31d44106f8f9eb15494ece665da213d2b8ddb93
Manoel <manoel.jose@unilinktransportes.com.br>
2022-10-31 15:46:29 -0300

    validar vínculo func, dep e planos

--- RH/plano de saúde.sql
+++ RH/plano de saúde.sql
@@ -19,36 +19,50 @@ select
 
     trim(SRB.RB_NOME) DEPENDENTE,
 	convert(date, SRB.RB_DTNASC, 103) as NASC_DEP,
-	trim(SRB.RB_SEXO) as SEXO_DEP,
+	trim(SRB.RB_SEXO) as SEXO_DEP
 
-
-from SRA010 SRA (nolock)
+from RHR010 RHR (nolock)
     left join RHK010 RHK (nolock)
         on RHK.D_E_L_E_T_ = ''
-        and RHK.RHK_FILIAL = SRA.RA_FILIAL
-        and RHK.RHK_MAT = SRA.RA_MAT
+        and RHK.RHK_FILIAL = RHR.RHR_FILIAL
+        and RHK.RHK_MAT = RHR.RHR_MAT
+        and RHK.RHK_TPPLAN = RHR.RHR_TPPLAN
+        and RHK.RHK_PLANO = RHR.RHR_PLANO
+        and RHK.RHK_TPFORN = RHR.RHR_TPFORN
+        and RHK.RHK_CODFOR = RHR.RHR_CODFOR
+
+        left join SRA010 SRA (nolock)
+            on SRA.D_E_L_E_T_ = ''
+            and SRA.RA_FILIAL = RHR.RHR_FILIAL
+            and SRA.RA_MAT = RHR.RHR_MAT
+
+            inner join SQB010 SQB (nolock)
+                on SQB.D_E_L_E_T_ = ''
+                and SQB.QB_FILIAL = substring(SRA.RA_FILIAL, 1, 4)
+            inner join SRJ010 SRJ (nolock)
+                on SRJ.D_E_L_E_T_ = ''
+                and SRJ.RJ_FILIAL = substring(SRA.RA_FILIAL, 1, 4)
+                and SRJ.RJ_FUNCAO = SRA.RA_CODFUNC
+            inner join CTT010 CTT (nolock)
+                on CTT.D_E_L_E_T_ = ''
+                and CTT.CTT_CUSTO = SRA.RA_CC
+            inner join CTD010 CTD (nolock)
+                on CTD.D_E_L_E_T_ = ''
+                and CTD.CTD_ITEM = SRA.RA_ITEM
+
+    left join RHL010 RHL (nolock)
+        on RHL.D_E_L_E_T_ = ''
+        and RHL.RHL_FILIAL = RHR.RHR_FILIAL
+        and RHL.RHL_MAT = RHR.RHR_MAT
+        and RHL.RHL_CODIGO = RHR.RHR_CODIGO
+        and RHL.RHL_TPPLAN = RHR.RHR_TPPLAN
+        and RHL.RHL_PLANO = RHR.RHR_PLANO
+        and RHL.RHL_TPFORN = RHR.RHR_TPFORN
+        and RHL.RHL_CODFOR = RHR.RHR_CODFOR
 
         left join SRB010 SRB (nolock)
             on SRB.D_E_L_E_T_ = ''
-            and SRB.RB_FILIAL = SRA.RA_FILIAL
-            and SRB.RB_MAT = SRA.RA_MAT
-
-            left join RHL010 RHL (nolock)
-                on RHL.D_E_L_E_T_ = ''
-                and RHL.RHL_FILIAL = SRB.RB_FILIAL
-                and RHL.RHL_MAT = SRB.RB_MAT
-
-        inner join SQB010 SQB (nolock)
-            on SQB.D_E_L_E_T_ = ''
-            and SQB.QB_FILIAL = substring(SRA.RA_FILIAL, 1, 4)
-        inner join SRJ010 SRJ (nolock)
-            on SRJ.D_E_L_E_T_ = ''
-            and SRJ.RJ_FILIAL = substring(SRA.RA_FILIAL, 1, 4)
-            and SRJ.RJ_FUNCAO = SRA.RA_CODFUNC
-        inner join CTT010 CTT (nolock)
-            on CTT.D_E_L_E_T_ = ''
-            and CTT.CTT_CUSTO = SRA.RA_CC
-        inner join CTD010 CTD (nolock)
-            on CTD.D_E_L_E_T_ = ''
-    	    and CTD.CTD_ITEM = SRA.RA_ITEM
+            and SRB.RB_FILIAL = RHL.RHL_FILIAL
+            and SRB.RB_MAT = RHL.RHL_MAT
+            and SRB.RB_COD = RHL.RHL_CODIGO
 where SRA.D_E_L_E_T_ = ''
