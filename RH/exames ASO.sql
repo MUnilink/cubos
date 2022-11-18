48fe28a6deb08b4438734aae54b4aced4895677a
Manoel <manoel.jose@unilinktransportes.com.br>
2022-11-18 13:13:49 -0300

    fator RH (14) não sai no relatório

--- RH/exames ASO.sql
+++ RH/exames ASO.sql
@@ -1,23 +1,38 @@
 select
-    TM0.TM0_FILIAL as FILIAL,
     TM0.TM0_NUMFIC as FICHA_MEDICA,
     TM0.TM0_NOMFIC as NOME_FICHA,
     TM0.TM0_DTIMPL as DATA_INI,
-    TM5.TM5_FILIAL as FILIAL,
+
+    trim(SRA.RA_FILIAL) as FILIAL,
+    trim(SRA.RA_MAT) as MATRICULA,
+    trim(SRA.RA_NOME) as NOME,
+    trim(SRJ.RJ_DESC) as FUNCAO,
+    convert(date, SRA.RA_ADMISSA, 103) as ADMISSAO,
+    case SRA.RA_SITFOLH when '' then 'OK' else SRA.RA_SITFOLH end as SITUACAO,
+    case when trim(SRA.RA_SITFOLH) != 'D' then 'S' else 'N' end as ATIVO,
+
+    trim(CTT.CTT_CUSTO) as CC,
+    trim(CTT.CTT_DESC01) as CCUSTO,
+    trim(CTD.CTD_ITEM) as ITCT,
+    trim(CTD.CTD_DESC01) as ATIVIDADE,
+    trim(SQB.QB_DEPTO) as DEPTO,
+    trim(SQB.QB_DESCRIC) as DEPARTAMENTO,
+
+    trim(SRJ.RJ_CODCBO) as CBO,
+    trim(SRA.RA_SEXO) as SEXO,
+    trim(SRA.RA_CIC) as CPF,
+    substring(TM5.TM5_DTPROG, 1, 6) as PERIODO,
+
     TM5.TM5_EXAME as EXAME,
     TM5.TM5_EXAME as contador,
     convert(date, TM5.TM5_DTPROG, 103) as DATA,
-    TM5.TM5_ORIGEX as ORIGEM_EXAME,
+    case TM5.TM5_ORIGEX when 1 then 'ASSISTENCIAL' when 2 then 'OCUPACIONAL' else 'OUTROS' end as ORIGEM_EXAME,
     TM5.TM5_NATEXA as NATUREZA_EXAME,
     trim(TM4.TM4_NOMEXA) as NOME_EXAME,
     trim(TM4.TM4_DESEXA) as DESC_EXAME,
     TMD.TMD_VALEXA as VALOR_EXAME,
     TM5.TM5_FORNEC as FORNECEDOR,
     TM5.TM5_LOJA as LOJA,
-    TM5.TM5_MAT as MATRICULA,
-    TM5.TM5_CC as CC,
-    SRJ.RJ_DESC as FUNCAO,
-    TM5.TM5_CBO as CBO,
 
     year(TM5.TM5_DTPROG) as ano_prog,
     month(TM5.TM5_DTPROG) as mes_prog
@@ -33,16 +48,31 @@ from TM0010 TM0 (nolock)
             and TM5.TM5_NUMASO = TMY.TMY_NUMASO
             and TM5.TM5_NUMFIC = TMY.TMY_NUMFIC
 
-            inner join SRJ010 SRJ (nolock)
-                on SRJ.D_E_L_E_T_ = ''
-                and SRJ.RJ_FILIAL = substring(TM5.TM5_FILFUN, 1, 4)
-                and SRJ.RJ_FUNCAO = TM5.TM5_CODFUN
+            inner join SRA010 SRA (nolock)
+                on SRA.D_E_L_E_T_ = ''
+                and SRA.RA_FILIAL = TM5.TM5_FILFUN
+                and SRA.RA_MAT = TM5.TM5_MAT
 
-            inner join TM4010 TM4 (nolock)
+                inner join SQB010 SQB (nolock)
+                    on SQB.D_E_L_E_T_ = ''
+                    and SQB.QB_FILIAL = substring(SRA.RA_FILIAL, 1, 4)
+                    and SQB.QB_DEPTO = SRA.RA_DEPTO
+                inner join SRJ010 SRJ (nolock)
+                    on SRJ.D_E_L_E_T_ = ''
+                    and SRJ.RJ_FILIAL = substring(SRA.RA_FILIAL, 1, 4)
+                    and SRJ.RJ_FUNCAO = SRA.RA_CODFUNC
+                inner join CTT010 CTT (nolock)
+                    on CTT.D_E_L_E_T_ = ''
+                    and CTT.CTT_CUSTO = SRA.RA_CC
+                inner join CTD010 CTD (nolock)
+                    on CTD.D_E_L_E_T_ = ''
+                    and CTD.CTD_ITEM = SRA.RA_ITEM
+
+            left join TM4010 TM4 (nolock)
                 on TM4.D_E_L_E_T_ = ''
                 and TM4.TM4_EXAME = TM5.TM5_EXAME
 
-                inner join TMD010 TMD (nolock)
+                left join TMD010 TMD (nolock)
                     on TMD.D_E_L_E_T_ = ''
                     and TMD.TMD_EXAME = TM4.TM4_EXAME
 where TM0.D_E_L_E_T_ = ''
