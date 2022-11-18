52182f214f4265c0bf7d6b9044beaaa973196c98
Manoel <manoel.jose@unilinktransportes.com.br>
2022-11-18 09:47:54 -0300

    nomes amigáveis, natureza e origem dos exames

--- RH/exames ASO.sql
+++ RH/exames ASO.sql
@@ -1,22 +1,23 @@
 select
-    TM0.TM0_FILIAL,
-    TM0.TM0_NUMFIC,
-    TM0.TM0_NOMFIC,
-    TM0.TM0_DTIMPL,
-    TM5.TM5_FILIAL,
-    TM5.TM5_EXAME,
+    TM0.TM0_FILIAL as FILIAL,
+    TM0.TM0_NUMFIC as FICHA_MEDICA,
+    TM0.TM0_NOMFIC as NOME_FICHA,
+    TM0.TM0_DTIMPL as DATA_INI,
+    TM5.TM5_FILIAL as FILIAL,
+    TM5.TM5_EXAME as EXAME,
     TM5.TM5_EXAME as contador,
-    convert(date, TM5.TM5_DTPROG, 103) as TM5_DTPROG,
-    trim(TM4.TM4_NOMEXA) as TM4_NOMEXA,
-    trim(TM4.TM4_DESEXA) as TM4_DESEXA,
-    TM5.TM5_FORNEC,
-    TM5.TM5_LOJA,
-    TM5.TM5_FILFUN,
-    TM5.TM5_MAT,
-    TM5.TM5_CC,
-    TM5.TM5_CODFUN,
-    SRJ.RJ_DESC,
-    TM5.TM5_CBO,
+    convert(date, TM5.TM5_DTPROG, 103) as DATA,
+    TM5.TM5_ORIGEX as ORIGEM_EXAME,
+    TM5.TM5_NATEXA as NATUREZA_EXAME,
+    trim(TM4.TM4_NOMEXA) as NOME_EXAME,
+    trim(TM4.TM4_DESEXA) as DESC_EXAME,
+    TMD.TMD_VALEXA as VALOR_EXAME,
+    TM5.TM5_FORNEC as FORNECEDOR,
+    TM5.TM5_LOJA as LOJA,
+    TM5.TM5_MAT as MATRICULA,
+    TM5.TM5_CC as CC,
+    SRJ.RJ_DESC as FUNCAO,
+    TM5.TM5_CBO as CBO,
 
     year(TM5.TM5_DTPROG) as ano_prog,
     month(TM5.TM5_DTPROG) as mes_prog
@@ -40,4 +41,8 @@ from TM0010 TM0 (nolock)
             inner join TM4010 TM4 (nolock)
                 on TM4.D_E_L_E_T_ = ''
                 and TM4.TM4_EXAME = TM5.TM5_EXAME
+
+                inner join TMD010 TMD (nolock)
+                    on TMD.D_E_L_E_T_ = ''
+                    and TMD.TMD_EXAME = TM4.TM4_EXAME
 where TM0.D_E_L_E_T_ = ''
