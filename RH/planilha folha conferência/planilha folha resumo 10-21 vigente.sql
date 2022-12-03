667c5fb58339db63d6c61197800a3d973f299857
Manoel <manoel.jose@unilinktransportes.com.br>
2022-12-03 09:01:47 -0300

    validar novas verbas add noturno

--- RH/planilha folha conferência/planilha folha resumo 10-21 vigente.sql
+++ RH/planilha folha conferência/planilha folha resumo 10-21 vigente.sql
@@ -193,7 +193,7 @@ from
                     and substring(SRC010.RC_FILIAL, 1, 4) = SRV010.RV_FILIAL
                     and SRC010.RC_PD = SRV010.RV_COD
             where
-                    SRC010.RC_PD in ('030', '041')
+                    SRC010.RC_PD in ('030', '041', '371', '372')
                 and SRC010.D_E_L_E_T_ = '' and SRV010.RV_TIPOCOD in ('1', '2', '3', '4')
                 and SRC010.RC_MAT = FOLHA.RC_MAT
                 and SRC010.RC_PERIODO = FOLHA.RC_PERIODO
