60e6aff8e99a95f2c053b6b48e380fa7bf82289b
Manoel <manoel.jose@unilinktransportes.com.br>
2022-12-27 15:51:31 -0300

    corrigir valores de verbas da rescisão

--- RH/relatório de rescisão.sql
+++ RH/relatório de rescisão.sql
@@ -19,6 +19,36 @@ select
 
     SRR.RR_PERIODO,
     SRR.RR_PD,
+
+    trim(isnull(SRV.RV_DESC, '-')) as DESC_VERBA1,
+    case SRV.RV_COD
+        when '183' then 'VALOR A RECEBER'
+        when '999' then 'VALOR A RECEBER'
+    else trim(SRV.RV_DESCDET) end as DESC_VERBA2,
+
+    case trim(SRV.RV_TIPOCOD)
+        when '1' then 'PROVENTO'
+        when '2' then 'DESCONTO'
+        when '3' then 'BASE PROVENTO'
+        when '4' then 'BASE DESCONTO'
+        else 'OUTROS'
+    end as RV_TIPOCOD,
+
+    convert(date, SRG.RG_DTAVISO, 103) as DT_AVISOPRE,
+    convert(date, SRG.RG_DATAHOM, 103) as DT_HOMOLOGA,
+    convert(date, SRG.RG_DATADEM, 103) as DT_DEMISSAO,
+    convert(date, SRG.RG_DTGERAR, 103) as DT_GERACAOF,
+    convert(date, SRG.RG_DTPROAV, 103) as DT_PROJAVIS,
+    
+    SRG.RG_DAVCUM as DIAS_AVISO_CUMPRIDO,
+    SRG.RG_DAVIND as DIAS_AVISO_INDENIZADO,
+    SRG.RG_DAVISO as DIAS_AVISO,
+    SRG.RG_DFERPRO as DIAS_FER_PROPOR,
+    SRG.RG_DFERVEN as DIAS_FER_VENCID,
+    SRG.RG_DFERAVI as DIAS_FER_AVISO,
+    
+    SRG.RG_TIPORES,
+    SRG.RG_OBS,
     SRR.RR_VALOR
 
 from SRG010 SRG (nolock)
@@ -34,15 +64,21 @@ from SRG010 SRG (nolock)
             on SRJ.D_E_L_E_T_ = ''
             and SRJ.RJ_FILIAL = substring(SRA.RA_FILIAL, 1, 4)
             and SRJ.RJ_FUNCAO = SRA.RA_CODFUNC
-        inner join CTT010 CTT (nolock)
-            on CTT.D_E_L_E_T_ = ''
-            and CTT.CTT_CUSTO = SRA.RA_CC
-        inner join CTD010 CTD (nolock)
-            on CTD.D_E_L_E_T_ = ''
-    	    and CTD.CTD_ITEM = SRA.RA_ITEM
     
     inner join SRR010 SRR (nolock)
         on SRR.D_E_L_E_T_ = ''
         and SRR.RR_FILIAL = SRG.RG_FILIAL
         and SRR.RR_MAT = SRG.RG_MAT
-where SRH.D_E_L_E_T_ = ''
+        and SRR.RR_ROTEIR = 'RES'
+
+        inner join CTT010 CTT (nolock)
+            on CTT.D_E_L_E_T_ = ''
+            and CTT.CTT_CUSTO = SRR.RR_CC
+        inner join CTD010 CTD (nolock)
+            on CTD.D_E_L_E_T_ = ''
+    	    and CTD.CTD_ITEM = SRR.RR_ITEM
+        inner join SRV010 SRV (nolock)
+			on SRV.D_E_L_E_T_ = ''
+			and SRV.RV_FILIAL = substring(SRR.RR_FILIAL, 1, 4)
+			and SRV.RV_COD = SRR.RR_PD
+where SRG.D_E_L_E_T_ = ''
