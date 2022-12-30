c1ad914040da7db3be3d45b9410def592c2b3a9f
Manoel <manoel.jose@unilinktransportes.com.br>
2022-12-30 14:07:33 -0300

    relatório RM ok; pendente ETL

--- FAST ANALYTICS/gooddata/query/dim/planilhaPCO.sql
+++ FAST ANALYTICS/gooddata/query/dim/planilhaPCO.sql
@@ -1,10 +1,8 @@
 select
     concat(trim(AK1.AK1_CODIGO), trim(AK1.AK1_VERSAO)) as ID_PLANILHAPCO,
-    AK1.AK1_CODIGO,
-    AK1.AK1_VERSAO,
-    
-    AK1.AK1_DESCRI,
-    AK1.AK1_INIPER,
-    AK1.AK1_FIMPER
+    trim(AK1.AK1_CODIGO) as AK1_CODIGO,
+    trim(AK1.AK1_VERSAO) as AK1_VERSAO,
+    trim(AK1.AK1_VERREV) as AK1_VERREV,
+    trim(AK1.AK1_DESCRI) as AK1_DESCRI
 from AK1010 AK1 (nolock)
 where AK1.D_E_L_E_T_ = ''
