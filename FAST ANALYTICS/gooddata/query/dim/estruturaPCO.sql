6cd7f0467e66fd634c2a9498759ad342bfbd6f30
Manoel <manoel.jose@unilinktransportes.com.br>
2023-01-02 14:31:42 -0300

    ajuste campos itens e estrutura PCO

--- FAST ANALYTICS/gooddata/query/dim/estruturaPCO.sql
+++ FAST ANALYTICS/gooddata/query/dim/estruturaPCO.sql
@@ -2,10 +2,14 @@ select
     concat(trim(AK3.AK3_ORCAME), trim(AK3.AK3_VERSAO), trim(AK3.AK3_CO)) as ID_ESTRUTURAPCO,
     AK3.AK3_ORCAME,
     AK3.AK3_VERSAO,
-    AK3.AK3_CO,
-    AK3.AK3_PAI,
+    AK3.AK3_CO as N2,
+    case when AK3.AK3_CO is null then AK3.AK3_CO else AK5.AK5_CODIGO end as N1,
     AK3.AK3_TIPO,
     AK3.AK3_NIVEL,
     AK3.AK3_DESCRI
 from AK3010 AK3 (nolock)
+    left join AK5010 AK5 (nolock)
+        on AK5.D_E_L_E_T_ = ''
+        and AK5.AK5_CODIGO = AK3.AK3_PAI
+        and AK5.AK5_COSUP != AK3.AK3_CO
 where AK3.D_E_L_E_T_ = ''
--- FAST ANALYTICS/gooddata/query/dim/itensPCO.sql
+++ FAST ANALYTICS/gooddata/query/dim/itensPCO.sql
@@ -1,15 +1,10 @@
 select
-    concat(trim(AK2.AK2_ORCAME), trim(AK2.AK2_VERSAO), substring(AK2.AK2_PERIOD, 1, 6), trim(AK2.AK2_CO), AK2.AK2_CLASSE, AK2.AK2_OPER) as ID_ITEMPCO,
+    concat(trim(AK2.AK2_ORCAME), trim(AK2.AK2_VERSAO), substring(AK2.AK2_PERIOD, 1, 6), trim(AK2.AK2_CO), trim(AK2.AK2_CLASSE), trim(AK2.AK2_OPER)) as ID_ITEMPCO,
     AK2.AK2_ORCAME,
     AK2.AK2_VERSAO,
-
     AK2.AK2_PERIOD,
-    
     AK2.AK2_CO,
     AK2.AK2_CLASSE,
-    AK2.AK2_OPER,
-
-    AK2.AK2_DATAI,
-    AK2.AK2_DATAF
+    AK2.AK2_OPER
 from AK2010 AK2 (nolock)
 where AK2.D_E_L_E_T_ = ''
