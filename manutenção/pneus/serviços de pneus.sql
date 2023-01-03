859566e5d8e82694f507f953b070fc8f53553381
Manoel <manoel.jose@unilinktransportes.com.br>
2023-01-03 11:57:58 -0300

    dados de lotes de OS; vincular a compras

--- manutenção/pneus/serviços de pneus.sql
+++ manutenção/pneus/serviços de pneus.sql
@@ -7,19 +7,13 @@ select
     
     ST9.T9_STATUS,
     trim(TQY.TQY_DESTAT) as STATUS_PNEU,
-    
-    STZ.TZ_ORDEM,
-    convert(datetime, concat(STZ.TZ_DATAMOV, ' ', STZ.TZ_HORAENT), 103) as DATA_ENT,
-    STZ.TZ_POSCONT,
-	convert(datetime, concat(STZ.TZ_DATASAI, ' ', STZ.TZ_HORASAI), 103) as DATA_SAI,
-    STZ.TZ_CONTSAI,
 
-    abs(STZ.TZ_CONTSAI - STZ.TZ_POSCONT) as km,
-
-	STZ.TZ_BEMPAI as ESTRUTURA,
-	STZ.TZ_TIPOMOV,
-	STZ.TZ_CAUSA,
-	trim(concat(cast(STZ.TZ_CAUSA as int), ' - ' , ST8.T8_NOME)) as DESTINO_PNEU /* quando com análise sem movimento, retorna o destino do movimento */
+    TR7.TR7_LOTE,
+    TR7.TR7_SERVIC,
+    convert(datetime, concat(TR7.TR7_DTLOTE, ' ', TR7.TR7_HRLOTE), 103) as DATA_LOTE,
+    convert(datetime, concat(TR7.TR7_DTRECI, ' ', TR7.TR7_HRRECI), 103) as DATA_RECEBIMENTO,
+    TR7.TR7_NFE,
+    TR7.TR7_SERIE,
 
 from TQS010 TQS (nolock)
     inner join ST9010 ST9 (nolock)
@@ -27,7 +21,17 @@ from TQS010 TQS (nolock)
 		and ST9.T9_CODBEM = TQS.TQS_CODBEM
     inner join TR8010 TR8 (nolock)
         on TR8.D_E_L_E_T_ = ''
-        and TR8.TR8_CODBEM = TQS.TQS_CODBEM
+        and TR8.TR8_CODBEM = TQS.TQS_CODBEM        
+
+        inner join TR7010 TR7 (nolock)
+            on TR7.D_E_L_E_T_ = ''
+            and TR7.TR7_FILIAL = TR8.TR8_FILIAL
+            and TR7.TR7_LOTE = TR8.TR8_LOTE
+
+        inner join SC7010 SC7 (nolock)
+            on SC7.D_E_L_E_T_ = ''
+            and SC7.C7_FILIAL = TR8.TR8_FILIAL
+            and SC7.C7_NUM = (select top 1 SC7.C7 = TR8.TR8)
     
     inner join TQY010 TQY (nolock)
         on TQY.D_E_L_E_T_ = ''
