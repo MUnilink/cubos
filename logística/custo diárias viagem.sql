22e31bb1adddac1ec65d55d491ca5e671ec77875
Manoel <manoel.jose@unilinktransportes.com.br>
2023-01-13 10:14:23 -0300

    aprovação das diárias e status sem descrição

--- logística/custo diárias viagem.sql
+++ logística/custo diárias viagem.sql
@@ -9,6 +9,9 @@ select
     convert(date, DYX.DYX_DATDIA, 103) as DYX_DATDIA,
     DYX.DYX_QTDE,
     DYX.DYX_VLRUNI,
+    DYX.DYX_STATUS,
+    DYX.DYX_USRAPR,
+    convert(date, DYX.DYX_DATAPR, 103) as DYX_DATAPR,
     SE2.E2_NUM,
     SE2.E2_VALOR as VALOR_DIARIA,
     
@@ -19,6 +22,11 @@ from DYV010 DYV (nolock)
         on DYX.D_E_L_E_T_ = ''
         and DYX.DYX_IDCDIA = DYV.DYV_IDCDIA
         and year(DYX.DYX_DATDIA) = 2022
+
+            inner join SX5010 SX5 (nolock)
+                on SX5.D_E_L_E_T_ = ''
+                and SX5.X5_TABELA = 'MS'
+                and SX5.X5_CHAVE = DYX.DYX_TIPVAL
         
         left join SE2010 SE2
             on SE2.D_E_L_E_T_ = ''
