5dbcde8bdc1649d9ac608cdc4457f0c584c883eb
Manoel <manoel.jose@unilinktransportes.com.br>
2023-01-14 11:25:10 -0300

    custo médio de pneus

--- manutenção/pneus/custo médio de pneus.sql
+++ manutenção/pneus/custo médio de pneus.sql
@@ -1,11 +1,13 @@
 select
-    SB9010.B9_FILIAL,
-    SB9010.B9_LOCAL,
-    SB9010.B9_DATA,
-    SB9010.B9_COD,
-    SB9010.B9_VINI1/SB9010.B9_QINI as B9_CM
-from SB9010
+    SB9.B9_FILIAL,
+    SB9.B9_LOCAL,
+    substring(SB9.B9_DATA, 1, 6),
+    SB9.B9_COD,
+    SB9.B9_VINI1,
+    SB9.B9_QINI,
+    SB9.B9_CM1
+from SB9010 SB9 (nolock)
 where
-        SB9010.D_E_L_E_T_ = ''
-    and SB9010.B9_COD like '1130%'
-    and SB9010.B9_QINI != 0
+        SB9.D_E_L_E_T_ = ''
+    and SB9.B9_COD like '1130%'
+    and SB9.B9_QINI != 0
