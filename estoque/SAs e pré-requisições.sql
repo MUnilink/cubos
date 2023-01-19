1a2c3b5e40a2669655c3fad0b95bf909966225f7
Manoel <manoel.jose@unilinktransportes.com.br>
2023-01-19 11:05:20 -0300

    verificar geração e baixa de pré-requisição

--- estoque/SAs e pré-requisições.sql
+++ estoque/SAs e pré-requisições.sql
@@ -1,12 +1,26 @@
 select
     SCP.CP_FILIAL as FILIAL,
+    SCP.CP_LOCAL as ARMAZEM,
     SCP.CP_NUM as NUM_SA,
     SCP.CP_ITEM as ITEM_SA,
     SCP.CP_UM as UN,
     SCP.CP_QUANT as QTD_SOLICTADA,
     SCP.CP_QUJE as QTD_ATENDIDA,
+    trim(isnull(SB1.B1_GRUPO, '-')) as B1_GRUPO,
+    
+    convert(date, SCP.CP_EMISSAO, 103) as DATA_SA,
+    substring(SCP.CP_EMISSAO, 1, 6) as PERIODO,
+    SCP.CP_USER,
+    SCP.CP_CODSOLI,
+    SCP.CP_PREREQU,
+    SCP.CP_STATUS,
+    SCP.CP_STATSA,
+    SCP.CP_SALBLQ,
 
-    trim(isnull(SB1.B1_GRUPO, '-')) as B1_GRUPO
+    SCQ.CQ_QUANT,
+    SCQ.CQ_QTDISP,
+    SCP.CP_NUMSC,
+    SCP.CP_ITSC
 
 from SCP010 SCP (nolock)
     left join SCQ010 SCQ (nolock)
@@ -14,8 +28,7 @@ from SCP010 SCP (nolock)
         and SCQ.CQ_FILIAL = SCP.CP_FILIAL
         and SCQ.CQ_NUM = SCP.CP_NUM
         and SCQ.CQ_ITEM = SCP.CP_ITEM
-        and SCQ.CQ_NUMSQ = SCP.CP_NUM
     left join SB1010 SB1 (nolock)
         on SB1.D_E_L_E_T_ = ''
-        and SB1.B1_COD = SCP.CP_COD
+        and SB1.B1_COD = SCP.CP_PRODUTO
 where SCP.D_E_L_E_T_ = ''
