b7af217d8ced63f00988c65e239e1060117dcd9e
Manoel <manoel.jose@unilinktransportes.com.br>
2023-01-20 08:29:19 -0300

    importação portal de compras produção

--- compras/campos importação PRODUTO.sql
+++ compras/campos importação PRODUTO.sql
@@ -1,16 +1,17 @@
 select
-    trim(SB1.B1_COD) as CODIGO,
-    trim(SB1.B1_DESC) as DESCRICAO,
-    trim(SAH.AH_DESCPO) as UNIDADE,
-    trim(SBM.BM_GRUPO) AS COD_GRUPO_ESTOQUE,
-    trim(SBM.BM_DESC) AS DESC_GRUPO_ESTOQUE
-from SB1010 SB1 (nolock)
-    inner join SBM010 SBM (nolock)
+    '01' as EMPRESA,
+    SB1.B1_COD as CODIGO,
+    SB1.B1_DESC as DESCRICAO,
+    SB1.B1_YDESCRI as COMPLEMENTO,
+    SB1.B1_YPARTNU as PART_NUMBER,
+    SB1.B1_POSIPI as NCM,
+    SB1.B1_UM as UNIDADE_MEDIDA,
+    SBM.BM_GRUPO as GRUPO,
+    SBM.BM_DESC as DESCRICAO_GRUPO
+from SB1010 SB1
+    inner join SBM010 SBM
         on SBM.D_E_L_E_T_ = ''
         and SBM.BM_GRUPO = SB1.B1_GRUPO
         and SBM.BM_GRUPO not like '3%'
-    inner join SAH010 SAH (nolock)
-        on SAH.D_E_L_E_T_ = ''
-        and SAH.AH_UNIMED = SB1.B1_UM
 where
         SB1.D_E_L_E_T_ = ''
