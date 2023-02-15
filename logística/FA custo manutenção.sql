428ba73f4ec86f8aa8f620034f67a8a6e4b9e569
Manoel <manoel.jose@unilinktransportes.com.br>
2023-02-15 11:38:37 -0300

    custo por OS por mês não trivial

--- logística/FA custo manutenção.sql
+++ logística/FA custo manutenção.sql
@@ -2,9 +2,9 @@ select
     STJ.TJ_CODBEM,
     STJ.TJ_ORDEM,
     STL.TL_DTINICI,
+    STL.TL_UNIDADE,
+    STL.TL_LOCAL,
     sum(STL.TL_QUANTID) as TL_QUANTID,
-
-    trim(isnull(STL.TL_CODIGO, '-')) as INSUMO,
 
     case STL.TL_TIPOREG
         when 'M' then 'MÃO-DE-OBRA'
@@ -14,25 +14,10 @@ select
         else 'OUTROS'
     end as TL_TIPOREG,
 
-    case STL.TL_TIPOREG
-        when 'M' then trim(ST1.T1_NOME)
-        when 'E' then trim(ST0.T0_NOME)
-        when 'P' then trim(SB1.B1_DESC)
-        when 'T' then trim(SA2.A2_NOME)
-        else 'OUTROS'
-    end as DESC_INSUMO,
-
     substring(STL.TL_DTINICI, 1, 6) as PERIODO_MNT,
 
-    case when STJ.TJ_SERVICO = 'PNEMOV' then 'PNEU' else 'MANUTENÇÃO' end as TIPO_CUSTO,
-
-    case when trim(STL.TL_CODIGO) in ('11380003', '11380004', '11380005') and STL.TL_LOCAL = '80' then sum(ADESIVO_CUSTO.B9_CM * STL.TL_QUANTID)
-    else
-        case when trim(STL.TL_CODIGO) like '1130%' then sum(PNEU_CUSTO.B9_CM * STL.TL_QUANTID)
-        else
-            sum(STL.TL_CUSTO)
-        end
-    end as TL_CUSTO
+    case when STL.TL_LOCAL in (20, 21, 22, 23, 24, 26) then 'PNEU' else 'MANUTENÇÃO' end as TIPO_CUSTO,
+    case when STL.TL_LOCAL in (20, 21, 22, 23, 24, 26) then sum(PNEU_CUSTO.B9_CM * STL.TL_QUANTID) else sum(STL.TL_CUSTO) end as TL_CUSTO
 
 from STJ010 STJ (nolock)
     inner join ST9010 ST9 (nolock)
@@ -45,29 +30,6 @@ from STJ010 STJ (nolock)
         and STL.TL_PLANO = STJ.TJ_PLANO
         and STL.TL_FILIAL = STJ.TJ_FILIAL
 
-        left join SA2010 SA2 (nolock)
-            on SA2.D_E_L_E_T_ = ''
-            and SA2.A2_COD = STL.TL_FORNEC
-            and SA2.A2_LOJA = STL.TL_LOJA
-        left join SB1010 SB1 (nolock)
-            on SB1.D_E_L_E_T_ = ''
-            and SB1.B1_COD = STL.TL_CODIGO
-        left join
-        (
-            select
-                SB9010.B9_COD,
-                min(SB9010.B9_VINI1/SB9010.B9_QINI) as B9_CM,
-                min(SB9010.B9_DATA) as B9_DATA
-            from SB9010 (nolock)
-            where
-                    SB9010.D_E_L_E_T_ = ''
-                and SB9010.B9_LOCAL = '01'
-                and SB9010.B9_COD in ('11380003', '11380004', '11380005')
-                and SB9010.B9_QINI != 0
-            group by
-                SB9010.B9_COD
-        ) ADESIVO_CUSTO
-            on ADESIVO_CUSTO.B9_COD = STL.TL_CODIGO
         left join
         (
             select
@@ -82,18 +44,8 @@ from STJ010 STJ (nolock)
                 and SB9010.B9_QINI != 0
             group by
                 SB9010.B9_COD
-        ) PNEU_CUSTO
+        ) PNEU_CUSTO /* custo médio de pneu novo do período */
             on PNEU_CUSTO.B9_COD = STL.TL_CODIGO
-        left join SH4010 SH4 (nolock)
-            on SH4.D_E_L_E_T_ = ''
-            and SH4.H4_CODIGO = STL.TL_CODIGO
-        left join ST0010 ST0 (nolock)
-            on ST0.D_E_L_E_T_ = ''
-            and ST0.T0_ESPECIA = STL.TL_CODIGO
-        left join ST1010 ST1 (nolock)
-            on ST1.D_E_L_E_T_ = ''
-            and ST1.T1_FILIAL = STL.TL_FILIAL
-            and ST1.T1_CODFUNC = STL.TL_CODIGO
 where
         STJ.D_E_L_E_T_ = ''
     and STL.TL_TIPOREG in ('P', 'T')
@@ -106,11 +58,6 @@ group by
     STJ.TJ_ORDEM,
     STL.TL_DTINICI,
     STL.TL_UNIDADE,
-    STL.TL_CODIGO,
     STL.TL_TIPOREG,
-    STJ.TJ_SERVICO,
-    ST1.T1_NOME,
-    ST0.T0_NOME,
-    SB1.B1_DESC,
-    SA2.A2_NOME,
-    STL.TL_LOCAL
+    STL.TL_LOCAL,
+    STL.TL_CODIGO
