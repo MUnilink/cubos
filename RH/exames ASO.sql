827ccb9ce0a7a5b40e5d66c815ab51f3954bbe18
Manoel <manoel.jose@unilinktransportes.com.br>
2023-02-16 14:19:18 -0300

    realização de exames

--- RH/exames ASO.sql
+++ RH/exames ASO.sql
@@ -28,6 +28,8 @@ select
     convert(date, TM5.TM5_DTPROG, 103) as DATA,
     case TM5.TM5_ORIGEX when 1 then 'ASSISTENCIAL' when 2 then 'OCUPACIONAL' else 'OUTROS' end as ORIGEM_EXAME,
     TM5.TM5_NATEXA as NATUREZA_EXAME,
+    convert(date, TM5.TM5_DTRESU, 103) as DT_RESULTADO,
+    case TM5.TM5_DTRESU when '' then 'NÃO REALIZADO' else 'REALIZADO' end as STATUS,
     trim(TM4.TM4_NOMEXA) as NOME_EXAME,
     trim(TM4.TM4_DESEXA) as DESC_EXAME,
     TMD.TMD_VALEXA as VALOR_EXAME,
