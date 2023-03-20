7c5cc95ecbe577ff07b4edfc16f23ef249ef0f5c
Manoel <manoel.jose@unilinktransportes.com.br>
2023-03-20 10:41:10 -0300

    analisar relatório de insumos com saldo
    produtos são por filial e armazém, independente do insumo da preventiva

--- manutenção/relação de preventivas.sql
+++ manutenção/relação de preventivas.sql
@@ -65,13 +65,10 @@ select
     case SB1.B1_MSBLQL when 1 then 'SIM' else 'NAO' end as BLOQUEADO,
     STG.TG_QUANTID as QTD,
     STG.TG_UNIDADE as UN,
-    STG.TG_LOCAL as ARMAZEM
-    /*,
-    
-    SBF.BF_FILIAL as FILIAL_PROD,
-    SBF.BF_LOCAL as ARMAZEM_PROD,
-    SBF.BF_LOCALIZ as ENDER_PROD,
-    SBF.BF_QUANT as SALDO_ENDERECO*/
+    STG.TG_LOCAL as ARMAZEM,  
+    SB2.B2_FILIAL as FILIAL_PROD,
+    SB2.B2_LOCAL as ARMAZEM_PROD,
+    SB2.B2_QATU as QTD_PROD
 
 from STJ010 STJ (nolock)
     inner join STI010 STI (nolock)
@@ -96,10 +93,10 @@ from STJ010 STJ (nolock)
             left join SB1010 SB1 (nolock)
                 on SB1.D_E_L_E_T_ = ''
                 and SB1.B1_COD = STG.TG_CODIGO
-
-                /*left join SBF010 SBF (nolock)
-                    on SBF.D_E_L_E_T_ = ''
-                    and SBF.BF_PRODUTO = SB1.B1_COD*/
+            left join SB2010 SB2 (nolock)
+                on SB2.D_E_L_E_T_ = ''
+                and SB2.B2_COD = STG.TG_CODIGO
+                and SB2.B2_QATU > 0
     
     inner join ST4010 ST4 (nolock)
 		on ST4.D_E_L_E_T_ = ''
