4a3cb0a23168bef9b499768639ca4d497a123d58
Manoel <manoel.jose@unilinktransportes.com.br>
2023-03-23 09:46:15 -0300

    remoção de CC taxas; separar custos CM e SR

--- FAST ANALYTICS/gooddata-TMS/Agent/query/fact/custo veiculos.sql
+++ FAST ANALYTICS/gooddata-TMS/Agent/query/fact/custo veiculos.sql
@@ -425,15 +425,13 @@ from
             sum(TS1.TS1_VALOR) as VALOR_TAXA,
             trim(isnull(TS0010.TS0_NOMDOC, '-')) as TS0_DOCTO,
             trim(isnull(ST9010.T9_CODBEM, '-')) as T9_CODBEM,
-            TS1.ANO_DOCTO,
-            TS1.CC
+            TS1.ANO_DOCTO
         from
             (
                 select
                     TS1010.TS1_DOCTO,
                     TS1010.TS1_CODBEM,
                     TS1010.TS1_VALOR,
-                    TS1010.TS1_YCC as CC,
                     year(TS1010.TS1_DTEMIS) as ANO_DOCTO
                 from TS1010
                 where TS1010.D_E_L_E_T_ = ''
