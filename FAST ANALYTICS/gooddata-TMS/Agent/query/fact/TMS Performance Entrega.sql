48f56be883a5dbbb3c93278ead8f76945d1eccf4
Manoel <manoel.jose@unilinktransportes.com.br>
2023-04-04 11:56:57 -0300

    correção campo chegade de cliente prevista

--- FAST ANALYTICS/gooddata-TMS/Agent/query/fact/TMS Performance Entrega.sql
+++ FAST ANALYTICS/gooddata-TMS/Agent/query/fact/TMS Performance Entrega.sql
@@ -211,12 +211,12 @@ FROM DT6010 DT6
                 DTC010.DTC_FILDOC,
                 DTC010.DTC_DOC,
                 DTC010.DTC_SERIE,
+
+                case when concat(DF1010.DF1_DATPRC, DF1010.DF1_HORPRC) = concat(DF1010.DF1_DATPRE, DF1010.DF1_HORPRE) and DF1010.DF1_DATPRC != ''
+                    then concat(DF1010.DF1_DATPRC, ' ', concat(substring(DF1010.DF1_HORPRC, 1, 2), ':', substring(DF1010.DF1_HORPRC, 3, 2), ':', substring(DF1010.DF1_HORPRC, 5, 2), '00'))
+                    else null
+                end as CHE_CLIDEV_PREV
                 
-                isnull
-                (
-                    concat(DF1010.DF1_DATPRC, ' ', concat(substring(DF1010.DF1_HORPRC, 1, 2), ':', substring(DF1010.DF1_HORPRC, 3, 2), ':', substring(DF1010.DF1_HORPRC, 5, 2), '00')),
-                    concat(DF1010.DF1_DATPRE, ' ', concat(substring(DF1010.DF1_HORPRE, 1, 2), ':', substring(DF1010.DF1_HORPRE, 3, 2), ':', substring(DF1010.DF1_HORPRE, 5, 2), '00'))
-                ) as CHE_CLIDEV_PREV
             from DF1010
                 inner join DTC010
                     on DTC010.D_E_L_E_T_ = ''
