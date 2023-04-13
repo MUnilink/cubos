24cd6f404f3d612951e9fe5b1fb1c9021a344ea1
Manoel <manoel.jose@unilinktransportes.com.br>
2023-04-13 13:19:15 -0300

    teste codificação de caractere consulta dim vendas

--- FAST ANALYTICS/gooddata/query/dim/estágio do processo de venda.sql
+++ FAST ANALYTICS/gooddata/query/dim/estágio do processo de venda.sql
@@ -1,6 +1,6 @@
 select
-    AC2.AC2_PROVEN,
-    AC2.AC2_STAGE,
-    AC2.AC2_DESCRI
+    substring(AC2.AC2_PROVEN, 1, 9) as AC2_PROVEN,
+    substring(AC2.AC2_STAGE, 1, 9) as AC2_STAGE,
+    substring(AC2.AC2_DESCRI, 1, 9) as AC2_DESCRI
 from AC2010 AC2
 where AC2.D_E_L_E_T_ = ''
