74f9fa239ffc12b75c2735bc8501d35ae55b9a7d
Manoel <manoel.jose@unilinktransportes.com.br>
2023-05-17 15:49:18 -0300

    hora da entrega, se não houver hora da coleta

--- FAST ANALYTICS/gooddata-TMS/Agent/query/fact/TMS Performance Entrega.sql
+++ FAST ANALYTICS/gooddata-TMS/Agent/query/fact/TMS Performance Entrega.sql
@@ -210,15 +210,16 @@ FROM DT6010 DT6
         (
             select
                 DF1010.DF1_NUMAGE,
+                DF1010.DF1_ITEAGE,
                 DF1010.DF1_YOSCLI,
                 DTC010.DTC_FILDOC,
                 DTC010.DTC_DOC,
                 DTC010.DTC_SERIE,
-
-                case when concat(DF1010.DF1_DATPRC, DF1010.DF1_HORPRC) = concat(DF1010.DF1_DATPRE, DF1010.DF1_HORPRE) and DF1010.DF1_DATPRC != ''
-                    then concat(DF1010.DF1_DATPRC, ' ', concat(substring(DF1010.DF1_HORPRC, 1, 2), ':', substring(DF1010.DF1_HORPRC, 3, 2), ':', substring(DF1010.DF1_HORPRC, 5, 2), '00'))
-                    else null
-                end as CHE_CLIDEV_PREV
+                isnull
+                (
+                    concat(DF1010.DF1_DATPRC, ' ', concat(substring(DF1010.DF1_HORPRC, 1, 2), ':', substring(DF1010.DF1_HORPRC, 3, 2), ':', substring(DF1010.DF1_HORPRC, 5, 2), '00')),
+                    concat(DF1010.DF1_DATPRE, ' ', concat(substring(DF1010.DF1_HORPRE, 1, 2), ':', substring(DF1010.DF1_HORPRE, 3, 2), ':', substring(DF1010.DF1_HORPRE, 5, 2), '00'))
+                ) as CHE_CLIDEV_PREV
 
             from DF1010
                 inner join DTC010
