ac93b751777eaead6af47851b7feb4ae971eb90d
Manoel <manoel.jose@unilinktransportes.com.br>
2023-05-17 15:49:18 -0300

    hora da entrega, se não houver hora da coleta

--- logística/FA - TMS Performance Entrega.sql
+++ logística/FA - TMS Performance Entrega.sql
@@ -217,7 +217,11 @@ FROM DT6010 DT6
                 DTC010.DTC_FILDOC,
                 DTC010.DTC_DOC,
                 DTC010.DTC_SERIE,
-                isnull(concat(DF1010.DF1_DATPRC, DF1010.DF1_HORPRC), concat(DF1010.DF1_DATPRE, DF1010.DF1_HORPRE)) as CHE_CLIDEV_PREV
+                isnull
+                (
+                    concat(DF1010.DF1_DATPRC, ' ', concat(substring(DF1010.DF1_HORPRC, 1, 2), ':', substring(DF1010.DF1_HORPRC, 3, 2), ':', substring(DF1010.DF1_HORPRC, 5, 2), '00')),
+                    concat(DF1010.DF1_DATPRE, ' ', concat(substring(DF1010.DF1_HORPRE, 1, 2), ':', substring(DF1010.DF1_HORPRE, 3, 2), ':', substring(DF1010.DF1_HORPRE, 5, 2), '00'))
+                ) as CHE_CLIDEV_PREV
 
             from DF1010
                 inner join DTC010
