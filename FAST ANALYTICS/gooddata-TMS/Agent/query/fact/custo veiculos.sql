ecbbdbf9f246ad63651b58a8fb4813ab577dbbaa
Manoel <manoel.jose@unilinktransportes.com.br>
2023-06-01 13:48:29 -0300

    inclusão da macro no filtro do vínculo de km ini e fim

--- FAST ANALYTICS/gooddata-TMS/Agent/query/fact/custo veiculos.sql
+++ FAST ANALYTICS/gooddata-TMS/Agent/query/fact/custo veiculos.sql
@@ -80,6 +80,7 @@
                         and DTW010.DTW_FILORI = DTQ.DTQ_FILORI
                         and DTW010.DTW_VIAGEM = DTQ.DTQ_VIAGEM
                         and ZB1010.ZB1_CODDA3 = DTR.DTR_CODVEI
+                        and ZB1010.ZB1_MACRON = 7
                         and DTW010.DTW_ATIVID = 50
                 ) as km_fim,
                 (
@@ -97,6 +98,7 @@
                         and DTW010.DTW_FILORI = DTQ.DTQ_FILORI
                         and DTW010.DTW_VIAGEM = DTQ.DTQ_VIAGEM
                         and ZB1010.ZB1_CODDA3 = DTR.DTR_CODVEI
+                        and ZB1010.ZB1_MACRON = 1
                         and DTW010.DTW_ATIVID = 49
                 ) as km_ini,
                 
@@ -566,6 +568,7 @@ union
                         and DTW010.DTW_FILORI = DTQ.DTQ_FILORI
                         and DTW010.DTW_VIAGEM = DTQ.DTQ_VIAGEM
                         and ZB1010.ZB1_CODDA3 = DTR.DTR_CODVEI
+                        and ZB1010.ZB1_MACRON = 7
                         and DTW010.DTW_ATIVID = 50
                 ) as km_fim,
                 (
@@ -583,6 +586,7 @@ union
                         and DTW010.DTW_FILORI = DTQ.DTQ_FILORI
                         and DTW010.DTW_VIAGEM = DTQ.DTQ_VIAGEM
                         and ZB1010.ZB1_CODDA3 = DTR.DTR_CODVEI
+                        and ZB1010.ZB1_MACRON = 1
                         and DTW010.DTW_ATIVID = 49
                 ) as km_ini,
                 
