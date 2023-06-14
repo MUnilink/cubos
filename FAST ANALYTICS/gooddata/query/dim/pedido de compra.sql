07afb987d66a10859aee11b6562fb3a1898d74b3
Manoel <manoel.jose@unilinktransportes.com.br>
2023-06-14 11:07:59 -0300

    verificar alterações no modelo antes de criar ETL

--- FAST ANALYTICS/gooddata/query/dim/pedido de compra.sql
+++ FAST ANALYTICS/gooddata/query/dim/pedido de compra.sql
@@ -1,15 +1,9 @@
 select
     concat(trim(SC7.C7_FILIAL), trim(SC7.C7_NUM)) as ID_PEDIDO,
-	trim(isnull(SC7.C7_ITEM, '-')) as ITEM_PC,
-	trim(isnull(SC7.C7_OBS, '-')) as OBS_PC,
-	trim(isnull(SC7.C7_OBSM, '-')) as MEMO_PC,
-
-	case SC7.C7_CONAPRO
-		when 'B' then 'PENDENTE'
-		when 'L' then 'APROVADO'
-		when 'R' then 'REJEITADO'
-		else 'OUTROS'
-	end as APROVACAO_PC,
+	trim(SC7.C7_NUM) as NUM_PC,
+	trim(SC7.C7_ITEM) as ITEM_PC,
+	trim(SC7.C7_OBS) as OBS_PC,
+	trim(SC7.C7_OBSM) as MEMO_PC
 
 from SC7010 SC7
 where SC7.D_E_L_E_T_ = ''
