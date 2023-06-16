869967c131e690af666b26bb38d443e4777f6f9f
Manoel <manoel.jose@unilinktransportes.com.br>
2023-06-16 11:31:17 -0300

    ajustes consultas custo TMS em planilha

--- logística/custo documentação.sql
+++ logística/custo documentação.sql
@@ -8,10 +8,9 @@ select
     trim(isnull(TS0.TS0_NOMDOC, '-')) as TS0_DOCTO,
     trim(isnull(ST9.T9_CODBEM, '-')) as T9_CODBEM,
 
-    lag(TPN.TPN_CCUSTO) over (partition by TPN.TPN_CODBEM order by TPN.TPN_DTINIC) as CC_ant,
-    TPN.TPN_CCUSTO as CC_atu,
-    case when TPN.TPN_CCUSTO != lag(TPN.TPN_CCUSTO) over (partition by TPN.TPN_CODBEM order by TPN.TPN_DTINIC) then TS1.TS1_VALOR * datediff(day, eomonth(TPN.TPN_DTINIC, -1), TPN.TPN_DTINIC) / datediff(day, eomonth(TPN.TPN_DTINIC, -1), eomonth(TPN.TPN_DTINIC)) else TS1.TS1_VALOR end /12 as CUSTO_INICC,
-    case when TPN.TPN_CCUSTO != lag(TPN.TPN_CCUSTO) over (partition by TPN.TPN_CODBEM order by TPN.TPN_DTINIC) then TS1.TS1_VALOR * datediff(day, TPN.TPN_DTINIC, eomonth(TPN.TPN_DTINIC)) / datediff(day, eomonth(TPN.TPN_DTINIC, -1), eomonth(TPN.TPN_DTINIC)) else TS1.TS1_VALOR end /12 as CUSTO_FIMCC
+    year(SE2.E2_VENCREA) as ano_VENCTO,
+    month(SE2.E2_VENCREA) as mes_VENCTO
+
 
 from TS1010 TS1
     left join TS0010 TS0
@@ -24,10 +23,6 @@ from TS1010 TS1
     left join ST9010 as ST9
         on ST9.D_E_L_E_T_ = ''
         and ST9.T9_CODBEM = TS1.TS1_CODBEM
-        
-        left join TPN010 TPN (nolock)
-            on TPN.D_E_L_E_T_ = ''
-            and TPN.TPN_CODBEM = ST9.T9_CODBEM
 where
         TS1.D_E_L_E_T_ = ''
-    and SE2.E2_VENCREA > 20211231
+    and SE2.E2_VENCREA > 20221231
