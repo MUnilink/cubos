f4ee26ba587f07947abd92440a22c9ad932e7a2b
unknown <stefsonsampaio01@gmail.com>
2023-06-26 16:19:52 -0300

    alterando capslock OS Custo Folha

--- faturamento/conciliação OS x folha.sql
+++ faturamento/conciliação OS x folha.sql
@@ -1,33 +1,33 @@
-    SELECT
+    select
         ZC2_NUM + '-' + ZC2_ITEM as OS_ITEM,
         RJ_DESC RH,
-        CONVERT(DATE,ZC2_COMPET) as PERIODO,
+        convert(date,ZC2_COMPET) as PERIODO,
         ZC2_CHVOS,
         ZC2_TOTAL as TOTAL_OS,
         0 TOTAL_FOLHA,
         ZC2.R_E_C_N_O_
-    FROM ZC2010 ZC2, SRJ010 SRJ
-    WHERE
+    from ZC2010 ZC2, SRJ010 SRJ
+    where
             ZC2.D_E_L_E_T_=''
-        AND SRJ.D_E_L_E_T_= ''
-        AND ZC2_COMPET <> ''
-        AND ZC2_TIPO = 2
-        AND ZC2_COD = RJ_FUNCAO
+        and SRJ.D_E_L_E_T_= ''
+        and ZC2_COMPET <> ''
+        and ZC2_TIPO = 2
+        and ZC2_COD = RJ_FUNCAO
         and ZC2.ZC2_INCLUS = 'M'
 
 union
 
-    SELECT
+    select
         'FOLHA',
         RV_DESC,
-        CONVERT(DATE,RD_DATARQ+'01'),
+        convert(date,RD_DATARQ+'01'),
         RD_YCHVOS as ZC2_CHVOS,
         0 TOTAL_OS,
         RD_VALOR as TOTAL_FOLHA,
         SRD.R_E_C_N_O_
-    FROM SRD010 SRD, SRV010 SRV
-    WHERE
+    from SRD010 SRD, SRV010 SRV
+    where
             SRD.D_E_L_E_T_ = ''
-        AND RD_YCHVOS <> ''
-        AND SRV.D_E_L_E_T_=''
-        AND RD_PD = RV_COD
+        and RD_YCHVOS <> ''
+        and SRV.D_E_L_E_T_=''
+        and RD_PD = RV_COD
