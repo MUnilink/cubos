48a29e6cd7819c62fe8b2951156d9f5a75464aca
stefsonsampaio <stefsonsampaio01@gmail.com>
2023-07-03 16:43:07 -0300

    alterando foreign/foward key da tabela dSQB o historico

--- RH/verbas pagas por período.sql
+++ RH/verbas pagas por período.sql
@@ -164,11 +164,10 @@ union
 			on SRA.D_E_L_E_T_ = ''
 			and SRA.RA_FILIAL = SRD.RD_FILIAL
 			and SRA.RA_MAT = SRD.RD_MAT
+		inner join SQB010 SQB (nolock)
+			on SQB.D_E_L_E_T_ = ''
+			and SQB.QB_DEPTO = SRD.RD_DEPTO
 
-			inner join SQB010 SQB (nolock)
-				on SQB.D_E_L_E_T_ = ''
-				and SQB.QB_FILIAL = substring(SRA.RA_FILIAL, 1, 4)
-				and SQB.QB_DEPTO = SRA.RA_DEPTO
 			inner join SRJ010 SRJ (nolock)
 				on SRJ.D_E_L_E_T_ = ''
 				and SRJ.RJ_FILIAL = substring(SRA.RA_FILIAL, 1, 4)
