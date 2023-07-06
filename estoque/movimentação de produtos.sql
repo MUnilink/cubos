5fb3ff2f427d6f4824fcec1e35a5c3aac32f78f1
Manoel <manoel.jose@unilinktransportes.com.br>
2023-07-06 16:44:59 -0300

    relatório de entradas do estoque

--- estoque/movimentação de produtos.sql
+++ estoque/movimentação de produtos.sql
@@ -1,42 +1,45 @@
-select
-    SB1.B1_COD as contador,
-    trim(isnull(SB1.B1_COD, '-')) as PRODUTO,
-	trim(isnull(SB1.B1_DESC, '-')) as NOMEPRODUTO,
-	trim(isnull(SB1.B1_GRUPO, '-')) as GRUPO,
-	trim(isnull(SB1.B1_UM, '-')) as UN,
-    isnull(SB1.B1_UPRC, 0.0) as ULT_PRECO,
-    substring(SD3.D3_EMISSAO, 1, 6) as PERIODO,
-    SD3.D3_LOCAL,
-    SD3.D3_FILIAL,
-    SD3.D3_TM,
-    SD3.D3_CF,
-    SD3.D3_DOC,
-    (select max(convert(date, SD1010.D1_DTDIGIT, 103)) from SD1010 where SD1010.D_E_L_E_T_ = '' and SD1010.D1_COD = SB1.B1_COD) as ULT_COMPRA,
-    SB2.B2_FILIAL,
-    SB2.B2_LOCAL,
-    isnull
-    (
-        (
-            select avg(SB9010.B9_QINI)
-            from SB9010 (nolock)
-            where
-                    SB9010.D_E_L_E_T_ = ''
-                and SB9010.B9_FILIAL = SD3.D3_FILIAL
-                and SB9010.B9_LOCAL = SD3.D3_LOCAL
-                and SB9010.B9_COD = SD3.D3_COD
-                and substring(SB9010.B9_DATA, 1, 6) = substring(SD3.D3_EMISSAO, 1, 6)
-        ),
-        SB2.B2_QATU
-    ) as SALDO
-from SB1010 SB1 (nolock)
-    left join SD3010 SD3 (nolock)
-        on SD3.D_E_L_E_T_ = ''
-        and SD3.D3_COD = SB1.B1_COD
-        and SD3.D3_TM in (501, 544)
-    left join SB2010 SB2 (nolock)
-        on SB2.D_E_L_E_T_ = ''
-        and SB2.B2_COD = SB1.B1_COD
-where
-        SB1.D_E_L_E_T_ = ''
-    and SB1.B1_GRUPO like '1%'
-    and SB1.B1_MSBLQL = 2
+    select
+        SB1.B1_COD as contador,
+        trim(isnull(SB1.B1_COD, '-')) as PRODUTO,
+        trim(isnull(SB1.B1_DESC, '-')) as NOMEPRODUTO,
+        trim(isnull(SB1.B1_GRUPO, '-')) as GRUPO,
+        trim(isnull(SB1.B1_UM, '-')) as UN,
+        substring(SD3.D3_EMISSAO, 1, 6) as PERIODO,
+        convert(date, SD3.D3_EMISSAO, 103) as EMISSAO,
+        SD3.D3_LOCAL as ARMAZEM,
+        SD3.D3_FILIAL as FILIAL,
+        SD3.D3_TM as TM,
+        SD3.D3_CF as CF,
+        SD3.D3_DOC as DOC,
+        SD3.D3_CUSTO1 as CUSTO_MOV
+    from SD3010 SD3 (nolock)
+        left join SB1010 SB1 (nolock)
+            on SB1.D_E_L_E_T_ = ''
+            and SB1.B1_COD = SD3.D3_COD
+            and SB1.B1_GRUPO like '1%'
+            and SB1.B1_MSBLQL = 2
+    where
+            SD3.D_E_L_E_T_ = ''
+union
+    select
+        SB1.B1_COD as contador,
+        trim(isnull(SB1.B1_COD, '-')) as PRODUTO,
+        trim(isnull(SB1.B1_DESC, '-')) as NOMEPRODUTO,
+        trim(isnull(SB1.B1_GRUPO, '-')) as GRUPO,
+        trim(isnull(SB1.B1_UM, '-')) as UN,
+        substring(SD1.D1_DTDIGIT, 1, 6) as PERIODO,
+        convert(date, SD1.D1_DTDIGIT, 103) as EMISSAO,
+        SD1.D1_LOCAL as ARMAZEM,
+        SD1.D1_FILIAL as FILIAL,
+        SD1.D1_TES as TM,
+        SD1.D1_CF as CF,
+        SD1.D1_DOC as DOC,
+        SD1.D1_CUSTO as CUSTO_MOV
+    from SD1010 SD1 (nolock)
+        left join SB1010 SB1 (nolock)
+            on SB1.D_E_L_E_T_ = ''
+            and SB1.B1_COD = SD1.D1_COD
+            and SB1.B1_GRUPO like '1%'
+            and SB1.B1_MSBLQL = 2
+    where
+            SD1.D_E_L_E_T_ = ''
