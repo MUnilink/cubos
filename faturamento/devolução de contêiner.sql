9684a8a69c955307b34a939a0fea6b4f42957543
Manoel <manoel.jose@unilinktransportes.com.br>
2023-07-07 13:53:04 -0300

    encontro de campo motorista e veículos

--- faturamento/devolução de contêiner.sql
+++ faturamento/devolução de contêiner.sql
@@ -35,34 +35,45 @@ select
     ZC2.ZC2_VLUREA,
     trim(ZC2.ZC2_CONTEI) as CONTEINER,
     trim(ZC2.ZC2_LACRE) as LACRE,
+    ZC2.ZC2_MOTORI as COD_MOT,
+    DA4.DA4_NOME as MOTORISTA,
+    ZC2.ZC2_VEICUL as CM,
+    ZC2.ZC2_CARRET as SR,
     
     substring(ZC2.ZC2_DTINI, 1, 6) as PERIODO_APONT,
     convert(datetime, concat(ZC2.ZC2_DTINI, ' ', ZC2.ZC2_HRINI), 103) as DTINI_APONT,
     convert(datetime, concat(ZC2.ZC2_DTFIM, ' ', ZC2.ZC2_HRFIM), 103) as DTFIM_APONT,
     trim(ZC2.ZC2_NMUSU) as USUARIO
 
-from ZC1010 ZC1 (nolock)
-    left join ZC2010 ZC2 (nolock)
-        on ZC2.D_E_L_E_T_ = ''
-        and ZC2.ZC2_FILIAL = ZC1.ZC1_FILIAL
-        and ZC2.ZC2_NUM = ZC1.ZC1_NUM
+from ZC2010 ZC2 (nolock)
+    left join ZC1010 ZC1 (nolock)
+        on ZC1.D_E_L_E_T_ = ''
+        and ZC1.ZC1_FILIAL = ZC2.ZC2_FILIAL
+        and ZC1.ZC1_NUM = ZC2.ZC2_NUM
+        and substring(ZC1.ZC1_NUM, 1, 4) > 2022
         
-        left join SB1010 SB1 (nolock)
-            on SB1.D_E_L_E_T_ = ''
-            and SB1.B1_COD = ZC2.ZC2_COD
+        left join SA1010 DEV (nolock)
+            on DEV.D_E_L_E_T_ = ''
+            and DEV.A1_COD = ZC1.ZC1_CODSA1
+            and DEV.A1_LOJA = ZC1.ZC1_LOJSA1
+        left join SA1010 ARM (nolock)
+            on ARM.D_E_L_E_T_ = ''
+            and ARM.A1_COD = ZC1.ZC1_ARMADO
+            and ARM.A1_LOJA = ZC1.ZC1_LJARMA
+        left join SA2010 DES (nolock)
+            on DES.D_E_L_E_T_ = ''
+            and DES.A2_COD = ZC1.ZC1_DESPA
+            and DES.A2_LOJA = ZC1.ZC1_LJDESP
     
-    left join SA1010 DEV (nolock)
-        on DEV.D_E_L_E_T_ = ''
-        and DEV.A1_COD = ZC1.ZC1_CODSA1
-        and DEV.A1_LOJA = ZC1.ZC1_LOJSA1
-    left join SA1010 ARM (nolock)
-        on ARM.D_E_L_E_T_ = ''
-        and ARM.A1_COD = ZC1.ZC1_ARMADO
-        and ARM.A1_LOJA = ZC1.ZC1_LJARMA
-    left join SA2010 DES (nolock)
-        on DES.D_E_L_E_T_ = ''
-        and DES.A2_COD = ZC1.ZC1_DESPA
-        and DES.A2_LOJA = ZC1.ZC1_LJDESP
+    left join SB1010 SB1 (nolock)
+        on SB1.D_E_L_E_T_ = ''
+        and SB1.B1_COD = ZC2.ZC2_COD
+    left join DA3010 DA3 (nolock)
+        on DA3.D_E_L_E_T_ = ''
+        and DA3.DA3_COD = ZC2.ZC2_VEICUL
+    left join DA4010 DA4 (nolock)
+        on DA4.D_E_L_E_T_ = ''
+        and DA4.DA4_COD = ZC2.ZC2_MOTORI
 where
         ZC2.D_E_L_E_T_ = ''
     /*and ZC2.ZC2_NUM = :NUMERO_OS*/
