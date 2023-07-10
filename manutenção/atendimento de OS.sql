f8c063b1286a479b3da5e0225205326e65b174d3
Manoel <manoel.jose@unilinktransportes.com.br>
2023-07-10 10:29:06 -0300

    cálculo das horas apontadas

--- manutenção/atendimento de OS.sql
+++ manutenção/atendimento de OS.sql
@@ -11,7 +11,13 @@ select
     substring(STL.TL_DTINICI, 1, 6) as PERIODO_OS,
     convert(datetime, datetimefromparts(year(STL.TL_DTINICI), month(STL.TL_DTINICI), day(STL.TL_DTINICI), substring(STL.TL_HOINICI, 1, 2), substring(STL.TL_HOINICI, 4, 5), 0, 0), 113) as DATA_INI,
 	convert(datetime, datetimefromparts(year(STL.TL_DTFIM), month(STL.TL_DTFIM), day(STL.TL_DTFIM), substring(STL.TL_HOFIM, 1, 2), substring(STL.TL_HOFIM, 4, 5), 0, 0), 113) as DATA_FIM,
-    datediff(minute, datetimefromparts(year(STL.TL_DTINICI), month(STL.TL_DTINICI), day(STL.TL_DTINICI), substring(STL.TL_HOINICI, 1, 2), substring(STL.TL_HOINICI, 4, 5), 0, 0), datetimefromparts(year(STL.TL_DTFIM), month(STL.TL_DTFIM), day(STL.TL_DTFIM), substring(STL.TL_HOFIM, 1, 2), substring(STL.TL_HOFIM, 4, 5), 0, 0))/60.0 as HORAS_APONT,
+
+    datediff
+        (
+            minute,
+            datetimefromparts(year(STL.TL_DTINICI), month(STL.TL_DTINICI), day(STL.TL_DTINICI), substring(STL.TL_HOINICI, 1, 2), substring(STL.TL_HOINICI, 4, 5), 0, 0),
+            datetimefromparts(year(STL.TL_DTFIM), month(STL.TL_DTFIM), day(STL.TL_DTFIM), substring(STL.TL_HOFIM, 1, 2), substring(STL.TL_HOFIM, 4, 5), 0, 0)
+        )/60.0 as HORAS_APONT,
 	
     STL.TL_LOCAL as ARMAZEM,
 	STJ.TJ_POSCONT as CONTADOR,
