58a688e278a9cf8c8f6789efabfac17a1af711ca
Manoel <manoel.jose@unilinktransportes.com.br>
2023-07-18 16:45:17 -0300

    ajuste nomes campos e FROM da OS portuária

--- faturamento/OS portuária.sql
+++ faturamento/OS portuária.sql
@@ -1,6 +1,8 @@
 select
     ZC1.ZC1_FILIAL,
     ZC1.ZC1_NUM,
+    substring(ZC1.ZC1_NUM, 6, 10) as OS,
+    substring(ZC1.ZC1_NUM, 1, 4) as ANO_OS,
     substring(ZC1.ZC1_EMISSA, 1, 6) as PERIODO_OS,
     convert(date, ZC1.ZC1_EMISSA, 103) as DATA_OS,
     
@@ -8,9 +10,10 @@ select
     (select trim(SX5010.X5_DESCRI) from SX5010 where SX5010.D_E_L_E_T_ = '' and SX5010.X5_TABELA = '_1' and SX5010.X5_CHAVE = ZC1.ZC1_PORTO) as DESC_PORTO,
     ZC1.ZC1_NAVIO,
     (select trim(ZA3010.ZA3_DESC) from ZA3010 where ZA3010.D_E_L_E_T_ = '' and ZA3010.ZA3_COD = ZC1.ZC1_NAVIO) as DESC_NAVIO,
-    trim(ZC1.ZC1_VIAGEM) as ZC1_VIAGEM,
+    trim(ZC1.ZC1_VIAGEM) as VIAGEM_PORT,
     
-    ZC2.ZC2_ITEM,
+    ZC2.ZC2_ITEM as ITEM,
+    SB1.B1_GRUPO as GRUPO,
     trim(ZC2.ZC2_COD) as INSUMO,
     trim(ZC2.ZC2_DESC) as DESC_INSUMO,
     ZC2.ZC2_INCLUS as TIPO_INCLUSAO,
@@ -29,12 +32,17 @@ select
         else 'OUTROS'
     end as STATUS_OS,
     
-    ZC2.ZC2_QTDPRV,
-    ZC2.ZC2_QTDREA,
-    ZC2.ZC2_VLUPRV,
-    ZC2.ZC2_VLUREA,
-    trim(ZC2.ZC2_CONTEI) as ZC2_CONTEI,
-    trim(ZC2.ZC2_LACRE) as ZC2_LACRE,
+    ZC2.ZC2_QTDPRV as QTD_PREV,
+    ZC2.ZC2_QTDREA as QTD_REAL,
+    ZC2.ZC2_VLUPRV as VAL_PREV,
+    ZC2.ZC2_VLUREA as VAL_REAL,
+    
+    trim(ZC2.ZC2_CONTEI) as CONTEINER,
+    trim(ZC2.ZC2_LACRE) as LACRE,
+    ZC2.ZC2_MOTORI as COD_MOT,
+    DA4.DA4_NOME as MOTORISTA,
+    ZC2.ZC2_VEICUL as CM,
+    ZC2.ZC2_CARRET as SR,
     
     substring(ZC2.ZC2_DTINI, 1, 6) as PERIODO_APONT,
     convert(datetime, concat(ZC2.ZC2_DTINI, ' ', ZC2.ZC2_HRINI), 103) as DTINI_APONT,
@@ -42,23 +50,35 @@ select
     datediff(minute, convert(datetime, concat(ZC2.ZC2_DTINI, ' ', ZC2.ZC2_HRINI), 103), convert(datetime, concat(ZC2.ZC2_DTFIM, ' ', ZC2.ZC2_HRFIM), 103))/60.0 as HORAS_APONT,
     trim(ZC2.ZC2_NMUSU) as ZC2_NMUSU
 
-from ZC1010 ZC1 (nolock)
-    left join ZC2010 ZC2 (nolock)
-        on ZC2.D_E_L_E_T_ = ''
-        and ZC2.ZC2_FILIAL = ZC1.ZC1_FILIAL
-        and ZC2.ZC2_NUM = ZC1.ZC1_NUM
-    left join SA1010 DEV (nolock)
-        on DEV.D_E_L_E_T_ = ''
-        and DEV.A1_COD = ZC1.ZC1_CODSA1
-        and DEV.A1_LOJA = ZC1.ZC1_LOJSA1
-    left join SA1010 ARM (nolock)
-        on ARM.D_E_L_E_T_ = ''
-        and ARM.A1_COD = ZC1.ZC1_ARMADO
-        and ARM.A1_LOJA = ZC1.ZC1_LJARMA
-    left join SA2010 DES (nolock)
-        on DES.D_E_L_E_T_ = ''
-        and DES.A2_COD = ZC1.ZC1_DESPA
-        and DES.A2_LOJA = ZC1.ZC1_LJDESP
+from ZC2010 ZC2 (nolock)
+    left join ZC1010 ZC1 (nolock)
+        on ZC1.D_E_L_E_T_ = ''
+        and ZC1.ZC1_FILIAL = ZC2.ZC2_FILIAL
+        and ZC1.ZC1_NUM = ZC2.ZC2_NUM
+        and substring(ZC1.ZC1_NUM, 1, 4) > 2022
+        
+        left join SA1010 DEV (nolock)
+            on DEV.D_E_L_E_T_ = ''
+            and DEV.A1_COD = ZC1.ZC1_CODSA1
+            and DEV.A1_LOJA = ZC1.ZC1_LOJSA1
+        left join SA1010 ARM (nolock)
+            on ARM.D_E_L_E_T_ = ''
+            and ARM.A1_COD = ZC1.ZC1_ARMADO
+            and ARM.A1_LOJA = ZC1.ZC1_LJARMA
+        left join SA2010 DES (nolock)
+            on DES.D_E_L_E_T_ = ''
+            and DES.A2_COD = ZC1.ZC1_DESPA
+            and DES.A2_LOJA = ZC1.ZC1_LJDESP
+    
+    left join SB1010 SB1 (nolock)
+        on SB1.D_E_L_E_T_ = ''
+        and SB1.B1_COD = ZC2.ZC2_COD
+    left join DA3010 DA3 (nolock)
+        on DA3.D_E_L_E_T_ = ''
+        and DA3.DA3_COD = ZC2.ZC2_VEICUL
+    left join DA4010 DA4 (nolock)
+        on DA4.D_E_L_E_T_ = ''
+        and DA4.DA4_COD = ZC2.ZC2_MOTORI
 where
         ZC2.D_E_L_E_T_ = ''
     and ZC2.ZC2_INCLUS != 'C'
