594d58807a4a41540147d9664f7a413b8c5100b5
Manoel <manoel.jose@unilinktransportes.com.br>
2023-08-05 10:21:53 -0300

    carga dim produto ok, após exclusão de CC

--- FAST ANALYTICS/gooddata-custom/query/dim/produto.sql
+++ FAST ANALYTICS/gooddata-custom/query/dim/produto.sql
@@ -1,14 +1,6 @@
 select
-	trim(isnull(SB1.B1_COD, '-')) as B1_COD,
-	trim(isnull(SB1.B1_DESC, '-')) as B1_DESC,
-	
-	trim(isnull(SBM.BM_GRUPO, '-')) as BM_GRUPO,
-	trim(isnull(CTT.CTT_CUSTO, '-')) as CTT_CUSTO
-from SB1010 as SB1
-	left join SBM010 as SBM
-		on SBM.D_E_L_E_T_ = ''
-		and SBM.BM_GRUPO = SB1.B1_GRUPO
-	left join CTT010 as CTT
-		on CTT.D_E_L_E_T_ = ''
-		and CTT.CTT_CUSTO = SB1.B1_CC
-where SB1.D_E_L_E_T_ = ''
+	trim(isnull(SB1010.B1_COD, '-')) as B1_COD,
+	trim(isnull(SB1010.B1_DESC, '-')) as B1_DESC,
+	SB1010.B1_GRUPO as B1_GRUPO
+from SB1010
+where SB1010.D_E_L_E_T_ = ''
