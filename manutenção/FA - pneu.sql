2f38f0a2ef7755db92ea0eb84b0d7ce2ada5db52
Manoel <manoel.jose@unilinktransportes.com.br>
2023-08-09 11:56:50 -0300

    início ajuste fato pneu 03/08/2023

--- manutenção/FA - pneu.sql
+++ manutenção/FA - pneu.sql
@@ -2,26 +2,13 @@ select
 	trim(isnull(STJ.TJ_FILIAL, '-')) as TJ_FILIAL,
 	trim(isnull(STZ.TZ_ORDEM, '-')) as TZ_ORDEM,
 	trim(isnull(STJ.TJ_ORDEM, '-')) as TJ_ORDEM,
-	trim(isnull(TR8.TR8_LOTE, '-')) as TR8_LOTE,
 	trim(isnull(ST9.T9_CODBEM, '-')) as T9_CODBEM,
 	trim(isnull(STZ.TZ_BEMPAI, '-')) as TZ_BEMPAI,
-	trim(isnull(ST4.T4_NOME, '-')) as T4_NOME,
-
-	STZ.TZ_POSCONT,
-	STZ.TZ_CONTSAI,
-	trim(isnull(STZ.TZ_DATAMOV, '-')) as TZ_DATAMOV,
-	trim(isnull(STZ.TZ_DATASAI, '-')) as TZ_DATASAI,
+	
+	trim(isnull(TR8.TR8_LOTE, '-')) as TR8_LOTE,
 	trim(isnull(STZ.TZ_TIPOMOV, '-')) as TZ_TIPOMOV,
-	trim(isnull(STZ.TZ_HORAENT, '-')) as TZ_HORAENT,
-	trim(isnull(STZ.TZ_HORASAI, '-')) as TZ_HORASAI,
-	trim(isnull(TQT.TQT_DESMED, '-')) as TQT_DESMED,
 
 	STJ.TJ_CUSTTER,
-	case when substring(STZ.TZ_DATAMOV, 1, 6) = substring(ST9.T9_DTCOMPR, 1, 6) then ST9.T9_VALCPA else 0.0 end as T9_VALCPA,
-	trim(isnull(ST9.T9_DTCOMPR, '-')) as T9_DTCOMPR,
-	SB1.B1_UPRC,
-
-	TQS.TQS_KMOR,
 	TQS.TQS_KMR1,
 	TQS.TQS_KMR2,
 	TQS.TQS_KMR3,
@@ -29,10 +16,24 @@ select
 	TQS.TQS_KMR5,
 	TQS.TQS_KMR6,
 	TQS.TQS_KMR7,
+	TQS.TQS_KMOR,
+	ST9.T9_VALCPA,
+	STZ.TZ_CONTSAI,
+	STZ.TZ_POSCONT,
 	TQS.TQS_KMOR + TQS.TQS_KMR1 + TQS.TQS_KMR2 + TQS.TQS_KMR3 + TQS.TQS_KMR4 + TQS.TQS_KMR5 + TQS.TQS_KMR6 + TQS.TQS_KMR7 as kmTOT,
-	case when cast(STZ.TZ_CODBEM as int) > 11140 then 'PNEU NOVO' else 'PNEU ANTIGO' end as TIPO_PNEU
+	trim(isnull(STZ.TZ_DATAMOV, '-')) as TZ_DATAMOV,
+	trim(isnull(STZ.TZ_DATASAI, '-')) as TZ_DATASAI,
+	ST9.T9_DTCOMPR,
 
-from STJ010 as STJ /* OS da manutenção*/
+	(
+        select top 1 first_value(STZ.TZ_DATAMOV) over (partition by STZ010.TZ_CODBEM order by STZ010.TZ_CODBEM)
+        from STZ010 (nolock)
+        where
+                STZ010.D_E_L_E_T_ = ''
+            and STZ010.TZ_CODBEM = STZ.TZ_CODBEM
+    ) as DATA
+
+from TQS010 TQS
 	left join TR8010 as TR8 /* movimentação em lote do pneu */
 		on TR8.D_E_L_E_T_ = ''
 		and TR8.TR8_FILIAL = STJ.TJ_FILIAL
@@ -44,7 +45,7 @@ from STJ010 as STJ /* OS da manutenção*/
 			and TR7.TR7_FILIAL = TR8.TR8_FILIAL
 			and TR7.TR7_LOTE = TR8.TR8_LOTE
 			
-	inner join TQS010 as TQS /* pneus */
+	inner join STJ010 STJ
 		on TQS.D_E_L_E_T_ = ''
 		and TQS.TQS_CODBEM = STJ.TJ_CODBEM
 
@@ -74,4 +75,4 @@ from STJ010 as STJ /* OS da manutenção*/
 			on SB1.D_E_L_E_T_ = ''
 			and SB1.B1_COD = STL.TL_CODIGO
 where
-		STJ.D_E_L_E_T_ = ''
+	STJ.D_E_L_E_T_ = ''
