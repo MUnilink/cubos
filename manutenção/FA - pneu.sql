b8bdb396222f66e6d1e289cff04926014ed97c35
Manoel <manoel.jose@unilinktransportes.com.br>
2023-08-09 16:24:48 -0300

    enviar consulta para FTP e extrair metadados para ETL

--- manutenção/FA - pneu.sql
+++ manutenção/FA - pneu.sql
@@ -1,10 +1,22 @@
 select
-	STJ.TJ_FILIAL as TJ_FILIAL,
-	STJ.TJ_ORDEM as TJ_ORDEM,
+	STJ.TJ_FILIAL as FILIAL,
+	STJ.TJ_ORDEM as OS,
 	cast(ST9.T9_CODBEM as int) as T9_CODBEM,
-	TR8.TR8_LOTE as TR8_LOTE,
+	TR8.TR8_LOTE as LOTE,
 	STJ.TJ_CCUSTO as CCUSTO,
-	STJ.TJ_YITMCT as ATIVIDADE,
+	STJ.TJ_SERVICO as SERVICO,
+
+	case when STJ.TJ_YITMCT is not null and STJ.TJ_YITMCT != '' then STJ.TJ_YITMCT
+	else
+		case STJ.TJ_CCUSTO
+			when 302 then 11
+			when 304 then 11
+			when 303 then 21
+			when 305 then 21
+			when 306 then 21
+			else 90
+		end
+	end as ATIVIDADE,
 
 	STJ.TJ_CUSTTER,
 	TQS.TQS_KMR1,
@@ -24,31 +36,19 @@ select
         where
                 STZ010.D_E_L_E_T_ = ''
             and STZ010.TZ_CODBEM = ST9.T9_CODBEM
-    ) as DATA,
+    ) as DATA
 
-	TR4.TR4_NUMANA,
-	TR4.TR4_ORDEM,
-	TR4.TR4_DESTIN,
-	TR4.TR4_MOTIVO,
-	convert(datetime, concat(TR4.TR4_DTANAL, ' ', TR4.TR4_HRANAL), 103) as DATA_ANALISE
-
-from STJ010 STJ
-	left join TR8010 TR8
+from STJ010 STJ (nolock)
+	left join TR8010 TR8 (nolock)
 		on TR8.D_E_L_E_T_ = ''
 		and TR8.TR8_FILIAL = STJ.TJ_FILIAL
 		and TR8.TR8_ORDEM = STJ.TJ_ORDEM
 		and TR8.TR8_PLANO = STJ.TJ_PLANO
-
-		left join TR4010 TR4
-			on TR4.D_E_L_E_T_ = ''
-			and TR4.TR4_CODBEM = TR8.TR8_CODBEM
-			and TR4.TR4_ORDEM = TR8.TR8_ORDEM
-			
-	inner join TQS010 TQS
+	inner join TQS010 TQS (nolock)
 		on TQS.D_E_L_E_T_ = ''
 		and TQS.TQS_CODBEM = STJ.TJ_CODBEM
 
-		inner join ST9010 as ST9
+		inner join ST9010 ST9 (nolock)
 			on ST9.D_E_L_E_T_ = ''
 			and ST9.T9_CODBEM = TQS.TQS_CODBEM
 
