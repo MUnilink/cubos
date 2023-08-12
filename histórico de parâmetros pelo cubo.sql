c9d78583611a5258589501cf078633739915fbef
Manoel <manoel.jose@unilinktransportes.com.br>
2023-08-12 11:27:19 -0300

    consulta para registro de parâmetros pelo cubo

--- histórico de parâmetros pelo cubo.sql
+++ histórico de parâmetros pelo cubo.sql
@@ -1,11 +1,11 @@
 select
 	SX6.X6_FIL as FILIAL,
-    SX6.X6_VAR as VARIAVEL,
+    SX6.X6_VAR as PARAMETRO,
     SX6.X6_TIPO as TIPO,
     trim(SX6.X6_DESCRIC) +' '+ trim(SX6.X6_DESC1) +' '+ trim(SX6.X6_DESC2) as DESCRICAO,
     SX6.X6_PROPRI as PROPRI,
     SX6.X6_PYME as PYME,
-    SX6.X6_CONTEUD as CONTEUDO
+    trim(SX6.X6_CONTEUD) as CONTEUDO
 from SX6010 SX6 (nolock)
 where
     SX6.D_E_L_E_T_ = ''
