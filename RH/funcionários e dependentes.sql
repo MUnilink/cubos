a3196f0f9e240a931fff28fae2c83f6f948c0c5a
Manoel <manoel.jose@unilinktransportes.com.br>
2023-08-18 16:36:30 -0300

    reorganização e funcionários com dep/agg ou não

--- RH/funcionários e dependentes.sql
+++ RH/funcionários e dependentes.sql
@@ -1,25 +1,30 @@
 select
-    trim(SRA.RA_FILIAL) as FILIAL,
+	trim(SRA.RA_FILIAL) as FILIAL,
 	trim(SRA.RA_MAT) as MATRICULA,
+	trim(SRA.RA_MAT) as contador,
 	trim(SRA.RA_NOME) as NOME,
-	trim(SRJ.RJ_DESC) as FUNCAO,
 	trim(SRA.RA_MUNICIP) as MUNICIPIO,
 	trim(SRA.RA_ESTADO) as UF,
-	convert(date, SRA.RA_ADMISSA, 103) as ADMISSAO,
-    case when trim(SRA.RA_SITFOLH) != 'D' then 'S' else 'N' end as ATIVO,
-
-	trim(CTT.CTT_CUSTO) as CC,
-	trim(CTT.CTT_DESC01) as CCUSTO,
-	trim(CTD.CTD_ITEM) as AT,
-	trim(CTD.CTD_DESC01) as ATIVIDADE,
-    trim(SQB.QB_DEPTO) as DEPTO,
-    trim(SQB.QB_DESCRIC) as DEPARTAMENTO,
-
 	trim(SRJ.RJ_CODCBO) as CBO,
 	trim(SRA.RA_SEXO) as SEXO,
 	trim(SRA.RA_CIC) as CPF,
+	trim(SRJ.RJ_FUNCAO) as COD_FUNCAO,
+	trim(SRJ.RJ_DESC) as FUNCAO,
+	trim(CTT.CTT_CUSTO) as COD_CC,
+	trim(CTT.CTT_DESC01) as CENTRO_CUSTO,
+	trim(CTD.CTD_ITEM) as COD_ITEM,
+	trim(CTD.CTD_DESC01) as ATIVIDADE,
+	trim(SQB.QB_DEPTO) as DEPTO,
+    trim(SQB.QB_DESCRIC) as DEPARTAMENTO,
+	convert(date, SRA.RA_NASC, 103) as NASCIMENTO,
+	convert(date, SRA.RA_ADMISSA, 103) as ADMISSAO,
+    case when trim(SRA.RA_SITFOLH) != 'D' then 'S' else 'N' end as ATIVO,
+
+	case when SRB.RB_NOME is not null then 'DEP' else case when RHM.RHM_NOME is not null then 'AGG' else 'NAO' end end as TEM_DEPAGG,
 
 	datepart (week, SRA.RA_NASC) as sem_ANIVERSARIO,
+	month(SRA.RA_NASC) as mes_ANIVERSARIO,
+	day(SRA.RA_NASC) as dia_ANIVERSARIO,
 	
 	trim(SRB.RB_NOME) DEPENDENTE,
 	convert(date, SRB.RB_DTNASC, 103) as DEP_NASC,
@@ -43,7 +48,7 @@ select
 	end as PARENTESCO
 
 from SRA010 SRA (nolock)
-	inner join SRB010 SRB (nolock)
+	left join SRB010 SRB (nolock)
 		on SRB.D_E_L_E_T_ = ''
 		and SRB.RB_FILIAL = SRA.RA_FILIAL
 		and SRB.RB_MAT = SRA.RA_MAT
