6c2b94a7856eca878a55756291cfc6f9dca13027
stefsonsampaio <stefsonsampaio01@gmail.com>
2023-08-29 11:32:35 -0300

    adicionando a tabela sd4 como inner join da tabela sc2 em ORDEM DE PRODUÇÃO

--- ordem de producao.sql
+++ ordem de producao.sql
@@ -13,10 +13,12 @@ select
     SC2.C2_NUM as NUM_OP,
 
     convert(date, SG1.G1_INI, 101) as DATA_INI,
-    convert(date, SG1.G1_FIM, 101) as DATA_FIM
+    convert(date, SG1.G1_FIM, 101) as DATA_FIM,
 
-from SC2010 as SC2 (nolock)
-    inner join SG1010 as SG1 (nolock)
+    SD4.D4_OP as NUM_OP
+
+from SC2010 SC2 (nolock)
+    inner join SG1010 SG1 (nolock)
         on SG1.D_E_L_E_T_ = ''
         and SG1.G1_COD = SC2.C2_PRODUTO
         left join SB1010 SB1COD (nolock)
@@ -25,4 +27,7 @@ from SC2010 as SC2 (nolock)
         left join SB1010 SB1COMP (nolock)
             on SB1COMP.D_E_L_E_T_ = ''
             and SB1COMP.B1_COD = SG1.G1_COMP
+    inner join SD4010 SD4 (nolock)
+        on SD4.D_E_L_E_T_ = ''
+        and SD4.D4_OP = SC2.C2_NUM + SC2.C2_ITEM + SC2.C2_SEQUEN + SC2.C2_ITEMGRD
 where SC2.D_E_L_E_T_ = ''
