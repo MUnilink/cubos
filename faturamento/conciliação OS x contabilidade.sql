8794e5c0ab3dfeb4a5d9f2e997a9633cfab9cf5b
Manoel <manoel.jose@unilinktransportes.com.br>
2023-09-15 11:06:27 -0300

    consulta de despesas rateadas nas OS portuárias

---
+++ faturamento/conciliação OS x contabilidade.sql
@@ -0,0 +1,14 @@
+SELECT
+    ZC2.ZC2_NUM as NUM_OS,
+    ZC2.ZC2_ITEM as OS_ITEM,
+    ZC2.ZC2_COD as PRODUTO,
+    trim(ZC2.ZC2_DESC) as DESCRICAO,
+    CONVERT(date, ZC2.ZC2_COMPET) as PERIODO,
+    ZC2.ZC2_TOTAL as TOTAL_OS,
+    ZC2.R_E_C_N_O_
+
+FROM ZC2010 ZC2 (nolock)
+WHERE
+        ZC2.D_E_L_E_T_ = ''
+    AND ZC2.ZC2_COMPET != ''
+    AND ZC2.ZC2_TIPO = 7
