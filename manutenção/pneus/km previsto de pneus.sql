fa5831d987ddec050186c3dbd7f8c200b0d225a6
Manoel <manoel.jose@unilinktransportes.com.br>
2023-09-18 11:58:09 -0300

    por pneu; vínculo medida e fabricante duplicados

--- manutenção/pneus/km previsto de pneus.sql
+++ manutenção/pneus/km previsto de pneus.sql
@@ -1,20 +1,32 @@
-select    
+select
+    ST9.T9_CODBEM as PNEU,
+    ST9.T9_SITBEM as SITUACAO,
+    ST9.T9_STATUS as STATUS,
     trim(isnull(TQT.TQT_DESMED, '-')) as MEDIDA,
     ST7.T7_NOME as FABRICANTE,
     TQR.TQR_TIPMOD,
     TQR.TQR_DESMOD as MODELO,
     TQX.TQX_KMESPO as km_ESPERADO
 
-from TQX010 TQX (nolock)
-    left join TQT010 TQT (nolock)
-        on TQT.D_E_L_E_T_ = ''
-        and TQT.TQT_MEDIDA = TQX.TQX_MEDIDA
-    left join TQR010 TQR (nolock)
-        on TQR.D_E_L_E_T_ = ''
-        and TQR.TQR_TIPMOD = TQX.TQX_TIPMOD
+from TQT010 TQT (nolock)  
+    left join TQS010 TQS (nolock)
+        on TQS.D_E_L_E_T_ = ''
+        and TQS.TQS_MEDIDA = TQT.TQT_MEDIDA
+
+        left join TQX010 TQX (nolock)
+            on TQX.D_E_L_E_T_ = ''
+            and TQX.TQX_MEDIDA = TQS.TQS_MEDIDA
         
-        left join ST7010 ST7 (nolock)
-            on ST7.D_E_L_E_T_ = ''
-            and ST7.T7_FABRICA = TQR.TQR_FABRIC
+            left join TQR010 TQR (nolock)
+                on TQR.D_E_L_E_T_ = ''
+                and TQR.TQR_TIPMOD = TQX.TQX_TIPMOD
+                
+                left join ST7010 ST7 (nolock)
+                    on ST7.D_E_L_E_T_ = ''
+                    and ST7.T7_FABRICA = TQR.TQR_FABRIC
+
+        left join ST9010 ST9 (nolock)
+            on ST9.D_E_L_E_T_ = ''
+            and ST9.T9_CODBEM = TQS.TQS_CODBEM
 where
-		TQX.D_E_L_E_T_ = ''
+		TQT.D_E_L_E_T_ = ''
