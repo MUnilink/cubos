2ec8d2378bfd415d9c2be8f5c2890793265e0933
Manoel <manoel.jose@unilinktransportes.com.br>
2023-09-18 16:37:06 -0300

    estruturas com componentes ok

--- manutenção/estrutura de bens.sql
+++ manutenção/estrutura de bens.sql
@@ -3,15 +3,21 @@ select
     STC.TC_COMPONE as COMPONENTE,
     ST9.T9_CCUSTO as CC,
     ST9.T9_ITEMCTA as ATIVIDADE,
-    case when ST9.T9_CODFAMI != 'PN' and (STC.TC_COMPONE is null or STC.TC_COMPONE = '') then 'BEM DESATRELADO' else 
-    case when STC.TC_COMPONE = ST9.T9_CODBEM then 'COMPONENTE ATRELADO' else case when STC.TC_CODBEM = ST9.T9_CODBEM then 'ESTRUTURA' else 'BEM DESATRELADO' end end as STATUS,
-    isnull(TQT.TQT_DESMED, 'SR') as TQT_DESMED,
-    ST9.T9_CODFAMI as FAMILIA,
-    case when STC.TC_COMPONE like 'SR%' then 1000 else 1 end as qtd_compone
-from STC010 STC (nolock)
-    left join ST9010 EST (nolock)
-        on EST.D_E_L_E_T_ = ''
-        and EST.T9_CODBEM = STC.TC_CODBEM
+    
+    case when STC.TC_COMPONE = (select ST9010.T9_CODBEM from ST9010 (nolock) where ST9010.D_E_L_E_T_ = '' and ST9010.T9_CODFAMI = 'VP' and ST9010.T9_CODBEM = STC.TC_COMPONE) then 'CM ATRELADO'
+        else
+        case when STC.TC_COMPONE = (select ST9010.T9_CODBEM from ST9010 (nolock) where ST9010.D_E_L_E_T_ = '' and ST9010.T9_CODFAMI = 'PN' and ST9010.T9_CODBEM = STC.TC_COMPONE) then 'PNEUS'
+            else
+            'OUTROS'
+        end
+    end as ATRELAMENTO,    
+    isnull(trim(TQT.TQT_DESMED), 'SR') as MEDIDA,
+    ST9.T9_CODFAMI as FAMILIA
+
+from ST9010 ST9 (nolock)
+    left join STC010 STC (nolock)
+        on ST9.D_E_L_E_T_ = ''
+        and ST9.T9_CODBEM = STC.TC_CODBEM
 
         left join TQS010 TQS (nolock)
             on TQS.D_E_L_E_T_ = ''
@@ -21,7 +27,4 @@ from STC010 STC (nolock)
                 on TQT.D_E_L_E_T_ = ''
                 and TQT.TQT_MEDIDA = TQS.TQS_MEDIDA
 
-    left join ST9010 COM (nolock)
-        on COM.D_E_L_E_T_ = ''
-        and COM.T9_CODBEM = STC.TC_COMPONE
 where STC.D_E_L_E_T_ = ''
