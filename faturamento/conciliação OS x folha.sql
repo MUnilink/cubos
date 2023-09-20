05e74298316d4884a478bb33d5ed1566e90e587d
Manoel <manoel.jose@unilinktransportes.com.br>
2023-09-20 14:09:19 -0300

    fortamação e eliminação de apontamentos '  :  '

--- faturamento/conciliação OS x folha.sql
+++ faturamento/conciliação OS x folha.sql
@@ -1,35 +1,38 @@
     select
-        ZC2_NUM + '-' + ZC2_ITEM as OS_ITEM,
-        RJ_DESC RH,
-        convert(date,ZC2_COMPET) as PERIODO,
-        ZC2_CHVOS,
-        NULL as VERBA,
-        ZC2_TOTAL as TOTAL_OS,
+        ZC2.ZC2_NUM + '-' + ZC2.ZC2_ITEM as OS_ITEM,
+        trim(SRJ.RJ_DESC) as RH,
+        convert(date, ZC2.ZC2_COMPET) as PERIODO,
+        ZC2.ZC2_CHVOS,
+        SRJ.RJ_FUNCAO as VERBA,
+        ZC2.ZC2_TOTAL as TOTAL_OS,
         0 TOTAL_FOLHA,
         ZC2.R_E_C_N_O_
     from ZC2010 ZC2
-    inner join SRJ010 SRJ
-        on ZC2.D_E_L_E_T_=''
-        and ZC2_COD = RJ_FUNCAO
-    where ZC2_COMPET != ''
-        and ZC2_TIPO = 2
-        and ZC2.ZC2_INCLUS = 'M'
+        inner join SRJ010 SRJ
+            on ZC2.D_E_L_E_T_=''
+            and ZC2.ZC2_COD = SRJ.RJ_FUNCAO
+        where 
+                ZC2.ZC2_COMPET != ''
+            and ZC2.ZC2_TIPO = 2
+            and ZC2.ZC2_INCLUS = 'M'
+            and ZC2.ZC2_HRINI != '  :  '
+            and ZC2.ZC2_HRFIM != '  :  '
 
 union
 
     select
         'FOLHA',
-        RV_DESC,
-        convert(date,RD_DATARQ+'01'),
-        RD_YCHVOS as ZC2_CHVOS,
-        RD_PD as VERBA,
+        (SRV.RV_DESC) as RH,
+        convert(date, SRD.RD_DATARQ+'01'),
+        SRD.RD_YCHVOS as ZC2_CHVOS,
+        SRD.RD_PD as VERBA,
         0 TOTAL_OS,
-        RD_VALOR as TOTAL_FOLHA,
+        SRD.RD_VALOR as TOTAL_FOLHA,
         SRD.R_E_C_N_O_
-    from SRD010 SRD 
-    inner join SRV010 SRV
-        on SRV.D_E_L_E_T_ = ''
-        and SRD.RD_PD = SRV.RV_COD
+    from SRD010 SRD
+        inner join SRV010 SRV
+            on SRV.D_E_L_E_T_ = ''
+            and SRD.RD_PD = SRV.RV_COD
     where
             SRD.RD_YCHVOS != ''
         and SRD.D_E_L_E_T_ = ''
