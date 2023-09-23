0ea53b5b1fdf95aa101580654af93babee2403d0
Manoel <manoel.jose@unilinktransportes.com.br>
2023-09-23 10:12:36 -0300

    tabela fato análise técnica do pneu

--- manutenção/FA - análise pneu.sql
+++ manutenção/FA - análise pneu.sql
@@ -1,20 +1,19 @@
 select
 	TQS.TQS_CODBEM as TQS_CODBEM,
-	TQS.TQS_MEDIDA as MEDIDA,
-	case ST9.T9_SITBEM when 'A' then 'ATIVO' when 'I' then 'INATIVO' else 'OUTROS' end as SITUACAO,
+	TQS.TQS_MEDIDA,
+    trim(TQT.TQT_DESMED) as MEDIDA,
     TQZ.TQZ_STATUS as STATUS,
 	TQZ.TQZ_PRODUT as PRODUTO,
     TQZ.TQZ_ALMOX as ARMAZEM,
 
-	TQS.TQS_KMR1,
-	TQS.TQS_KMR2,
-	TQS.TQS_KMR3,
-	TQS.TQS_KMR4,
-	TQS.TQS_KMR5,
-	TQS.TQS_KMR6,
-	TQS.TQS_KMR7,
-	TQS.TQS_KMOR,
-	TQS.TQS_KMOR + TQS.TQS_KMR1 + TQS.TQS_KMR2 + TQS.TQS_KMR3 + TQS.TQS_KMR4 + TQS.TQS_KMR5 + TQS.TQS_KMR6 + TQS.TQS_KMR7 as kmTOT,
+    TR4.TR4_NUMANA as NUMANA,
+    TR4.TR4_DESTIN as DESTINO,
+    trim(ST8.T8_NOME) as MOTIVO,
+    TR4.TR4_MOTIVO,
+    TR4.TR4_SULCO as SULCO,
+    TR4.TR4_PAREC as PARECER,
+
+    concat(TR4.TR4_DTANAL, ' ', TR4.TR4_HRANAL) as DATA,
 
     1 as QTD_ANALISE
 
@@ -26,11 +25,17 @@ from TR4010 TR4 (nolock)
 		inner join ST9010 ST9 (nolock)
 			on ST9.D_E_L_E_T_ = ''
 			and ST9.T9_CODBEM = TQS.TQS_CODBEM
+        inner join TQT010 TQT (nolock)
+            on TQT.D_E_L_E_T_ = ''
+            and TQT.TQT_MEDIDA = TQS.TQS_MEDIDA
     
     inner join TQZ010 TQZ (nolock)
         on TQZ.D_E_L_E_T_ = ''
         and TQZ.TQZ_CODBEM = TR4.TR4_CODBEM
         and TQZ.TQZ_DTSTAT = TR4.TR4_DTANAL
         and TQZ.TQZ_HRSTAT = TR4.TR4_HRANAL
+    inner join ST8010 ST8 (nolock)
+        on ST8.D_E_L_E_T_ = ''
+        and ST8.T8_CODOCOR = TR4.TR4_MOTIVO
 where
 		TR4.D_E_L_E_T_ = ''
