bebc2a78a5666d1b107336769557b0b5688c4435
Manoel <manoel.jose@unilinktransportes.com.br>
2023-09-26 10:36:42 -0300

    formato e adição de campo apenas com OS

--- faturamento/conciliação OS x estoque.sql
+++ faturamento/conciliação OS x estoque.sql
@@ -1,12 +1,13 @@
     SELECT
         ZC2.ZC2_NUM + '-' + ZC2.ZC2_ITEM as OS_ITEM,
+        ZC2.ZC2_NUM as OS,
         ZC2.ZC2_COD as PRODUTO,
         ZC2.ZC2_DESC as DESCRICAO,
         CONVERT(date, ZC2.ZC2_COMPET) as PERIODO,
         ZC2.ZC2_CHVOS,
         ZC2.ZC2_TOTAL as TOTAL_OS,
         0 as TOTAL_ESTOQUE,
-        R_E_C_N_O_
+        ZC2.R_E_C_N_O_
     FROM ZC2010 ZC2
     WHERE
             ZC2.D_E_L_E_T_ = ''
@@ -17,8 +18,9 @@ union
 
     SELECT
         'ESTOQUE',
+        SD3.D3_YOS as OS,
         SD3.D3_COD,
-        SB1.B1_DESC as DESCICAO,
+        SB1.B1_DESC as DESCRICAO,
         CONVERT(date, SD3.D3_EMISSAO),
         SD3.D3_YCHVOS as ZC2_CHVOS,
         0 as TOTAL_OS,
