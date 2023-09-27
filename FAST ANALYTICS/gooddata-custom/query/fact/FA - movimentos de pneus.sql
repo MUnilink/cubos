4c623a7e18b0b494026c6b6b279750507f1176b2
Manoel <manoel.jose@unilinktransportes.com.br>
2023-09-27 15:04:06 -0300

    movimentação a partir da OS de rodados

--- FAST ANALYTICS/gooddata-custom/query/fact/FA - movimentos de pneus.sql
+++ FAST ANALYTICS/gooddata-custom/query/fact/FA - movimentos de pneus.sql
@@ -6,49 +6,48 @@ select
 	SB1.B1_COD as PRODUTO,
     PNEU.T9_LOCPAD,
 	TQS.TQS_MEDIDA,
-    trim(TQT.TQT_DESMED) as MEDIDA,
-	PNEU.T9_MOVIBEM as MOVIMENTA_BEM,
-
 	PNEU.T9_STATUS,
-    trim(TQY.TQY_DESTAT) as STATUS_PNEU,
-
-    STZ.TZ_CONTSAI - STZ.TZ_POSCONT as km,
-
+	trim(isnull(STZ.TZ_TIPOMOV, '-')) as TZ_TIPOMOV,
+    
+	STZ.TZ_CONTSAI - STZ.TZ_POSCONT as km,
 	STZ.TZ_POSCONT,
 	STZ.TZ_CONTSAI,
-	convert(datetime, concat(STZ.TZ_DATAMOV, ' ', STZ.TZ_HORAENT), 103) as TZ_DATAMOV,
-	convert(datetime, concat(STZ.TZ_DATASAI, ' ', STZ.TZ_HORASAI), 103) as TZ_DATASAI,
-	datediff(minute, concat(STZ.TZ_DATAMOV, ' ', STZ.TZ_HORAENT), concat(STZ.TZ_DATASAI, ' ', STZ.TZ_HORASAI))/(60*24) as TEMPO_RODADO,
-	trim(isnull(STZ.TZ_TIPOMOV, '-')) as TZ_TIPOMOV,
-	trim(isnull(STZ.TZ_HORAENT, '-')) as TZ_HORAENT,
-	trim(isnull(STZ.TZ_HORASAI, '-')) as TZ_HORASAI,
+	concat(STZ.TZ_DATAMOV, ' ', STZ.TZ_HORAENT) as TZ_DATAMOV,
+	concat(STZ.TZ_DATASAI, ' ', STZ.TZ_HORASAI) as TZ_DATASAI,
+	datediff(minute, concat(STZ.TZ_DATAMOV, ' ', STZ.TZ_HORAENT), concat(STZ.TZ_DATASAI, ' ', STZ.TZ_HORASAI))/(60*24) as TEMPO_RODADO
 
-from STZ010 STZ (nolock)			
-	inner join TQS010 TQS (nolock)
-		on TQS.D_E_L_E_T_ = ''
-		and TQS.TQS_CODBEM = STZ.TZ_CODBEM
-
-		inner join ST9010 PNEU (nolock)
-			on PNEU.D_E_L_E_T_ = ''
-			and PNEU.T9_CODBEM = TQS.TQS_CODBEM
-			and trim(PNEU.T9_CODBEM) like '[0-9]%'
-
-			inner join TQY010 TQY (nolock)
-				on TQY.D_E_L_E_T_ = ''
-				and TQY.TQY_STATUS = PNEU.T9_STATUS
+from STL010 STL (nolock)
+	left join STJ010 STJ (nolock)
+		on STJ.D_E_L_E_T_ = ''
+		and STJ.TJ_FILIAL = STL.TL_FILIAL
+		and STJ.TJ_ORDEM = STL.TL_ORDEM
+		and STJ.TJ_PLANO = STL.TL_PLANO
 		
-		inner join TQT010 TQT (nolock)
-            on TQT.D_E_L_E_T_ = ''
-            and TQT.TQT_MEDIDA = TQS.TQS_MEDIDA
-			
-			left join SB1010 SB1 (nolock)
-				on SB1.D_E_L_E_T_ = ''
-				and SB1.B1_XMEDIDA = TQT.TQT_MEDIDA
+		left join STZ010 STZ (nolock)
+			on STZ.D_E_L_E_T_ = ''
+			and STZ.TZ_ORDEM = STJ.TJ_ORDEM
+			and STZ.TZ_PLANO = STJ.TJ_PLANO
+			and STZ.TZ_BEMPAI = STJ.TJ_CODBEM
 
+			inner join TQS010 TQS (nolock)
+				on TQS.D_E_L_E_T_ = ''
+				and TQS.TQS_CODBEM = STZ.TZ_CODBEM
 
-	inner join ST9010 CARRO (nolock)
-		on CARRO.D_E_L_E_T_ = ''
-		and CARRO.T9_CODBEM = STZ.TZ_BEMPAI
-		and trim(CARRO.T9_CODBEM) not like '[0-9]%'
+				inner join ST9010 PNEU (nolock)
+					on PNEU.D_E_L_E_T_ = ''
+					and PNEU.T9_CODBEM = TQS.TQS_CODBEM
+					and PNEU.T9_CATBEM = 3
+				inner join TQT010 TQT (nolock)
+					on TQT.D_E_L_E_T_ = ''
+					and TQT.TQT_MEDIDA = TQS.TQS_MEDIDA
+					
+					left join SB1010 SB1 (nolock)
+						on SB1.D_E_L_E_T_ = ''
+						and SB1.B1_XMEDIDA = TQT.TQT_MEDIDA
+
+			inner join ST9010 CARRO (nolock)
+				on CARRO.D_E_L_E_T_ = ''
+				and CARRO.T9_CODBEM = STZ.TZ_BEMPAI
+				and CARRO.T9_CATBEM in (1, 4)
 where
-		STZ.D_E_L_E_T_ = ''
+		STL.D_E_L_E_T_ = ''
