8d9c16d7f6c4ee2f67e67c546f6c5a553be149c9
Manoel <manoel.jose@unilinktransportes.com.br>
2023-09-29 16:38:46 -0300

    reajuste NOLOCK e pendente TQZ para vínculo MNT e EST

--- manutenção/FA - movimentos de pneus.sql
+++ manutenção/FA - movimentos de pneus.sql
@@ -22,40 +22,43 @@ select
 	STL.TL_QUANTID,
 	STL.TL_CUSTO
 
-from STL010 STL
-	left join STJ010 STJ
+from STL010 STL (nolock)
+	left join STJ010 STJ (nolock)
 		on STJ.D_E_L_E_T_ = ''
 		and STJ.TJ_FILIAL = STL.TL_FILIAL
 		and STJ.TJ_ORDEM = STL.TL_ORDEM
 		and STJ.TJ_PLANO = STL.TL_PLANO
-
-		left join TQZ010 TQZ
 		
-			left join STZ010 STZ
-				on STZ.D_E_L_E_T_ = ''
-				and STZ.TZ_ORDEM = STJ.TJ_ORDEM
-				and STZ.TZ_PLANO = STJ.TJ_PLANO
-				and STZ.TZ_BEMPAI = STJ.TJ_CODBEM
+		left join STZ010 STZ (nolock)
+			on STZ.D_E_L_E_T_ = ''
+			and STZ.TZ_ORDEM = STJ.TJ_ORDEM
+			and STZ.TZ_PLANO = STJ.TJ_PLANO
+			and STZ.TZ_BEMPAI = STJ.TJ_CODBEM
 
-				inner join TQS010 TQS
+			left join TQZ010 TQZ (nolock)
+				on TQZ.D_E_L_E_T_ = ''
+				and TQZ.TQZ_CODBEM = STZ.TZ_CODBEM
+
+				inner join TQS010 TQS (nolock)
 					on TQS.D_E_L_E_T_ = ''
 					and TQS.TQS_CODBEM = STZ.TZ_CODBEM
 
-					inner join ST9010 PNEU
+					inner join ST9010 PNEU (nolock)
 						on PNEU.D_E_L_E_T_ = ''
 						and PNEU.T9_CODBEM = TQS.TQS_CODBEM
 						and PNEU.T9_CATBEM = 3
-					inner join TQT010 TQT
+					inner join TQT010 TQT (nolock)
 						on TQT.D_E_L_E_T_ = ''
 						and TQT.TQT_MEDIDA = TQS.TQS_MEDIDA
 						
-						left join SB1010 SB1
+						left join SB1010 SB1 (nolock)
 							on SB1.D_E_L_E_T_ = ''
 							and SB1.B1_XMEDIDA = TQT.TQT_MEDIDA
 
-				inner join ST9010 CARRO
+				inner join ST9010 CARRO (nolock)
 					on CARRO.D_E_L_E_T_ = ''
 					and CARRO.T9_CODBEM = STZ.TZ_BEMPAI
 					and CARRO.T9_CATBEM in (1, 2, 4)
+
 where
 		STL.D_E_L_E_T_ = ''
