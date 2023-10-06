16f082a818f99ceb4f32962edf81e437d66247cd
Manoel <manoel.jose@unilinktransportes.com.br>
2023-10-06 11:36:31 -0300

    CC anterior

--- manutenção/histórico de CC bens MNT.sql
+++ manutenção/histórico de CC bens MNT.sql
@@ -1,10 +1,11 @@
 select
     substring(TPN.TPN_DTINIC, 1, 6) as PERIODO,
-    TPN.TPN_CCUSTO as CC,
-    TPN.TPN_CODBEM as BEM,
+    trim(TPN.TPN_CCUSTO) as CC,
+    lag(TPN.TPN_CCUSTO) over(partition by TPN.TPN_CODBEM order by TPN.R_E_C_N_O_) as CC_ANT,
+    trim(TPN.TPN_CODBEM) as BEM,
     convert(datetime, concat(TPN.TPN_DTINIC, ' ', TPN.TPN_HRINIC), 113) as DATA_MOV,
-    lag(convert(datetime, concat(TPN.TPN_DTINIC, ' ', TPN.TPN_HRINIC), 113)) over(partition by TPN.TPN_CODBEM order by TPN.R_E_C_N_O_) as DATA_ANT,
-    cast(datediff(minute, lag(concat(TPN.TPN_DTINIC, ' ', TPN.TPN_HRINIC), 1, null) over(partition by TPN.TPN_CODBEM order by TPN.R_E_C_N_O_), concat(TPN.TPN_DTINIC, ' ', TPN.TPN_HRINIC))/(60*24.0) as numeric(15,2)) as TEMPO_MOV
+    convert(datetime, isnull(lag(concat(TPN.TPN_DTINIC, ' ', TPN.TPN_HRINIC), 1, null) over(partition by TPN.TPN_CODBEM order by TPN.R_E_C_N_O_), '20201231 00:00'), 113) as DATA_ANT,
+    cast(datediff(minute, isnull(lag(concat(TPN.TPN_DTINIC, ' ', TPN.TPN_HRINIC), 1, null) over(partition by TPN.TPN_CODBEM order by TPN.R_E_C_N_O_), '20201231 00:00'), concat(TPN.TPN_DTINIC, ' ', TPN.TPN_HRINIC))/(60*24.0) as numeric(15,2)) as TEMPO_ANT
     
 from TPN010 TPN (nolock)
     inner join ST9010 ST9 (nolock)
