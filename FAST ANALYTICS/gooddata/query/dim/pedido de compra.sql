747428aaba01f0a2dd38a5825096c0a55f80375b
stefsonsampaio <stefsonsampaio01@gmail.com>
2023-10-10 10:20:40 -0300

    fix: eliminando campo memo da consulta de pedido de compra. Esse campo continha quebra de linhas, e isso estava quebrando o ETL

--- FAST ANALYTICS/gooddata/query/dim/pedido de compra.sql
+++ FAST ANALYTICS/gooddata/query/dim/pedido de compra.sql
@@ -2,8 +2,7 @@ select
     concat(trim(SC7.C7_FILIAL), trim(SC7.C7_NUM)) as ID_PEDIDO,
 	trim(SC7.C7_NUM) as NUM_PC,
 	trim(SC7.C7_ITEM) as ITEM_PC,
-	trim(SC7.C7_OBS) as OBS_PC,
-	trim(SC7.C7_OBSM) as MEMO_PC
+	trim(SC7.C7_OBS) as OBS_PC
 
 from SC7010 SC7
 where SC7.D_E_L_E_T_ = ''
