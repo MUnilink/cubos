36b8b5cfe45faee58bdda7ef29d2ac138e265e55
stefsonsampaio <stefsonsampaio01@gmail.com>
2023-10-11 10:12:33 -0300

    fix: o memo_pc continua dando erro. Por hora, vou removê-lo da consulta.

--- FAST ANALYTICS/gooddata/query/dim/pedido de compra.sql
+++ FAST ANALYTICS/gooddata/query/dim/pedido de compra.sql
@@ -2,8 +2,7 @@ select
     concat(trim(SC7.C7_FILIAL), trim(SC7.C7_NUM)) as ID_PEDIDO,
 	trim(SC7.C7_NUM) as NUM_PC,
 	trim(SC7.C7_ITEM) as ITEM_PC,
-	trim(SC7.C7_OBS) as OBS_PC,
-	trim(replace(SC7.C7_OBSM, Char(13) + Char(10), '')) as MEMO_PC
+	trim(SC7.C7_OBS) as OBS_PC
 
 from SC7010 SC7
 where SC7.D_E_L_E_T_ = ''
