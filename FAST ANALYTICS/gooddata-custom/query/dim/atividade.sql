5b01b93e33987512a8e3d9d3360205e88ebceb93
Manoel <manoel.jose@unilinktransportes.com.br>
2023-10-31 10:53:47 -0300

    ajustes consulta dim atividade/item contábil

--- FAST ANALYTICS/gooddata-custom/query/dim/atividade.sql
+++ FAST ANALYTICS/gooddata-custom/query/dim/atividade.sql
@@ -1,5 +1,7 @@
 select
-	trim(isnull(CTD.CTD_ITEM, '-')) as ITEM_CTA,
-	trim(isnull(CTD.CTD_DESC01, '-')) as ATIVIDADE
-from CTD010 as CTD (nolock)
-where CTD.D_E_L_E_T_ = '' and cast(CTD.CTD_ITEM as int) > 9
+	trim(CTD.CTD_ITEM) as ITEM_CTA,
+	trim(CTD.CTD_DESC01) as ATIVIDADE
+from CTD010 CTD
+where
+		CTD.D_E_L_E_T_ = ''
+	and cast(CTD.CTD_ITEM as int) > 9
