8ad733424a7ec2493be68f00385cfcdef0708396
Manoel <manoel.jose@unilinktransportes.com.br>
2023-11-23 13:41:15 -0300

    inclusão de novas verbas 13º 2023

--- RH/planilha folha conferência/planilha folha resumo 10-21 vigente.sql
+++ RH/planilha folha conferência/planilha folha resumo 10-21 vigente.sql
@@ -399,7 +399,7 @@ from
                     on substring(SRC010.RC_FILIAL, 1, 4) = SRV010.RV_FILIAL
                     and SRC010.RC_PD = SRV010.RV_COD
             where
-                    SRC010.RC_PD in ('009', '010', '011', '017', '167', '202', '290', '304', '768', '769', '770')
+                    SRC010.RC_PD in ('009', '010', '011', '017', '167', '202', '290', '304', '768', '769', '770', '772')
                 and SRC010.D_E_L_E_T_ = '' and SRV010.RV_TIPOCOD in ('1', '2', '3', '4')
                 and SRC010.RC_PERIODO = FOLHA.RC_PERIODO
                 and SRC010.RC_FILIAL = FOLHA.RC_FILIAL
