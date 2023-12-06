0708fca22d2944117aa5b3e22fb4584cd3bcceba
Manoel <manoel.jose@unilinktransportes.com.br>
2023-12-06 15:00:01 -0300

    tratamento de caracteres OBS e MEMO

--- FAST ANALYTICS/gooddata/query/dim/pedido de compra.sql
+++ FAST ANALYTICS/gooddata/query/dim/pedido de compra.sql
@@ -1,8 +1,9 @@
 select
-    concat(trim(SC7.C7_FILIAL), trim(SC7.C7_NUM)) as ID_PEDIDO,
+    concat(trim(SC7.C7_FILIAL), trim(SC7.C7_NUM), trim(SC7.C7_ITEM)) as ID_PEDIDO,
 	trim(SC7.C7_NUM) as NUM_PC,
 	trim(SC7.C7_ITEM) as ITEM_PC,
 	trim(SC7.C7_OBS) as OBS_PC,
+	trim(replace(replace(replace(replace(replace(replace(string_escape(SC7.C7_OBSM, 'json'), '\n', ' '), '\r', ' '), '\"', '<<'), '\\', '\'), '\/', '/'), '\u0000', '')) as MEMO_PC,
 	trim(SC7.C7_RESIDUO) as RESIDUO_PC
 
 from SC7010 SC7
