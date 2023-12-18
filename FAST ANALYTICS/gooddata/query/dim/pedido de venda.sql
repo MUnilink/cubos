e29d2bafbc018e98c052024f53b06639ee293c83
Manoel <manoel.jose@unilinktransportes.com.br>
2023-12-18 14:55:36 -0300

    cadastro de pedidos de venda (cabeçalhos)

--- FAST ANALYTICS/gooddata/query/dim/pedido de venda.sql
+++ FAST ANALYTICS/gooddata/query/dim/pedido de venda.sql
@@ -1,5 +1,6 @@
 select
     concat(trim(SC5.C5_FILIAL), trim(SC5.C5_NUM)) as ID_PEDIDO,
-	trim(SC5.C5_NUM) as NUM_PV
+	trim(SC5.C5_NUM) as NUM_PV,
+    trim(SC5.C5_MENNOTA) as OBS
 from SC5010 SC5
 where SC5.D_E_L_E_T_ = ''
