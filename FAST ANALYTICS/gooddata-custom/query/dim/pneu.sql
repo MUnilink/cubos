fd5ceaeae18c3ea588117f359ca5cc7c0f009339
Manoel <manoel.jose@unilinktransportes.com.br>
2023-12-20 14:55:02 -0300

    alterações para modelo dim pneu

--- FAST ANALYTICS/gooddata-custom/query/dim/pneu.sql
+++ FAST ANALYTICS/gooddata-custom/query/dim/pneu.sql
@@ -1,21 +1,11 @@
 select
-    ST9.T9_FILIAL as FILIAL,
-	trim(isnull(ST9.T9_CODBEM, '-')) as T9_CODBEM,
-	trim(isnull(ST9.T9_DTCOMPR, '-')) as T9_DTCOMPR,
-	trim(isnull(ST9.T9_NFCOMPR, '-')) as T9_NFCOMPR,
-
-	case ST9.T9_CATBEM
-		when 1 then 'BEM'
-		when 2 then 'FROTA NAO INTEGRADA'
-        when 3 then 'PNEU'
-		when 4 then 'FROTA INTEGRADA'
-		else '-'
-	end as T9_CATBEM,
-
-	case ST9.T9_SITBEM when 'A' then 'ATIVO' when 'I' then 'INATIVO' else 'OUTROS' end as T9_SITBEM
+    concat(trim(ST9.T9_FILIAL), trim(ST9.T9_CODBEM)) as ID_PNEU,
+	trim(ST9.T9_CODBEM) as PNEU_FERRO,
+	trim(isnull(ST9.T9_NFCOMPR, '-')) as NF_COMPRA,
+	case ST9.T9_SITBEM when 'A' then 'ATIVO' when 'I' then 'INATIVO' else 'OUTROS' end as SITUACAO_PNEU
 
 from ST9010 ST9
-    inner join 
+    inner join TQS010 TQS
 		on TQS.D_E_L_E_T_ = ''
 		and TQS.TQS_CODBEM = ST9.T9_CODBEM
 where
