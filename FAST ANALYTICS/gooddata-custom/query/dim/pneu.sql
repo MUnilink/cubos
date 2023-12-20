9e57222668163771441a1d35bed7808e7d7d1e5e
Manoel <manoel.jose@unilinktransportes.com.br>
2023-12-20 14:44:41 -0300

    separação de dimensões bens e pneus

--- FAST ANALYTICS/gooddata-custom/query/dim/pneu.sql
+++ FAST ANALYTICS/gooddata-custom/query/dim/pneu.sql
@@ -1,16 +1,13 @@
 select
+    ST9.T9_FILIAL as FILIAL,
 	trim(isnull(ST9.T9_CODBEM, '-')) as T9_CODBEM,
-	trim(isnull(ST9.T9_NOME, '-')) as T9_NOME,
 	trim(isnull(ST9.T9_DTCOMPR, '-')) as T9_DTCOMPR,
-	trim(isnull(ST9.T9_PLACA, '-')) as T9_PLACA,
 	trim(isnull(ST9.T9_NFCOMPR, '-')) as T9_NFCOMPR,
-	trim(isnull(ST9.T9_TIPMOD, '-')) as T9_TIPMOD,
-	trim(isnull(ST9.T9_CODFAMI, '-')) as T9_CODFAMI,
 
 	case ST9.T9_CATBEM
 		when 1 then 'BEM'
 		when 2 then 'FROTA NAO INTEGRADA'
-		when 3 then 'PNEU'
+        when 3 then 'PNEU'
 		when 4 then 'FROTA INTEGRADA'
 		else '-'
 	end as T9_CATBEM,
@@ -18,6 +15,9 @@ select
 	case ST9.T9_SITBEM when 'A' then 'ATIVO' when 'I' then 'INATIVO' else 'OUTROS' end as T9_SITBEM
 
 from ST9010 ST9
+    inner join 
+		on TQS.D_E_L_E_T_ = ''
+		and TQS.TQS_CODBEM = ST9.T9_CODBEM
 where
 		ST9.D_E_L_E_T_ = ''
-	and ST9.T9_CATBEM != 3
+	and ST9.T9_CATBEM = 3
