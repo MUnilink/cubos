762c2d7ea1b25d6e96c327533516397e952e4671
Manoel <manoel.jose@unilinktransportes.com.br>
2024-01-02 13:36:57 -0300

    erro no km ini, pois ZB1 vê dois apontamentos DTW

--- logística/tratamento de km autotrac ausente.sql
+++ logística/tratamento de km autotrac ausente.sql
@@ -24,7 +24,33 @@ select DTQ.DTQ_FILORI, DTQ.DTQ_VIAGEM,
             and APT.DTW_FILORI = DTR.DTR_FILORI
             and APT.DTW_VIAGEM = DTR.DTR_VIAGEM
             and APT.DTW_ATIVID = 50
-    ) as km_fim_atual,
+    ) as km_fim,
+    (
+        select
+            isnull
+            (
+                (
+                    select nullif(substring(ZB1010.ZB1_MSGTXT, 2, len(ZB1010.ZB1_MSGTXT)), '')
+                    from ZB1010 (nolock)
+                    where
+                            ZB1010.D_E_L_E_T_ = ''
+                        and ZB1010.ZB1_STATUS = 'OK'
+                        and
+                            dateadd(hour, -3, datetimefromparts(substring(ZB1010.ZB1_MSGTIM, 1, 4), substring(ZB1010.ZB1_MSGTIM, 6, 2), substring(ZB1010.ZB1_MSGTIM, 9, 2), substring(ZB1010.ZB1_MSGTIM, 12, 2), substring(ZB1010.ZB1_MSGTIM, 15, 2), 0, 0))
+                            =
+                            datetimefromparts(year(APT.DTW_DATREA), month(APT.DTW_DATREA), day(APT.DTW_DATREA), substring(APT.DTW_HORREA, 1, 2), substring(APT.DTW_HORREA, 3, 4), 0, 0)
+                        and ZB1010.ZB1_MSGTXT like '&_%' escape '&'
+                        and ZB1010.ZB1_MACRON = 1
+                        and ZB1010.ZB1_CODDA3 = DTR.DTR_CODVEI
+                )
+            , APT.DTW_YHODIN)
+        from DTW010 APT (nolock)
+        where
+                APT.D_E_L_E_T_ = ''
+            and APT.DTW_FILORI = DTR.DTR_FILORI
+            and APT.DTW_VIAGEM = DTR.DTR_VIAGEM
+            and APT.DTW_ATIVID = 49
+    ) as km_ini,
 
     case DTQ.DTQ_STATUS
         when '1' then 'EXCLUÍDA'
