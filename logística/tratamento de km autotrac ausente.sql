97041b3047c47c7e3c35d459805953c13b4b9820
Manoel <manoel.jose@unilinktransportes.com.br>
2024-01-02 11:47:38 -0300

    confirmar que o tratamento funcionou

--- logística/tratamento de km autotrac ausente.sql
+++ logística/tratamento de km autotrac ausente.sql
@@ -1,25 +1,29 @@
 select DTQ.DTQ_FILORI, DTQ.DTQ_VIAGEM,
     (
         select
-        (
-            select substring(ZB1010.ZB1_MSGTXT, 2, len(ZB1010.ZB1_MSGTXT))
-            from ZB1010 (nolock)
-            where
-                    ZB1010.D_E_L_E_T_ = ''
-                and
-                    dateadd(hour, -3, datetimefromparts(substring(ZB1010.ZB1_MSGTIM, 1, 4), substring(ZB1010.ZB1_MSGTIM, 6, 2), substring(ZB1010.ZB1_MSGTIM, 9, 2), substring(ZB1010.ZB1_MSGTIM, 12, 2), substring(ZB1010.ZB1_MSGTIM, 15, 2), 0, 0))
-                    =
-                    datetimefromparts(year(DTW.DTW_DATREA), month(DTW.DTW_DATREA), day(DTW.DTW_DATREA), substring(DTW.DTW_HORREA, 1, 2), substring(DTW.DTW_HORREA, 3, 4), 0, 0)
-                and ZB1010.ZB1_MSGTXT like '&_%' escape '&'
-                and ZB1010.ZB1_MACRON = 7
-                and ZB1010.ZB1_CODDA3 = DTR.DTR_CODVEI
-        )
-        from DTW010 DTW (nolock)
+            isnull
+            (
+                (
+                    select nullif(substring(ZB1010.ZB1_MSGTXT, 2, len(ZB1010.ZB1_MSGTXT)), '')
+                    from ZB1010 (nolock)
+                    where
+                            ZB1010.D_E_L_E_T_ = ''
+                        and ZB1010.ZB1_STATUS = 'OK'
+                        and
+                            dateadd(hour, -3, datetimefromparts(substring(ZB1010.ZB1_MSGTIM, 1, 4), substring(ZB1010.ZB1_MSGTIM, 6, 2), substring(ZB1010.ZB1_MSGTIM, 9, 2), substring(ZB1010.ZB1_MSGTIM, 12, 2), substring(ZB1010.ZB1_MSGTIM, 15, 2), 0, 0))
+                            =
+                            datetimefromparts(year(APT.DTW_DATREA), month(APT.DTW_DATREA), day(APT.DTW_DATREA), substring(APT.DTW_HORREA, 1, 2), substring(APT.DTW_HORREA, 3, 4), 0, 0)
+                        and ZB1010.ZB1_MSGTXT like '&_%' escape '&'
+                        and ZB1010.ZB1_MACRON = 7
+                        and ZB1010.ZB1_CODDA3 = DTR.DTR_CODVEI
+                )
+            , APT.DTW_YHODFI)
+        from DTW010 APT (nolock)
         where
-                DTW.D_E_L_E_T_ = ''
-            and DTW.DTW_FILORI = DTR.DTR_FILORI
-            and DTW.DTW_VIAGEM = DTR.DTR_VIAGEM
-            and DTW.DTW_ATIVID = 50
+                APT.D_E_L_E_T_ = ''
+            and APT.DTW_FILORI = DTR.DTR_FILORI
+            and APT.DTW_VIAGEM = DTR.DTR_VIAGEM
+            and APT.DTW_ATIVID = 50
     ) as km_fim_atual,
 
     case DTQ.DTQ_STATUS
