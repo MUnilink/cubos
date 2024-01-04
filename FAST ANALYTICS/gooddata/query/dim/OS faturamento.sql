bc74ab82c9700b6598c3f835813c2518dc3d129e
Manoel <manoel.jose@unilinktransportes.com.br>
2024-01-04 11:47:00 -0300

    consulta dim das OS portuárias para projeto FA-BO

--- FAST ANALYTICS/gooddata/query/dim/OS faturamento.sql
+++ FAST ANALYTICS/gooddata/query/dim/OS faturamento.sql
@@ -1,6 +1,7 @@
 select
     concat(trim(ZC1.ZC1_FILIAL), trim(ZC1.ZC1_NUM)) as ID_OS,
-    substring(ZC1.ZC1_NUM, 6, 10) as OS,
+    cast(substring(ZC1.ZC1_NUM, 6, 10) as int) as OS,
+    trim(ZC1.ZC1_VIAGEM) as VIAGEM_PORT,
     (select trim(ZA3010.ZA3_DESC) from ZA3010 where ZA3010.D_E_L_E_T_ = '' and ZA3010.ZA3_COD = ZC1.ZC1_NAVIO) as DESC_NAVIO,
     (select trim(SX5010.X5_DESCRI) from SX5010 where SX5010.D_E_L_E_T_ = '' and SX5010.X5_TABELA = '_1' and SX5010.X5_CHAVE = ZC1.ZC1_PORTO) as DESC_PORTO,
     
@@ -9,5 +10,8 @@ select
         when 6 then 'FECHADA'
         else 'OUTROS'
     end as STATUS_OS
+
 from ZC1010 ZC1
-where ZC1.D_E_L_E_T_ = ''
+where
+        ZC1.D_E_L_E_T_ = ''
+    and year(ZC1.ZC1_EMISSA) > 2022
