1ed00828070190dd331e5dc8962668da4ea050af
Manoel <manoel.jose@unilinktransportes.com.br>
2024-01-11 13:34:33 -0300

    add refs. a pedido e NF de saída; ver refs. itens

--- FAST ANALYTICS/gooddata/query/fact/itens das OS portuária.sql
+++ FAST ANALYTICS/gooddata/query/fact/itens das OS portuária.sql
@@ -1,36 +1,16 @@
 select
-    ZC1.ZC1_FILIAL as FILIAL,
-    ZC1.ZC1_NUM as NUM_OS,
-    cast(substring(ZC1.ZC1_NUM, 6, 10) as int) as OS,
-    substring(ZC1.ZC1_NUM, 1, 4) as ANO_OS,
-    substring(ZC1.ZC1_EMISSA, 1, 6) as PERIODO_OS,
-    convert(date, ZC1.ZC1_EMISSA, 103) as DATA_OS,
-    
-    ZC1.ZC1_PORTO as PORTO,
-    (select trim(SX5010.X5_DESCRI) from SX5010 where SX5010.D_E_L_E_T_ = '' and SX5010.X5_TABELA = '_1' and SX5010.X5_CHAVE = ZC1.ZC1_PORTO) as DESC_PORTO,
-    ZC1.ZC1_NAVIO as NAVIO,
-    (select trim(ZA3010.ZA3_DESC) from ZA3010 where ZA3010.D_E_L_E_T_ = '' and ZA3010.ZA3_COD = ZC1.ZC1_NAVIO) as DESC_NAVIO,
-    trim(ZC1.ZC1_VIAGEM) as VIAGEM_PORT,
+    concat(trim(ZC1.ZC1_FILIAL), trim(ZC1.ZC1_NUM)) as ID_OS,
+    'P |01|SA1010|'+ COALESCE(NULLIF(RTRIM(COALESCE(DEV.A1_FILIAL, ' '))+'|'+RTRIM(COALESCE(DEV.A1_COD, ' '))+RTRIM(COALESCE(DEV.A1_LOJA, ' ')), ' '), '|') as BK_CLIENTE,
+    concat(trim(SC5.C5_FILIAL), trim(SC5.C5_NUM)) as BK_PEDIDODEVENDA,
+    concat('SF2', trim(SF2.F2_FILIAL), trim(SF2.F2_CLIENTE), trim(SF2.F2_LOJA), trim(SF2.F2_DOC), trim(SF2.F2_SERIE)) as ID_NF,
 
-    DEV.A1_COD as CLI_CODIGO,
-    DEV.A1_LOJA as CLI_LOJA,
-    DEV.A1_CGC as CLI_CNPJ,
-    trim(DEV.A1_NOME) as CLIENTE,
-
-    ARM.A1_COD as ARM_CODIGO,
-    ARM.A1_LOJA as ARM_LOJA,
-    ARM.A1_CGC as ARM_CNPJ,
-    trim(DEV.A1_NOME) as ARMADORA,
-
-    DES.A2_COD as DESP_CODIGO,
-    DES.A2_LOJA as DESP_LOJA,
-    DES.A2_CGC as DESP_CNPJ,
-    trim(DES.A2_NOME) as DESPACHANTE,
+    ZC1.ZC1_EMISSA as DATA_OS,    
+    /*concat(ARM.A1_COD, ARM.A1_LOJA) as ARMADORA,*/
+    concat(DES.A2_COD, DES.A2_LOJA) as DESPACHANTE,
+    ZC1.ZC1_TABPRC as TABELA_PRECO,
     
     ZC2.ZC2_ITEM as ITEM,
     ZC2.ZC2_INCLUS as TIPO_INCLUSAO,
-    ZC1.ZC1_TABPRC as TABELADEPRECO,
-    (select trim(DA0010.DA0_DESCRI) from DA0010 where DA0010.D_E_L_E_T_ = '' and DA0010.DA0_CODTAB = ZC1.ZC1_TABPRC) as TABELA_PRECO,
     
     case ZC2.ZC2_TIPO
         when 1 then 'RECEITA'
@@ -44,10 +24,10 @@ select
 
     trim(ZC2.ZC2_COD) as INSUMO,
     case ZC2.ZC2_TIPO
-        when 1 then (select case when SB1010.B1_DESC like 'TRANSPORTE PORTUARIO - %' then replace(SB1010.B1_DESC, 'TRANSPORTE PORTUARIO - ', '') else trim(SB1010.B1_DESC) end from DA1010 (nolock) inner join SB1010 (nolock) on SB1010.D_E_L_E_T_ = '' and SB1010.B1_COD = DA1010.DA1_CODPRO where DA1010.D_E_L_E_T_ = '' and DA1010.DA1_CODTAB = ZC1.ZC1_TABPRC and trim(SB1010.B1_COD) = trim(ZC2.ZC2_COD) and ZC2.ZC2_TIPO = 1)
-        when 2 then (select trim(SRJ010.RJ_DESC) from SRJ010 (nolock) where SRJ010.D_E_L_E_T_ = '' and SRJ010.RJ_FUNCAO = trim(ZC2.ZC2_COD) and ZC2.ZC2_TIPO = 2)
+        when 1 then (select case when SB1010.B1_DESC like 'TRANSPORTE PORTUARIO - %' then replace(SB1010.B1_DESC, 'TRANSPORTE PORTUARIO - ', '') else trim(SB1010.B1_DESC) end from DA1010 inner join SB1010 on SB1010.D_E_L_E_T_ = '' and SB1010.B1_COD = DA1010.DA1_CODPRO where DA1010.D_E_L_E_T_ = '' and DA1010.DA1_CODTAB = ZC1.ZC1_TABPRC and trim(SB1010.B1_COD) = trim(ZC2.ZC2_COD) and ZC2.ZC2_TIPO = 1)
+        when 2 then (select trim(SRJ010.RJ_DESC) from SRJ010 where SRJ010.D_E_L_E_T_ = '' and SRJ010.RJ_FUNCAO = trim(ZC2.ZC2_COD) and ZC2.ZC2_TIPO = 2)
         when 3 then trim(ST9.T9_CODBEM)
-        when 4 then (select trim(SB1010.B1_DESC) from SB1010 (nolock) where SB1010.D_E_L_E_T_ = '' and trim(SB1010.B1_COD) = trim(ZC2.ZC2_COD) and ZC2.ZC2_TIPO = 4)
+        when 4 then (select trim(SB1010.B1_DESC) from SB1010 where SB1010.D_E_L_E_T_ = '' and trim(SB1010.B1_COD) = trim(ZC2.ZC2_COD) and ZC2.ZC2_TIPO = 4)
         when 6 then trim(ST9.T9_CODBEM)
         when 7 then trim(ZA7.ZA7_DESC)
         else trim(ZC2.ZC2_DESC)
@@ -62,50 +42,59 @@ select
     trim(ZC2.ZC2_CONTEI) as CONTEINER,
     trim(ZC2.ZC2_LACRE) as LACRE,
     ZC2.ZC2_MOTORI as COD_MOT,
-    DA4.DA4_NOME as MOTORISTA,
     ZC2.ZC2_VEICUL as CM,
     ZC2.ZC2_CARRET as SR,
 
-    substring(ZC2.ZC2_DTINI, 1, 6) as PERIODO_APONT,
-    ZC2.ZC2_DTINI as DATA_INIAPONT,
-    ZC2.ZC2_DTFIM as DATA_FIMAPONT,
-    convert(datetime, concat(ZC2.ZC2_DTINI, ' ', ZC2.ZC2_HRINI), 103) as DTINI_APONT,
-    convert(datetime, concat(ZC2.ZC2_DTFIM, ' ', ZC2.ZC2_HRFIM), 103) as DTFIM_APONT,
-    datediff(minute, convert(datetime, concat(ZC2.ZC2_DTINI, ' ', ZC2.ZC2_HRINI), 103), convert(datetime, concat(ZC2.ZC2_DTFIM, ' ', ZC2.ZC2_HRFIM), 103))/60.0 as HORAS_APONT,
+    concat(ZC2.ZC2_DTINI, ' ', ZC2.ZC2_HRINI) as DTINI_APONT,
+    concat(ZC2.ZC2_DTFIM, ' ', ZC2.ZC2_HRFIM) as DTFIM_APONT,
     trim(upper(ZC2.ZC2_NMUSU)) as USUARIO
 
-from ZC2010 ZC2 (nolock)
-    left join ZC1010 ZC1 (nolock)
+from ZC2010 ZC2
+    left join ZC1010 ZC1
         on ZC1.D_E_L_E_T_ = ''
         and ZC1.ZC1_FILIAL = ZC2.ZC2_FILIAL
         and ZC1.ZC1_NUM = ZC2.ZC2_NUM
         and substring(ZC1.ZC1_NUM, 1, 4) > 2022
         
-        left join SA1010 DEV (nolock)
+        left join SA1010 DEV
             on DEV.D_E_L_E_T_ = ''
             and DEV.A1_COD = ZC1.ZC1_CODSA1
             and DEV.A1_LOJA = ZC1.ZC1_LOJSA1
-        left join SA1010 ARM (nolock)
+        left join SA1010 ARM
             on ARM.D_E_L_E_T_ = ''
             and ARM.A1_COD = ZC1.ZC1_ARMADO
             and ARM.A1_LOJA = ZC1.ZC1_LJARMA
-        left join SA2010 DES (nolock)
+        left join SA2010 DES
             on DES.D_E_L_E_T_ = ''
             and DES.A2_COD = ZC1.ZC1_DESPA
-            and DES.A2_LOJA = ZC1.ZC1_LJDESP
+            and DES.A2_LOJA = ZC1.ZC1_LJDESP    
+        left join SC5010 SC5
+            on SC5.D_E_L_E_T_ = ''
+            and SC5.C5_FILIAL = ZC1.ZC1_FILIAL
+            and SC5.C5_YOS = ZC1.ZC1_NUM
 
-    left join ST9010 ST9 (nolock)
+            left join SD2010 SD2
+                on SD2.D_E_L_E_T_ = ''
+                and SD2.D2_FILIAL = SC5.C5_FILIAL
+                and SD2.D2_PEDIDO = SC5.C5_NUM
+
+                left join SF2010 SF2
+                    on SF2.D_E_L_E_T_= ' '
+                    and SF2.F2_FILIAL = SD2.D2_FILIAL
+                    and SF2.F2_CLIENTE = SD2.D2_CLIENTE
+                    and SF2.F2_LOJA = SD2.D2_LOJA
+                    and SF2.F2_DOC = SD2.D2_DOC
+                    and SF2.F2_SERIE = SD2.D2_SERIE
+
+    left join ST9010 ST9
         on ST9.D_E_L_E_T_ = ''
         and trim(ST9.T9_CODBEM) = trim(ZC2.ZC2_COD)
-    left join ZA7010 ZA7 (nolock)
+    left join ZA7010 ZA7
         on ZA7.D_E_L_E_T_ = ''
         and trim(ZA7.ZA7_COD) = trim(ZC2.ZC2_COD)
-    left join DA4010 DA4 (nolock)
+    left join DA4010 DA4
         on DA4.D_E_L_E_T_ = ''
         and DA4.DA4_COD = ZC2.ZC2_MOTORI
 where
-        ZC2.D_E_L_E_T_ = ''
-    and ZC2.ZC2_INCLUS != 'C'
-    and ZC2.ZC2_HRINI != '  :  '
-    and ZC2.ZC2_HRFIM != '  :  '
-    and year(ZC1.ZC1_EMISSA) > 2022
+        ZC2.ZC2_DTFIM between <<START_DATE>> and <<FINAL_DATE>>
+    and ZC2.D_E_L_E_T_ = ''
