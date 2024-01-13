bb9b8ccd0f80e76e9e003b8716e4c126371499a0
Manoel <manoel.jose@unilinktransportes.com.br>
2024-01-13 12:39:08 -0300

    analisar como testar

--- faturamento/custo OP - log de cálculo.sql
+++ faturamento/custo OP - log de cálculo.sql
@@ -1,10 +1,81 @@
-select ZG1.*
+select
+    ZG1.ZG1_FILORI as FILIAL_LOG,
+    ZG1.ZG1_TABELA as TABELA_LOG,
+    ZG1.ZG1_CODIGO as CODIGO_LOG,
+    ZG1.ZG1_TIPO as TIPOCOD_LOG,
+    
+    ZG1.ZG1_HRPAD as HORA_PADRAO,
+    ZG1.ZG1_VLTOTL as VALOR_TOTAL,
+    ZG1.ZG1_VLHORA as VALOR_HORA,
+    
+    ZG1.ZG1_HRPRO as HORA_PRODT,
+    ZG1.ZG1_VLPROD as VALOR_PRODT,
+    ZG1.ZG1_HRIMPR as HORA_IMPRO,
+    ZG1.ZG1_VLIMPR as VALOR_IMPRO,
+    
+    ZG1.ZG1_COMPET as PERIODO_LOG,
+    ZG1.ZG1_DTCALC as CALCULO_CUSTO,
+    ZG1.ZG1_USRCAL as USUARIO_CUSTO,
+    ZG1.ZG1_ATIVO as LOG_ATIVO,
+
+    ZC1.ZC1_FILIAL as FILIAL,
+    ZC1.ZC1_NUM as NUM_OS,
+    cast(substring(ZC1.ZC1_NUM, 6, 10) as int) as OS,
+    
+    substring(ZC2.ZC2_DTFIM, 1, 6) as PERIODO_APONT,
+    convert(datetime, concat(ZC2.ZC2_DTINI, ' ', ZC2.ZC2_HRINI), 103) as DTINI_APONT,
+    convert(datetime, concat(ZC2.ZC2_DTFIM, ' ', ZC2.ZC2_HRFIM), 103) as DTFIM_APONT,
+    datediff(minute, concat(ZC2.ZC2_DTINI, ' ', ZC2.ZC2_HRINI), concat(ZC2.ZC2_DTFIM, ' ', ZC2.ZC2_HRFIM))/60.0 as HORAS_APONT,
+
+    case ZC1.ZC1_STATUS
+        when 1 then 'ABERTA'
+        when 6 then 'FECHADA'
+        else 'OUTROS'
+    end as STATUS_OS,
+    
+    case ZC2.ZC2_TIPO
+        when 1 then 'RECEITA'
+        when 2 then 'RH'
+        when 3 then 'EQUIPAMENTO'
+        when 4 then 'MATERIAIS'
+        when 6 then 'DEPRECIAÇÃO'
+        when 7 then 'CONTABILIDADE'
+        when 8 then 'DESPESAS FINANCEIRAS'
+
+        when 13 then 'TARIFA'
+        else 'OUTROS'
+    end as TIPO_INSUMO,
+    
+    ZC2.ZC2_COD as INSUMO,
+    ZC2.ZC2_ITEM as ITEM,
+
+    SD3.D3_CUSTO1,
+    TQN.TQN_VALTOT,
+    MNT.TL_CUSTO,
+    DEP.N4_VLROC1
+
 from ZC2010 ZC2 (nolock)
+    left join ZC1010 ZC1 (nolock)
+        on ZC1.D_E_L_E_T_ = ''
+        and ZC1.ZC1_FILIAL = ZC2.ZC2_FILIAL
+        and ZC1.ZC1_NUM = ZC2.ZC2_NUM
     inner join ZG1010 ZG1 (nolock)
         on ZG1.D_E_L_E_T_ = ''
-        and ZG1.ZG1_COD = ZC2.ZC2_COD
+        and ZG1.ZG1_CODIGO = ZC2.ZC2_COD
         and ZG1.ZG1_FILORI = ZC2.ZC2_FILIAL
     
+    left join SD3010 SD3 (nolock)
+        on SD3.D_E_L_E_T_ = ''
+        and SD3.D3_FILIAL = ZC2.ZC2_FILIAL
+        and SD3.D3_YOS = ZC2.ZC2_NUM
+        and substring(SD3.D3_EMISSAO, 1, 6) = ZC2.ZC2_COMPET
+        and ZC2.ZC2_TIPO = 4
+    left join TQN010 TQN (nolock)
+        on TQN.D_E_L_E_T_ = ''
+        and TQN.TQN_FILIAL = ZC2.ZC2_FILIAL
+        and TQN.TQN_FROTA = ZC2.ZC2_COD
+        and substring(TQN.TQN_DTABAS, 1, 6) = ZC2.ZC2_COMPET
+        and ZC2.ZC2_TIPO = 3
     /*left join SRD010 SRD (nolock)*/
     left join
     (   
@@ -15,7 +86,8 @@ from ZC2010 ZC2 (nolock)
             STL010.TL_CODIGO,
             STL010.TL_SEQRELA,
             STL010.TL_CUSTO,
-            STL010.TL_INI
+            STL010.TL_DTINICI,
+            substring(STL010.TL_DTINICI, 1, 6) as PERIODO
         from STJ010 (nolock)
             left join STL010 (nolock)
                 on STL010.D_E_L_E_T_ = ''
@@ -26,25 +98,19 @@ from ZC2010 ZC2 (nolock)
     ) MNT
         on ZC2.ZC2_TIPO = 3
         and MNT.TJ_CODBEM = ZC2.ZC2_COD
-        and MNT.TL_INI = ZC2.ZC2_COMPET
-    
-    left join SD3010 SD3 (nolock)
-        on SD3.D_E_L_E_T_ = ''
-        and SD3.D3_FILIAL = ZC2.ZC2_FILIAL
-        and SD3.D3_YOS = ZC2.ZC2_NUM
-        and ZC2.ZC2_TIPO = 4
+        and MNT.PERIODO = ZC2.ZC2_COMPET
     
     left join
     (
         select
             SN1010.N1_CODBEM,
             SN4010.N4_VLROC1,
-            convert(datetime, concat(SN4.N4_DATA, ' ', SN4.N4_HORA), 113) as DATA_MOV,
+            convert(datetime, concat(SN4010.N4_DATA, ' ', SN4010.N4_HORA), 113) as DATA_MOV,
             substring(SN4010.N4_DATA, 1, 6) as PERIODO,
             trim(SN1010.N1_CBASE) as ATIVO,
             trim(SN1010.N1_DESCRIC) as DESC_ATIVO,
             trim(ST9010.T9_CODBEM) as T9_CODBEM
-        from SN4010 (nolock)            
+        from SN4010 (nolock)
             inner join SN3010 (nolock)
                 on SN3010.D_E_L_E_T_ = ''
                 and SN3010.N3_CBASE = SN4010.N4_CBASE
@@ -55,14 +121,16 @@ from ZC2010 ZC2 (nolock)
                     and SN1010.N1_CBASE = SN3010.N3_CBASE
                     and SN1010.N1_ITEM = SN3010.N3_ITEM
 
-                    left join ST9010 (nolock)
+                    inner join ST9010 (nolock)
                         on ST9010.D_E_L_E_T_ = ''
                         and ST9010.T9_CODBEM = SN1010.N1_CODBEM
         where
-                SN4.D_E_L_E_T_ = ''
-            and SN4.N4_OCORR = 6
+                SN4010.D_E_L_E_T_ = ''
+            and SN4010.N4_OCORR = 6
     ) DEP
         on ZC2.ZC2_TIPO = 6
+        and DEP.T9_CODBEM = ZC2.ZC2_COD
+        and DEP.PERIODO = ZC2.ZC2_COMPET
     /*left join CT2010 CT2 (nolock) and ZC2.ZC2_TIPO = 7*/
 where
             ZC2.D_E_L_E_T_ = ''
