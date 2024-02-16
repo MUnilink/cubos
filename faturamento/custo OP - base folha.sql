240c8142897d81ac314a1ae6de5b2bb57c9921f9
Manoel <manoel.jose@unilinktransportes.com.br>
2024-02-16 16:25:34 -0300

    conversão de horas da folha para horas trabalhadas esperadas
    verificar grande quantidade de dados para todos os meses

--- faturamento/custo OP - base folha.sql
+++ faturamento/custo OP - base folha.sql
@@ -12,18 +12,17 @@ select
 	SRD.RD_HORAS,
 	
 	ZC2.OS,
-	ZC2.HORAS_APONT,
 	ZC2.INSUMO,
-	ZC2.QTD_PREV,
 	ZC2.QTD_REAL,
-	ZC2.VAL_PREV,
 	ZC2.VAL_REAL,
+	cast(ZC2.HORAS_APONT as numeric(15, 2)) as HORAS_APONT,
 
-	case when lag(ZC2.OS, 1, 0) over (partition by SRD.RD_FILIAL, SRD.RD_PERIODO, SRD.RD_MAT, SRD.RD_PD order by SRD.R_E_C_N_O_) = 0 then SRD.RD_VALOR else 0 end as VALOR_FOLHA,
-	
 	case
-		when lag(ZC2.OS, 1, 0) over (partition by SRD.RD_FILIAL, SRD.RD_PERIODO, SRD.RD_MAT, SRD.RD_PD order by SRD.R_E_C_N_O_) = 0 and SRD.RD_PD = 20 then SRD.RD_HORAS
-		when lag(ZC2.OS, 1, 0) over (partition by SRD.RD_FILIAL, SRD.RD_PERIODO, SRD.RD_MAT, SRD.RD_PD order by SRD.R_E_C_N_O_) = 0 and SRD.RD_PD in (440, 445) then SRD.RD_HORAS*-1
+		when lag(ZC2.OS, 1, 0) over (partition by SRD.RD_FILIAL, SRD.RD_PERIODO, SRD.RD_MAT, SRD.RD_PD order by SRD.R_E_C_N_O_) = 0 and SRD.RD_PD = 20 then SRD.RD_VALOR
+		when lag(ZC2.OS, 1, 0) over (partition by SRD.RD_FILIAL, SRD.RD_PERIODO, SRD.RD_MAT, SRD.RD_PD order by SRD.R_E_C_N_O_) = 0 and SRD.RD_PD in (440, 445) then SRD.RD_VALOR
+	else 0 end as VALOR_FOLHA,
+	
+	case when lag(ZC2.OS, 1, 0) over (partition by SRD.RD_FILIAL, SRD.RD_PERIODO, SRD.RD_MAT, SRD.RD_PD order by SRD.R_E_C_N_O_) = 0 and SRD.RD_PD = 20 then SRD.RD_HORAS*SRJ.RJ_YHRPADR/30
 	else 0 end as HORAS_FOLHA,
 
 	case trim(SRV.RV_TIPOCOD)
@@ -58,10 +57,10 @@ from SRD010 SRD (nolock)
 			trim(ZC2010.ZC2_COD) as INSUMO,
 			cast(substring(ZC1010.ZC1_NUM, 6, 10) as int) as OS,
 			ZC2010.ZC2_COMPET as PERIODO,
-			sum(ZC2010.ZC2_QTDPRV) as QTD_PREV,
-			sum(ZC2010.ZC2_QTDREA) as QTD_REAL,
-			sum(ZC2010.ZC2_VLUPRV) as VAL_PREV,
-			sum(ZC2010.ZC2_VLUREA) as VAL_REAL,
+			cast(sum(ZC2010.ZC2_QTDPRV) as numeric(15, 2)) as QTD_PREV,
+			cast(sum(ZC2010.ZC2_QTDREA) as numeric(15, 2)) as QTD_REAL,
+			cast(sum(ZC2010.ZC2_VLUPRV) as numeric(15, 2)) as VAL_PREV,
+			cast(sum(ZC2010.ZC2_VLUREA) as numeric(15, 2)) as VAL_REAL,
 			sum(datediff(minute, concat(ZC2010.ZC2_DTINI, ' ', ZC2010.ZC2_HRINI), concat(ZC2010.ZC2_DTFIM, ' ', ZC2010.ZC2_HRFIM))/60.0) as HORAS_APONT
 		from ZC2010 (nolock)
 			left join ZC1010 (nolock)
@@ -85,5 +84,4 @@ from SRD010 SRD (nolock)
 		and trim(SRA.RA_CODFUNC) = ZC2.INSUMO
 where
         SRD.D_E_L_E_T_ = ''
-    and SRD.RD_PERIODO = 202310
 	and SRD.RD_PD in (020,113,344,039,030,029,749,719,796,738,800,962,950,955,960,961,817,830,845,442,440,441,444,446,591,038,025,051,134,170,171,172,173,371,445,739,831,832,833,834,846,847,848)
