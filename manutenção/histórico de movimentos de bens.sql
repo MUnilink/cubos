f043e01cc3440aa43e3dd8d53a9ba931ab1dfd19
Manoel <manoel.jose@unilinktransportes.com.br>
2024-02-29 09:38:22 -0300

    histórico de CC em adição ao histórico de movimento

--- manutenção/histórico de movimentos de bens.sql
+++ manutenção/histórico de movimentos de bens.sql
@@ -6,14 +6,16 @@ select
 	SR.T9_MOVIBEM as MOVIMENTA_BEM,
 	STZ.TZ_POSCONT,
 	STZ.TZ_CONTSAI,
-	convert(datetime, concat(STZ.TZ_DATAMOV, ' ', STZ.TZ_HORAENT), 103) as TZ_DATAMOV,
-	convert(datetime, concat(STZ.TZ_DATASAI, ' ', STZ.TZ_HORASAI), 103) as TZ_DATASAI,
+	convert(datetime, concat(STZ.TZ_DATAMOV, ' ', STZ.TZ_HORAENT), 103) as DATAMOV,
+	convert(datetime, concat(STZ.TZ_DATASAI, ' ', STZ.TZ_HORASAI), 103) as DATASAI,
 
 	trim(isnull(STZ.TZ_TIPOMOV, '-')) as TZ_TIPOMOV,
-	trim(isnull(STZ.TZ_HORAENT, '-')) as TZ_HORAENT,
-	trim(isnull(STZ.TZ_HORASAI, '-')) as TZ_HORASAI,
+    trim(SR.T9_CCUSTO) as SR_CC,
+	trim(CM.T9_CCUSTO) as CM_CC,
+	(select top 1 last_value(TPN010.TPN_CCUSTO) over (partition by TPN010.TPN_CODBEM order by TPN010.TPN_CODBEM) from TPN010 where TPN010.D_E_L_E_T_ = '' and TPN010.TPN_CODBEM = STZ.TZ_CODBEM and concat(TPN010.TPN_DTINIC, ' ', TPN010.TPN_HRINIC) < (concat(STZ.TZ_DATAMOV, ' ', STZ.TZ_HORAENT))) as ULT_CC,
+    (select top 1 last_value(convert(datetime, concat(TPN010.TPN_DTINIC, ' ', TPN010.TPN_HRINIC), 103)) over (partition by TPN010.TPN_CODBEM order by TPN010.TPN_CODBEM) from TPN010 where TPN010.D_E_L_E_T_ = '' and TPN010.TPN_CODBEM = STZ.TZ_CODBEM and concat(TPN010.TPN_DTINIC, ' ', TPN010.TPN_HRINIC) < (concat(STZ.TZ_DATAMOV, ' ', STZ.TZ_HORAENT))) as ULT_TRA,
 
-	substring(STZ.TZ_DATAMOV, 1, 6) as PERIODO_ENT,
+	substring(STZ.TZ_DATAMOV, 1, 6) as PERIODO_MOV,
 	substring(STZ.TZ_DATASAI, 1, 6) as PERIODO_SAI,
     
     STZ.TZ_CONTSAI - STZ.TZ_POSCONT as km,
