c9293bee00557bbbdfbd0ed219a950b3c88af541
Manoel <manoel.jose@unilinktransportes.com.br>
2024-03-16 11:51:02 -0300

    consulta de provisões mensais infrutífera até então

--- RH/provisões de cálculo.sql
+++ RH/provisões de cálculo.sql
@@ -1,20 +1,18 @@
 select
-	trim(isnull(SRT.RT_FILIAL, '-')) as RT_FILIAL,
-	substring(SRT.RT_DATACAL, 1, 6) as RT_PERIODO,
-    trim(isnull(SRT.RT_TIPPROV, '-')) as RT_TIPPROV,
-	trim(isnull(SRT.RT_MAT, '-')) as RT_MAT,
-	trim(isnull(SRA.RA_NOMECMP, '-')) as RA_NOME,
-	trim(isnull(SRT.RT_VERBA, '-')) as RT_PD,
-	trim(isnull(SRV.RV_DESC, '-')) as RV_DESC,
-	trim(isnull(SRV.RV_DESCDET, '-')) as RV_DESCDET,
-	trim(isnull(SRJ.RJ_DESC, '-')) as RJ_DESC,
+	trim(SRT.RT_FILIAL) as FILIAL,
+	substring(SRT.RT_DATACAL, 1, 6) as PERIODO,
+	trim(SRT.RT_MAT) as MAT,
+	trim(SRA.RA_NOMECMP) as NOME,
+	trim(SRT.RT_VERBA) as VERBA,
+	trim(SRV.RV_DESC) as RV_DESC,
+	trim(SRV.RV_DESCDET) as RV_DESCDET,
+	trim(SRA.RA_CODFUNC) as FUNCAO,
+	trim(SRJ.RJ_DESC) as RJ_DESC,
+	
+	trim(SRT.RT_CC) as CC,
+	trim(SRT.RT_ITEM) as ATIVIDADE,
 
-    trim(isnull(CTT.CTT_DESC01, '-')) as CC,
-    trim(isnull(CTD.CTD_DESC01, '-')) as ATIVIDADE,
-	trim(isnull(SRA.RA_CC, '-')) as RA_CC,
-	trim(isnull(SRA.RA_ITEM, '-')) as RA_ITEM,
-
-	case trim(SRV.RV_TIPOCOD)
+	case trim(SRT.RT_TIPPROV)
 		when '1' then 'PROVENTO'
 		when '2' then 'DESCONTO'
 		when '3' then 'BASE PROVENTO'
@@ -22,8 +20,9 @@ select
 		else '-'
 	end as RV_TIPOCOD,
 
-	SRT.RT_VALOR as RT_ACUMULADO,
-	SRT.RT_SALARIO as RT_SALARIO
+	SRT.RT_VALOR as PROV_ACUMULADA,
+	SRT.RT_VALOR - lag(SRT.RT_VALOR, 1, SRT.RT_SALARIO/12.0) over (partition by SRT.RT_FILIAL, SRT.RT_MAT, SRT.RT_VERBA, SRT.RT_TIPPROV order by SRT.RT_DATACAL) as PROV_MENSAL,
+	SRT.RT_SALARIO as SALARIO_BASE
 from SRT010 SRT (nolock)
     inner join SRV010 SRV (nolock)
     	on SRV.D_E_L_E_T_ = ''
@@ -38,11 +37,12 @@ from SRT010 SRT (nolock)
             on SRJ.D_E_L_E_T_ = ''
             and SRJ.RJ_FILIAL = substring(SRA.RA_FILIAL, 1, 4)
             and SRJ.RJ_FUNCAO = SRA.RA_CODFUNC
-    inner join CTT010 CTT (nolock)
+    
+	inner join CTT010 CTT (nolock)
         on CTT.D_E_L_E_T_ = ''
-        and CTT.CTT_CUSTO = SRA.RA_CC
+        and CTT.CTT_CUSTO = SRT.RT_CC
     inner join CTD010 CTD (nolock)
         on CTD.D_E_L_E_T_ = ''
-        and CTD.CTD_ITEM = SRA.RA_ITEM
+        and CTD.CTD_ITEM = SRT.RT_ITEM
 where
         SRT.D_E_L_E_T_ = ''
