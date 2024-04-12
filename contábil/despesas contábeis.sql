780c455121d5ea36cbaac9e8e2738382ac4b8e0d
Manoel <manoel.jose@unilinktransportes.com.br>
2024-04-12 13:52:09 -0300

    consulta para conferência das despesas contábeis

---
+++ contábil/despesas contábeis.sql
@@ -0,0 +1,24 @@
+select
+    ZA7010.ZA7_COD as COD_DESPESA,
+    ZA7010.ZA7_DESC as DESPESA,
+    CT2010.CT2_DATA as DATA,
+    eomonth(CT2010.CT2_DATA) as PERIODO,
+    CT2010.CT2_VALOR as VALOR,
+    CT2010.CT2_DEBITO as CONTA_DEBITO,
+    CT2010.CT2_CREDIT as CONTA_CREDITO,
+    CT2010.CT2_ITEMD as ATIVIDADE_DEB,
+    CT2010.CT2_ITEMC as ATIVIDADE_CRE,
+    CT2010.CT2_CCD as CC_DEB,
+    CT2010.CT2_CCC as CC_DEB
+from CT2010 (nolock)
+    inner join ZA8010 (nolock)
+        on ZA8010.D_E_L_E_T_ = ''
+        and CT2010.CT2_DEBITO between ZA8010.ZA8_CT1INI and ZA8010.ZA8_CT1FIM
+        and CT2010.CT2_ITEMD between ZA8010.ZA8_CTDINI and ZA8010.ZA8_CTDFIM
+        and CT2010.CT2_CCD between ZA8010.ZA8_CTTINI and ZA8010.ZA8_CTTFIM
+
+        inner join ZA7010 (nolock)
+            on ZA7010.D_E_L_E_T_ = ''
+            and ZA7010.ZA7_COD = ZA8010.ZA8_COD
+where
+        CT2010.D_E_L_E_T_ = ''
