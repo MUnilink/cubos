e2eebf11aebf74a2c077a3339ef1d71b8a27799f
Manoel <manoel.jose@unilinktransportes.com.br>
2024-05-22 10:05:01 -0300

    criação da regra do aprovador de maior nível

--- FAST ANALYTICS/gooddata/query/fact/aprovador do pedido de compra de maior nível.sql
+++ FAST ANALYTICS/gooddata/query/fact/aprovador do pedido de compra de maior nível.sql
@@ -6,9 +6,26 @@ select
     SCR.CR_TIPO,
     SCR.CR_NUM,
     SCR.CR_NIVEL,
+    case SCR.CR_STATUS
+        when 1 then 'PENDENTE'
+        when 2 then 'PENDENTE'
+        when 3 then 'APROVADA'
+        when 5 then 'APROVADA'
+        when 6 then 'REJEITADA'
+        when 7 then 'REJEITADA'
+        else 'LIBERADA'
+    end as STATUS,
     'P |01|SAK010|'+ COALESCE(NULLIF(RTRIM(COALESCE(SAK010.AK_FILIAL, ' '))+'|'+RTRIM(COALESCE(SAK010.AK_COD, ' ')), ' '), '|') as BK_APROVADOR,
+    count(*)
+from SCR010 SCR
+    inner join SAK010
+        on SAK010.D_E_L_E_T_ = ''
+        and SAK010.AK_COD = SCR.CR_LIBAPRO
+where
+        SCR.D_E_L_E_T_ = ''
+    and SCR.CR_NIVEL = 
     (
-        select max(SCR010.CR_NIVEL) as NIVEL_MAX
+        select max(SCR010.CR_NIVEL)
         from SCR010 (nolock)
         where
                 SCR010.D_E_L_E_T_ = ''
@@ -20,9 +37,12 @@ select
             SCR010.CR_TIPO,
             SCR010.CR_NUM
     )
-from SCR010 SCR
-    inner join SAK010
-        on SAK010.D_E_L_E_T_ = ''
-        and SAK010.AK_COD = SCR.CR_LIBAPRO
-where
-        SCR.D_E_L_E_T_ = ''
+group by
+    SAK010.AK_FILIAL,
+    SAK010.AK_COD,
+    SCR.CR_LIBAPRO,
+    SCR.CR_FILIAL,
+    SCR.CR_TIPO,
+    SCR.CR_NUM,
+    SCR.CR_NIVEL,
+    SCR.CR_STATUS
