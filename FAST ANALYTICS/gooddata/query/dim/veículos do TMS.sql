c509c691bf09e2a8eedb498de2d3e4797ed365f3
Manoel <manoel.jose@unilinktransportes.com.br>
2024-06-11 16:07:07 -0300

    na realidade os veículos são da manutenção de ativos

--- FAST ANALYTICS/gooddata/query/dim/veículos do TMS.sql
+++ FAST ANALYTICS/gooddata/query/dim/veículos do TMS.sql
@@ -1,8 +1,10 @@
-select
-    concat(trim(DA3.DA3_FILIAL), trim(DA3.DA3_COD)) as ID_VEICULOTMS,
-	trim(DA3.DA3_COD) as COD_VEICULO,
-	trim(DA3.DA3_DESC) as DESC_VEICULO,
-	trim(DA3.DA3_PLACA) as PLACA_VEICULO,
-	(select trim(ST9010.T9_YPORTAL) from ST9010 where ST9010.D_E_L_E_T_ = '' and ST9010.T9_CODBEM = DA3.DA3_COD) as APP_PORT
-from DA3010 DA3	
-where DA3.D_E_L_E_T_ = ''
+	select
+		concat(trim(ST9.T9_FILIAL), trim(ST9.T9_CODBEM)) as ID_VEICULOTMS,
+		trim(ST9010.T9_CODBEM) as COD_VEICULO,
+		trim(ST9.T9_NOME) as DESC_VEICULO,
+		trim(ST9.T9_PLACA) as PLACA_VEICULO,
+		trim(ST9010.T9_YPORTAL) as APP_PORT
+	from ST9010 ST9	
+	where ST9.D_E_L_E_T_ = ''
+union
+		select null, null, null, null, null
