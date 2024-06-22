650786e4f4470c627fd559c4885eb363051395d9
Manoel <manoel.jose@unilinktransportes.com.br>
2024-06-22 10:56:53 -0300

    Revert "alteração para bens da manutenção além dos do TMS"
    This reverts commit 9198af5c550dc517c12288d7902e854dbb61d1a0.

--- FAST ANALYTICS/gooddata-TMS/Agent/query/dim/veiculosTMS.sql
+++ FAST ANALYTICS/gooddata-TMS/Agent/query/dim/veiculosTMS.sql
@@ -1,14 +1,8 @@
 select
-    trim(ST9.T9_CODBEM) as ID_VEI,
-    trim(ST9.T9_CODBEM) as DA3_COD,
-    trim(ST9.T9_NOME) as DA3_DESC,
-    trim(ST9.T9_PLACA) as DA3_PLACA,
+    trim(DA3.DA3_COD) as ID_VEI,
+    trim(DA3.DA3_COD) as DA3_COD,
+    trim(DA3.DA3_DESC) as DA3_DESC,
+    trim(DA3.DA3_PLACA) as DA3_PLACA,
     <<CODE_INSTANCE>> AS INSTANCIA
-from ST9010 ST9
-    left join DA3010 DA3
-        on DA3.D_E_L_E_T_ = ''
-        and DA3.DA3_COD = ST9.T9_CODBEM
-where
-        ST9.D_E_L_E_T_ = ''
-    and ST9.T9_CATBEM in (2, 4)
-    and ST9.T9_CCUSTO in (304, 305)
+from DA3010 DA3
+where DA3.D_E_L_E_T_ = ''
