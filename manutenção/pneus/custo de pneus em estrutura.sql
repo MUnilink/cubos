b2a910790d1768dfef1f88304fd052e68a56218c
Manoel <manoel.jose@unilinktransportes.com.br>
2024-07-12 11:15:54 -0300

    custo de pneus conforme regra calculada pelo Rodrigo

---
+++ manutenção/pneus/custo de pneus em estrutura.sql
@@ -0,0 +1,50 @@
+select
+	trim(ST9.T9_CODBEM) as PNEU,
+	ST9.T9_STATUS,
+	ST9.T9_CCUSTO as CCUSTO,
+	ST9.T9_ITEMCTA as ATIVIDADE,
+	ST9.T9_ESTRUTU as APLICADO,
+	ST9.T9_CONTACU as CONT_ACU,
+	trim(TQT.TQT_DESMED) as MEDIDA,
+
+	ZC5.ZC5_PNEU,
+	ZC5.ZC5_VLRCOM,
+	ZC5.ZC5_VLRMAN,
+	ZC5.ZC5_BANDA,
+	ZC5.ZC5_KMEXPE,
+	ZC5.ZC5_KMRODM,
+	ZC5.ZC5_ANOMES,
+
+	ST9.T9_VALCPA as T9_VALCPA,
+	cast(ST9.T9_DTCOMPR as date) as T9_DTCOMPR,
+	ST9.T9_SITBEM,
+
+    ZC6.ZC6_BEMPAI as ESTRUTURA,
+    ZC6.ZC6_BEMPA2 as COMPONENTE,
+    cast(ZC6.ZC6_DTIAPL as date) as DT_INIAPP,
+    cast(ZC6.ZC6_DTFAPL as date) as DT_FIMAPP,
+    cast(ZC6.ZC6_DTICUS as date) as DT_INICUSTO,
+    cast(ZC6.ZC6_DTFCUS as date) as DT_FIMCUSTO,
+    ZC6.ZC6_HODOMI as HODOMINI,
+    ZC6.ZC6_HODOMF as HODOMFIM,
+    cast(ZC6.ZC6_KMRODA as numeric(15, 2)) as km_EST,
+    cast(ZC6.ZC6_CUSTO as numeric(15, 2)) as CUSTO_PNEUS,
+    ZC6.ZC6_ANOMES as PERIODO
+
+from ZC5010 ZC5 (nolock)
+	left join TQS010 TQS
+		on TQS.D_E_L_E_T_ = ''
+		and TQS.TQS_CODBEM = ZC5.ZC5_PNEU
+		
+		left join TQT010 TQT
+			on TQT.D_E_L_E_T_ = ''
+			and TQT.TQT_MEDIDA = TQS.TQS_MEDIDA
+		left join ST9010 ST9
+			on ST9.D_E_L_E_T_ = ''
+			and ST9.T9_CODBEM = TQS.TQS_CODBEM
+	
+    left join ZC6010 ZC6 (nolock)
+        on ZC6.D_E_L_E_T_ = ''
+        and ZC6.ZC6_PNEU = ZC5.ZC5_PNEU
+where
+		ZC5.D_E_L_E_T_ = ''
