3c1bf3b18589a164a62f4f081a6e20f47489df0c
Manoel <manoel.jose@unilinktransportes.com.br>
2024-07-26 11:53:25 -0300

    anualização do valor do seguro

--- patrimônio/seguro da frota portuária.sql
+++ patrimônio/seguro da frota portuária.sql
@@ -3,12 +3,13 @@ select
 	ZC4.ZC4_CODBEM as VEICULO,
 	ZC4.ZC4_VLBEM as VALOR_VEI,
 	ZC4.ZC4_VLSEG as VALOR_SEG,
-	ZC4.ZC4_VLSEG/(datediff(day, ZC4.ZC4_DTVGIN, ZC4.ZC4_DTVGFI)/30) as VALOR_SEG_MES,
+	cast((ZC4.ZC4_VLSEG/(datediff(day, ZC4.ZC4_DTVGIN, ZC4.ZC4_DTVGFI)/365.0))/12 as numeric(15, 2)) as VALOR_SEG_MES,
+	cast(ZC4.ZC4_VLSEG/(datediff(day, ZC4.ZC4_DTVGIN, ZC4.ZC4_DTVGFI)/365.0) as numeric(15, 2)) as VALOR_SEG_ANO,
 	ZC4.ZC4_PROPRI as PROPRIO,
 	convert(date, ZC4.ZC4_DTVGIN, 103) as INI_VIG,
 	convert(date, ZC4.ZC4_DTVGFI, 103) as FIM_VIG,
 	substring(ZC4.ZC4_DTVGIN, 1, 6) as PERIODO_FIM,
 	substring(ZC4.ZC4_DTVGFI, 1, 6) as PERIODO_INI,
-	datediff(day, ZC4.ZC4_DTVGIN, ZC4.ZC4_DTVGFI)/30.0 as MESES
+	cast(datediff(day, ZC4.ZC4_DTVGIN, ZC4.ZC4_DTVGFI)/365.0 as numeric(15, 5)) as ANOS
 from ZC4010 ZC4 (nolock)
 where ZC4.D_E_L_E_T_ = ''
