0605889d7cb6cad1c7b26f320e2526c4f99e82bb
Manoel <manoel.jose@unilinktransportes.com.br>
2024-07-27 10:44:30 -0300

    necessário otimizar consulta, devido ao grande volume de dados

--- estoque/saldo atual de produtos.sql
+++ estoque/saldo atual de produtos.sql
@@ -1,18 +1,29 @@
 select
     SB1.B1_COD as contador,
-    trim(isnull(SB1.B1_COD, '-')) as PRODUTO,
-	trim(isnull(SB1.B1_DESC, '-')) as NOMEPRODUTO,
-	trim(isnull(SB1.B1_GRUPO, '-')) as GRUPO,
-	trim(isnull(SB1.B1_UM, '-')) as UN,
+    trim(SB1.B1_COD) as PRODUTO,
+	trim(SB1.B1_DESC) as NOMEPRODUTO,
+	trim(SB1.B1_GRUPO) as GRUPO,
+    trim(SB1.B1_CONTA) as CONTA_ATIVO,
     isnull(SB1.B1_UPRC, 0.0) as ULT_PRECO,
-    SB2.B2_FILIAL as FILIAL,
-    SB2.B2_LOCAL as ARMAZEM,
-    SB2.B2_QATU as QTD,
-    SB2.B2_VATU1 as VALOR,
-    SB2.B2_CM1 as CM
-from SB2010 SB2 (nolock)
+    SB2.B2_FILIAL as FILIAL_ATU,
+    SB2.B2_LOCAL as ARMAZEM_ATU,
+    SB2.B2_QATU as QTD_ATU,
+    SB2.B2_VATU1 as VALOR_ATU,
+    SB2.B2_CM1 as CM_ATU,
+    substring(SB9.B9_DATA, 1, 6) as PERIODO,
+    SB9.B9_FILIAL as FILIAL_INI,
+    SB9.B9_LOCAL as ARMAZEM_INI,
+    SB9.B9_QINI as QTD_INI,
+    SB9.B9_VINI1 as VALOR_INI,
+    SB9.B9_CM1 as CM_INI,
+    trim(SB1.B1_UM) as UN
+from SB9010 SB9 (nolock)
+    left join SB2010 SB2 (nolock)
+        on SB2.D_E_L_E_T_ = ''
+        and SB2.B2_COD = SB9.B9_COD
     left join SB1010 SB1 (nolock)
         on SB1.D_E_L_E_T_ = ''
-        and SB1.B1_COD = SB2.B2_COD
+        and SB1.B1_COD = SB9.B9_COD
+        and SB1.B1_GRUPO like '1%'
 where
-        SB2.D_E_L_E_T_ = ''
+        SB9.D_E_L_E_T_ = ''
