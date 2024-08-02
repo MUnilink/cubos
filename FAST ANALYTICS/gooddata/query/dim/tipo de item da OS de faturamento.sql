eaf3205532c3704346a29a236016fd24232db850
Manoel <manoel.jose@unilinktransportes.com.br>
2024-08-02 10:22:41 -0300

    atualização dos tipos de itens das OS

--- FAST ANALYTICS/gooddata/query/dim/tipo de item da OS de faturamento.sql
+++ FAST ANALYTICS/gooddata/query/dim/tipo de item da OS de faturamento.sql
@@ -2,18 +2,19 @@
         cast(ZC2.ZC2_TIPO as int) as ID_TIPO_ITEM,
         case cast(ZC2.ZC2_TIPO as int)
             when 1 then 'RECEITA'
-            when 2 then 'FUNÇÃO'
+            when 2 then 'FOLHA'
             when 3 then 'MANUTENÇÃO'
             when 4 then 'MATERIAIS'
             when 5 then 'COMPRAS'
             when 6 then 'DEPRECIAÇÃO'
             when 7 then 'CONTABILIDADE'
             when 8 then 'DESPESAS FINANCEIRAS'
-            when 9 then 'DOCUMENTAÇÃO E TAXAS'
+            when 9 then 'OUTROS CUSTOS - TAXAS'
             when 10 then 'COMBUSTIVEL'
-            when 11 then 'TAXAS CIPP'
+            when 11 then 'SERVIÇOS TOMADOS'
             when 12 then 'SEGURO'
             when 13 then 'PNEUS'
+            when 14 then 'PROVISÕES'
             else 'OUTROS'
         end as TIPO_INSUMO
     from ZC2010 ZC2
