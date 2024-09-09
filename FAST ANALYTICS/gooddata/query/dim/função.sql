0c32bb1d7fee8cdda92f5609e1be05d234545836
Manoel <manoel.jose@unilinktransportes.com.br>
2024-09-09 10:57:47 -0300

    troca da tabela de função pela de cargo

--- FAST ANALYTICS/gooddata/query/dim/função.sql
+++ FAST ANALYTICS/gooddata/query/dim/função.sql
@@ -1,9 +1,9 @@
     select
-        concat(trim(SRJ.RJ_FILIAL), trim(SRJ.RJ_FUNCAO)) as ID_FUNCAO,
-        trim(SRJ.RJ_FUNCAO) as COD_FUNCAO,
-        trim(SRJ.RJ_DESC) as DESC_FUNCAO,
-        trim(SRJ.RJ_YPORTAL) as APP_PORT
-    from SRJ010 SRJ
-    where SRJ.D_E_L_E_T_ = ''
+        concat(trim(SQ3.Q3_FILIAL), trim(SQ3.Q3_CARGO)) as ID_FUNCAO,
+        trim(SQ3.Q3_CARGO) as COD_FUNCAO,
+        trim(SQ3.Q3_DESCSUM) as DESC_FUNCAO,
+        null as APP_PORT
+    from SQ3010 SQ3
+    where SQ3.D_E_L_E_T_ = ''
 union
         select null, null, null, null
