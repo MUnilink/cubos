464e50d23639386afeac59c41c39657d965c2159
Manoel <manoel.jose@unilinktransportes.com.br>
2024-10-28 10:48:56 -0300

    correção do vínculo do recurso do item da OS

--- FAST ANALYTICS/gooddata/query/fact/itens das OS portuária.sql
+++ FAST ANALYTICS/gooddata/query/fact/itens das OS portuária.sql
@@ -120,7 +120,7 @@ union
         
         case
             when cast(ZC2.ZC2_TIPO as int) in (5, 11) then concat(trim(ZC2.ZC2_TIPO), ' ', trim(ZC2.ZC2_YFORNE))
-            when cast(ZC2.ZC2_TIPO as int) in (2, 14, 3, 6, 9, 12) then concat(trim(ZC2.ZC2_TIPO), 'p')
+            when cast(ZC2.ZC2_TIPO as int) in (2, 14, 3, 6, 9, 12) then concat(trim(ZC2.ZC2_TIPO), 'p', trim(ZC2.ZC2_COD))
             else concat(trim(ZC2.ZC2_TIPO), ' ', trim(ZC2.ZC2_COD))
         end as ID_RECURSO,
 
