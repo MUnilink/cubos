ed817183a6022f47267e03d5b09f85a2019ebd4c
Manoel <manoel.jose@unilinktransportes.com.br>
2024-10-31 10:44:14 -0300

    remoção de campos para redução da quantidade de dados

--- compras/pedidos e entradas.sql
+++ compras/pedidos e entradas.sql
@@ -11,7 +11,6 @@ select
 	trim(SC1.C1_NUM) as SC,
 	trim(SC1.C1_ITEM) as ITEM_SC,
 	trim(upper(SC1.C1_SOLICIT)) as SOLICITANTE_SC,
-	trim(SC1.C1_OBS) as OBS_SC,
 	cast(SC1.C1_EMISSAO as date) as DATA_SC,
 	substring(SC1.C1_EMISSAO, 1, 6) as PERIODO_SC,
 	substring(SC1.C1_OP, 1, 6) as OS,
@@ -78,11 +77,10 @@ select
 	trim(SC7.C7_FORNECE) as FORNECEDOR,
 	trim(SC7.C7_LOJA) as LOJA,
 	trim(SA2.A2_NOME) as NOME_FORNECEDOR,
-	trim(SA2.A2_NREDUZ) as NOMERED_FORNECEDOR,
 	trim(SA2.A2_CGC) as CNPJ,
 	trim(SA2.A2_EST) as UF,
-	replace(replace(SC7.C7_OBS, char(10), ''), char(13), '') as OBS_PC,
-	replace(replace(SC7.C7_OBSM, char(10), ''), char(13), '') as MEMO_PC,
+	trim(replace(replace(SC7.C7_OBS, char(10), ''), char(13), '')) as OBS_PC,
+	trim(replace(replace(SC7.C7_OBSM, char(10), ''), char(13), '')) as MEMO_PC,
 
 	cast(SC7.C7_EMISSAO as date) as DATA_PEDIDO,
 	substring(SC7.C7_EMISSAO, 1, 6) as PERIODO_PC,
@@ -94,34 +92,6 @@ select
 		when 'R' then 'REJEITADO'
 		else 'OUTROS'
 	end as APROVACAO_PC,
-
-    (
-        select max('P |01|SAK010|'+ COALESCE(NULLIF(RTRIM(COALESCE(SAK010.AK_FILIAL, ' '))+'|'+RTRIM(COALESCE(SAK010.AK_COD, ' ')), ' '), '|'))
-        from SCR010 SCR
-            inner join SAK010
-                on SAK010.D_E_L_E_T_ = ''
-                and SAK010.AK_COD = SCR.CR_LIBAPRO
-        where
-                SCR.D_E_L_E_T_ = ''
-            and SCR.CR_FILIAL = SC7.C7_FILIAL
-            and SCR.CR_NUM = SC7.C7_NUM
-            and SCR.CR_STATUS < 6
-            and SCR.CR_NIVEL =
-            (
-                select max(SCR010.CR_NIVEL)
-                from SCR010 (nolock)
-                where
-                        SCR010.D_E_L_E_T_ = ''
-                    and SCR010.CR_TIPO = 'PC'
-                    and SCR010.CR_FILIAL = SCR.CR_FILIAL
-                    and SCR010.CR_TIPO = SCR.CR_TIPO
-                    and SCR010.CR_NUM = SCR.CR_NUM
-                group by
-                    SCR010.CR_FILIAL,
-                    SCR010.CR_TIPO,
-                    SCR010.CR_NUM
-            )
-    ) as BK_APROVADOR,
 
 	(
 		select top 1 cast(SCR010.CR_DATALIB as date)
@@ -156,6 +126,30 @@ select
 			and SCR010.CR_FILIAL = SC7.C7_FILIAL
             and SCR010.CR_NUM = SC7.C7_NUM
 	) as NUM_NIVEL,
+
+	(
+        select max('P |01|SAK010|'+ COALESCE(NULLIF(RTRIM(COALESCE(SAK010.AK_FILIAL, ' '))+'|'+RTRIM(COALESCE(SAK010.AK_COD, ' ')), ' '), '|'))
+        from SCR010
+            inner join SAK010
+                on SAK010.D_E_L_E_T_ = ''
+                and SAK010.AK_COD = SCR010.CR_LIBAPRO
+		where
+				SCR010.D_E_L_E_T_ = ''
+			and SCR010.CR_TIPO = 'PC'
+			and SCR010.CR_FILIAL = SC7.C7_FILIAL
+			and SCR010.CR_NUM = SC7.C7_NUM
+			and SCR010.CR_NIVEL =
+		(
+			select max(SCR010.CR_NIVEL)
+			from SCR010 (nolock)
+			where
+					SCR010.D_E_L_E_T_ = ''
+				and SCR010.CR_TIPO = SCR.CR_TIPO
+				and SCR010.CR_FILIAL = SCR.CR_FILIAL
+				and SCR010.CR_NUM = SCR.CR_NUM
+				and SCR010.CR_STATUS = '3'
+		)
+    ) as BK_APROVADOR,
 
 	(
 		select upper(trim(max(SAK010.AK_LOGIN)))
@@ -194,24 +188,14 @@ select
 	year(SC7.C7_EMISSAO) as ANO_PEDIDO,
 	month(SC7.C7_EMISSAO) as MES_PEDIDO,
 
-	case when trim(SC7.C7_CONAPRO) = 'B' and (cast(SC7.C7_QUJE as numeric(15, 2)) < cast(SC7.C7_QUANT as numeric(15, 2))) then 'BLOQUEADO' /* AZUL */
-	else
-		case when cast(SC7.C7_QTDACLA as numeric(15, 2)) > 0.0 then 'PRÉ-NOTA' /* LARANJA */
-		else
-			case when cast(SC7.C7_TIPO as int) = 1 and SC7.C7_RESIDUO = '' then 'APROVADO' /* VERDE */
-			else
-				case when cast(SC7.C7_QUJE as numeric(15, 2)) != 0.0 and (cast(SC7.C7_QUJE as numeric(15, 2)) < cast(SC7.C7_QUANT as numeric(15, 2))) then 'REC. PARCIAL' /* AMARELO */
-				else
-					case when cast(SC7.C7_QUJE as numeric(15, 2)) >= cast(SC7.C7_QUANT as numeric(15, 2)) then 'RECEBIDO' /* VERMELHO */
-					else
-						case when trim(SC7.C7_RESIDUO) = 'S' then 'ELIMINAÇÃO DE RESÍDUO' /* CINZA */
-						else 'OUTROS'
-						end
-					end
-				end
-			end
-		end
-	end as STATUS_COMPRA,
+	case
+		when trim(SC7.C7_RESIDUO) = 'S' then 'ELIMINADO' /* CINZA */
+		when trim(SC7.C7_CONAPRO) = 'B' and (cast(SC7.C7_QUJE as numeric(15, 2)) < cast(SC7.C7_QUANT as numeric(15, 2))) then 'BLOQUEADO' /* AZUL */
+		when cast(SC7.C7_QUJE as numeric(15, 2)) >= cast(SC7.C7_QUANT as numeric(15, 2)) then 'RECEBIDO' /* VERMELHO */
+		when cast(SC7.C7_QUJE as numeric(15, 2)) != 0.00 and (cast(SC7.C7_QUJE as numeric(15, 2)) < cast(SC7.C7_QUANT as numeric(15, 2))) then 'REC. PARCIAL' /* AMARELO */
+		when cast(SC7.C7_QTDACLA as numeric(15, 2)) > 0.00 then 'PRÉ-NOTA' /* LARANJA */
+		when cast(SC7.C7_TIPO as int) = 1 and SC7.C7_RESIDUO = '' then 'APROVADO' /* VERDE */
+	else 'OUTROS' end as STATUS_COMPRA,
 
 	SD1.D1_DOC as NF_DOC,
 	SD1.D1_SERIE as NF_SERIE,
@@ -241,12 +225,7 @@ select
 	SD1.D1_TOTAL NF_TOTAL,
 	SD1.D1_TES NF_TES,
 	SD1.D1_CUSTO NF_CUSTO,
-	SD1.D1_QTDPEDI as NF_QTDPEDI,
 	SD1.D1_VALDESC as NF_VALDESC,
-	SD1.D1_SEGURO as NF_SEGURO,
-
-	case when trim(SC7.C7_YOS) = '2024/0' then right(left(replace(replace(SC7.C7_OBS, char(10), ''), char(13), ''), 63), 11) else SC7.C7_YOS end as OS_PORT,
-	isnull(nullif(SC7.C7_YOSIT, ''), '0') as ITEMOS_PORT,
 
     cast(SC7.C7_VALICM as numeric(14, 2)) as VL_PC_ICMS,
     cast(SC7.C7_VALIPI as numeric(14, 2)) as VL_PC_IPI,
@@ -265,7 +244,6 @@ select
     cast(SD1.D1_VALIPI as numeric(14, 2)) as VL_NFENT_IPI,
     cast(SD1.D1_VALFRE as numeric(14, 2)) as VL_NFENT_FRETE_NF,
     cast(SD1.D1_DESPESA as numeric(14, 2)) as VL_NFENT_DESPESA,
-    cast(SD1.D1_TOTAL as numeric(14, 2)) as VL_NFENT_MERCADORIA,
     cast(SD1.D1_VALIMP6 as numeric(14, 2)) as VL_NFENT_PIS,
     cast(SD1.D1_VALIMP5 as numeric(14, 2)) as VL_NFENT_COFINS,
     cast(SD1.D1_VALISS as numeric(14, 2)) as VL_NFENT_ISS,
@@ -274,10 +252,10 @@ select
     cast(SD1.D1_VALIRR as numeric(14, 2)) as VL_NFENT_IRF,
     cast(SD1.D1_VALINS as numeric(14, 2)) as VL_NFENT_INSS,
 	cast(SD1.D1_SEGURO as numeric(14, 2)) as VL_NFENT_SEGURO,
-	cast(SD1.D1_QUANT as numeric(13, 3)) as QTD_ITEM_NFENT,
 	cast(SD1.D1_PESO * SD1.D1_QUANT as numeric(12, 4)) as PESO_LIQUIDO_NFENT,
 
-	case when lag(SC7.C7_NUM, 1, 0) over (partition by SC7.C7_FILIAL, SC7.C7_NUM, SD1.D1_DOC, SD1.D1_SERIE order by SC7.R_E_C_N_O_) = 0 then 1 else 0 end as QTD_PEDIDOS
+	case when trim(SC7.C7_YOS) = '2024/0' then right(left(replace(replace(SC7.C7_OBS, char(10), ''), char(13), ''), 63), 11) else SC7.C7_YOS end as OS_PORT,
+	isnull(nullif(SC7.C7_YOSIT, ''), '0') as ITEMOS_PORT
 
 from SC7010 SC7 (nolock)
 	left join SC8010 SC8 (nolock)
