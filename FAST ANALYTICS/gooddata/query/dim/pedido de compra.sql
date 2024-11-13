e862b1b7f3f264d41d86ea1acf76d971ec106127
Manoel <manoel.jose@unilinktransportes.com.br>
2024-11-13 16:44:24 -0300

    remoção do item do pedido de compra

--- FAST ANALYTICS/gooddata/query/dim/pedido de compra.sql
+++ FAST ANALYTICS/gooddata/query/dim/pedido de compra.sql
@@ -1,9 +1,8 @@
-select
+select distinct
     concat(trim(SC7.C7_FILIAL), trim(SC7.C7_NUM)) as ID_PEDIDO,
 	trim(SC7.C7_NUM) as NUM_PC,
-	trim(SC7.C7_ITEM) as ITEM_PC,
 	trim(SC7.C7_RESIDUO) as RESIDUO_PC
 from SC7010 SC7
 where SC7.D_E_L_E_T_ = ''
 
-union select null, null, null, null
+union select null, null, null
