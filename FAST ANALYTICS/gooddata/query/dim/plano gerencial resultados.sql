d224065b8132df73e5aa0530731c168ec1a7678e
Manoel <manoel.jose@unilinktransportes.com.br>
2024-11-22 17:51:56 -0300

    necessário separar as descrições para cada nível da conta

--- FAST ANALYTICS/gooddata/query/dim/plano gerencial resultados.sql
+++ FAST ANALYTICS/gooddata/query/dim/plano gerencial resultados.sql
@@ -1,13 +1,18 @@
 select
+    case when len(trim(ZE2.ZE2_COD)) <= 2 then trim(ZE2.ZE2_COD) else left(trim(ZE2.ZE2_COD), 2) end as CODIGO_C1,
+    case when len(trim(ZE2.ZE2_COD)) <= 3 then trim(ZE2.ZE2_COD) else left(trim(ZE2.ZE2_COD), 3) end as CODIGO_C2,
+    case when len(trim(ZE2.ZE2_COD)) <= 4 then trim(ZE2.ZE2_COD) else left(trim(ZE2.ZE2_COD), 4) end as CODIGO_C3,
+    case when len(trim(ZE2.ZE2_COD)) <= 7 then trim(ZE2.ZE2_COD) else left(trim(ZE2.ZE2_COD), 7) end as CODIGO_C4,
+    
     trim(ZE2.ZE2_COD) as CODIGO,
     upper(trim(ZE2.ZE2_DESC)) as DESCRICAO,
     concat(trim(ZE2.ZE2_COD), ' ', upper(trim(ZE2.ZE2_DESC))) as CODDESC,
+    
     trim(ZE2.ZE2_ORIGEM) as ORIGEM,
     ZE2.ZE2_MSBLQL as BLOQUEADO,
     
     case
         when len(trim(ZE2.ZE2_COD)) = 7 then left(trim(ZE2.ZE2_COD), 4)
-        when len(trim(ZE2.ZE2_COD)) = 6 then left(trim(ZE2.ZE2_COD), 3)
         when len(trim(ZE2.ZE2_COD)) = 4 then left(trim(ZE2.ZE2_COD), 3)
         when len(trim(ZE2.ZE2_COD)) = 3 then left(trim(ZE2.ZE2_COD), 2)
         else null
