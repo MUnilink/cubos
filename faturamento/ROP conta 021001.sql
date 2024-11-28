060fea34eab5cdc6162e18486e66dfccfde7ad77
Manoel <manoel.jose@unilinktransportes.com.br>
2024-11-28 11:58:24 -0300

    vínculo de deduções da receita com a OS portuária

--- faturamento/ROP conta 021001.sql
+++ faturamento/ROP conta 021001.sql
@@ -3,12 +3,20 @@ select
     SC6.C6_YOS as NUM,
     sum(cast(coalesce(SD2.D2_VALICM, 0) as decimal(14, 2))) as TOTAL
 from SD2010 SD2
-    left join SC6010 SC6
+    inner join SC6010 SC6
         on SC6.D_E_L_E_T_ = ''
         and SC6.C6_FILIAL = SD2.D2_FILIAL
         and SC6.C6_NUM = SD2.D2_PEDIDO
         and SC6.C6_ITEM = SD2.D2_ITEMPV
-where SD2.D_E_L_E_T_ = ''
+        
+        inner join ZC2010 ZC2 (nolock)
+            on ZC2.D_E_L_E_T_ = ''
+            and ZC2.ZC2_FILIAL = SC6.C6_FILIAL
+            and ZC2.ZC2_NUM = SC6.C6_YOS
+            and ZC2.ZC2_ITEM = SC6.C6_YITOS
+where
+        SD2.D_E_L_E_T_ = ''
+    and left(SD2.D2_EMISSAO, 6) = '"+cCompt+"' and SD2.D2_FILIAL between '"+cFilIni+"' and '"+cFilFim+"'
 group by
     SD2.D2_FILIAL,
     SC6.C6_YOS
