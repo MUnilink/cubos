c97a6e3ebd461e4dcbf50fdaca7ec0facc6b0de2
Manoel <manoel.jose@unilinktransportes.com.br>
2024-12-11 14:28:19 -0300

    lógica da mudança de função não se aplica, pois a transferência pode ser múltipla

--- manutenção/horas de equipamentos por CC.sql
+++ manutenção/horas de equipamentos por CC.sql
@@ -6,8 +6,97 @@ select
 	trim(ST7.T7_NOME) as FABRICANTE,
 	ST9.T9_STATUS,
     trim(TQY.TQY_DESTAT) as STATUS,
+    STP.PERIODO,
 
-    case when ;
+	(
+        select top 1 last_value(trim(TPN010.TPN_CCUSTO)) over (partition by TPN010.TPN_CODBEM order by TPN010.TPN_DTINIC, TPN010.TPN_HRINIC)
+        from TPN010
+        where
+                TPN010.D_E_L_E_T_ = ''
+            and TPN010.TPN_CODBEM = STP.TP_CODBEM
+            and convert(datetime, concat(TPN010.TPN_DTINIC, ' ', TPN010.TPN_HRINIC)) <= STP.MESINI
+	) as CC_ANT,
+
+    floor
+    (
+        datediff
+        (
+            hour,
+            STP.MESINI,
+            case
+                when
+                (
+                    select max(convert(datetime, concat(TPN010.TPN_DTINIC, ' ', TPN010.TPN_HRINIC)))
+                    from TPN010
+                    where
+                            TPN010.D_E_L_E_T_ = ''
+                        and TPN010.TPN_CODBEM = STP.TP_CODBEM
+                        and convert(datetime, concat(TPN010.TPN_DTINIC, ' ', TPN010.TPN_HRINIC)) <= cast(concat(cast(eomonth(STP.MESINI) as varchar), ' 23:59') as datetime)
+                ) >= STP.MESINI /* verifica mudança dentro do mês */
+                then
+                (
+                    select max(convert(datetime, concat(TPN010.TPN_DTINIC, ' ', TPN010.TPN_HRINIC)))
+                    from TPN010
+                    where
+                            TPN010.D_E_L_E_T_ = ''
+                        and TPN010.TPN_CODBEM = STP.TP_CODBEM
+                        and convert(datetime, concat(TPN010.TPN_DTINIC, ' ', TPN010.TPN_HRINIC)) <= cast(concat(cast(eomonth(STP.MESINI) as varchar), ' 23:59') as datetime)
+                ) /* se, pega mudança */
+            else cast(concat(cast(eomonth(STP.MESINI) as varchar), ' 23:59') as datetime) end /* senão, fim do mês*/
+        )/24.0
+    ) as DIAS_ANT,
+
+	(
+        select top 1 last_value(trim(TPN010.TPN_CCUSTO)) over (partition by TPN010.TPN_CODBEM order by TPN010.TPN_DTINIC, TPN010.TPN_HRINIC)
+        from TPN010
+        where
+                TPN010.D_E_L_E_T_ = ''
+            and TPN010.TPN_CODBEM = STP.TP_CODBEM
+            and convert(datetime, concat(TPN010.TPN_DTINIC, ' ', TPN010.TPN_HRINIC)) <= cast(concat(cast(eomonth(STP.MESINI) as varchar), ' 23:59') as datetime)
+	) as CC_PRO,
+
+	floor
+    (
+        datediff
+        (
+            hour,
+            case
+                when
+                (
+                    select max(convert(datetime, concat(TPN010.TPN_DTINIC, ' ', TPN010.TPN_HRINIC)))
+                    from TPN010
+                    where
+                            TPN010.D_E_L_E_T_ = ''
+                        and TPN010.TPN_CODBEM = STP.TP_CODBEM
+                        and convert(datetime, concat(TPN010.TPN_DTINIC, ' ', TPN010.TPN_HRINIC)) <= cast(concat(cast(eomonth(STP.MESINI) as varchar), ' 23:59') as datetime)
+                ) >= STP.MESINI /* verifica mudança dentro do mês */
+                then
+                (
+                    select max(convert(datetime, concat(TPN010.TPN_DTINIC, ' ', TPN010.TPN_HRINIC)))
+                    from TPN010
+                    where
+                            TPN010.D_E_L_E_T_ = ''
+                        and TPN010.TPN_CODBEM = STP.TP_CODBEM
+                        and convert(datetime, concat(TPN010.TPN_DTINIC, ' ', TPN010.TPN_HRINIC)) <= cast(concat(cast(eomonth(STP.MESINI) as varchar), ' 23:59') as datetime)
+                ) /* se, pega mudança */
+            else cast(concat(cast(eomonth(STP.MESINI) as varchar), ' 23:59') as datetime) end, /* senão, fim do mês*/
+            
+            dateadd(hour, 24, cast(concat(cast(eomonth(STP.MESINI) as varchar), ' 23:59') as datetime))
+        )/24.0
+    ) as DIAS_PRO,
+
+    ceiling(datediff(hour, STP.MESINI, cast(concat(cast(eomonth(STP.MESINI) as varchar), ' 23:59') as datetime))/24.0) as DIAS_PERIODO,
+	STP.MESINI as INI_PERIODO,
+	cast(concat(cast(eomonth(STP.MESINI) as varchar), ' 23:59') as datetime) as FIM_PERIODO,
+
+    (
+        select max(convert(datetime, concat(TPN010.TPN_DTINIC, ' ', TPN010.TPN_HRINIC)))
+        from TPN010
+        where
+                TPN010.D_E_L_E_T_ = ''
+            and TPN010.TPN_CODBEM = STP.TP_CODBEM
+            and convert(datetime, concat(TPN010.TPN_DTINIC, ' ', TPN010.TPN_HRINIC)) <= cast(concat(cast(eomonth(STP.MESINI) as varchar), ' 23:59') as datetime)
+    ) as MUD_CC
 
 from ST9010 ST9 (nolock)
 	inner join TQR010 TQR (nolock)
@@ -21,17 +110,15 @@ from ST9010 ST9 (nolock)
 	left join TQY010 TQY (nolock)
 		on TQY.D_E_L_E_T_ = ''
 		and TQY.TQY_STATUS = ST9.T9_STATUS
-	left join
+	inner join
 	(
 		select distinct
 			STP010.TP_CODBEM,
-            STP010.TP_CCUSTO,
-			STP010.TP_TIPOLAN as TIPO,
-			cast(STP010.TP_DTORIGI as date) as DATA_ORI,
-			cast(STP010.TP_DTLEITU as date) as DATA_LEI,
-			left(STP010.TP_DTLEITU, 6) as PERIODO
+            trim(STP010.TP_CCUSTO) as CC,
+			concat(left(STP010.TP_DTLEITU, 6), '01', ' 00:00') as MESINI,
+            left(STP010.TP_DTLEITU, 6) as PERIODO
 		from STP010 (nolock)
-		where STP.D_E_L_E_T_ = ''
+		where STP010.D_E_L_E_T_ = ''
 	) STP
 		on STP.TP_CODBEM = ST9.T9_CODBEM
 where
