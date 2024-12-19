ed0ee6d447f152fd1ddbc681512238cc78597a3c
Manoel <manoel.jose@unilinktransportes.com.br>
2024-12-19 16:53:01 -0300

    lançamento pode ser de OS ou não, para contemplar contas de despesa

--- FAST ANALYTICS/gooddata/query/fact/lançamentos resultado gerencial.sql
+++ FAST ANALYTICS/gooddata/query/fact/lançamentos resultado gerencial.sql
@@ -26,7 +26,7 @@ from ZE3010 ZE3
     inner join ZE2010 ZE2
         on ZE2.D_E_L_E_T_ = ''
         and ZE2.ZE2_COD = ZE3.ZE3_ITEMPL
-    inner join ZC1010 ZC1
+    left join ZC1010 ZC1
         on ZC1.D_E_L_E_T_ = ''
         and concat(ZC1.ZC1_FILIAL, ZC1.ZC1_NUM) = ZE3.ZE3_NUM
 
