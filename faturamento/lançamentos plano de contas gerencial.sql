b84599e74b6202f2add273675f16c7b5ac4b40b0
Manoel <manoel.jose@unilinktransportes.com.br>
2024-12-19 11:54:41 -0300

    adição da filial, da OS e da conta
    origem do lançamento mostra o CC correspondente

--- faturamento/lançamentos plano de contas gerencial.sql
+++ faturamento/lançamentos plano de contas gerencial.sql
@@ -2,9 +2,13 @@ select distinct
     ZE2.ZE2_COD as CODIGO,
     upper(trim(translate(lower(replace(ZE2.ZE2_DESC, ',', ' ')), 'áéíóúãõç', 'aeiouaoc'))) as DESCRICAO,
     concat(trim(ZE2.ZE2_COD), ' ', upper(trim(translate(lower(replace(ZE2.ZE2_DESC, ',', ' ')), 'áéíóúãõç', 'aeiouaoc')))) as CODDESC,
-    trim(ZE2.ZE2_ORIGEM) as ORIGEM,
+    trim(ZE3.ZE3_ORIGEM) as CC,
     ZE2.ZE2_MSBLQL as BLOQUEADO,
     ZE3.ZE3_COMPET as PERIODO,
+    ZE3.ZE3_NUM,
+    trim(ZE2.ZE2_CONTA) as CONTA,
+    left(ZE3.ZE3_NUM, 6) as FILORI,
+    right(trim(ZE3.ZE3_NUM), 11) as NUM_OS,
     
     case
         when len(trim(ZE2.ZE2_COD)) = 8 then left(trim(ZE2.ZE2_COD), 5)
@@ -14,8 +18,8 @@ select distinct
     end as CODSUP,
     ZE3.ZE3_VALOR as VALOR
 
-from ZE3010 ZE3
-    inner join ZE2010 ZE2
+from ZE3010 ZE3 (nolock)
+    inner join ZE2010 ZE2 (nolock)
         on ZE2.D_E_L_E_T_ = ''
         and ZE2.ZE2_COD = ZE3.ZE3_ITEMPL
 where
