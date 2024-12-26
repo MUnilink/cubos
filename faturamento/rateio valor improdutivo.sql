c30a66b09a807770849332b245d246029971c1dc
Manoel <manoel.jose@unilinktransportes.com.br>
2024-12-26 17:19:27 -0300

    lógica do rateio pendente de mais testes

--- faturamento/rateio valor improdutivo.sql
+++ faturamento/rateio valor improdutivo.sql
@@ -4,13 +4,13 @@ select
     ZC2.ZC2_ITEM as ITEM,
     ZG1.ZG1_FILORI,
     ZG1.ZG1_COMPET,
-    ZG1.ZG1_CODIGO,
-    ZG1.ZG1_TIPO,
+    case when cast(ZC2.ZC2_TIPO as int) in (15, 16) then ZG1.ZG1_TIPO else cast(ZC2.ZC2_TIPO as int) end as ZC2_TIPO,
+    trim(ZG1.ZG1_CODIGO) as INSUMO,
     cast(ZG1.ZG1_VLIMPR as numeric(15, 2)) as VAL_IMPR,
-    cast(ZG1.VL_ITOTAL as numeric(15, 2)) as VAL_TOTAL,
+    cast(ZG1.perc as numeric(15, 2)) as SOMA_IMPR,
     cast(ZC2.ZC2_TOTAL as numeric(15, 2)) as VAL_ITEM,
-    cast(ZG1.VL_RIMP as numeric(15, 2)) as RATEIO,
-    cast(ZC2.ZC2_TOTAL * ZG1.VL_RIMP as numeric(15, 2)) as VIMP_ITEM
+    cast(ZG1.RATEIO as numeric(15, 2)) as RATEIO,
+    cast(ZC2.ZC2_TOTAL * ZG1.RATEIO as numeric(15, 2)) as VIMP_ITEM
 from ZC2010 ZC2 (nolock)
     inner join
     (
@@ -20,33 +20,33 @@ from ZC2010 ZC2 (nolock)
             G1.ZG1_CODIGO,
             G1.ZG1_TIPO,
             G1.ZG1_VLIMPR,
-            G1.ZG1_VLIMPR/
+            case when G1.ZG1_HRIMPR != 0 then G1.ZG1_VLIMPR/
             (
-                select sum(ZG1010.ZG1_VLIMPR)
+                select case when coalesce(nullif(floor(sum(ZG1010.ZG1_VLIMPR)), 0), 0) = 0 then 1 else sum(ZG1010.ZG1_VLIMPR) end
                 from ZG1010
                 where
-                    ZG1010.D_E_L_E_T_ = ''
-                and ZG1010.ZG1_VLIMPR != 0
+                    ZG1010.ZG1_VLIMPR != 0
                 and ZG1010.ZG1_FILORI = G1.ZG1_FILORI
                 and ZG1010.ZG1_COMPET = G1.ZG1_COMPET
                 and ZG1010.ZG1_CODIGO = G1.ZG1_CODIGO
-            ) as VL_RIMP,
+                and ZG1010.D_E_L_E_T_ = ''
+            ) else 0.0 end as RATEIO,
             (
-                select sum(ZG1010.ZG1_VLIMPR)
+                select case when coalesce(nullif(floor(sum(ZG1010.ZG1_VLIMPR)), 0), 0) = 0 then 1 else sum(ZG1010.ZG1_VLIMPR) end
                 from ZG1010
                 where
-                    ZG1010.D_E_L_E_T_ = ''
-                and ZG1010.ZG1_VLIMPR != 0
+                    ZG1010.ZG1_VLIMPR != 0
                 and ZG1010.ZG1_FILORI = G1.ZG1_FILORI
                 and ZG1010.ZG1_COMPET = G1.ZG1_COMPET
                 and ZG1010.ZG1_CODIGO = G1.ZG1_CODIGO
-            ) as VL_ITOTAL
+                and ZG1010.D_E_L_E_T_ = ''
+            ) as perc
         from ZG1010 G1 (nolock)
-        where G1.D_E_L_E_T_ = '' and G1.ZG1_TIPO = '2'
+        where G1.D_E_L_E_T_ = ''
     ) ZG1
         on left(ZC2.ZC2_COMPET, 6) = ZG1.ZG1_COMPET
         and ZC2.ZC2_FILIAL = ZG1.ZG1_FILORI
         and ZC2.ZC2_COD = trim(ZG1.ZG1_CODIGO)
+        and cast(ZC2.ZC2_TIPO as int) = case when ZG1.ZG1_TIPO in (2, 14) then 15 when ZG1.ZG1_TIPO in (3, 6, 9, 12) then 16 else 0 end
 where
         ZC2.D_E_L_E_T_ = ''
-    and ZC2.ZC2_TIPO = '15'
