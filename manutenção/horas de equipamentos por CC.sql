24bbd67ed2fa149c32d9c11f0d5c200f69ed28c0
Manoel <manoel.jose@unilinktransportes.com.br>
2024-12-31 11:46:42 -0300

    pendente regra de transferência múltipla no período

--- manutenção/horas de equipamentos por CC.sql
+++ manutenção/horas de equipamentos por CC.sql
@@ -17,12 +17,29 @@ select
             and convert(datetime, concat(TPN010.TPN_DTINIC, ' ', TPN010.TPN_HRINIC)) <= STP.MESINI
 	) as CC_ANT,
 
+    case
+        when
+                exists (select * from TPN010 where TPN010.D_E_L_E_T_ = '' and TPN010.TPN_CODBEM = STP.TP_CODBEM and left(TPN010.TPN_DTINIC, 6) = STP.PERIODO)
+            and (select min(convert(datetime, concat(TPN010.TPN_DTINIC, ' ', TPN010.TPN_HRINIC))) from TPN010 where TPN010.D_E_L_E_T_ = '' and TPN010.TPN_CODBEM = STP.TP_CODBEM and left(TPN010.TPN_DTINIC, 6) = STP.PERIODO) > STP.MESINI
+            /* se mudança no período e se depois do princípio, então dias do princípio até a mudança */
+        then datediff(hour, STP.MESINI, (select min(convert(datetime, concat(TPN010.TPN_DTINIC, ' ', TPN010.TPN_HRINIC))) from TPN010 where TPN010.D_E_L_E_T_ = '' and TPN010.TPN_CODBEM = STP.TP_CODBEM and left(TPN010.TPN_DTINIC, 6) = STP.PERIODO))
+        when not exists (select * from TPN010 where TPN010.D_E_L_E_T_ = '' and TPN010.TPN_CODBEM = STP.TP_CODBEM and left(TPN010.TPN_DTINIC, 6) = STP.PERIODO) then ceiling(datediff(hour, STP.MESINI, cast(concat(cast(eomonth(STP.MESINI) as varchar), ' 23:59') as datetime))/24.0)
+    else 0 end/24.0 as DIAS_ANT,
+    
+    case
+        when
+                exists (select * from TPN010 where TPN010.D_E_L_E_T_ = '' and TPN010.TPN_CODBEM = STP.TP_CODBEM and left(TPN010.TPN_DTINIC, 6) = STP.PERIODO)
+            and (select max(convert(datetime, concat(TPN010.TPN_DTINIC, ' ', TPN010.TPN_HRINIC))) from TPN010 where TPN010.D_E_L_E_T_ = '' and TPN010.TPN_CODBEM = STP.TP_CODBEM and left(TPN010.TPN_DTINIC, 6) = STP.PERIODO) < cast(concat(cast(eomonth(STP.MESINI) as varchar), ' 23:59') as datetime)
+            /* se mudança no período e se antes do fim, então dias da mudança até o fim */
+        then datediff(hour, (select max(convert(datetime, concat(TPN010.TPN_DTINIC, ' ', TPN010.TPN_HRINIC))) from TPN010 where TPN010.D_E_L_E_T_ = '' and TPN010.TPN_CODBEM = STP.TP_CODBEM and left(TPN010.TPN_DTINIC, 6) = STP.PERIODO), cast(concat(cast(eomonth(STP.MESINI) as varchar), ' 23:59') as datetime))
+        when not exists (select * from TPN010 where TPN010.D_E_L_E_T_ = '' and TPN010.TPN_CODBEM = STP.TP_CODBEM and left(TPN010.TPN_DTINIC, 6) = STP.PERIODO) then ceiling(datediff(hour, STP.MESINI, cast(concat(cast(eomonth(STP.MESINI) as varchar), ' 23:59') as datetime))/24.0)
+    else 0 end/24.0 as DIAS_PRO,
+
     floor
     (
         datediff
         (
             hour,
-            STP.MESINI,
             case
                 when
                 (
@@ -32,7 +49,7 @@ select
                             TPN010.D_E_L_E_T_ = ''
                         and TPN010.TPN_CODBEM = STP.TP_CODBEM
                         and convert(datetime, concat(TPN010.TPN_DTINIC, ' ', TPN010.TPN_HRINIC)) <= cast(concat(cast(eomonth(STP.MESINI) as varchar), ' 23:59') as datetime)
-                ) >= STP.MESINI /* verifica mudança dentro do mês */
+                ) > STP.MESINI /* verifica mudança após princípio do mês */
                 then
                 (
                     select max(convert(datetime, concat(TPN010.TPN_DTINIC, ' ', TPN010.TPN_HRINIC)))
@@ -41,8 +58,28 @@ select
                             TPN010.D_E_L_E_T_ = ''
                         and TPN010.TPN_CODBEM = STP.TP_CODBEM
                         and convert(datetime, concat(TPN010.TPN_DTINIC, ' ', TPN010.TPN_HRINIC)) <= cast(concat(cast(eomonth(STP.MESINI) as varchar), ' 23:59') as datetime)
+                ) /* se, mudança */
+            else STP.MESINI end, /* se não, princípio */
+            case
+                when
+                (
+                    select min(convert(datetime, concat(TPN010.TPN_DTINIC, ' ', TPN010.TPN_HRINIC)))
+                    from TPN010
+                    where
+                            TPN010.D_E_L_E_T_ = ''
+                        and TPN010.TPN_CODBEM = STP.TP_CODBEM
+                        and convert(datetime, concat(TPN010.TPN_DTINIC, ' ', TPN010.TPN_HRINIC)) > STP.MESINI
+                ) > cast(concat(cast(eomonth(STP.MESINI) as varchar), ' 23:59') as datetime) /* verifica antes do fim do mês */
+                then
+                (
+                    select min(convert(datetime, concat(TPN010.TPN_DTINIC, ' ', TPN010.TPN_HRINIC)))
+                    from TPN010
+                    where
+                            TPN010.D_E_L_E_T_ = ''
+                        and TPN010.TPN_CODBEM = STP.TP_CODBEM
+                        and convert(datetime, concat(TPN010.TPN_DTINIC, ' ', TPN010.TPN_HRINIC)) > STP.MESINI
                 ) /* se, pega mudança */
-            else cast(concat(cast(eomonth(STP.MESINI) as varchar), ' 23:59') as datetime) end /* senão, fim do mês*/
+            else cast(concat(cast(eomonth(STP.MESINI) as varchar), ' 23:59') as datetime) end /* se não, fim do mês*/
         )/24.0
     ) as DIAS_ANT,
 
@@ -79,7 +116,7 @@ select
                         and TPN010.TPN_CODBEM = STP.TP_CODBEM
                         and convert(datetime, concat(TPN010.TPN_DTINIC, ' ', TPN010.TPN_HRINIC)) <= cast(concat(cast(eomonth(STP.MESINI) as varchar), ' 23:59') as datetime)
                 ) /* se, pega mudança */
-            else cast(concat(cast(eomonth(STP.MESINI) as varchar), ' 23:59') as datetime) end, /* senão, fim do mês*/
+            else cast(concat(cast(eomonth(STP.MESINI) as varchar), ' 23:59') as datetime) end, /* se não, fim do mês*/
             
             dateadd(hour, 24, cast(concat(cast(eomonth(STP.MESINI) as varchar), ' 23:59') as datetime))
         )/24.0
