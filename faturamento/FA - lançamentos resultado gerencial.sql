690f80f84ce4deeaebad38a66bfac5e69d7f480b
Manoel <manoel.jose@unilinktransportes.com.br>
2025-01-06 10:15:03 -0300

    remoção do tratamento das contas N1 a N3 para relatório N3

--- faturamento/FA - lançamentos resultado gerencial.sql
+++ faturamento/FA - lançamentos resultado gerencial.sql
@@ -18,8 +18,9 @@ select distinct
     
     case
         when ZE2.ZE2_ORIGEM = 'F' then abs(ZE3.ZE3_VALOR)
-        when len(ZE2.ZE2_COD) = 8 and left(ZE2.ZE2_COD, 2) = '01' then ZE3.ZE3_VALOR
-        when len(ZE2.ZE2_COD) = 8 and left(ZE2.ZE2_COD, 2) like '_[2-9]' then abs(ZE3.ZE3_VALOR)*-1
+        when left(ZE2.ZE2_COD, 2) = '01' then ZE3.ZE3_VALOR
+        when left(ZE2.ZE2_COD, 2) like '0[2-9]' then abs(ZE3.ZE3_VALOR)*-1
+        when left(ZE2.ZE2_COD, 2) like '_[1-9]' then abs(ZE3.ZE3_VALOR)*-1
     else 0.0 end as VALOR,
     
     /* para validação no RM */
