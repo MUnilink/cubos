88dad4768c8b09559251e2973d4c75e624e7a512
Manoel <manoel.jose@unilinktransportes.com.br>
2025-01-22 11:32:34 -0300

    número da OS de int para string

--- FAST ANALYTICS/gooddata/query/dim/OS faturamento.sql
+++ FAST ANALYTICS/gooddata/query/dim/OS faturamento.sql
@@ -1,6 +1,6 @@
 select
     concat(trim(ZC1.ZC1_FILIAL), trim(ZC1.ZC1_NUM)) as BK_OSPORTUARIA,
-    cast(substring(ZC1.ZC1_NUM, 6, 10) as int) as OS,
+    substring(ZC1.ZC1_NUM, 6, 10) as OS,
     trim(ZC1.ZC1_VIAGEM) as VIAGEM_PORT,
     (select trim(ZA3010.ZA3_DESC) from ZA3010 where ZA3010.D_E_L_E_T_ = '' and ZA3010.ZA3_COD = ZC1.ZC1_NAVIO) as DESC_NAVIO,
     (select trim(SX5010.X5_DESCRI) from SX5010 where SX5010.D_E_L_E_T_ = '' and SX5010.X5_TABELA = '_1' and SX5010.X5_CHAVE = ZC1.ZC1_PORTO) as DESC_PORTO,
