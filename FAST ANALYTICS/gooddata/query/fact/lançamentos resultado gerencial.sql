8eea148bd5d0ad602e0f61a140f702fa8fddd52f
Manoel <manoel.jose@unilinktransportes.com.br>
2025-01-24 14:42:10 -0300

    sintaxe

--- FAST ANALYTICS/gooddata/query/fact/lançamentos resultado gerencial.sql
+++ FAST ANALYTICS/gooddata/query/fact/lançamentos resultado gerencial.sql
@@ -39,7 +39,7 @@ select distinct
         when left(ZE2.ZE2_COD, 2) = '01' then ZE3.ZE3_VALOR
         when left(ZE2.ZE2_COD, 2) = '11' then ZE3.ZE3_VALOR*-1
         when left(ZE2.ZE2_COD, 2) like '[0-9][2-9]' then ZE3.ZE3_VALOR*-1
-    else 0.0 end as VALOR,
+    else 0.0 end as VALOR
 
 from ZE3010 ZE3
     inner join ZE2010 ZE2
