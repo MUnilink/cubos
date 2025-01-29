d926d8a1ad266b9365626d7b9a53b1398afbcb13
Manoel <manoel.jose@unilinktransportes.com.br>
2025-01-29 09:58:17 -0300

    contas a pagar

---
+++ financeiro/contas a pagar.sql
@@ -0,0 +1,204 @@
+select
+	trim(SE2.E2_FILIAL) as FILIAL_TITULO,
+	trim(SE2.E2_PREFIXO) as PREFIXO,
+	trim(SE2.E2_NUM) as TITULO,
+	trim(SE2.E2_TIPO) as TIPO_TITULO,
+	cast(SE2.E2_EMISSAO as date) as DATA_TITULO,
+	cast(SE2.E2_VENCTO as date) as VENCIMENTO,
+	cast(SE2.E2_VENCREA as date) as VENCREAL,
+	cast(SE2.E2_BAIXA as date) as BAIXA,
+	SE2.E2_PARCELA as PARCELA,
+	SE2.E2_VALOR as VALOR_TITULO,
+
+    trim(SE2.E2_HIST) as HISTORICO,
+    SE2.E2_SALDO as SALDO,
+    SE2.E2_DESCONT as DESCONT,
+    SE2.E2_MULTA as MULTA,
+    SE2.E2_JUROS as JUROS,
+    SE2.E2_CORREC as CORREC,
+    SE2.E2_VALLIQ as VALOR_LIQ,
+    SE2.E2_NUMBOR as BORDERO,
+    trim(SE2.E2_TITORIG) as TITULO_ORI,
+    trim(SE2.E2_ORIGEM) as ORIGEM,
+    cast(SE2.E2_DATALIB as date) as DT_LIBTIT,
+    trim(SE2.E2_APROVA) as APROVA_TIT,
+    trim(SE2.E2_USUALIB) as LIBERA_TIT,
+	
+    trim(SC7.C7_FILIAL) as FILIAL,
+	trim(SB1.B1_COD) as PRODUTO,
+	trim(SB1.B1_DESC) as NOMEPRODUTO,
+	trim(SB1.B1_GRUPO) as GRUPO,
+	trim(SB1.B1_UM) as UN,
+	trim(CTD.CTD_DESC01) as ATIVIDADE,
+	trim(CTT.CTT_DESC01) as CCUSTO,
+	trim(SC7.C7_ITEMCTA) as AT,
+	trim(SC7.C7_CC) as CC,
+	trim(SC1.C1_NUM) as SC,
+	trim(SC1.C1_ITEM) as ITEM_SC,
+	trim(upper(SC1.C1_SOLICIT)) as SOLICITANTE_SC,
+	cast(SC1.C1_EMISSAO as date) as DATA_SC,
+	substring(SC1.C1_EMISSAO, 1, 6) as PERIODO_SC,
+	substring(SC1.C1_OP, 1, 6) as OS,
+	
+	SC1.C1_QUANT as QTD_SC_PEDIDA,
+	SC1.C1_QUJE as QTD_SC_ATENDIDA,
+	case SC1.C1_RESIDUO when 'S' then 'ELIMINADA' else '' end as C1_RESIDUO,
+
+	case SC1.C1_APROV
+		when 'B' then 'PENDENTE'
+		when 'L' then 'APROVADO'
+		when 'R' then 'REJEITADO'
+		else 'OUTROS'
+	end as SITAPR_SC,
+
+	trim(SC7.C7_NUM) as PEDIDO,
+	trim(SC7.C7_ITEM) as ITEM_PC,
+	trim(SC7.C7_FORNECE) as FORNECEDOR,
+	trim(SC7.C7_LOJA) as LOJA,
+	trim(SA2.A2_NOME) as NOME_FORNECEDOR,
+	trim(SA2.A2_CGC) as CNPJ,
+	trim(SA2.A2_EST) as UF,
+	trim(replace(replace(SC7.C7_OBS, char(10), ''), char(13), '')) as OBS_PC,
+	trim(replace(replace(SC7.C7_OBSM, char(10), ''), char(13), '')) as MEMO_PC,
+
+	cast(SC7.C7_EMISSAO as date) as DATA_PEDIDO,
+	substring(SC7.C7_EMISSAO, 1, 6) as PERIODO_PC,
+	(select trim(upper(SY1010.Y1_NOME)) from SY1010 where SY1010.Y1_COD = SC7.C7_COMPRA) as SOLICITANTE_PC,
+	trim(upper(SY1.Y1_NOME)) as DIGITACAO_PC,
+
+	case SC7.C7_CONAPRO
+		when 'B' then 'PENDENTE'
+		when 'L' then 'APROVADO'
+		when 'R' then 'REJEITADO'
+		else 'OUTROS'
+	end as APROVACAO_PC,
+
+	(
+		select upper(trim(max(SAK010.AK_LOGIN)))
+        from SCR010 SCR
+            inner join SAK010
+                on SAK010.D_E_L_E_T_ = ''
+                and SAK010.AK_COD = SCR.CR_LIBAPRO
+		where
+				SCR.D_E_L_E_T_ = ''
+			and SCR.CR_TIPO = 'PC'
+			and SCR.CR_FILIAL = SC7.C7_FILIAL
+			and SCR.CR_NUM = SC7.C7_NUM
+			and SCR.CR_NIVEL =
+		(
+			select max(SCR010.CR_NIVEL)
+			from SCR010 (nolock)
+			where
+					SCR010.D_E_L_E_T_ = ''
+				and SCR010.CR_TIPO = SCR.CR_TIPO
+				and SCR010.CR_FILIAL = SCR.CR_FILIAL
+				and SCR010.CR_NUM = SCR.CR_NUM
+				and SCR010.CR_STATUS = '3'
+		)
+	) as APROVADOR,
+
+	SC7.C7_COND as COND,
+	trim(SE4.E4_DESCRI) as CONDPGTO,
+	SC7.C7_QUANT as QTD_PC_PEDIDA,
+	SC7.C7_QUJE as QTD_PC_ATENDIDA,
+	SC7.C7_PRECO as PC_PRECO,
+	SC7.C7_TOTAL as PC_TOTAL,
+
+	year(SC1.C1_EMISSAO) as ANO_SOLICITA,
+	month(SC1.C1_EMISSAO) as MES_SOLICITA,
+
+	year(SC7.C7_EMISSAO) as ANO_PEDIDO,
+	month(SC7.C7_EMISSAO) as MES_PEDIDO,
+
+	case
+		when trim(SC7.C7_RESIDUO) = 'S' then 'ELIMINADO' /* CINZA */
+		when trim(SC7.C7_CONAPRO) = 'B' and (cast(SC7.C7_QUJE as numeric(15, 2)) < cast(SC7.C7_QUANT as numeric(15, 2))) then 'BLOQUEADO' /* AZUL */
+		when cast(SC7.C7_QUJE as numeric(15, 2)) >= cast(SC7.C7_QUANT as numeric(15, 2)) then 'RECEBIDO' /* VERMELHO */
+		when cast(SC7.C7_QUJE as numeric(15, 2)) != 0.00 and (cast(SC7.C7_QUJE as numeric(15, 2)) < cast(SC7.C7_QUANT as numeric(15, 2))) then 'REC. PARCIAL' /* AMARELO */
+		when cast(SC7.C7_QTDACLA as numeric(15, 2)) > 0.00 then 'PRÉ-NOTA' /* LARANJA */
+		when cast(SC7.C7_TIPO as int) = 1 and SC7.C7_RESIDUO = '' then 'APROVADO' /* VERDE */
+	else 'OUTROS' end as STATUS_COMPRA,
+
+	SD1.D1_DOC as NF_DOC,
+	SD1.D1_SERIE as NF_SERIE,
+	cast(SD1.D1_EMISSAO as date) as NF_EMI,
+	cast(SD1.D1_DTDIGIT as date) as NF_DATA,
+	substring(SD1.D1_DTDIGIT, 1, 6) as NF_PERIODO,
+	
+	SD1.D1_CC as NF_CC,
+	SD1.D1_ITEMCTA as NF_AT,
+	SD1.D1_ITEM as NF_ITEM,
+	SD1.D1_QUANT as NF_QUANT,
+	SD1.D1_VUNIT as NF_VUNIT,
+	SD1.D1_TOTAL as NF_TOTAL,
+	SD1.D1_TES as NF_TES,
+	SD1.D1_CUSTO as NF_CUSTO,
+	SD1.D1_VALDESC as NF_VALDESC,
+
+    cast(SC7.C7_VALICM as numeric(14, 2)) as VL_PC_ICMS,
+    cast(SC7.C7_VALIPI as numeric(14, 2)) as VL_PC_IPI,
+    cast(SC7.C7_VALFRE as numeric(14, 2)) as VL_PC_FRETE_NF,
+    cast(SC7.C7_DESPESA as numeric(14, 2)) as VL_PC_DESPESA,
+    cast(SC7.C7_VALIMP6 as numeric(14, 2)) as VL_PC_PIS,
+    cast(SC7.C7_VALIMP5 as numeric(14, 2)) as VL_PC_COFINS,
+    cast(SC7.C7_VALISS as numeric(14, 2)) as VL_PC_ISS,
+    cast(SC7.C7_ICMSRET as numeric(14, 2)) as VL_PC_ICMS_SUBST,
+    cast(SC7.C7_DESC as numeric(12, 2)) as VL_PC_DESCONTO,
+    cast(SC7.C7_VALINS as numeric(14, 2)) as VL_PC_INSS,
+	cast(SC7.C7_SEGURO as numeric(14, 2)) as VL_PC_SEGURO,
+	cast(SC7.C7_QUANT as numeric(13, 3)) as QTD_ITEM_PC,
+
+    cast(SD1.D1_VALICM as numeric(14, 2)) as VL_NFENT_ICMS,
+    cast(SD1.D1_VALIPI as numeric(14, 2)) as VL_NFENT_IPI,
+    cast(SD1.D1_VALFRE as numeric(14, 2)) as VL_NFENT_FRETE_NF,
+    cast(SD1.D1_DESPESA as numeric(14, 2)) as VL_NFENT_DESPESA,
+    cast(SD1.D1_VALIMP6 as numeric(14, 2)) as VL_NFENT_PIS,
+    cast(SD1.D1_VALIMP5 as numeric(14, 2)) as VL_NFENT_COFINS,
+    cast(SD1.D1_VALISS as numeric(14, 2)) as VL_NFENT_ISS,
+    cast(SD1.D1_ICMSRET as numeric(14, 2)) as VL_NFENT_ICMS_SUBST,
+    cast(SD1.D1_DESC as numeric(12, 2)) as VL_NFENT_DESCONTO,
+    cast(SD1.D1_VALIRR as numeric(14, 2)) as VL_NFENT_IRF,
+    cast(SD1.D1_VALINS as numeric(14, 2)) as VL_NFENT_INSS,
+	cast(SD1.D1_SEGURO as numeric(14, 2)) as VL_NFENT_SEGURO,
+	cast(SD1.D1_PESO * SD1.D1_QUANT as numeric(12, 4)) as PESO_LIQUIDO_NFENT
+
+from SE2010 SE2 (nolock)
+	left join SB1010 SB1 (nolock)
+		on SB1.D_E_L_E_T_ = ''
+		and SB1.B1_COD = SC7.C7_PRODUTO
+
+		inner join SBM010 SBM (nolock)
+			on SBM.D_E_L_E_T_ = ''
+			and SBM.BM_GRUPO = SB1.B1_GRUPO
+
+	inner join SA2010 SA2 (nolock)
+		on SA2.D_E_L_E_T_ = ''
+		and SA2.A2_COD = SE2.E2_FORNECE
+		and SA2.A2_LOJA = SE2.E2_LOJA
+	left join CTT010 CTT (nolock)
+		on CTT.D_E_L_E_T_ = ''
+		and CTT.CTT_CUSTO = SC7.C7_CC
+	left join CTD010 CTD (nolock)
+		on CTD.D_E_L_E_T_ = ''
+		and CTD.CTD_ITEM = SC7.C7_ITEMCTA
+	
+    left join SD1010 SD1 (nolock)
+        on SD1.D_E_L_E_T_ = ''
+        and SD1.D1_FILIAL = SE2.E2_FILIAL
+        and SD1.D1_DOC = SE2.E2_NUM
+        and SD1.D1_FORNECE = SE2.E2_FORNECE
+        and SD1.D1_LOJA = SE2.E2_LOJA
+	
+        left join SC7010 SC7 (nolock)
+            on SC7.D_E_L_E_T_ = ''
+            and SC7.C7_FILIAL = SD1.D1_FILIAL
+            and SC7.C7_NUM = SD1.D1_PEDIDO
+            and SC7.C7_ITEM = SD1.D1_ITEMPC
+
+            left join SE4010 SE4 (nolock)
+                on SE4.D_E_L_E_T_ = ''
+                and SE4.E4_CODIGO = SC7.C7_COND
+            left join SY1010 SY1 (nolock)
+		        on SY1.Y1_USER = SC7.C7_USER
+
+where SE2.D_E_L_E_T_ = ''
