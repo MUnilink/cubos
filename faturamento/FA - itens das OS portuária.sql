7732d7a524abef7f418d1fb44e7620e59f031422
Manoel <manoel.jose@unilinktransportes.com.br>
2025-01-30 17:13:36 -0300

    correção da conversão da hora UTC

--- faturamento/FA - itens das OS portuária.sql
+++ faturamento/FA - itens das OS portuária.sql
@@ -1,5 +1,5 @@
 select
-    convert(datetime, dateadd(hour, -3, getutcdate()), 113) as ULTIMA_CARGA,
+    convert(datetime, getutcdate(), 113) as ULTIMA_CARGA,
     'P |01|01' AS BK_EMPRESA,
     CASE WHEN ZC1.ZC1_FILIAL IS NULL THEN 'P |01||' ELSE 'P |01|01'+ CAST(ZC1.ZC1_FILIAL AS CHAR (6)) END AS BK_FILIAL,
     'P |01|SA1010|'+ COALESCE(NULLIF(RTRIM(COALESCE(SA1.A1_FILIAL, ' '))+'|'+RTRIM(COALESCE(SA1.A1_COD, ' '))+RTRIM(COALESCE(SA1.A1_LOJA, ' ')), ' '), '|') as BK_CLIENTE,
