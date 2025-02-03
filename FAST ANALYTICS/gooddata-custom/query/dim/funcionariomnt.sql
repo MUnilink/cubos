b90d9b1e7f1d2ed68c12c68377ceceae7777726f
Manoel <manoel.jose@unilinktransportes.com.br>
2025-02-03 14:00:50 -0300

    adição do turno do funcionário

--- FAST ANALYTICS/gooddata-custom/query/dim/funcionariomnt.sql
+++ FAST ANALYTICS/gooddata-custom/query/dim/funcionariomnt.sql
@@ -1,5 +1,9 @@
 select
 	trim(ST1.T1_CODFUNC) as T1_CODFUNC,
-	trim(ST1.T1_NOME) as T1_NOME
-from ST1010 as ST1
+	trim(ST1.T1_NOME) as T1_NOME,
+	trim(SH7.H7_DESCRI) as TURNO
+from ST1010 ST1
+	left join SH7010 SH7
+		on SH7.D_E_L_E_T_ = ''
+		and SH7.H7_CODIGO = ST1.T1_TURNO
 where ST1.D_E_L_E_T_ = ''
