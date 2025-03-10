dc3e42bc2a1020c11e692631fb94f2ce51e640b4
Manoel <manoel.jose@unilinktransportes.com.br>
2025-03-10 10:15:10 -0300

    encontradas tabelas e chaves, mas tornada obsoleta pela consulta do compras

--- TOP MANAGER/lista de tabelas e campos.sql
+++ TOP MANAGER/lista de tabelas e campos.sql
@@ -1,10 +1,15 @@
 select
-    tabs.object_id as ID_tabela,
-    tabs.name as tabela,
-	tabs.type_desc as tipo_obj,
+	tabori.type_desc as tipo_ori,
+    tabori.name as tabela_ori,
+	fk_colsori.name as nome_fkcolunaori,
+	tabref.type_desc as tipo_ref,
+	tabref.name as tabela_ref,
+	fk_colsref.name as nome_fkcolunaref,
+	tabori.object_id as ID_tabela_ori,
+	tabref.object_id as ID_tabela_ref,
 	fk.name as chave_estr,
-	fk.name as nome_fk,
-	fk_colsref.name as nome_fkcolunaref
+	fk.name as nome_fk
+
 from sys.foreign_keys fk
 	inner join sys.foreign_key_columns fk_ref
 		inner join sys.all_columns fk_colsref
@@ -16,11 +21,12 @@ from sys.foreign_keys fk
 			on fk_colsori.object_id = fk_ori.parent_object_id
 			and fk_colsori.column_id = fk_ori.parent_column_id
 		on fk_ori.constraint_object_id = fk.object_id
-	inner join sys.all_objects tabsref on fk.referenced_object_id = tabsref.object_id
-	inner join sys.all_objects tabsori on fk.parent_object_id = tabsori.object_id
+	inner join sys.all_objects tabref on tabref.object_id = fk.referenced_object_id
+	inner join sys.all_objects tabori on tabori.object_id = fk.parent_object_id
 
 where
-        tabs.schema_id = 1
-    and tabs.type_desc in ('USER_TABLE', 'VIEW') --('FOREIGN_KEY_CONSTRAINT', 'PRIMARY_KEY_CONSTRAINT')
-    --and lower(tabs.name) in ('tblan', 'tblnt', 'tbtra')
-	and lower(tabs.name) = 'tbcld'
+        tabref.schema_id = 1 and tabori.schema_id = 1
+    --and tabref.type_desc in ('USER_TABLE', 'VIEW')
+    and lower(tabref.name) in ('tbdoc')
+	--and lower(tabori.name) in ('tbpes')
+order by 2
