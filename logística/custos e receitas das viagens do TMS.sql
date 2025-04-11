dc07d33dd2978923ae631b92a05e1719b4a8a471
Manoel <manoel.jose@unilinktransportes.com.br>
2025-04-11 11:47:19 -0300

    adição da viagem, do motorista e dos veículos

--- logística/custos e receitas das viagens do TMS.sql
+++ logística/custos e receitas das viagens do TMS.sql
@@ -1,10 +1,19 @@
 select
+    ZE1.ZE1_NUM as VIAGEM,
+    ZE4.ZE4_DATAFI as DATA_FIM,
+    left(ZE4.ZE4_DATAFI, 6) as PERIODO,
     ZE4.ZE4_TOTHR as VGA_HORAS,
     ZE4.ZE4_STATUS as STATUS_TMS,
     ZE4.ZE4_KMINI as km_ini,
     ZE4.ZE4_KMFIM as km_fim,
     ZE4.ZE4_KMFIM - ZE4.ZE4_KMINI as km_VIAGEM,
-    ZE5.ZE5_ITENS as VGA_ITEMCOM,
+    
+    trim(ZE5.ZE5_ITENS) as VGA_ITEMCOM,
+    trim(ZE5.ZE5_MOTORI) as MOTORISTA,
+    trim(ZE5.ZE5_BEMCAV) as CM,
+    trim(ZE5.ZE5_CARR1) as SR1,
+    trim(ZE5.ZE5_CARR2) as SR2,
+    trim(ZE5.ZE5_CARR3) as SR3,
     
     trim(ZE1.ZE1_COD) as VGA_CODIGO,
     cast(ZE1.ZE1_TOTAL as numeric(15, 2)) as VGA_VALOR,
@@ -42,7 +51,7 @@ select
     else null end as DESC_RECURSO
 
 from ZE1010 ZE1 (nolock)
-    inner join ZE4010 ZE4 (nolock)
+    left join ZE4010 ZE4 (nolock)
         on ZE4.D_E_L_E_T_ = ''
         and ZE4.ZE4_FILIAL = ZE1.ZE1_FILIAL
         and ZE4.ZE4_VIAGEM = ZE1.ZE1_NUM
