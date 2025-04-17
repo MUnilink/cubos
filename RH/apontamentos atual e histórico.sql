753d37bd4fc07b0ce06b71a5aa230b7baaf7ae8f
Manoel <manoel.jose@unilinktransportes.com.br>
2025-04-17 17:03:02 -0300

    consulta de eventos do ponto

---
+++ RH/apontamentos atual e histórico.sql
@@ -0,0 +1,105 @@
+    select 
+        trim(SRA.RA_FILIAL) as FILIAL,
+        trim(SRA.RA_MAT) as MATRICULA,
+        trim(SRA.RA_NOMECMP) as NOME,
+        trim(SRJ.RJ_DESC) as FUNCAO,
+        trim(SQ3.Q3_DESCSUM) as CARGO,
+        convert(date, SRA.RA_ADMISSA, 103) as ADMISSAO,
+        case SRA.RA_SITFOLH when '' then 'OK' else SRA.RA_SITFOLH end as SITUACAO,
+        case when trim(SRA.RA_SITFOLH) != 'D' then 'S' else 'N' end as ATIVO,
+        trim(CTT.CTT_CUSTO) as CC,
+        trim(CTT.CTT_DESC01) as CCUSTO,
+        trim(CTD.CTD_ITEM) as ITCT,
+        trim(CTD.CTD_DESC01) as ATIVIDADE,
+        trim(SQB.QB_DEPTO) as DEPTO,
+        trim(SQB.QB_DESCRIC) as DEPARTAMENTO,
+        trim(SRJ.RJ_CODCBO) as CBO,
+        trim(SRA.RA_SEXO) as SEXO,
+        trim(SRA.RA_CIC) as CPF,
+
+        SPH.PH_PD as EVENTO,
+        concat(SPH.PH_PD, ' - ', (select trim(SP9010.P9_DESC) from SP9010 (nolock) where SP9010.D_E_L_E_T_ = '' and SP9010.P9_CODIGO = SPH.PH_PD)) as DESC_EVENTO,
+        cast(SPH.PH_DATA as date) as DATA,
+        left(SPH.PH_DATA, 6) as PERIODO,
+        cast(SPH.PH_QUANTC as numeric(15, 2)) as QTD,
+        cast(floor(SPH.PH_QUANTC) as int) as HORAS,
+        cast((SPH.PH_QUANTC - floor(SPH.PH_QUANTC))*60.0 as numeric(15,2)) as MINUTOS
+    from SPH010 SPH (nolock)
+        inner join SRA010 SRA (nolock)
+            on SRA.D_E_L_E_T_ = ''
+            and SRA.RA_FILIAL = SPH.PH_FILIAL
+            and SRA.RA_MAT = SPH.PH_MAT
+
+            inner join SQB010 SQB (nolock)
+                on SQB.D_E_L_E_T_ = ''
+                and SQB.QB_FILIAL = substring(SRA.RA_FILIAL, 1, 4)
+                and SQB.QB_DEPTO = SRA.RA_DEPTO
+            inner join SRJ010 SRJ (nolock)
+                on SRJ.D_E_L_E_T_ = ''
+                and SRJ.RJ_FILIAL = substring(SRA.RA_FILIAL, 1, 4)
+                and SRJ.RJ_FUNCAO = SRA.RA_CODFUNC
+
+                left join SQ3010 SQ3 (nolock)
+                    on SQ3.D_E_L_E_T_ = ''
+                    and SQ3.Q3_CARGO = SRJ.RJ_CARGO
+
+            inner join CTT010 CTT (nolock)
+                on CTT.D_E_L_E_T_ = ''
+                and CTT.CTT_CUSTO = SRA.RA_CC
+            inner join CTD010 CTD (nolock)
+                on CTD.D_E_L_E_T_ = ''
+                and CTD.CTD_ITEM = SRA.RA_ITEM
+    where SPH.D_E_L_E_T_ = ''
+union
+    select 
+        trim(SRA.RA_FILIAL) as FILIAL,
+        trim(SRA.RA_MAT) as MATRICULA,
+        trim(SRA.RA_NOMECMP) as NOME,
+        trim(SRJ.RJ_DESC) as FUNCAO,
+        trim(SQ3.Q3_DESCSUM) as CARGO,
+        convert(date, SRA.RA_ADMISSA, 103) as ADMISSAO,
+        case SRA.RA_SITFOLH when '' then 'OK' else SRA.RA_SITFOLH end as SITUACAO,
+        case when trim(SRA.RA_SITFOLH) != 'D' then 'S' else 'N' end as ATIVO,
+        trim(CTT.CTT_CUSTO) as CC,
+        trim(CTT.CTT_DESC01) as CCUSTO,
+        trim(CTD.CTD_ITEM) as ITCT,
+        trim(CTD.CTD_DESC01) as ATIVIDADE,
+        trim(SQB.QB_DEPTO) as DEPTO,
+        trim(SQB.QB_DESCRIC) as DEPARTAMENTO,
+        trim(SRJ.RJ_CODCBO) as CBO,
+        trim(SRA.RA_SEXO) as SEXO,
+        trim(SRA.RA_CIC) as CPF,
+
+        SPC.PC_PD as EVENTO,
+        concat(SPC.PC_PD, ' - ', (select trim(SP9010.P9_DESC) from SP9010 (nolock) where SP9010.D_E_L_E_T_ = '' and SP9010.P9_CODIGO = SPC.PC_PD)) as DESC_EVENTO,
+        cast(SPC.PC_DATA as date) as DATA,
+        left(SPC.PC_DATA, 6) as PERIODO,
+        cast(SPC.PC_QUANTC as numeric(15, 2)) as QTD,
+        cast(floor(SPC.PC_QUANTC) as int) as HORAS,
+        cast((SPC.PC_QUANTC - floor(SPC.PC_QUANTC))*60.0 as numeric(15,2)) as MINUTOS
+    from SPC010 SPC (nolock)
+        inner join SRA010 SRA (nolock)
+            on SRA.D_E_L_E_T_ = ''
+            and SRA.RA_FILIAL = SPC.PC_FILIAL
+            and SRA.RA_MAT = SPC.PC_MAT
+
+            inner join SQB010 SQB (nolock)
+                on SQB.D_E_L_E_T_ = ''
+                and SQB.QB_FILIAL = substring(SRA.RA_FILIAL, 1, 4)
+                and SQB.QB_DEPTO = SRA.RA_DEPTO
+            inner join SRJ010 SRJ (nolock)
+                on SRJ.D_E_L_E_T_ = ''
+                and SRJ.RJ_FILIAL = substring(SRA.RA_FILIAL, 1, 4)
+                and SRJ.RJ_FUNCAO = SRA.RA_CODFUNC
+
+                left join SQ3010 SQ3 (nolock)
+                    on SQ3.D_E_L_E_T_ = ''
+                    and SQ3.Q3_CARGO = SRJ.RJ_CARGO
+
+            inner join CTT010 CTT (nolock)
+                on CTT.D_E_L_E_T_ = ''
+                and CTT.CTT_CUSTO = SRA.RA_CC
+            inner join CTD010 CTD (nolock)
+                on CTD.D_E_L_E_T_ = ''
+                and CTD.CTD_ITEM = SRA.RA_ITEM
+    where SPC.D_E_L_E_T_ = ''
