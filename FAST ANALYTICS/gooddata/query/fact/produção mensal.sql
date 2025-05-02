37dc54c21fe4ad5e522da3217ef6cce315e92965
Manoel <manoel.jose@unilinktransportes.com.br>
2025-05-02 14:24:56 -0300

    correção das consultas

--- FAST ANALYTICS/gooddata/query/fact/produção mensal.sql
+++ FAST ANALYTICS/gooddata/query/fact/produção mensal.sql
@@ -3,6 +3,8 @@ select
     'P |01|CTT010|'+ COALESCE(NULLIF(RTRIM(COALESCE(CTT.CTT_FILIAL, ' '))+'|'+RTRIM(COALESCE(ZC7.ZC7_CC, ' ')), ' '), '|') AS BK_CENTRO_DE_CUSTO,
     case when ZC7.ZC7_ORIGEM = 'ST9' then (select concat(trim(ST9010.T9_FILIAL), trim(ST9010.T9_CODBEM)) from ST9010 (nolock) where ST9010.D_E_L_E_T_ = '' and trim(ST9010.T9_CODBEM) = trim(ZC7.ZC7_CODIGO)) else null end as COD_DA3,
     case when ZC7.ZC7_ORIGEM = 'SQ3' then (select concat(trim(SQ3010.Q3_FILIAL), trim(SQ3010.Q3_CARGO)) from SQ3010 (nolock) where SQ3010.D_E_L_E_T_ = '' and trim(SQ3010.Q3_CARGO) = trim(ZC7.ZC7_CODIGO)) else null end as COD_SRJ,
+    concat(trim(ZG1.ZG1_TIPO), ' ', trim(ZG1.ZG1_CODIGO)) as ID_RECURSO,
+    
     cast(isnull(ZC7.ZC7_HRPAD, 0.0) as numeric(15, 2)) as HORA_PADRAO,
     cast(isnull(ZC7.ZC7_HRPROD, 0.0) as numeric(15, 2)) as HORA_PROD,
     cast(isnull(ZC7.ZC7_HRIMPR, 0.0) as numeric(15, 2)) as HORA_IMPROD,
@@ -24,5 +26,5 @@ from ZC7010 ZC7
         and ZG1.ZG1_TABELA = ZC7.ZC7_ORIGEM
         and ZG1.ZG1_ATIVO = 'S'
 where
-            concat(ZC7.ZC7_COMPET, '01') BETWEEN <<START_DATE>> AND <<FINAL_DATE>>
-        and ZC7.D_E_L_E_T_ = ''
+        concat(ZC7.ZC7_COMPET, '01') BETWEEN <<START_DATE>> AND <<FINAL_DATE>>
+    and ZC7.D_E_L_E_T_ = ''
