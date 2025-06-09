3d2aff6585b4b9f41c4f85e27e8b34e0f34bd3dc
Manoel <manoel.jose@unilinktransportes.com.br>
2025-06-09 11:50:47 -0300

    teste separação OPP e TMS no lançamento da DRE resultado

---
+++ faturamento/ROP conta 05103004.sql
@@ -0,0 +1,23 @@
+    select
+        ZE1.ZE1_FILIAL as FILIAL,
+        ZE1.ZE1_NUM as NUM,
+        '305' as CC,
+        sum(cast(coalesce(ZE1.ZE1_TOTAL, 0) as decimal (14, 2))) as TOTAL
+    from ZE1010 ZE1
+    where
+            ZE1.D_E_L_E_T_ = ''
+        and ZE1.ZE1_TIPO = 6
+        and left(ZE1.ZE1_COMPET, 6) = '"+cCompt+"' and ZE1.ZE1_FILIAL between '"+cFilIni+"' and '"+cFilFim+"'
+    group by ZE1.ZE1_FILIAL, ZE1.ZE1_NUM
+union
+    select
+        ZC2.ZC2_FILIAL as FILIAL,
+        ZC2.ZC2_NUM as NUM,
+        '305' as CC,
+        sum(cast(coalesce(ZC2.ZC2_TOTAL, 0) as decimal (14, 2))) as TOTAL
+    from ZC2010 ZC2
+    where
+            ZC2.D_E_L_E_T_ = ''
+        and ZC2.ZC2_TIPO = 6
+        and left(ZC2.ZC2_COMPET, 6) = '"+cCompt+"' and ZC2.ZC2_FILIAL between '"+cFilIni+"' and '"+cFilFim+"'
+    group by ZC2.ZC2_FILIAL, ZC2.ZC2_NUM
