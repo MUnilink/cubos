387365d5c1e95f88fb62473a8a8ea126aec9b851
Manoel <manoel.jose@unilinktransportes.com.br>
2025-06-10 11:23:51 -0300

    teste parece ok; rever alterações das regras das demais contas

--- faturamento/ROP conta 05103004.sql
+++ faturamento/ROP conta 05103004.sql
@@ -1,16 +1,4 @@
     select
-        ZE1.ZE1_FILIAL as FILIAL,
-        ZE1.ZE1_NUM as NUM,
-        '305' as CC,
-        sum(cast(coalesce(ZE1.ZE1_TOTAL, 0) as decimal (14, 2))) as TOTAL
-    from ZE1010 ZE1
-    where
-            ZE1.D_E_L_E_T_ = ''
-        and ZE1.ZE1_TIPO = 6
-        and left(ZE1.ZE1_COMPET, 6) = '"+cCompt+"' and ZE1.ZE1_FILIAL between '"+cFilIni+"' and '"+cFilFim+"'
-    group by ZE1.ZE1_FILIAL, ZE1.ZE1_NUM
-union
-    select
         ZC2.ZC2_FILIAL as FILIAL,
         ZC2.ZC2_NUM as NUM,
         '305' as CC,
@@ -21,3 +9,15 @@ union
         and ZC2.ZC2_TIPO = 6
         and left(ZC2.ZC2_COMPET, 6) = '"+cCompt+"' and ZC2.ZC2_FILIAL between '"+cFilIni+"' and '"+cFilFim+"'
     group by ZC2.ZC2_FILIAL, ZC2.ZC2_NUM
+union
+    select
+        ZE1.ZE1_FILIAL as FILIAL,
+        ZE1.ZE1_NUM as NUM,
+        '304' as CC,
+        sum(cast(coalesce(ZE1.ZE1_TOTAL, 0) as decimal (14, 2))) as TOTAL
+    from ZE1010 ZE1
+    where
+            ZE1.D_E_L_E_T_ = ''
+        and ZE1.ZE1_TIPO = 6
+        and left(ZE1.ZE1_COMPET, 6) = '"+cCompt+"' and ZE1.ZE1_FILIAL between '"+cFilIni+"' and '"+cFilFim+"'
+    group by ZE1.ZE1_FILIAL, ZE1.ZE1_NUM
