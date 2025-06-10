fe7c4c60b730498d195fd7abbe97abd3b98ef13a
Manoel <manoel.jose@unilinktransportes.com.br>
2025-06-10 11:51:23 -0300

    edit

--- FAST ANALYTICS/gooddata/query/dim/cadastro de funções.sql
+++ FAST ANALYTICS/gooddata/query/dim/cadastro de funções.sql
@@ -1,7 +1,7 @@
     select
-        concat(trim(SRJ.RJ_FILIAL), trim(SRJ.RJ_CARGO)) as ID_CARGO,
-        trim(SRJ.RJ_CARGO) as COD_CARGO,
-        trim(SRJ.RJ_DESCSUM) as DESC_CARGO,
+        concat(trim(SRJ.RJ_FILIAL), trim(SRJ.RJ_FUNCAO)) as ID_FUNCAO,
+        trim(SRJ.RJ_CARGO) as COD_FUNCAO,
+        trim(SRJ.RJ_DESCSUM) as DESC_FUNCAO,
     from SRJ010 SRJ
     where SRJ.D_E_L_E_T_ = ''
 union
