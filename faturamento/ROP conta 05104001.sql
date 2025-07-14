29824994ecedaf5999ac0ff3b1cea9216a676953
Manoel <manoel.jose@unilinktransportes.com.br>
2025-07-14 17:56:27 -0300

    alteração de contas da DRE gerencial

--- faturamento/ROP conta 05104001.sql
+++ faturamento/ROP conta 05104001.sql
@@ -1,22 +1,19 @@
 select
-    SC7.C7_FILIAL as FILIAL,
+    ZC2.ZC2_FILIAL as FILIAL,
     ZC2.ZC2_NUM as NUM,
-    SD3.D3_CC as CC,
-    sum(cast(coalesce(SD1.D1_TOTAL, 0) as decimal (14, 2))) as TOTAL
-from SC7010 SC7
-	left join SD1010 SD1
-		on SD1.D_E_L_E_T_ = ''
-		and SD1.D1_FILIAL = SC7.C7_FILIAL
-		and SD1.D1_PEDIDO = SC7.C7_NUM
-		and SD1.D1_ITEMPC = SC7.C7_ITEM
-    inner join ZC2010 ZC2
-        on case when trim(SC7.C7_YOS) = '2024/0' then right(left(replace(replace(SC7.C7_OBS, char(10), ''), char(13), ''), 63), 11) else SC7.C7_YOS end = ZC2.ZC2_NUM
-        and SC7.C7_YOSIT = ZC2.ZC2_ITEM
-        and SC7.D_E_L_E_T_ = ''
+    (
+        select isnull(avg(cast(nullif(SC6010.C6_CC, '') as int)), '305') /* pois cortesia */
+        from SC6010
+        where
+                SC6010.D_E_L_E_T_ = ''
+            and SC6010.C6_FILIAL = ZC2.ZC2_FILIAL
+            and SC6010.C6_YOS = ZC2.ZC2_NUM
+            and SC6010.C6_YITOS = ZC2.ZC2_ITEM
+    ) as CC,
+    sum(cast(coalesce(ZC2.ZC2_TOTAL, 0) as decimal (14, 2))) as TOTAL
+from ZC2010 ZC2
 where
-        cast(SC7.C7_FORNECE as int) = 52
-    and SC7.D_E_L_E_T_ = ''
-    and left(SC7.C7_EMISSAO, 6) = '"+cCompt+"' and SC7.C7_FILIAL between '"+cFilIni+"' and '"+cFilFim+"'
-group by
-    SC7.C7_FILIAL,
-    ZC2.ZC2_NUM
+        ZC2.D_E_L_E_T_ = ''
+    and ZC2.ZC2_TIPO = 11 and ZC2.ZC2_YFORNE = '000052'
+    and left(ZC2.ZC2_COMPET, 6) = '"+cCompt+"' and ZC2.ZC2_FILIAL between '"+cFilIni+"' and '"+cFilFim+"'
+group by ZC2.ZC2_FILIAL, ZC2.ZC2_NUM
