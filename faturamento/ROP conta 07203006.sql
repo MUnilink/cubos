c69df08da6b693b9a7d968259387cec1fcfb66ae
Manoel <manoel.jose@unilinktransportes.com.br>
2025-07-15 11:03:40 -0300

    adição das contas contábeis para rateio de custos fixos

---
+++ faturamento/ROP conta 07203006.sql
@@ -0,0 +1,33 @@
+    select
+        ZC2.ZC2_FILIAL as FILIAL,
+        ZC2.ZC2_NUM as NUM,
+        (
+            select isnull(avg(cast(nullif(SC6010.C6_CC, '') as int)), '305') /* pois cortesia */
+            from SC6010
+            where
+                    SC6010.D_E_L_E_T_ = ''
+                and SC6010.C6_FILIAL = ZC2.ZC2_FILIAL
+                and SC6010.C6_YOS = ZC2.ZC2_NUM
+                and SC6010.C6_YITOS = ZC2.ZC2_ITEM
+        ) as CC,
+        sum(cast(coalesce(ZC2.ZC2_TOTAL, 0) as decimal (14, 2))) as TOTAL
+    from ZC2010 ZC2
+    where
+            ZC2.D_E_L_E_T_ = ''
+        and ZC2.ZC2_TIPO = 7
+        and ZC2.ZC2_COD = '000012'
+        and left(ZC2.ZC2_COMPET, 6) = '"+cCompt+"' and ZC2.ZC2_FILIAL between '"+cFilIni+"' and '"+cFilFim+"'
+    group by ZC2.ZC2_FILIAL, ZC2.ZC2_NUM
+union
+    select
+        ZE1.ZE1_FILIAL as FILIAL,
+        ZE1.ZE1_NUM as NUM,
+        '304' as CC,
+        sum(cast(coalesce(ZE1.ZE1_TOTAL, 0) as decimal (14, 2))) as TOTAL
+    from ZE1010 ZE1
+    where
+            ZE1.D_E_L_E_T_ = ''
+        and ZE1.ZE1_TIPO = 7
+        and ZE1.ZE1_COD = '000012'
+        and left(ZE1.ZE1_COMPET, 6) = '"+cCompt+"' and ZE1.ZE1_FILIAL between '"+cFilIni+"' and '"+cFilFim+"'
+    group by ZE1.ZE1_FILIAL, ZE1.ZE1_NUM
