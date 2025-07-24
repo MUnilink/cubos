d013f3565874164682a95a83b5e69d2d4eac7cd7
Manoel <manoel.jose@unilinktransportes.com.br>
2025-07-24 14:02:14 -0300

    dedução da atividade das receitas vinculadas à viagem (rever)

--- faturamento/lançamentos plano de contas gerencial.sql
+++ faturamento/lançamentos plano de contas gerencial.sql
@@ -3,23 +3,73 @@ select
     upper(trim(translate(lower(replace(ZE2.ZE2_DESC, ',', ' ')), 'áéíóúãõç', 'aeiouaoc'))) as DESCRICAO,
     concat(trim(ZE2.ZE2_COD), ' ', upper(trim(translate(lower(replace(ZE2.ZE2_DESC, ',', ' ')), 'áéíóúãõç', 'aeiouaoc')))) as CODDESC,
     trim(ZE3.ZE3_ORIGEM) as CC,
+    coalesce
     (
-        select min('P |01|CTD010|'+ COALESCE(NULLIF(RTRIM(COALESCE(CTD010.CTD_FILIAL, ' '))+'|'+RTRIM(COALESCE(SC6010.C6_ITEMCTA, ' ')), ' '), '|'))
-        from SC6010
-            inner join CTD010
-                on CTD010.CTD_FILIAL = ''
-                and CTD010.CTD_ITEM = SC6010.C6_ITEMCTA
-                and CTD010.D_E_L_E_T_ = ''
+        (
+            select min('P |01|CTD010|'+ COALESCE(NULLIF(RTRIM(COALESCE(CTD010.CTD_FILIAL, ' '))+'|'+RTRIM(COALESCE(SC6010.C6_ITEMCTA, ' ')), ' '), '|'))
+            from SC6010
+                inner join CTD010
+                    on CTD010.CTD_FILIAL = ''
+                    and CTD010.CTD_ITEM = SC6010.C6_ITEMCTA
+                    and CTD010.D_E_L_E_T_ = ''
 
-                inner join SD2010
-                    on SD2010.D_E_L_E_T_= ''
-                    and SD2010.D2_FILIAL = SC6010.C6_FILIAL
-                    and SD2010.D2_PEDIDO = SC6010.C6_NUM
-                    and SD2010.D2_ITEMPV = SC6010.C6_ITEM
-        where
-                SC6010.D_E_L_E_T_ = ''
-            and concat(SC6010.C6_FILIAL, SC6010.C6_YOS) = ZE3.ZE3_NUM
+                    inner join SD2010
+                        on SD2010.D_E_L_E_T_= ''
+                        and SD2010.D2_FILIAL = SC6010.C6_FILIAL
+                        and SD2010.D2_PEDIDO = SC6010.C6_NUM
+            where
+                    SC6010.D_E_L_E_T_ = ''
+                and concat(SC6010.C6_FILIAL, SC6010.C6_YOS) = ZE3.ZE3_NUM
+        ),
+        (
+            select min('P |01|CTD010|'+ COALESCE(NULLIF(RTRIM(COALESCE(AT1.CTD_FILIAL, AT2.CTD_FILIAL, AT3.CTD_FILIAL, ' '))+'|'+RTRIM(COALESCE(AT1.CTD_ITEM, AT2.CTD_ITEM, AT3.CTD_ITEM, ' ')), ' '), '|'))
+            from DUD010 (nolock)
+                left join DT6010 DT6 (nolock)
+                    on DT6.D_E_L_E_T_ = ''
+                    and DT6.DT6_FILDOC = DUD010.DUD_FILDOC
+                    and DT6.DT6_DOC = DUD010.DUD_DOC
+                    and DT6.DT6_SERIE = DUD010.DUD_SERIE
+
+                    left join SD2010 COMP (nolock)
+                        on COMP.D_E_L_E_T_ = ''
+                        and COMP.D2_NFORI = DT6.DT6_DOC
+                        and COMP.D2_SERIORI = DT6.DT6_SERIE
+                        and COMP.D2_CLIENTE = DT6.DT6_CLIDEV
+                        and COMP.D2_LOJA = DT6.DT6_LOJDEV
+
+                        left join CTD010 AT1
+                            on AT1.CTD_FILIAL = ''
+                            and AT1.CTD_ITEM = COMP.D2_ITEMCC
+                            and AT1.D_E_L_E_T_ = ''
+                
+                left join SC5010 (nolock)
+                    on SC5010.D_E_L_E_T_ = ''
+                    and trim(SC5010.C5_YVIAGEM) = DUD010.DUD_VIAGEM
+
+                    left join SC6010 (nolock)
+                        on SC6010.D_E_L_E_T_ = ' '
+                        and SC6010.C6_FILIAL = SC5010.C5_FILIAL
+                        and SC6010.C6_NUM = SC5010.C5_NUM
+
+                        left join CTD010 AT2
+                            on AT2.CTD_FILIAL = ''
+                            and AT2.CTD_ITEM = SC6010.C6_ITEMCTA
+                            and AT2.D_E_L_E_T_ = ''
+                
+                left join SE1010 (nolock)
+                    on SE1010.D_E_L_E_T_ = ''
+                    and (trim(SE1010.E1_YVIATMS) = DUD010.DUD_VIAGEM or SE1010.E1_YVIAGEM = DUD010.DUD_VIAGEM)
+
+                    left join CTD010 AT3
+                        on AT3.CTD_FILIAL = ''
+                        and AT3.CTD_ITEM = SE1010.E1_ITEMCTA
+                        and AT3.D_E_L_E_T_ = ''
+            
+            where DUD010.D_E_L_E_T_ = ''
+            and concat(DUD010.DUD_FILDOC, DUD010.DUD_VIAGEM) = ZE3.ZE3_NUM
+        )
     ) as ATIVIDADE,
+    
     ZE2.ZE2_MSBLQL as BLOQUEADO,
     ZE3.ZE3_COMPET as PERIODO,
     ZE3.ZE3_NUM,
