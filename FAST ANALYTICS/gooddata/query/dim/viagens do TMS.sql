172a8d91ac5fabfed21c8792d4fcfbc2a2bb8482
Manoel <manoel.jose@unilinktransportes.com.br>
2025-07-29 17:38:39 -0300

    adição do vínculo com as viagens aos lançamentos da DRE gerencial

---
+++ FAST ANALYTICS/gooddata/query/dim/viagens do TMS.sql
@@ -0,0 +1,23 @@
+select
+    concat(trim(DUD.DUD_FILIAL), trim(DUD.DUD_VIAGEM)) as BK_VIAGEMTMS,
+    DUD.DUD_VIAGEM as VIAGEM,
+
+    case DTQ.DTQ_STATUS
+        when '1' then upper('Em Aberto')
+        when '2' then upper('Em Transito')
+        when '3' then upper('Encerrada')
+        when '4' then upper('Chegada em Filial')
+        when '5' then upper('Fechada')
+        when '9' then upper('Cancelada')
+        else 'Outros'
+    end as STATUS_VGA
+
+from DUD010 DUD
+    inner join DTQ010 DTQ
+        on DTQ.D_E_L_E_T_ = ''
+        and DTQ.DTQ_FILIAL = DUD.DUD_FILIAL
+        and DTQ.DTQ_VIAGEM = DUD.DUD_VIAGEM
+where
+        DUD.D_E_L_E_T_ = ''
+
+union select null, null, null
--- FAST ANALYTICS/gooddata/query/fact/lançamentos resultado gerencial.sql
+++ FAST ANALYTICS/gooddata/query/fact/lançamentos resultado gerencial.sql
@@ -4,6 +4,7 @@ select distinct
     'P |01|SA1010|'+ COALESCE(NULLIF(RTRIM(COALESCE(SA1.A1_FILIAL, ' '))+'|'+RTRIM(COALESCE(SA1.A1_COD, ' '))+RTRIM(COALESCE(SA1.A1_LOJA, ' ')), ' '), '|') as BK_CLIENTE,
     'P |01|SA2010|'+ COALESCE(NULLIF(RTRIM(COALESCE(SA2.A2_FILIAL, ' '))+'|'+RTRIM(COALESCE(SA2.A2_COD, ' '))+RTRIM(COALESCE(SA2.A2_LOJA, ' ')), ' '), '|') as BK_FORNECEDOR,
     concat(trim(ZC1.ZC1_FILIAL), trim(ZC1.ZC1_NUM)) as ID_OSPORTUARIA,
+    concat(trim(DUD.DUD_FILIAL), trim(DUD.DUD_VIAGEM)) as ID_VIAGEMTMS,
     'P |01|SB1010|'+ COALESCE(NULLIF(RTRIM(COALESCE(SB1.B1_FILIAL, ' '))+'|'+RTRIM(COALESCE(ZC1.ZC1_MERCAD, ' ')), ' '), '|') as ID_MERCADORIA,
     null as ID_PEDIDODEVENDA,
     null as ID_NFS,
@@ -60,6 +61,9 @@ from ZE3010 ZE3
     left join ZC1010 ZC1
         on ZC1.D_E_L_E_T_ = ''
         and concat(ZC1.ZC1_FILIAL, ZC1.ZC1_NUM) = ZE3.ZE3_NUM
+    left join DUD010 DUD
+        on DUD.D_E_L_E_T_ = ''
+        and concat(DUD.DUD_FILIAL, DUD.DUD_VIAGEM) = ZE3.ZE3_NUM
 
         left join SA1010 SA1
             on SA1.D_E_L_E_T_ = ''
