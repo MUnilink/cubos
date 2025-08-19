38ae5c3ba10cd6bf112f7db70eb0bd5aef0f54d5
Manoel <manoel.jose@unilinktransportes.com.br>
2025-08-19 16:54:36 -0300

    campo para validação de usuário ativo ou inativo

--- faturamento/OS portuária.sql
+++ faturamento/OS portuária.sql
@@ -171,7 +171,8 @@ select
         else 'item OK'
     end as STATUS_APONT,
 
-    trim(upper(ZC2.ZC2_NMUSU)) as USUARIO
+    trim(upper(ZC2.ZC2_NMUSU)) as USUARIO,
+    case when (select SYS_USR.USR_MSBLQL from SYS_USR where USR_CODIGO = ZC2.ZC2_NMUSU and SYS_USR.D_E_L_E_T_ = '') = 2 then 'ATIVO' else 'INATIVO' end as TIPO_USR
 
 from ZC2010 ZC2 (nolock)
     left join ZC1010 ZC1 (nolock)
