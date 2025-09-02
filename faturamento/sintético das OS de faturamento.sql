9f744e5c3ac96d836daf11e844c267a59e8d62ed
Manoel <manoel.jose@unilinktransportes.com.br>
2025-09-02 10:22:58 -0300

    para análise e correção das horas produtivas e improdutivas

--- faturamento/sintético das OS de faturamento.sql
+++ faturamento/sintético das OS de faturamento.sql
@@ -1,7 +1,28 @@
 select
+    CASE WHEN ZG1.ZG1_FILORI IS NULL THEN 'P |01||' ELSE 'P |01|01'+ CAST(ZG1.ZG1_FILORI AS CHAR (6)) END AS BK_FILIAL,
+    'P |01|CTT010|'+ COALESCE(NULLIF(RTRIM(COALESCE(CTT.CTT_FILIAL, ' '))+'|'+RTRIM(COALESCE(ZG1.ZG1_CC, ' ')), ' '), '|') AS BK_CENTRO_DE_CUSTO,
+    case when ZG1.ZG1_TABELA = 'ST9' then (select concat(trim(ST9010.T9_FILIAL), trim(ST9010.T9_CODBEM)) from ST9010 (nolock) where ST9010.D_E_L_E_T_ = '' and trim(ST9010.T9_CODBEM) = trim(ZG1.ZG1_CODIGO)) else null end as COD_DA3,
+    case when ZG1.ZG1_TABELA = 'SQ3' then (select concat(trim(SQ3010.Q3_FILIAL), trim(SQ3010.Q3_CARGO)) from SQ3010 (nolock) where SQ3010.D_E_L_E_T_ = '' and trim(SQ3010.Q3_CARGO) = trim(ZG1.ZG1_CODIGO)) else null end as COD_SRJ,
+    concat(trim(ZG1.ZG1_TIPO), ' ', trim(ZG1.ZG1_CODIGO)) as ID_RECURSO,
+    
+    cast(isnull(ZG1.ZG1_HRPAD, 0.0) as numeric(15, 2)) as HORA_PADRAO,
+    cast(isnull(ZG1.ZG1_HRPRO, 0.0) as numeric(15, 2)) as HORA_PROD,
+    cast(isnull(ZG1.ZG1_HRIMPR, 0.0) as numeric(15, 2)) as HORA_IMPROD,
+    cast(ZG1.ZG1_TIPO as int) as ID_TIPO_ITEM,
+    cast(isnull(ZG1.ZG1_VLTOTL, 0.0) as numeric(15, 2)) as VL_TOTAL,
+    cast(isnull(ZG1.ZG1_VLHORA, 0.0) as numeric(15, 2)) as VL_HORA,
+    cast(isnull(ZG1.ZG1_VLPROD, 0.0) as numeric(15, 2)) as VL_PROD,
+    cast(isnull(ZG1.ZG1_VLIMPR, 0.0) as numeric(15, 2)) as VL_IMPROD,
+    cast(isnull(ZG1.ZG1_IMPR1, 0.0) as numeric(15, 2)) as HR_IMPR1,
+    cast(isnull(ZG1.ZG1_IMPR2, 0.0) as numeric(15, 2)) as HR_IMPR2,
+    cast(isnull(ZG1.ZG1_IMPR3, 0.0) as numeric(15, 2)) as HR_IMPR3,
+    concat(ZG1.ZG1_COMPET, '01') as COMPETENCIA,
+
+    /* PARA RM */
     ZG1.ZG1_FILORI as FILIAL,
     ZG1.ZG1_COMPET as PERIODO,
     ZG1.ZG1_TABELA as TABELA,
+    ZG1.ZG1_CC as CC,
     trim(ZG1.ZG1_CODIGO) as RECURSO,
     
     case ZG1.ZG1_TABELA
@@ -37,17 +58,6 @@ select
     else 'OUTROS' end as TIPO_RODA,
     
     ZG1.ZG1_TIPO as TIPO,
-    ZG1.ZG1_VLTOTL as VL_TOTAL,
-    ZG1.ZG1_VLHORA as VL_HORA,
-    ZG1.ZG1_VLIMPR as VL_IMP,
-    ZG1.ZG1_VLPROD as VL_PRO,
-
-    ZG1.ZG1_HRPAD as HORA_PAD,
-    ZG1.ZG1_HRPRO as HORA_PRO,
-    ZG1.ZG1_HRIMPR as HORA_IMP,
-    ZG1.ZG1_IMPR1 as HIMP_AFAMNT,
-    ZG1.ZG1_IMPR2 as HIMP_FER,
-    ZG1.ZG1_IMPR3 as HIMP_PON,
 
     ZG1.ZG1_VLIMPR/
     (
@@ -59,7 +69,10 @@ select
         and ZG1010.ZG1_FILORI = ZG1.ZG1_FILORI
         and ZG1010.ZG1_COMPET = ZG1.ZG1_COMPET
         and ZG1010.ZG1_CODIGO = ZG1.ZG1_CODIGO
-    ) as VL_RIMP
+    ) as RAT_VLIMP
 
 from ZG1010 ZG1 (nolock)
+    left join CTT010 CTT
+        on CTT.D_E_L_E_T_ = ''
+        and CTT.CTT_CUSTO = ZG1.ZG1_CC
 where ZG1.D_E_L_E_T_ = ''
