c54c6a7b321d554e956ca9b7a44e930c458317a8
Manoel <manoel.jose@unilinktransportes.com.br>
2025-09-16 14:59:50 -0300

    para controle da consulta de carga da produtividade em horas

---
+++ faturamento/FA - produtividade mensal.sql
@@ -0,0 +1,36 @@
+select
+    CASE WHEN ZG1.ZG1_FILORI IS NULL THEN 'P |01||' ELSE 'P |01|01'+ CAST(ZG1.ZG1_FILORI AS CHAR (6)) END AS BK_FILIAL,
+    'P |01|CTT010|'+ COALESCE(NULLIF(RTRIM(COALESCE(CTT.CTT_FILIAL, ' '))+'|'+RTRIM(COALESCE(ZG1.ZG1_CC, ' ')), ' '), '|') AS BK_CENTRO_DE_CUSTO,
+    case when ZG1.ZG1_TABELA = 'ST9' then (select concat(trim(ST9010.T9_FILIAL), trim(ST9010.T9_CODBEM)) from ST9010 (nolock) where ST9010.D_E_L_E_T_ = '' and trim(ST9010.T9_CODBEM) = trim(ZG1.ZG1_CODIGO)) else null end as COD_DA3,
+    case when ZG1.ZG1_TABELA = 'SQ3' then (select concat(trim(SQ3010.Q3_FILIAL), trim(SQ3010.Q3_CARGO)) from SQ3010 (nolock) where SQ3010.D_E_L_E_T_ = '' and trim(SQ3010.Q3_CARGO) = trim(ZG1.ZG1_CODIGO)) else null end as COD_SRJ,
+    concat(trim(ZG1.ZG1_TIPO), ' ', trim(ZG1.ZG1_CODIGO)) as ID_RECURSO,
+    cast(ZG1.ZG1_TIPO as int) as ID_TIPO_ITEM,
+    cast(isnull(sum(ZG1.ZG1_HRPAD), 0.0) as numeric(15, 2)) as HORA_PADRAO,
+    cast(isnull(sum(ZG1.ZG1_HRPRO), 0.0) as numeric(15, 2)) as HORA_PROD,
+    cast(isnull(sum(ZG1.ZG1_HRIMPR), 0.0) as numeric(15, 2)) as HORA_IMPROD,
+    cast(isnull(sum(ZG1.ZG1_VLTOTL), 0.0) as numeric(15, 2)) as VL_TOTAL,
+    cast(isnull(sum(ZG1.ZG1_VLHORA), 0.0) as numeric(15, 2)) as VL_HORA,
+    cast(isnull(sum(ZG1.ZG1_VLPROD), 0.0) as numeric(15, 2)) as VL_PROD,
+    cast(isnull(sum(ZG1.ZG1_VLIMPR), 0.0) as numeric(15, 2)) as VL_IMPROD,
+    cast(isnull(sum(ZG1.ZG1_IMPR1), 0.0) as numeric(15, 2)) as HR_IMPR1,
+    cast(isnull(sum(ZG1.ZG1_IMPR2), 0.0) as numeric(15, 2)) as HR_IMPR2,
+    cast(isnull(sum(ZG1.ZG1_IMPR3), 0.0) as numeric(15, 2)) as HR_IMPR3,
+    concat(ZG1.ZG1_COMPET, '01') as COMPETENCIA
+
+from ZG1010 ZG1
+    left join CTT010 CTT
+        on CTT.D_E_L_E_T_ = ''
+        and CTT.CTT_CUSTO = ZG1.ZG1_CC
+where
+        ZG1.D_E_L_E_T_ = ''
+    and ZG1.ZG1_ATIVO = 'S'
+    and ZG1.ZG1_TIPO in (2, 3)
+group by
+    ZG1.ZG1_FILORI,
+    CTT.CTT_FILIAL,
+    CTT.CTT_CUSTO,
+    ZG1.ZG1_CC,
+    ZG1.ZG1_TIPO,
+    ZG1.ZG1_CODIGO,
+    ZG1.ZG1_TABELA,
+    ZG1.ZG1_COMPET
