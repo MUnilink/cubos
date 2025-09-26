d012e7c0597138ea9445cab2be06ead740f6cf11
Manoel <manoel.jose@unilinktransportes.com.br>
2025-09-26 10:55:29 -0300

    tratamento de quando não há mudança de cargo ou turno

--- RH/folha com cargo e turno.sql
+++ RH/folha com cargo e turno.sql
@@ -35,22 +35,41 @@ select
 		when '4' then 'BASE DESCONTO'
 		else '-'
 	end as TIPO_VERBA,
+
+    1 + datediff(day, concat(SRD.RD_DATARQ, '01'), eomonth(concat(SRD.RD_DATARQ, '01'))) as DIAS_PERIODO,
+	cast(concat(SRD.RD_DATARQ, '01') as date) as INI_PERIODO,
+	eomonth(concat(SRD.RD_DATARQ, '01')) as FIM_PERIODO,
     
     cast(SR7.DATA_MUD as date) as DATA_MUD,
     SR7.CARGO_ANT,
     SR7.CARGO_PRO,
     SR7.DIASANT_CARGO,
-    SR7.DIASPRO_CARGO,
+    case isnull(SR7.qtd_SR7, 0) when 0 then 1 + datediff(day, concat(SRD.RD_DATARQ, '01'), eomonth(concat(SRD.RD_DATARQ, '01'))) else SR7.DIASPRO_CARGO end as DIASPRO_CARGO,
     
     cast(SPF.DATA_TRA as date) as DATA_TRA,
     SPF.TURNO_ANT,
     SPF.TURNO_PRO,
     SPF.DIASANT_TURNO,
     SPF.DIASPRO_TURNO,
-    SPF.CARGA_HANT,
-    SPF.CARGA_HPRO,
+    SPF.CARGA_HANT as HORAS_ANT,
     
-    SR7.qtd_SR7,SPF.qtd_SPF
+    case isnull(SPF.qtd_SPF, 0) when 0 then
+        (
+            select avg(SR6010.R6_HRNORMA)
+            from SPF010
+                inner join SR6010
+                    on SR6010.D_E_L_E_T_ = ''
+                    and SR6010.R6_TURNO = SPF010.PF_TURNOPA
+            where
+                    SPF010.D_E_L_E_T_ = ''
+                and left(SPF010.PF_DATA, 6) = SRD.RD_DATARQ
+                and SPF010.PF_FILIAL = SRD.RD_FILIAL
+                and SPF010.PF_MAT = SRD.RD_MAT
+        )
+        else SPF.CARGA_HPRO
+    end as HORAS_PRO,
+    
+    isnull(SR7.qtd_SR7, 0) as qtd_SR7, isnull(SPF.qtd_SPF, 0) as qtd_SPF
 
 from SRD010 SRD (nolock)
 	inner join SRV010 SRV (nolock)
@@ -99,8 +118,9 @@ from SRD010 SRD (nolock)
                 end, /* se a última mudança ocorreu dentro do período da folha, data da mudança; senão, se admitido após o início do período, admissão, senão, início do período */
                 case when left(SRA010.RA_DEMISSA, 6) = left(SR7010.R7_DATA, 6) then SRA010.RA_DEMISSA else dateadd(day, 1, eomonth(concat(left(SR7010.R7_DATA, 6), '01'))) end
             ) as DIASPRO_CARGO,
+            
             1 as qtd_SR7
-        from SR7010
+        from SR7010 (nolock)
             inner join SRA010 (nolock)
                 on SRA010.D_E_L_E_T_ = ''
                 and SRA010.RA_FILIAL = SR7010.R7_FILIAL
@@ -124,31 +144,26 @@ from SRD010 SRD (nolock)
             cast(isnull(SR6_ANT.R6_HRNORMA, 0) as numeric(15, 2)) as CARGA_HANT,
             cast(isnull(SR6_PRO.R6_HRNORMA, 0) as numeric(15, 2)) as CARGA_HPRO,
 
-            case when SPF010.PF_TURNODE != SPF010.PF_TURNOPA then
-                datediff
-                (
-                    day, /* se a última mudança ocorreu dentro do período da folha, data da mudança; senão, e se demitido antes do fim do período, demissão, senão, fim do período*/
-                    case when SRA010.RA_ADMISSA >= concat(left(SPF010.PF_DATA, 6), '01') then SRA010.RA_ADMISSA else concat(left(SPF010.PF_DATA, 6), '01') end,
-                    case when SPF010.PF_DATA >= concat(left(SPF010.PF_DATA, 6), '01') then SPF010.PF_DATA
-                        else
-                        case when nullif(SRA010.RA_DEMISSA, '') <= eomonth(concat(left(SPF010.PF_DATA, 6), '01')) then SRA010.RA_DEMISSA else eomonth(concat(left(SPF010.PF_DATA, 6), '01')) end
-                    end
-                )
-                else 0
-            end as DIASANT_TURNO,
+            datediff
+            (
+                day, /* se a última mudança ocorreu dentro do período da folha, data da mudança; senão, e se demitido antes do fim do período, demissão, senão, fim do período*/
+                case when SRA010.RA_ADMISSA >= concat(left(SPF010.PF_DATA, 6), '01') then SRA010.RA_ADMISSA else concat(left(SPF010.PF_DATA, 6), '01') end,
+                case when SPF010.PF_DATA >= concat(left(SPF010.PF_DATA, 6), '01') then SPF010.PF_DATA
+                    else
+                    case when nullif(SRA010.RA_DEMISSA, '') <= eomonth(concat(left(SPF010.PF_DATA, 6), '01')) then SRA010.RA_DEMISSA else eomonth(concat(left(SPF010.PF_DATA, 6), '01')) end
+                end
+            ) as DIASANT_TURNO,
                 
-            case when SPF010.PF_TURNODE != SPF010.PF_TURNOPA then
-                datediff
-                (
-                    day, /* se a última mudança ocorreu dentro do período da folha, data da mudança; senão, se admitido após o início do período, admissão, senão, início do período */
-                    case when SPF010.PF_DATA >= concat(left(SPF010.PF_DATA, 6), '01') then SPF010.PF_DATA
-                        else
-                        case when SRA010.RA_ADMISSA >= concat(left(SPF010.PF_DATA, 6), '01') then SRA010.RA_ADMISSA else concat(left(SPF010.PF_DATA, 6), '01') end
-                    end,
-                    case when left(SRA010.RA_DEMISSA, 6) = left(SPF010.PF_DATA, 6) then SRA010.RA_DEMISSA else dateadd(day, 1, eomonth(concat(left(SPF010.PF_DATA, 6), '01'))) end
-                )
-                else 0
-            end as DIASPRO_TURNO,
+            datediff
+            (
+                day, /* se a última mudança ocorreu dentro do período da folha, data da mudança; senão, se admitido após o início do período, admissão, senão, início do período */
+                case when SPF010.PF_DATA >= concat(left(SPF010.PF_DATA, 6), '01') then SPF010.PF_DATA
+                    else
+                    case when SRA010.RA_ADMISSA >= concat(left(SPF010.PF_DATA, 6), '01') then SRA010.RA_ADMISSA else concat(left(SPF010.PF_DATA, 6), '01') end
+                end,
+                case when left(SRA010.RA_DEMISSA, 6) = left(SPF010.PF_DATA, 6) then SRA010.RA_DEMISSA else dateadd(day, 1, eomonth(concat(left(SPF010.PF_DATA, 6), '01'))) end
+            ) as DIASPRO_TURNO,
+            
             1 as qtd_SPF
         from SPF010
             inner join SRA010 (nolock)
