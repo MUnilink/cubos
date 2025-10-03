bb987dd909d3fd8e3d8f7a67d3581f94ca7d5468
Manoel <manoel.jose@unilinktransportes.com.br>
2025-10-03 15:11:17 -0300

    adição da atividade da tabela de rateio dos custos gerenciais

--- faturamento/sintético das OS de faturamento.sql
+++ faturamento/sintético das OS de faturamento.sql
@@ -1,6 +1,7 @@
 select
     CASE WHEN ZG1.ZG1_FILORI IS NULL THEN 'P |01||' ELSE 'P |01|01'+ CAST(ZG1.ZG1_FILORI AS CHAR (6)) END AS BK_FILIAL,
     'P |01|CTT010|'+ COALESCE(NULLIF(RTRIM(COALESCE(CTT.CTT_FILIAL, ' '))+'|'+RTRIM(COALESCE(ZG1.ZG1_CC, ' ')), ' '), '|') AS BK_CENTRO_DE_CUSTO,
+    'P |01|CTD010|'+ COALESCE(NULLIF(RTRIM(COALESCE(CTD.CTD_FILIAL, ' '))+'|'+RTRIM(COALESCE(ZG1.ZG1_ITEMCT, ' ')), ' '), '|') as BK_ITEM_CONTABIL,
     case when ZG1.ZG1_TABELA = 'ST9' then (select concat(trim(ST9010.T9_FILIAL), trim(ST9010.T9_CODBEM)) from ST9010 (nolock) where ST9010.D_E_L_E_T_ = '' and trim(ST9010.T9_CODBEM) = trim(ZG1.ZG1_CODIGO)) else null end as COD_DA3,
     case when ZG1.ZG1_TABELA = 'SQ3' then (select concat(trim(SQ3010.Q3_FILIAL), trim(SQ3010.Q3_CARGO)) from SQ3010 (nolock) where SQ3010.D_E_L_E_T_ = '' and trim(SQ3010.Q3_CARGO) = trim(ZG1.ZG1_CODIGO)) else null end as COD_SRJ,
     concat(trim(ZG1.ZG1_TIPO), ' ', trim(ZG1.ZG1_CODIGO)) as ID_RECURSO,
@@ -16,13 +17,14 @@ select
     cast(isnull(ZG1.ZG1_IMPR2, 0.0) as numeric(15, 2)) as HR_IMPR2,
     cast(isnull(ZG1.ZG1_IMPR3, 0.0) as numeric(15, 2)) as HR_IMPR3,
     concat(ZG1.ZG1_COMPET, '01') as COMPETENCIA,
-    case when ZG1.ZG1_TABELA = 'SQ3' then (select SRJ010.RJ_YHRPADR as numeric(15, 2))) from SRJ010 where SRJ010.D_E_L_E_T_ = '' and SRJ010.RJ_CARGO = ZG1.ZG1_CODIGO) else ZG1.ZG1_HRPAD end as QTD_UNI,
+    case when ZG1.ZG1_TABELA = 'SQ3' then (select cast(SRJ010.RJ_YHRPADR as numeric(15, 2)) from SRJ010 where SRJ010.D_E_L_E_T_ = '' and SRJ010.RJ_CARGO = ZG1.ZG1_CODIGO) else ZG1.ZG1_HRPAD end as QTD_UNI,
 
     /* PARA RM */
     ZG1.ZG1_FILORI as FILIAL,
     ZG1.ZG1_COMPET as PERIODO,
     ZG1.ZG1_TABELA as TABELA,
     ZG1.ZG1_CC as CC,
+    ZG1.ZG1_ITEMCT as ATIVIDADE,
     trim(ZG1.ZG1_CODIGO) as RECURSO,
     
     case ZG1.ZG1_TABELA
@@ -67,6 +69,8 @@ select
             ZG1010.D_E_L_E_T_ = ''
         and ZG1010.ZG1_VLIMPR != 0
         and ZG1010.ZG1_FILORI = ZG1.ZG1_FILORI
+        and ZG1010.ZG1_CC = ZG1.ZG1_CC
+        and ZG1010.ZG1_ITEMCT = ZG1.ZG1_ITEMCT
         and ZG1010.ZG1_COMPET = ZG1.ZG1_COMPET
         and ZG1010.ZG1_CODIGO = ZG1.ZG1_CODIGO
     ) as RAT_VLIMP
@@ -75,4 +79,7 @@ from ZG1010 ZG1 (nolock)
     left join CTT010 CTT
         on CTT.D_E_L_E_T_ = ''
         and CTT.CTT_CUSTO = ZG1.ZG1_CC
+    left join CTD010 CTD
+        on CTD.D_E_L_E_T_ = ''
+        and CTD.CTD_ITEM = ZG1.ZG1_ITEMCT
 where ZG1.D_E_L_E_T_ = ''
