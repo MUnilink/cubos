36e3ee38df677683790fe08dd45f8c98c9f29f44
Manoel <manoel.jose@unilinktransportes.com.br>
2025-10-06 10:34:46 -0300

    separação da carga horária para CFS e placas

--- FAST ANALYTICS/gooddata/query/fact/produtividade mensal.sql
+++ FAST ANALYTICS/gooddata/query/fact/produtividade mensal.sql
@@ -17,7 +17,13 @@ select
     cast(isnull(ZG1.ZG1_IMPR1, 0.0) as numeric(15, 2)) as HR_IMPR1,
     cast(isnull(ZG1.ZG1_IMPR2, 0.0) as numeric(15, 2)) as HR_IMPR2,
     cast(isnull(ZG1.ZG1_IMPR3, 0.0) as numeric(15, 2)) as HR_IMPR3,
-    case when ZG1.ZG1_TABELA = 'SQ3' and ZG1.ZG1_CC = '305' then (select max(SRJ010.RJ_YHRPADR) from SRJ010 where SRJ010.D_E_L_E_T_ = '' and SRJ010.RJ_CARGO = ZG1.ZG1_CODIGO) when ZG1.ZG1_TABELA = 'SQ3' and ZG1.ZG1_CC = '304' then '220' else ZG1.ZG1_HRPAD end as QTD_UNI
+    
+    case
+        when ZG1.ZG1_TABELA = 'SQ3' and ZG1.ZG1_CC = '305' and ZG1.ZG1_ITEMCT != '32' then (select max(SRJ010.RJ_YHRPADR) from SRJ010 where SRJ010.D_E_L_E_T_ = '' and SRJ010.RJ_CARGO = ZG1.ZG1_CODIGO)
+        when ZG1.ZG1_TABELA = 'SQ3' and ZG1.ZG1_CC = '305' and ZG1.ZG1_ITEMCT = '32' then '220'
+        when ZG1.ZG1_TABELA = 'SQ3' and ZG1.ZG1_CC = '304' then '220'
+        else ZG1.ZG1_HRPAD
+    end as QTD_UNI
 
 from ZG1010 ZG1
     left join CTT010 CTT
--- faturamento/FA - produtividade mensal.sql
+++ faturamento/FA - produtividade mensal.sql
@@ -17,7 +17,13 @@ select
     cast(isnull(ZG1.ZG1_IMPR2, 0.0) as numeric(15, 2)) as HR_IMPR2,
     cast(isnull(ZG1.ZG1_IMPR3, 0.0) as numeric(15, 2)) as HR_IMPR3,
     concat(ZG1.ZG1_COMPET, '01') as COMPETENCIA,
-    case when ZG1.ZG1_TABELA = 'SQ3' and ZG1.ZG1_CC = '305' then (select max(SRJ010.RJ_YHRPADR) from SRJ010 where SRJ010.D_E_L_E_T_ = '' and SRJ010.RJ_CARGO = ZG1.ZG1_CODIGO) when ZG1.ZG1_TABELA = 'SQ3' and ZG1.ZG1_CC = '304' then '220' else ZG1.ZG1_HRPAD end as QTD_UNI,
+    
+    case
+        when ZG1.ZG1_TABELA = 'SQ3' and ZG1.ZG1_CC = '305' and ZG1.ZG1_ITEMCT != '32' then (select max(SRJ010.RJ_YHRPADR) from SRJ010 where SRJ010.D_E_L_E_T_ = '' and SRJ010.RJ_CARGO = ZG1.ZG1_CODIGO)
+        when ZG1.ZG1_TABELA = 'SQ3' and ZG1.ZG1_CC = '305' and ZG1.ZG1_ITEMCT = '32' then '220'
+        when ZG1.ZG1_TABELA = 'SQ3' and ZG1.ZG1_CC = '304' then '220'
+        else ZG1.ZG1_HRPAD
+    end as QTD_UNI,
 
     /* PARA RM */
     ZG1.ZG1_FILORI as FILIAL,
