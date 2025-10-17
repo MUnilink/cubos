c7e03b2982175ba3cab02e57331b83dd8d90220f
Manoel <manoel.jose@unilinktransportes.com.br>
2025-10-17 18:19:11 -0300

    filtro por família de bens e correção da carga horária de pessoal

--- faturamento/sintético das OS de faturamento.sql
+++ faturamento/sintético das OS de faturamento.sql
@@ -17,7 +17,13 @@ select
     cast(isnull(ZG1.ZG1_IMPR2, 0.0) as numeric(15, 2)) as HR_IMPR2,
     cast(isnull(ZG1.ZG1_IMPR3, 0.0) as numeric(15, 2)) as HR_IMPR3,
     concat(ZG1.ZG1_COMPET, '01') as COMPETENCIA,
-    case when ZG1.ZG1_TABELA = 'SQ3' then (select cast(SRJ010.RJ_YHRPADR as numeric(15, 2)) from SRJ010 where SRJ010.D_E_L_E_T_ = '' and SRJ010.RJ_CARGO = ZG1.ZG1_CODIGO) else ZG1.ZG1_HRPAD end as QTD_UNI,
+    
+    case
+        when ZG1.ZG1_TABELA = 'SQ3' and ZG1.ZG1_CC = '305' and ZG1.ZG1_ITEMCT != '32' then (select max(SRJ010.RJ_YHRPADR) from SRJ010 where SRJ010.D_E_L_E_T_ = '' and SRJ010.RJ_CARGO = ZG1.ZG1_CODIGO)
+        when ZG1.ZG1_TABELA = 'SQ3' and ZG1.ZG1_CC = '305' and ZG1.ZG1_ITEMCT = '32' then '220'
+        when ZG1.ZG1_TABELA = 'SQ3' and ZG1.ZG1_CC = '304' then '220'
+        else ZG1.ZG1_HRPAD
+    end as QTD_UNI,
 
     /* PARA RM */
     ZG1.ZG1_FILORI as FILIAL,
@@ -84,4 +90,8 @@ from ZG1010 ZG1 (nolock)
         and CTD.CTD_ITEM = ZG1.ZG1_ITEMCT
 where
         ZG1.D_E_L_E_T_ = ''
-    and case when ZG1.ZG1_TABELA = 'SQ3' then (select trim(ST9010.T9_CODFAMI) from ST9010 (nolock) where ST9010.D_E_L_E_T_ = '' and trim(ST9010.T9_CODBEM) = ZG1.ZG1_CODIGO and ZG1.ZG1_TIPO in (3, 6, 9, 12)) else null end in ('VP', 'MP', 'GD', 'GD AUX', 'ML', 'VM')
+    and
+        case
+            when ZG1.ZG1_TABELA = 'ST9' then (select trim(ST9010.T9_CODFAMI) from ST9010 (nolock) where ST9010.D_E_L_E_T_ = '' and trim(ST9010.T9_CODBEM) = ZG1.ZG1_CODIGO and ZG1.ZG1_TIPO in (3, 6, 9, 12))
+            else 'SQ3'
+        end in ('SQ3', 'VP', 'MP', 'GD', 'GD AUX', 'ML', 'VM')
