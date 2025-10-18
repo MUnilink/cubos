294b71692767e05c655a6ac974c8f7a691af2aec
Manoel <manoel.jose@unilinktransportes.com.br>
2025-10-18 11:15:59 -0300

    informações adicionais da SS, duração, período etc.
    sem vínculo com OS, por algum motivo

--- manutenção/acompanhamento de SS.sql
+++ manutenção/acompanhamento de SS.sql
@@ -1,7 +1,9 @@
 select
     TQB.TQB_SOLICI as SS,
+    left(TQB.TQB_DTABER, 6) PERIODO,
     convert(datetime, concat(TQB.TQB_DTABER, ' ', TQB.TQB_HOABER), 113) as DT_INISS,
     convert(datetime, concat(TQB.TQB_DTFECH, ' ', TQB.TQB_HOFECH), 113) as DT_ENCSS,
+    case when TQB.TQB_DTFECH != '' then cast(datediff(minute, concat(TQB.TQB_DTABER, ' ', TQB.TQB_HOABER), concat(TQB.TQB_DTFECH, ' ', TQB.TQB_HOFECH))/60.0 as numeric(15, 2)) else 0.0 end as DURACAO_SS,
     
     case TQB.TQB_SOLUCA
         when 'A' then upper('Aguardando Analise')
@@ -11,17 +13,19 @@ select
         else 'OUTROS'
     end as SITUACAO_SS,
     
-    concat(trim(TQB.TQB_CDSERV), ' - ', (select upper(trim(TQ3010.TQ3_NMSERV)) from TQ3010 where TQ3010.D_E_L_E_T_ = '' and TQ3010.TQ3_CDSERV = TQB.TQB_CDSERV)) as SERVICO_SS,
-    concat(trim(TQB.TQB_CDEXEC), ' - ', (select upper(trim(TQ4010.TQ4_NMEXEC)) from TQ4010 where TQ4010.D_E_L_E_T_ = '' and TQ4010.TQ4_CDEXEC = TQB.TQB_CDEXEC)) as EXECUTA_SS,
+    concat(trim(TQB.TQB_CDSERV), ' - ', (select upper(trim(TQ3010.TQ3_NMSERV)) from TQ3010 where TQ3010.D_E_L_E_T_ = '' and TQ3010.TQ3_FILIAL = TQB.TQB_FILIAL and TQ3010.TQ3_CDSERV = TQB.TQB_CDSERV)) as SERVICO_SS,
+    (select upper(trim(TQ4010.TQ4_NMEXEC)) from TQ4010 where TQ4010.D_E_L_E_T_ = '' and TQ4010.TQ4_FILIAL = TQB.TQB_FILIAL and TQ4010.TQ4_CDEXEC = TQB.TQB_CDEXEC) as EXECUTA_SS,
+    TQB.TQB_POSCON as CONTADOR,
+    trim(ST9.T9_CODBEM) as EQUIPAMENTO,
+    trim(ST9.T9_CODFAMI) as FAMILIA,
+    trim(TQB.TQB_CCUSTO) as CC,
+    trim(STJ.TJ_YITMCT) as ATIVIDADE,
     upper(TQB.TQB_USUARI) as USR_SS,
     
     STJ.TJ_FILIAL as FILIAL,
     STJ.TJ_ORDEM as OS,
-    trim(STJ.TJ_CODBEM) as EQUIPAMENTO,
     cast(STJ.TJ_DTORIGI as date) as DATA_OS,
     left(STJ.TJ_DTORIGI, 6) as PERIODO_OS,
-    ST9.T9_CODFAMI as FAMILIA,
-    cast(ST9.T9_DTBAIXA as date) as DT_BAIXA,
 	trim(upper(STJ.TJ_USUAFIM)) as USR_FIM,
     trim(upper(STJ.TJ_USUARIO)) as USR_INI,
     cast(STI.TI_DATAPLA as date) as DATA_PLANO,
@@ -41,10 +45,6 @@ select
 	case when isdate(concat(STJ.TJ_DTMRFIM, ' ', STJ.TJ_HOMRFIM)) = 1 then convert(datetime, concat(STJ.TJ_DTMRFIM, ' ', STJ.TJ_HOMRFIM), 113) else null end as DTH_FIMMNT,
     case when isdate(concat(STJ.TJ_DTPRFIM, ' ', STJ.TJ_HOPRFIM)) = 1 then convert(datetime, concat(STJ.TJ_DTPRFIM, ' ', STJ.TJ_HOPRFIM), 113) else null end as DTH_FIMPAR,
 
-    STJ.TJ_CCUSTO as CC,
-    STJ.TJ_YITMCT as ATIVIDADE,
-
-
     trim(STJ.TJ_TIPO) as COD_CTIPO,
     trim(STE.TE_TIPOMAN) as TE_TIPOMAN,
 	trim(STE.TE_NOME) as CARAC_TIPO,
@@ -61,9 +61,6 @@ from TQB010 TQB (nolock)
         left join ST4010 ST4 (nolock)
             on ST4.D_E_L_E_T_ = ''
             and ST4.T4_SERVICO = STJ.TJ_SERVICO
-        left join ST9010 ST9 (nolock)
-            on ST9.D_E_L_E_T_ = ''
-            and ST9.T9_CODBEM = STJ.TJ_CODBEM
         left join STI010 STI (nolock)
             on STI.D_E_L_E_T_ = ''
             and STI.TI_FILIAL = STJ.TJ_FILIAL
@@ -71,5 +68,9 @@ from TQB010 TQB (nolock)
         left join STE010 STE (nolock)
             on STE.D_E_L_E_T_ = ''
             and STE.TE_TIPOMAN = STJ.TJ_TIPO
+    
+    inner join ST9010 ST9 (nolock)
+        on ST9.D_E_L_E_T_ = ''
+        and ST9.T9_CODBEM = TQB.TQB_CODBEM
 
 where TQB.D_E_L_E_T_ = ''
