621696977bf05f647a4d576f1d404052b7f2077f
Manoel <manoel.jose@unilinktransportes.com.br>
2025-10-23 12:08:48 -0300

    adição da atividade das OS e das viagens

--- faturamento/ROP conta 07102004.sql
+++ faturamento/ROP conta 07102004.sql
@@ -1,9 +1,10 @@
     select
         ZG1.ZG1_FILORI as FILIAL,
         ZC2.ZC2_NUM as NUM,
-        trim(ZG1.ZG1_CC) as CC, null as ITEM,
+        ZC1.ZC1_CC as CC,
+        ZC1.ZC1_ATIVD as ITEM,
         sum(ZC2.ZC2_TOTAL * ZG1.VL_RIMP) as TOTAL
-    from ZC2010 ZC2 (nolock)
+    from ZC2010 ZC2
         inner join
         (
             select
@@ -12,6 +13,7 @@
                 G1.ZG1_CODIGO,
                 G1.ZG1_TIPO,
                 G1.ZG1_CC,
+                G1.ZG1_ITEMCT,
                 G1.ZG1_VLIMPR,
                 G1.ZG1_VLIMPR/
                 (
@@ -24,27 +26,32 @@
                     and ZG1010.ZG1_COMPET = G1.ZG1_COMPET
                     and ZG1010.ZG1_CODIGO = G1.ZG1_CODIGO
                     and ZG1010.ZG1_CC = G1.ZG1_CC
+                    and ZG1010.ZG1_ITEMCT = G1.ZG1_ITEMCT
                 ) as VL_RIMP
-            from ZG1010 G1 (nolock)
+            from ZG1010 G1
             where
                     G1.D_E_L_E_T_ = ''
-                and G1.ZG1_CC = '305'
                 and G1.ZG1_TIPO = '9'
         ) ZG1
             on left(ZC2.ZC2_COMPET, 6) = ZG1.ZG1_COMPET
-            and ZC2.ZC2_COD = trim(ZG1.ZG1_CODIGO)
+            and ZC2.ZC2_COD = ZG1.ZG1_CODIGO
+        inner join ZC1010 ZC1
+            on ZC1.D_E_L_E_T_ = ''
+            and ZC1.ZC1_FILIAL = ZC2.ZC2_FILIAL
+            and ZC1.ZC1_NUM = ZC2.ZC2_NUM
     where
             ZC2.D_E_L_E_T_ = ''
         and ZC2.ZC2_TIPO = '16'
         and left(ZC2.ZC2_COMPET, 6) = '"+cCompt+"' and ZC2.ZC2_FILIAL between '"+cFilIni+"' and '"+cFilFim+"'
-    group by ZG1.ZG1_FILORI, ZC2.ZC2_NUM, ZG1.ZG1_CC
+    group by ZG1.ZG1_FILORI, ZC2.ZC2_NUM, ZC1.ZC1_CC, ZC1.ZC1_ATIVD
 union
     select
         ZG1.ZG1_FILORI as FILIAL,
         ZE1.ZE1_NUM as NUM,
-        trim(ZG1.ZG1_CC) as CC, null as ITEM,
-        null as ITEM, sum(ZE1.ZE1_TOTAL * ZG1.VL_RIMP) as TOTAL
-    from ZE1010 ZE1 (nolock)
+        ZG1.ZG1_CC as CC,
+        ZG1.ZG1_ITEMCT as ITEM,
+        sum(ZE1.ZE1_TOTAL * ZG1.VL_RIMP) as TOTAL
+    from ZE1010 ZE1
         inner join
         (
             select
@@ -53,6 +60,7 @@ union
                 G1.ZG1_CODIGO,
                 G1.ZG1_TIPO,
                 G1.ZG1_CC,
+                G1.ZG1_ITEMCT,
                 G1.ZG1_VLIMPR,
                 G1.ZG1_VLIMPR/
                 (
@@ -65,17 +73,17 @@ union
                     and ZG1010.ZG1_COMPET = G1.ZG1_COMPET
                     and ZG1010.ZG1_CODIGO = G1.ZG1_CODIGO
                     and ZG1010.ZG1_CC = G1.ZG1_CC
+                    and ZG1010.ZG1_ITEMCT = G1.ZG1_ITEMCT
                 ) as VL_RIMP
             from ZG1010 G1 (nolock)
             where
                     G1.D_E_L_E_T_ = ''
-                and G1.ZG1_CC = '304'
                 and G1.ZG1_TIPO = '9'
         ) ZG1
             on left(ZE1.ZE1_COMPET, 6) = ZG1.ZG1_COMPET
-            and ZE1.ZE1_COD = trim(ZG1.ZG1_CODIGO)
+            and ZE1.ZE1_COD = ZG1.ZG1_CODIGO
     where
             ZE1.D_E_L_E_T_ = ''
         and ZE1.ZE1_TIPO = '16'
         and left(ZE1.ZE1_COMPET, 6) = '"+cCompt+"' and ZE1.ZE1_FILIAL between '"+cFilIni+"' and '"+cFilFim+"'
-    group by ZG1.ZG1_FILORI, ZE1.ZE1_NUM, ZG1.ZG1_CC
+    group by ZG1.ZG1_FILORI, ZE1.ZE1_NUM, ZG1.ZG1_CC, ZG1.ZG1_ITEMCT
