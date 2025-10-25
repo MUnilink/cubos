804e2ec4c65d6909dc24ad79119741f3a2c45fca
Manoel <manoel.jose@unilinktransportes.com.br>
2025-10-25 09:53:47 -0300

    tratamento de movimentação sem CC ou atividade

--- FAST ANALYTICS/gooddata-custom/query/dim/atividade.sql
+++ FAST ANALYTICS/gooddata-custom/query/dim/atividade.sql
@@ -1,7 +1,8 @@
 select
-	trim(CTD.CTD_ITEM) as ITEM_CTA,
-	trim(CTD.CTD_DESC01) as ATIVIDADE
-from CTD010 CTD
+	trim(CTD010.CTD_ITEM) as ITEM_CTA,
+	trim(CTD010.CTD_DESC01) as ATIVIDADE
+from CTD010
 where
-		CTD.D_E_L_E_T_ = ''
-	and cast(CTD.CTD_ITEM as int) > 9
+		CTD010.D_E_L_E_T_ = ''
+	and cast(CTD010.CTD_ITEM as int) > 9
+union select null, null
--- FAST ANALYTICS/gooddata-custom/query/dim/ccusto.sql
+++ FAST ANALYTICS/gooddata-custom/query/dim/ccusto.sql
@@ -1,5 +1,6 @@
 select
-	trim(CTT.CTT_CUSTO) as CTT_CUSTO,
-	trim(CTT.CTT_DESC01) as CTT_DESC01
-from CTT010 as CTT
-where CTT.D_E_L_E_T_ = ''
+	trim(CTT010.CTT_CUSTO) as CTT_CUSTO,
+	trim(CTT010.CTT_DESC01) as CTT_DESC01
+from CTT010
+where CTT010.D_E_L_E_T_ = ''
+union select null, null
