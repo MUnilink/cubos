16b032484630b522f1e43f372feadc6e5197a18a
Manoel <manoel.jose@unilinktransportes.com.br>
2025-11-14 17:13:59 -0300

    para testar solução de folha da atividade 31

--- faturamento/ROP conta 07101001.sql
+++ faturamento/ROP conta 07101001.sql
@@ -3,15 +3,19 @@
         ZC2.ZC2_NUM as NUM,
         ZC2.ZC2_CC as CC,
         ZC2.ZC2_ATIVD as ITEM,
-        sum(ZC2.ZC2_TOTAL * ZG1.VL_RIMP) as TOTAL
+        
+        case when G1.ZG1_TIPO in ('2', '14') and ZC2.ZC2_CC = '305' and ZC2.ZC2_ATIVD != '32' /* ATIVIDADES NÃO CFS DA OPP SEM FOLHA */
+            then sum(ZG1.VL_IMPRRAT) * (select sum(ZC2.ZC2_TOTAL) from ZC2010 where ZC2010.D_E_L_E_T_ = ''and ZC2010.ZC2_FILIAL = ZC2.ZC2_FILIAL and ZC2010.ZC2_NUM = ZC2.ZC2_NUM and ZC2010.ZC2_TIPO = '1')/(select sum(ZC2.ZC2_TOTAL) from ZC2010 where ZC2010.D_E_L_E_T_ = ''and ZC2010.ZC2_FILIAL = ZC2.ZC2_FILIAL and ZC2010.ZC2_TIPO = '1') as REC_OS
+            else sum(ZC2.ZC2_TOTAL * ZG1.VL_RIMP)
+        end as TOTAL
     from
     (
         select
             ZC1010.ZC1_FILIAL as ZC2_FILIAL,
             ZC1010.ZC1_CC as ZC2_CC,
             ZC1010.ZC1_ATIVD as ZC2_ATIVD,
-            ZC2010.ZC2_NUM,
             left(ZC2010.ZC2_COMPET, 6) as ZC2_COMPET,
+            ZC2010.ZC2_NUM,
             ZC2010.ZC2_COD,
             ZC2010.ZC2_TIPO,
             ZC2010.ZC2_TOTAL
@@ -31,7 +35,29 @@
                 G1.ZG1_TIPO,
                 G1.ZG1_CC,
                 G1.ZG1_ITEMCT,
+                G1.ZG1_VLTOTL,
+                G1.ZG1_VLPROD,
                 G1.ZG1_VLIMPR,
+                
+                case when G1.ZG1_TIPO in ('2', '14') and G1.ZG1_CC = '305' and G1.ZG1_ITEMCT != '32' /* ATIVIDADES NÃO CFS DA OPP SEM FOLHA */
+                    then G1.ZG1_VLTOTL - 
+                    (
+                        select isnull(sum(ZC2010.ZC2_TOTAL), 0.0) /* VALOR PROD DAS ATIVIDADES*/
+                        from ZC2010
+                            inner join ZC1010
+                                on ZC1010.D_E_L_E_T_ = ''
+                                and ZC1010.ZC1_FILIAL = ZC2010.ZC2_FILIAL
+                                and ZC1010.ZC1_NUM = ZC2010.ZC2_NUM
+                        where
+                                ZC2010.D_E_L_E_T_ = ''
+                            and ZC1010.ZC1_CC = G1.ZG1_CC
+                            and left(ZC2010.ZC2_COMPET, 6) = G1.ZG1_COMPET
+                            and ZC2010.ZC2_COD = G1.ZG1_CODIGO
+                            and ZC2010.ZC2_TIPO = G1.ZG1_TIPO
+                    )
+                    else G1.ZG1_VLIMPR
+                end as VL_IMPRRAT,
+                
                 G1.ZG1_VLIMPR/
                 (
                     select sum(ZG1010.ZG1_VLIMPR)
