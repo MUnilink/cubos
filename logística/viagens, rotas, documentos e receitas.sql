8e1550e922832460e4ef15a61c66e0b3b7b7a39c
Manoel <manoel.jose@unilinktransportes.com.br>
2025-11-24 11:28:05 -0300

    relação dos veículos com a viagem em vez de com os documentos

--- logística/viagens, rotas, documentos e receitas.sql
+++ logística/viagens, rotas, documentos e receitas.sql
@@ -414,20 +414,20 @@ from DUD010 DUD (nolock)
             on DA8.D_E_L_E_T_ = ''
             and DA8.DA8_COD = DTQ.DTQ_ROTA
     
-    inner join DTR010 DTR (nolock)
-        on DTR.D_E_L_E_T_ = ''
-        and DTR.DTR_FILORI = DUD.DUD_FILORI
-        and DTR.DTR_VIAGEM = DUD.DUD_VIAGEM
+        inner join DTR010 DTR (nolock)
+            on DTR.D_E_L_E_T_ = ''
+            and DTR.DTR_FILORI = DTQ.DTQ_FILORI
+            and DTR.DTR_VIAGEM = DTQ.DTQ_VIAGEM
 
-        inner join DUP010 DUP (nolock)
-            on DUP.D_E_L_E_T_ = ''
-            and DUP.DUP_FILORI = DTR.DTR_FILORI
-            and DUP.DUP_VIAGEM = DTR.DTR_VIAGEM
-            and DUP.DUP_ITEDTR = DTR.DTR_ITEM
-            and DUP.DUP_CODVEI = DTR.DTR_CODVEI
+            inner join DUP010 DUP (nolock)
+                on DUP.D_E_L_E_T_ = ''
+                and DUP.DUP_FILORI = DTR.DTR_FILORI
+                and DUP.DUP_VIAGEM = DTR.DTR_VIAGEM
+                and DUP.DUP_ITEDTR = DTR.DTR_ITEM
+                and DUP.DUP_CODVEI = DTR.DTR_CODVEI
 
-            inner join DA4010 DA4 (nolock)
-                on DA4.DA4_COD = DUP.DUP_CODMOT
+                inner join DA4010 DA4 (nolock)
+                    on DA4.DA4_COD = DUP.DUP_CODMOT
 
     left join DT5010 DT5 (nolock)
         on DT5.D_E_L_E_T_ = ''
