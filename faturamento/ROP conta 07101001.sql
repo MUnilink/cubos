0c68336218c2cfd2f5ab467f35f6575d69b3a19f
Manoel <manoel.jose@unilinktransportes.com.br>
2025-11-29 11:31:30 -0300

    adaptação para adição ao plano de contas

--- faturamento/ROP conta 07101001.sql
+++ faturamento/ROP conta 07101001.sql
@@ -4,9 +4,30 @@
         ZC2.ZC2_CC as CC,
         ZC2.ZC2_ATIVD as ITEM,
         
-        case when G1.ZG1_TIPO in ('2', '14') and ZC2.ZC2_CC = '305' and ZC2.ZC2_ATIVD != '32' /* ATIVIDADES NÃO CFS DA OPP SEM FOLHA */
-            then sum(ZG1.VL_IMPRRAT) * (select sum(ZC2.ZC2_TOTAL) from ZC2010 where ZC2010.D_E_L_E_T_ = ''and ZC2010.ZC2_FILIAL = ZC2.ZC2_FILIAL and ZC2010.ZC2_NUM = ZC2.ZC2_NUM and ZC2010.ZC2_TIPO = '1')/(select sum(ZC2.ZC2_TOTAL) from ZC2010 where ZC2010.D_E_L_E_T_ = ''and ZC2010.ZC2_FILIAL = ZC2.ZC2_FILIAL and ZC2010.ZC2_TIPO = '1') as REC_OS
-            else sum(ZC2.ZC2_TOTAL * ZG1.VL_RIMP)
+        case
+            when ZC2.ZC2_CC = '305' and ZC2.ZC2_ATIVD = '32' and ZC2.ZC2_TIPO = '15' then sum(ZC2.ZC2_TOTAL * ZG1.RAT_IMP)
+            when ZC2.ZC2_CC = '305' and ZC2.ZC2_ATIVD != '32' and ZC2.ZC2_TIPO not in ('15', '1') then sum(ZG1.ZG1_VLIMPR) * avg(ZC2.VL_RECEITA) / 
+                case when ZC2.qtd = 1 then
+                    coalesce
+                    (
+                        (
+                            select sum(ZC2010.ZC2_TOTAL)
+                            from ZC2010
+                                inner join ZC1010
+                                    on ZC1010.D_E_L_E_T_ = ''
+                                    and ZC1010.ZC1_FILIAL = ZC2.ZC2_FILIAL
+                                    and ZC1010.ZC1_NUM = ZC2.ZC2_NUM
+                            where
+                                    ZC2010.D_E_L_E_T_ = ''
+                                and ZC2010.ZC2_TIPO = '1'
+                                and ZC2010.ZC2_FILIAL = ZC2.ZC2_FILIAL
+                                and ZC1010.ZC1_CC = ZC2.ZC2_CC
+                                and ZC1010.ZC1_ATIVD = ZC2.ZC2_ATIVD
+                                and left(ZC2010.ZC2_COMPET, 6) = ZC2.ZC2_COMPET
+                        )
+                    , 999999999)
+                else 999999999 end
+            else 0.0
         end as TOTAL
     from
     (
@@ -14,17 +35,21 @@
             ZC1010.ZC1_FILIAL as ZC2_FILIAL,
             ZC1010.ZC1_CC as ZC2_CC,
             ZC1010.ZC1_ATIVD as ZC2_ATIVD,
-            left(ZC2010.ZC2_COMPET, 6) as ZC2_COMPET,
-            ZC2010.ZC2_NUM,
-            ZC2010.ZC2_COD,
-            ZC2010.ZC2_TIPO,
-            ZC2010.ZC2_TOTAL
-        from ZC2010
+            left(Z2.ZC2_COMPET, 6) as ZC2_COMPET,
+            Z2.ZC2_NUM,
+            Z2.ZC2_COD,
+            Z2.ZC2_TIPO,
+            sum(Z2.ZC2_TOTAL) as ZC2_TOTAL,
+            row_number() over(partition by Z2.ZC2_COMPET, Z2.ZC2_NUM, Z2.ZC2_TIPO, ZC1010.ZC1_FILIAL, ZC1010.ZC1_CC, ZC1010.ZC1_ATIVD, Z2.ZC2_FILIAL order by Z2.ZC2_COD) as qtd,
+            (select sum(ZC2010.ZC2_TOTAL) from ZC2010 where ZC2010.D_E_L_E_T_ = '' and ZC2010.ZC2_TIPO = '1' and ZC2010.ZC2_FILIAL = Z2.ZC2_FILIAL and ZC2010.ZC2_NUM = Z2.ZC2_NUM and left(ZC2010.ZC2_COMPET, 6) = left(Z2.ZC2_COMPET, 6)) as VL_RECEITA
+
+        from ZC2010 Z2
             inner join ZC1010
                 on ZC1010.D_E_L_E_T_ = ''
-                and ZC1010.ZC1_FILIAL = ZC2010.ZC2_FILIAL
-                and ZC1010.ZC1_NUM = ZC2010.ZC2_NUM
-        where ZC2010.D_E_L_E_T_ = '' and ZC2010.ZC2_TIPO = '15'
+                and ZC1010.ZC1_FILIAL = Z2.ZC2_FILIAL
+                and ZC1010.ZC1_NUM = Z2.ZC2_NUM
+        where Z2.D_E_L_E_T_ = '' and Z2.ZC2_TIPO in ('2', '15')
+        group by Z2.ZC2_COMPET, Z2.ZC2_NUM, Z2.ZC2_TIPO, Z2.ZC2_COD, ZC1010.ZC1_FILIAL, ZC1010.ZC1_CC, ZC1010.ZC1_ATIVD, Z2.ZC2_FILIAL
     ) ZC2
         inner join
         (
@@ -39,6 +64,20 @@
                 G1.ZG1_VLPROD,
                 G1.ZG1_VLIMPR,
                 
+                G1.ZG1_VLIMPR/
+                (
+                    select sum(ZG1010.ZG1_VLIMPR)
+                    from ZG1010
+                    where
+                            ZG1010.D_E_L_E_T_ = ''
+                        and ZG1010.ZG1_VLIMPR != 0
+                        and ZG1010.ZG1_FILORI = G1.ZG1_FILORI
+                        and ZG1010.ZG1_COMPET = G1.ZG1_COMPET
+                        and ZG1010.ZG1_CODIGO = G1.ZG1_CODIGO
+                        and ZG1010.ZG1_CC = G1.ZG1_CC
+                        and ZG1010.ZG1_ITEMCT = G1.ZG1_ITEMCT
+                ) as RAT_IMP,
+
                 case when G1.ZG1_TIPO in ('2', '14') and G1.ZG1_CC = '305' and G1.ZG1_ITEMCT != '32' /* ATIVIDADES NÃO CFS DA OPP SEM FOLHA */
                     then G1.ZG1_VLTOTL - 
                     (
@@ -55,34 +94,23 @@
                             and ZC2010.ZC2_COD = G1.ZG1_CODIGO
                             and ZC2010.ZC2_TIPO = G1.ZG1_TIPO
                     )
-                    else G1.ZG1_VLIMPR
-                end as VL_IMPRRAT,
-                
-                G1.ZG1_VLIMPR/
-                (
-                    select sum(ZG1010.ZG1_VLIMPR)
-                    from ZG1010
-                    where
-                        ZG1010.D_E_L_E_T_ = ''
-                    and ZG1010.ZG1_VLIMPR != 0
-                    and ZG1010.ZG1_FILORI = G1.ZG1_FILORI
-                    and ZG1010.ZG1_COMPET = G1.ZG1_COMPET
-                    and ZG1010.ZG1_CODIGO = G1.ZG1_CODIGO
-                    and ZG1010.ZG1_CC = G1.ZG1_CC
-                    and ZG1010.ZG1_ITEMCT = G1.ZG1_ITEMCT
-                ) as VL_RIMP
+                    else 0.0
+                end as VL_IMPRRAT
+            
             from ZG1010 G1 (nolock)
             where
                     G1.D_E_L_E_T_ = ''
-                and G1.ZG1_TIPO = '2'
+                and G1.ZG1_TIPO in ('2')
+                and exists (select 1 from SRD010 where SRD010.D_E_L_E_T_ = '' and left(SRD010.RD_DATARQ, 6) = left(G1.ZG1_COMPET, 6) and SRD010.RD_CC = G1.ZG1_CC and SRD010.RD_ITEM = G1.ZG1_ITEMCT)
         ) ZG1
             on left(ZC2.ZC2_FILIAL, 4) = ZG1.ZG1_FILORI
             and ZC2.ZC2_COMPET = ZG1.ZG1_COMPET
             and ZC2.ZC2_COD = ZG1.ZG1_CODIGO
             and ZC2.ZC2_CC = ZG1.ZG1_CC
-            and ZC2.ZC2_ATIVD = ZG1.ZG1_ITEMCT
-    where ZC2.ZC2_COMPET = '"+cCompt+"' and ZC2.ZC2_FILIAL between '"+cFilIni+"' and '"+cFilFim+"'
-    group by ZC2.ZC2_FILIAL, ZC2.ZC2_NUM, ZC2.ZC2_CC, ZC2.ZC2_ATIVD
+            and case when ZC2.ZC2_CC = '305' and ZC2.ZC2_ATIVD != '32' then '21' else ZC2.ZC2_ATIVD end = ZG1.ZG1_ITEMCT
+    /*where ZC2.ZC2_COMPET = '"+cCompt+"' and ZC2.ZC2_FILIAL between '"+cFilIni+"' and '"+cFilFim+"'*/
+    where ZC2.ZC2_COMPET = '202501' and ZC2.ZC2_COD = '00005'
+    group by ZC2.ZC2_FILIAL, ZC2.ZC2_CC, ZC2.ZC2_ATIVD, ZC2.ZC2_COMPET, ZC2.ZC2_NUM, ZC2.ZC2_TIPO
 union
     select
         ZE1.ZE1_FILIAL as FILIAL,
