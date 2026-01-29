483102687735beb223499326001f2e1755bb99ba
Manoel José <manoel.jose@unilinktransportes.com.br>
2026-01-29 14:53:02 -0300

    para nova consulta de receita nacional TMS e rodotrens dépôt

--- faturamento/ROP conta 01202002.sql
+++ faturamento/ROP conta 01202002.sql
@@ -191,7 +191,7 @@ from
                 when trim(CFOP.X5_CHAVE) = '6353' and SD2.D2_TES = '501'
                 then '310101001'
             else null end
-        = '310101002' /* NACIONAL */
+        = '310101002' /* EXPORTAÇÃO */
         and left(SD2.D2_EMISSAO, 6) = '"+cCompt+"' and SD2.D2_FILIAL between '"+cFilIni+"' and '"+cFilFim+"'
 ) RECEITA_TMS
 group by
