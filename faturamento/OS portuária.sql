611c5946d8668e6d51bf9e938ffca1434d919fd2
Manoel José <manoel.jose@unilinktransportes.com.br>
2026-02-10 15:47:55 -0300

    tratamento de vazio na validação do horário apontado

--- faturamento/OS portuária.sql
+++ faturamento/OS portuária.sql
@@ -152,10 +152,12 @@ select
     convert(date, ZC2.ZC2_DTINI, 103) as DATA_INIAPONT,
     convert(date, ZC2.ZC2_DTFIM, 103) as DATA_FIMAPONT,
     convert(date, ZC2.ZC2_DATA, 103) as DATA_ITEM,
-    convert(datetime, case isdate(ZC2.ZC2_HRINI) when 1 then concat(ZC2.ZC2_DTINI, ' ', ZC2.ZC2_HRINI) else concat(ZC2.ZC2_DTINI, ' ', '00:00') end, 113) as DTINI_APONT,
-    convert(datetime, case isdate(ZC2.ZC2_HRFIM) when 1 then concat(ZC2.ZC2_DTFIM, ' ', ZC2.ZC2_HRFIM) else concat(ZC2.ZC2_DTFIM, ' ', '00:00') end, 113) as DTFIM_APONT,
-    case when cast(ZC2.ZC2_TIPO as int) in (2, 3) then case when isdate(ZC2.ZC2_HRINI) + isdate(ZC2.ZC2_HRFIM) = 2 then cast(datediff(minute, concat(ZC2.ZC2_DTINI, ' ', ZC2.ZC2_HRINI), concat(ZC2.ZC2_DTFIM, ' ', ZC2.ZC2_HRFIM))/60.0 as numeric(15, 4)) else 0.0 end else 0.0 end as HORAS_APONT,
-	case when cast(ZC2.ZC2_TIPO as int) in (2, 3) then case when isdate(ZC2.ZC2_HRINI) + isdate(ZC2.ZC2_HRFIM) = 2 then cast(ZC2.ZC2_QTDREC * datediff(minute, concat(ZC2.ZC2_DTINI, ' ', ZC2.ZC2_HRINI), concat(ZC2.ZC2_DTFIM, ' ', ZC2.ZC2_HRFIM))/60.0 as numeric(15, 4)) else 0.0 end else 0.0 end as HORAS_TOTAIS,
+    trim(ZC2.ZC2_HRINI) as HORA_INIAPONT,
+    trim(ZC2.ZC2_HRFIM) as HORA_FIMAPONT,
+    convert(datetime, case when isdate(nullif(ZC2.ZC2_HRINI, '')) = 1 then concat(ZC2.ZC2_DTINI, ' ', ZC2.ZC2_HRINI) else concat(ZC2.ZC2_DTINI, ' ', '00:00') end, 113) as DTINI_APONT,
+    convert(datetime, case when isdate(nullif(ZC2.ZC2_HRFIM, '')) = 1 then concat(ZC2.ZC2_DTFIM, ' ', ZC2.ZC2_HRFIM) else concat(ZC2.ZC2_DTFIM, ' ', '00:00') end, 113) as DTFIM_APONT,
+    case when cast(ZC2.ZC2_TIPO as int) in (2, 3) then case when isdate(nullif(ZC2.ZC2_HRINI, '')) = 1 then cast(datediff(minute, concat(ZC2.ZC2_DTINI, ' ', ZC2.ZC2_HRINI), concat(ZC2.ZC2_DTFIM, ' ', ZC2.ZC2_HRFIM))/60.0 as numeric(15, 4)) else 0.0 end else 0.0 end as HORAS_APONT,
+	case when cast(ZC2.ZC2_TIPO as int) in (2, 3) then case when isdate(nullif(ZC2.ZC2_HRFIM, '')) = 1 then cast(datediff(minute, concat(ZC2.ZC2_DTINI, ' ', ZC2.ZC2_HRINI), concat(ZC2.ZC2_DTFIM, ' ', ZC2.ZC2_HRFIM))/60.0 as numeric(15, 4)) else 0.0 end else 0.0 end * ZC2.ZC2_QTDREC as HORAS_TOTAIS,
 
     case
         when cast(ZC2.ZC2_TIPO as int) not in (2, 3) then 'N/A'
@@ -168,15 +170,14 @@ select
     
     case
         when cast(ZC2.ZC2_TIPO as int) not in (2, 3) then 'não se aplica'
-        when isdate(ZC2.ZC2_DTINI) = 0 or nullif(ZC2.ZC2_DTINI, '') is null or isdate(ZC2.ZC2_HRINI) = 0 or nullif(ZC2.ZC2_HRINI, '') is null then 'data ou hora ini ausente'
-        when isdate(ZC2.ZC2_DTFIM) = 0 or nullif(ZC2.ZC2_DTFIM, '') is null or isdate(ZC2.ZC2_HRFIM) = 0 or nullif(ZC2.ZC2_HRFIM, '') is null then 'data ou hora fim ausente'
+        when isdate(ZC2.ZC2_DTINI) = 0 or nullif(ZC2.ZC2_DTINI, '') is null or isdate(nullif(ZC2.ZC2_HRINI, '')) = 0 or nullif(ZC2.ZC2_HRINI, '') is null then 'data ou hora ini ausente'
+        when isdate(ZC2.ZC2_DTFIM) = 0 or nullif(ZC2.ZC2_DTFIM, '') is null or isdate(nullif(ZC2.ZC2_HRFIM, '')) = 0 or nullif(ZC2.ZC2_HRFIM, '') is null then 'data ou hora fim ausente'
         when datediff(minute, concat(ZC2.ZC2_DTINI, ' ', ZC2.ZC2_HRINI), concat(ZC2.ZC2_DTFIM, ' ', ZC2.ZC2_HRFIM))/60.0 > 12.999 then 'mais que 13 h apontadas'
         when datediff(minute, concat(ZC2.ZC2_DTINI, ' ', ZC2.ZC2_HRINI), concat(ZC2.ZC2_DTFIM, ' ', ZC2.ZC2_HRFIM)) < 0.0 then 'data/hora ini maior que data/hora fim'
         else 'item OK'
     end as STATUS_APONT,
 
     dense_rank() over(partition by ZC1.ZC1_FILIAL, ZC1.ZC1_NUM, ZC2.ZC2_DTINI, ZC2.ZC2_DTFIM order by ZC2.ZC2_DTINI, ZC2.ZC2_DTFIM) as TURNO_OS,
-
     trim(upper(ZC2.ZC2_NMUSU)) as USUARIO,
     case when (select SYS_USR.USR_MSBLQL from SYS_USR where USR_CODIGO = ZC2.ZC2_NMUSU and SYS_USR.D_E_L_E_T_ = '') = 2 then 'S' else 'N' end as USR_ATIVO
 
