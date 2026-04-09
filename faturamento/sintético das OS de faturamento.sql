939fd5fb823f8de3b56109a695777e7ece2659a8
Manoel José <manoel.jose@unilinktransportes.com.br>
2026-04-09 11:57:32 -0300

    nomes de tipos improdutivos pneu e combustível

--- faturamento/sintético das OS de faturamento.sql
+++ faturamento/sintético das OS de faturamento.sql
@@ -55,6 +55,11 @@ select
         when 14 then 'PROVISÕES'
         when 15 then 'TIPO RH IMPROD'
         when 16 then 'TIPO MNT IMPROD'
+        when 17 then 'DIÁRIA'
+        when 18 then 'SEGURO AVARIA'
+        when 19 then 'SEGURO ROUBO'
+        when 20 then 'IMPR COMB'
+        when 21 then 'IMPR PNEU'
         else 'OUTROS'
     end as TIPO_INSUMO,
 
@@ -90,8 +95,4 @@ from ZG1010 ZG1 (nolock)
         and CTD.CTD_ITEM = ZG1.ZG1_ITEMCT
 where
         ZG1.D_E_L_E_T_ = ''
-    and
-        case
-            when ZG1.ZG1_TABELA = 'ST9' then (select trim(ST9010.T9_CODFAMI) from ST9010 (nolock) where ST9010.D_E_L_E_T_ = '' and trim(ST9010.T9_CODBEM) = ZG1.ZG1_CODIGO and ZG1.ZG1_TIPO in (3, 6, 9, 12))
-            else 'SQ3'
-        end in ('SQ3', 'VP', 'MP', 'GD', 'GD AUX', 'ML', 'VM')
+    and ZG1.ZG1_COMPET > '202412'
