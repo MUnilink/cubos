408c04b30fdd6b6adb5fb7d1749dda8eb0e9db70
Manoel José <manoel.jose@unilinktransportes.com.br>
2026-04-20 17:53:43 -0300

    adição de tipos improdutivo combustível e pneus

--- FAST ANALYTICS/gooddata/query/dim/tipo de item da OS de faturamento.sql
+++ FAST ANALYTICS/gooddata/query/dim/tipo de item da OS de faturamento.sql
@@ -3,7 +3,7 @@
         case cast(ZC2.ZC2_TIPO as int)
             when 1 then 'RECEITA'
             when 2 then 'PESSOAL'
-            when 3 then 'EQUIPAMENTO'
+            when 3 then 'MANUTENÇÃO'
             when 4 then 'MATERIAIS'
             when 5 then 'COMPRAS'
             when 6 then 'DEPRECIAÇÃO'
@@ -18,8 +18,11 @@
             when 17 then 'DIÁRIA'
             when 18 then 'SEGURO AVARIA'
             when 19 then 'SEGURO ROUBO'
+            when 20 then 'IMPR COMB'
+            when 21 then 'IMPR PNEU'
             else 'OUTROS'
-        end as TIPO_INSUMO
+        end as TIPO_INSUMO,
+        case when cast(ZC2.ZC2_TIPO as int) in (3, 6, 9, 10, 12, 13, 20, 21) then 'EQUIPAMENTO' when cast(ZC2.ZC2_TIPO as int) in (2, 14, 17) then 'PESSOAL' else 'OUTROS' end as ENTIDADE
     from ZC2010 ZC2
     where ZC2.D_E_L_E_T_ = ''
-union select null, null
+union select null, null, null
