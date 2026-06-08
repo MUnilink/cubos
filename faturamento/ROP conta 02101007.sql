dfe086f101829ce5b8d2d6544360b4a54a0090f8
Manoel José <manoel.jose@unilinktransportes.com.br>
2026-06-08 10:34:24 -0300

    correção de nome

--- faturamento/ROP conta 02101007.sql
+++ faturamento/ROP conta 02101007.sql
@@ -21,7 +21,7 @@ union
         ZE1.ZE1_NUM as VIAGEM,
         '304      ' as CC,
         '11       ' as ITEM,
-        sum(cast(coalesce(ZE1.ZE1_TOTAL, 0) as decimal (14, 2))) as VALOR
+        sum(cast(coalesce(ZE1.ZE1_TOTAL, 0) as decimal (14, 2))) as TOTAL
     from ZE1010 ZE1
     where
             ZE1.D_E_L_E_T_ = ''
