b89ca41d8a8410d2f95f9aca51ba424c6fdaba94
Manoel José <manoel.jose@unilinktransportes.com.br>
2026-06-18 10:32:52 -0300

    adição do km apontado e, se vazio, km da rota

--- FAST ANALYTICS/gooddata-TMS/Agent/query/fact/TMS Performance Entrega.sql
+++ FAST ANALYTICS/gooddata-TMS/Agent/query/fact/TMS Performance Entrega.sql
@@ -73,7 +73,8 @@ SELECT
     
     row_number() over(partition by DF1.ID_AGENDAMENTO order by DF1.ID_AGENDAMENTO) as qtd_age,
     row_number() over(partition by VIAGEM.ID_VIAGEM order by VIAGEM.ID_VIAGEM) as qtd_vga,
-    row_number() over(partition by DT6.DT6_FILDOC, DT6.DT6_DOC, DT6.DT6_SERIE order by DT6.DT6_FILDOC, DT6.DT6_DOC, DT6.DT6_SERIE) as qtd_doc
+    row_number() over(partition by DT6.DT6_FILDOC, DT6.DT6_DOC, DT6.DT6_SERIE order by DT6.DT6_FILDOC, DT6.DT6_DOC, DT6.DT6_SERIE) as qtd_doc,
+    VIAGEM.km_fim - VIAGEM.km_ini as km_vga
 
 FROM DT6010 DT6
     LEFT JOIN SA1010 REM
@@ -116,6 +117,68 @@ FROM DT6010 DT6
     left join /* ver modelo para adição de dimensão motorista */
     (
         select
+            cast
+            (
+                (
+                    select
+                        coalesce
+                        (
+                            (
+                                select top 1 nullif(substring(ZB1010.ZB1_MSGTXT, 2, len(ZB1010.ZB1_MSGTXT)), '')
+                                from ZB1010 (nolock)
+                                where
+                                        ZB1010.D_E_L_E_T_ = ''
+                                    and ZB1010.ZB1_STATUS = 'OK'
+                                    and
+                                        dateadd(hour, -3, datetimefromparts(substring(ZB1010.ZB1_MSGTIM, 1, 4), substring(ZB1010.ZB1_MSGTIM, 6, 2), substring(ZB1010.ZB1_MSGTIM, 9, 2), substring(ZB1010.ZB1_MSGTIM, 12, 2), substring(ZB1010.ZB1_MSGTIM, 15, 2), 0, 0))
+                                        =
+                                        datetimefromparts(year(APT.DTW_DATREA), month(APT.DTW_DATREA), day(APT.DTW_DATREA), substring(APT.DTW_HORREA, 1, 2), substring(APT.DTW_HORREA, 3, 4), 0, 0)
+                                    and ZB1010.ZB1_MSGTXT like '&_%' escape '&'
+                                    and ZB1010.ZB1_MACRON = 7
+                                    and ZB1010.ZB1_CODDA3 = DTR.DTR_CODVEI
+                            ),
+                            nullif(APT.DTW_YHODFI, ''),
+                            (select DTQ010.DTQ_KMVGE from DTQ010 where DTQ010.D_E_L_E_T_ = '' and DTQ010.DTQ_FILIAL = DTR.DTR_FILIAL and DTQ010.DTQ_FILORI = DTR.DTR_FILORI and DTQ010.DTQ_VIAGEM = DTR.DTR_VIAGEM)
+                        )
+                    from DTW010 APT (nolock)
+                    where
+                            APT.D_E_L_E_T_ = ''
+                        and APT.DTW_FILORI = DTR.DTR_FILORI
+                        and APT.DTW_VIAGEM = DTR.DTR_VIAGEM
+                        and APT.DTW_ATIVID = 50
+                ) as numeric(15, 2)
+            ) as km_fim,
+            cast
+            (
+                (
+                    select
+                        coalesce
+                        (
+                            (
+                                select top 1 nullif(substring(ZB1010.ZB1_MSGTXT, 2, len(ZB1010.ZB1_MSGTXT)), '')
+                                from ZB1010 (nolock)
+                                where
+                                        ZB1010.D_E_L_E_T_ = ''
+                                    and ZB1010.ZB1_STATUS = 'OK'
+                                    and
+                                        dateadd(hour, -3, datetimefromparts(substring(ZB1010.ZB1_MSGTIM, 1, 4), substring(ZB1010.ZB1_MSGTIM, 6, 2), substring(ZB1010.ZB1_MSGTIM, 9, 2), substring(ZB1010.ZB1_MSGTIM, 12, 2), substring(ZB1010.ZB1_MSGTIM, 15, 2), 0, 0))
+                                        =
+                                        datetimefromparts(year(APT.DTW_DATREA), month(APT.DTW_DATREA), day(APT.DTW_DATREA), substring(APT.DTW_HORREA, 1, 2), substring(APT.DTW_HORREA, 3, 4), 0, 0)
+                                    and ZB1010.ZB1_MSGTXT like '&_%' escape '&'
+                                    and ZB1010.ZB1_MACRON = 1
+                                    and ZB1010.ZB1_CODDA3 = DTR.DTR_CODVEI
+                            ),
+                            nullif(APT.DTW_YHODIN, ''),
+                            0
+                        )
+                    from DTW010 APT (nolock)
+                    where
+                            APT.D_E_L_E_T_ = ''
+                        and APT.DTW_FILORI = DTR.DTR_FILORI
+                        and APT.DTW_VIAGEM = DTR.DTR_VIAGEM
+                        and APT.DTW_ATIVID = 49
+                ) as numeric(15, 2)
+            ) as km_ini,
             DA4010.DA4_COD,
             DTR.DTR_CODVEI,
             DTR.DTR_CODRB1,
