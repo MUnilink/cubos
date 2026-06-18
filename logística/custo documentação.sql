6c68b9705688895432654b71a6354610fdc248ba
Manoel José <manoel.jose@unilinktransportes.com.br>
2026-06-18 16:51:00 -0300

    adição de CC do lançamento

--- logística/custo documentação.sql
+++ logística/custo documentação.sql
@@ -1,6 +1,7 @@
 select
-    convert(date, TS1.TS1_DTEMIS, 103) as TS1_DTEMIS,
-    convert(date, SE2.E2_VENCREA, 103) as TS1_DTVENC,
+    cast(TS1.TS1_DTEMIS as date) as TS1_DTEMIS,
+    cast(SE2.E2_VENCREA as date) as TS1_DTVENC,
+    trim(TS1.TS1_YCC) as CC,
     TS1.TS1_QTDPAR,
     TS1.TS1_VALOR/TS1.TS1_QTDPAR as VALPARC,
     TS1.TS1_VALOR,
@@ -9,6 +10,7 @@ select
     trim(isnull(ST9.T9_CODBEM, '-')) as T9_CODBEM,
     year(SE2.E2_VENCREA) as ano_VENCTO,
     month(SE2.E2_VENCREA) as mes_VENCTO,
+    left(SE2.E2_VENCREA, 6) as PERIODO_VENCE,
     year(TS1.TS1_DTEMIS) as COMPETENCIA
 from TS1010 TS1
     left join TS0010 TS0
@@ -18,7 +20,7 @@ from TS1010 TS1
         on SE2.D_E_L_E_T_ = ''
         and trim(SE2.E2_PREFIXO) = 'MNT'
         and SE2.E2_NUM = TS1.TS1_NUMSE2
-    left join ST9010 as ST9
+    left join ST9010 ST9
         on ST9.D_E_L_E_T_ = ''
         and ST9.T9_CODBEM = TS1.TS1_CODBEM
 where
--- manutenção/documentação veicular.sql
+++ manutenção/documentação veicular.sql
@@ -1,6 +1,7 @@
 select
     trim(ST9.T9_CODBEM) as EQUIPAMENTO,
     trim(ST9.T9_CODFAMI) as FAMILIA,
+    trim(TS1.TS1_YCC) as CC,
     case ST9.T9_SITBEM when 'A' then 'ATIVO' when 'I' then 'INATIVO' else 'OUTROS' end as SITUACAO,
     cast(TS1.TS1_DTEMIS as date) as DT_EMITE,
     cast(SE2.E2_VENCREA as date) as DT_VENCE,
@@ -19,7 +20,7 @@ from TS1010 TS1 (nolock)
         and TS0.TS0_DOCTO = TS1.TS1_DOCTO
     left join SE2010 SE2 (nolock)
         on SE2.D_E_L_E_T_ = ''
-        and trim(SE2.E2_PREFIXO) = 'MNT'
+        and SE2.E2_PREFIXO = 'MNT'
         and SE2.E2_NUM = TS1.TS1_NUMSE2
     left join ST9010 ST9 (nolock)
         on ST9.D_E_L_E_T_ = ''
