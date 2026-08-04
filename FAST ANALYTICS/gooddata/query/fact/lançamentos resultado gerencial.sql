3f1280978e6e92256255e1229c9c77d38bebf199
Manoel José <manoel.jose@unilinktransportes.com.br>
2026-08-04 16:47:45 -0300

    ajuste do sinal da comissão do TMS

--- FAST ANALYTICS/gooddata/query/fact/lançamentos resultado gerencial.sql
+++ FAST ANALYTICS/gooddata/query/fact/lançamentos resultado gerencial.sql
@@ -46,7 +46,8 @@ select distinct
     
     case
         when ZE2.ZE2_ORIGEM = 'F' then ZE3.ZE3_VALOR
-        when left(ZE2.ZE2_COD, 8) = '02101007' then ZE3.ZE3_VALOR*-1
+        when left(ZE2.ZE2_COD, 8) = '02101007' then ZE3.ZE3_VALOR
+        when left(ZE2.ZE2_COD, 8) = '02501002' then ZE3.ZE3_VALOR*-1
         when left(ZE2.ZE2_COD, 5) = '072' then ZE3.ZE3_VALOR
         when left(ZE2.ZE2_COD, 2) = '01' then ZE3.ZE3_VALOR
         when left(ZE2.ZE2_COD, 2) = '11' then ZE3.ZE3_VALOR*-1
